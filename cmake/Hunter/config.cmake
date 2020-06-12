c62bb21c2e2c8f23181621222f1d4f85314e92c7
Martin Peterlin <martin.peterlin7@gmail.com>
2020-06-12 13:59:52 +0200

    Updated XLink and JSON Schema Validator

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,8 @@
 hunter_config(
     nlohmann_json_schema_validator
     VERSION "2.1.1"
-    URL "https://github.com/luxonis/json-schema-validator/archive/v2.1.0-hunter.tar.gz"
-    SHA1 "7ea2ce83223be4a52cd205396360255449ece00f"
+    URL "https://github.com/luxonis/json-schema-validator/archive/hunter-2.1.1.tar.gz"
+    SHA1 "1c410bcf1418155215e6b6b8f31284ff66f0f3b4"
     CMAKE_ARGS
         BUILD_TESTS=OFF
         BUILD_EXAMPLE=OFF
@@ -14,8 +14,8 @@ hunter_config(
 hunter_config(
     XLink
     VERSION "luxonis-2020.2"
-    URL "https://github.com/luxonis/XLink/archive/cc33e15c01453742799c945cf443c177d824f76b.tar.gz"
-    SHA1 "950b00439022850b951aa97164ea335a611e2f8f"
+    URL "https://github.com/luxonis/XLink/archive/1717591c8bfb078a9a6ed30bca9c82273296e86f.tar.gz"
+    SHA1 "06419E6F806B9A3DF64E4929E1262497C172CACE"
     CMAKE_ARGS
         CMAKE_POSITION_INDEPENDENT_CODE=ON
 )
