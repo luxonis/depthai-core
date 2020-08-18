ed487f7086cec83fb9b5a09788ed23b0f7958f4d
iamsg <>
2020-08-18 03:50:08 +0300

    update firmware commit hash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ac2b4143d35f2b0e3cf933b2cc210654515f0880")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fa658cf7633881c1625728060ce62203b3cfcc53")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
