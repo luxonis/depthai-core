309225366da8028f32bf819c37aa5691ac5b3dac
TheMarpe <martin.peterlin7@gmail.com>
2020-08-20 20:27:11 +0200

    Fixed XLink hunter config

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -18,13 +18,6 @@ hunter_config(
     SHA1 "00494618711aa285282af1c9937e39f42e568680"
     CMAKE_ARGS
         CMAKE_POSITION_INDEPENDENT_CODE=ON
-)hunter_config(
-    XLink
-    VERSION "luxonis-2020.2"
-    URL "https://github.com/luxonis/XLink/archive/3f8c17eef960d0a76fcb8f78581407d74dade2c7.tar.gz"
-    SHA1 "00494618711aa285282af1c9937e39f42e568680"
-    CMAKE_ARGS
-        CMAKE_POSITION_INDEPENDENT_CODE=ON
 )
 
 hunter_config(
