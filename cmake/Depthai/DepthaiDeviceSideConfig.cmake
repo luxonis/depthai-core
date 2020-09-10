d95d7279d93af6c9d7a129a122bcb2c17f9edb26
Martin Peterlin <martin.peterlin7@gmail.com>
2020-09-10 23:59:07 +0200

    Updated device side binary

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1f27e8cf4302e2f284b00f5cac6d44adb42bc285")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9cc2165af5e408f257cfdfc6e7fc0282b34ec4e4")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
