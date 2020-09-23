6784f5d47ed41e1041413f34b2a55de9956a1bf0
SzabolcsGergely <szabi@luxonis.com>
2020-09-24 01:54:24 +0300

    Use get_dimension instead raw buffer

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e23d8309e8c1281c7fb57247d7b237a5dcf9738c")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d90ebbfd9c529cdc2cb0ed7667e3188fb42b7ed5")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
