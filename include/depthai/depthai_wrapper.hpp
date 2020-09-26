614ea22110699e6760bf0b11cb382f5e386434b8
saching13 <sachin.guruswamy@gmail.com>
2020-09-26 10:23:04 -0700

    updated with PR changes

--- include/depthai/depthai_wrapper.hpp
+++ include/depthai/depthai_wrapper.hpp
@@ -1,67 +1,64 @@
 #include "depthai/device.hpp"
-#include <string>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <fstream>
-#include <sstream>
 #include "nlohmann/json.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
-namespace DepthAI
-{
+namespace DepthAI {
 
-using CV_mat_ptr = std::shared_ptr<cv::Mat>;  // shared ptr for cv::Mat
+using CV_mat_ptr = std::shared_ptr<cv::Mat>; // shared ptr for cv::Mat
 using PacketsTuple = std::tuple<
-        std::list<std::shared_ptr<NNetPacket>>,
-        std::list<std::shared_ptr<HostDataPacket>>>;  // tuple of data packets on which Depthai is
-                                                      // publishing the streams
+    std::list<std::shared_ptr<NNetPacket>>,
+    std::list<std::shared_ptr<HostDataPacket>>>; // tuple of data packets on which Depthai is
+// publishing the streams
 
 /**  DepthAI:
  *   This is a wrapper on the host side of the device which 
- *   helps in initializing OAK device, creating pipeline
+ *   helps in initializing device, creating pipeline
  *   and fetching the frames as cv::Mat. 
  */
 
-class DepthAI : public Device
-{
+class DepthAI : public Device {
 
 public:
     /**  Constructor:
-     *   initializing OAK device and creating pipeline
+     *   initializing device and creating pipeline
      *  @param usb_device (std::string): Provide the path of the device
      *  or pass empty string to choose the default device.
      *  @param config_file (std::string): Provides the json file which is
-     *  used by the Depthai to configure the OAK-D.
+     *  used by the Depthai to configure the device
      *  @param usb2_mode (bool): set to true to connect over usb2
      */
     DepthAI(const std::string& usb_device, const std::string& config_file, bool usb2_mode = false);
 
-    /* API to stream output frames from OAK.
+    /* API to stream output frames from device.
      * AI data and metadata is WIP
      */
     void get_frames(std::unordered_map<std::string, CV_mat_ptr>& output_streams);
 
     /* Destructor */
-    ~DepthAI();
+    ~DepthAI() = default;
+    static const std::unordered_map<int, int> width_map_; // mapping of width with height
 
 private:
-    
-    std::shared_ptr<CNNHostPipeline> pipeline_;  // Depthai's pipeline object.
+    std::shared_ptr<CNNHostPipeline> pipeline_; // Depthai's pipeline object.
 
     PacketsTuple packets_;
-    nlohmann::json config_json_;
+    nlohmann::json config_json_; // Config file's json holder
 
-    int rgb_width_;
-    int rgb_height_;
-    int mono_width_;
-    int mono_height_;
+    int rgb_width_; //  represents the width and height of the rgb frame
+    int rgb_height_; //  represents the width and height of the rgb frame
+    int mono_width_; //  represents the width and height of the stereo camera frame
+    int mono_height_; //  represents the width and height of the stereo camera frame
 
-    std::vector<std::string> stream_names_;
-    std::vector<CV_mat_ptr> image_streams_;
-    std::unordered_map<int, int> width_map_;
-    void set_resolution();
-    void create_frame_holders();
+    std::vector<std::string> stream_names_; // camera streams enabled using config file
+    std::vector<CV_mat_ptr> image_streams_; // place holders for image stream
+    // std::unordered_map<std::string, CV_mat_ptr> image_stream_holder;
+
+    void set_resolution(); // fetches the width and height of the color and stereo frames from thr config
+    void create_frame_holders(); // sets the spare of the placeholders of image streams
 };
 
-}  // namespace DepthAI
-
+} // namespace DepthAI
--- src/depthai_wrapper.cpp
+++ src/depthai_wrapper.cpp
@@ -1,60 +1,69 @@
 #include "depthai/depthai_wrapper.hpp"
+#include <fstream>
+#include <sstream>
 
-namespace DepthAI
+#define WARNING "\033[1;5;31m" // Adds color to error statements
+#define ENDC "\033[0m"
+
+namespace DepthAI {
+
+const std::unordered_map<int, int> DepthAI::width_map_ = { { 720, 1280 }, { 800, 1280 }, { 400, 640 } };
+
+DepthAI::DepthAI(const std::string& usb_device, const std::string& config_file, bool usb2_mode)
+    : Device(usb_device, usb2_mode)
 {
-
-DepthAI::DepthAI(std::string usb_device, std::string config_file, bool usb2_mode) :
-      Device(usb_device, usb2_mode)
-{
-
     std::ifstream file(config_file);
     std::ostringstream file_stream;
-    file_stream << file.rdbuf();
+    if (file) {
+        file_stream << file.rdbuf();
+    } else {
+        std::cerr << WARNING "config file provided is not found" ENDC << std::endl;
+        abort();
+    }
     std::string config_str = file_stream.str();
 
-    pipeline_ = this->create_pipeline(config_str);
+    pipeline_ = create_pipeline(config_str);
     config_json_ = nlohmann::json::parse(config_str);
 
-    this->set_resolution();
-    this->create_frame_holders();
+    set_resolution();
+    create_frame_holders();
 
-    width_map_[720] = 1280;
-    width_map_[800] = 1280;
-    width_map_[400] = 640;
+    // width_map_[720] = 1280;
+    // width_map_[800] = 1280;
+    // width_map_[400] = 640;
 }
 
 void DepthAI::set_resolution()
 {
-
-    if (config_json_.contains("camera"))
-    {
+    // if camera is defined check if resolution of stereo or rgb is defined. else set to default
+    if (config_json_.contains("camera")) {
         auto& camera_conf_obj = config_json_.at("camera");
-        if (camera_conf_obj.contains("rgb"))
-        {
+        /*
+         * if rgb res is set in config_json_ fetch it to set 
+         * the height of the image else set the default
+         */
+        if (camera_conf_obj.contains("rgb")) {
             auto& rgb_camera_conf_obj = camera_conf_obj.at("rgb");
             rgb_height_ = rgb_camera_conf_obj.at("resolution_h").get<int32_t>();
             // _rgb_width = width_map_[_rgb_height];
-        }
-        else
-        {
+        } else {
             rgb_height_ = 3040;
             rgb_width_ = 4056;
         }
-
-        if (camera_conf_obj.contains("mono"))
-        {
+        /*
+         * if stereo camera res is set in config_json_ fetch it to set 
+         * the height of the image else set the default
+         */
+        if (camera_conf_obj.contains("mono")) {
             auto& mono_camera_conf_obj = camera_conf_obj.at("mono");
             mono_height_ = mono_camera_conf_obj.at("resolution_h").get<int32_t>();
-            mono_width_ = width_map_[mono_height_];
-        }
-        else
-        {
+            auto it = width_map_.find(mono_height_);
+            mono_width_ = it->second;
+        } else {
             mono_height_ = 720;
             mono_width_ = 1280;
         }
-    }
-    else
-    {
+    } else {
         rgb_height_ = 3040;
         rgb_width_ = 4056;
         mono_height_ = 720;
@@ -64,53 +73,28 @@ void DepthAI::set_resolution()
 
 void DepthAI::create_frame_holders()
 {
-
-    if (config_json_.contains("streams"))
-    {
-        for (auto it : config_json_.at("streams"))
-        {
-            // "metaout"
-            if (it.is_string())
-            {
+    // creating place holders for all the streams enabled in config
+    if (config_json_.contains("streams")) {
+        for (const auto& it : config_json_.at("streams")) {
+            if (it.is_string()) {
                 if (it.get<std::string>() == "metaout")
                     continue;
                 stream_names_.push_back(it.get<std::string>());
-                if (it.get<std::string>() == "previewout")
-                {
-                    std::shared_ptr<cv::Mat> img =
-                        std::make_shared<cv::Mat>(rgb_height_, rgb_width_, CV_8UC3);
+                if (it.get<std::string>() == "previewout") {
+                    CV_mat_ptr img = std::make_shared<cv::Mat>(rgb_height_, rgb_width_, CV_8UC3);
                     image_streams_.push_back(img);
                 }
-                else
-                {
-                    std::shared_ptr<cv::Mat> img =
-                        std::make_shared<cv::Mat>(mono_height_, mono_width_, CV_8UC3);
-                    image_streams_.push_back(img);
-                }
-            }
-            else
-            {
+            } else {
                 const auto& name = it.at("name").get<std::string>();
-                // streams.emplace_back(name);
-                // auto &stream = streams.back();
-                // std::cout << name << " -streams" << std::endl;
                 stream_names_.push_back(name);
-                if (name == "previewout")
-                {
-                    std::shared_ptr<cv::Mat> img =
-                        std::make_shared<cv::Mat>(rgb_height_, rgb_width_, CV_8UC3);
+                if (name == "previewout") {
+                    CV_mat_ptr img = std::make_shared<cv::Mat>(rgb_height_, rgb_width_, CV_8UC3);
                     image_streams_.push_back(img);
-                }
-                else if (name == "depth")
-                {
-                    std::shared_ptr<cv::Mat> img =
-                        std::make_shared<cv::Mat>(mono_height_, mono_width_, CV_16UC1);
+                } else if (name == "depth") {
+                    CV_mat_ptr img = std::make_shared<cv::Mat>(mono_height_, mono_width_, CV_16UC1);
                     image_streams_.push_back(img);
-                }
-                else if (name == "disparity_color")
-                {
-                    std::shared_ptr<cv::Mat> img =
-                        std::make_shared<cv::Mat>(mono_height_, mono_width_, CV_16UC1);
+                } else if (name == "disparity_color") {
+                    CV_mat_ptr img = std::make_shared<cv::Mat>(mono_height_, mono_width_, CV_16UC3);
                     image_streams_.push_back(img);
                 }
             }
@@ -118,40 +102,29 @@ void DepthAI::create_frame_holders()
     }
 }
 
-DepthAI::~DepthAI() = default;
-
 // How about creating streams for everything in the vector in constructor and then updating them
-// each time get frames is called ?
+// each time get frames is called ? How will it receive if it is not called for a small span of time ?
 
 // TODO: Crash on size mismatch. DO this later.
 // TODO: save config as json and check if the streams are in the config and crash if they aren't
 
 void DepthAI::get_frames(std::unordered_map<std::string, CV_mat_ptr>& output_streams)
 {
-
     int count = stream_names_.size();
     std::vector<std::string>::iterator it;
     std::vector<bool> dirty_check(stream_names_.size(), false);
-    while (count)
-    {
+    while (count) {
         packets_ = pipeline_->getAvailableNNetAndDataPackets(true);
 
-        for (auto sub_packet : std::get<1>(packets_))
-        {
+        for (const auto& sub_packet : std::get<1>(packets_)) {
             it = std::find(stream_names_.begin(), stream_names_.end(), sub_packet->stream_name);
-            if (it != stream_names_.end())
-            {
-                // std::cout << "foubd it-----" << sub_packet->size() << " ~~ " <<
-                // sub_packet->stream_name << std::endl;
+            if (it != stream_names_.end()) {
                 int index = it - stream_names_.begin();
-                unsigned char* img_ptr =
-                    reinterpret_cast<unsigned char*>(image_streams_[index]->data);
-                auto received_data = sub_packet->getData();
+                unsigned char* img_ptr = reinterpret_cast<unsigned char*>(image_streams_[index]->data);
+                const auto& received_data = sub_packet->getData();
 
-                // assert()
                 memcpy(img_ptr, received_data, sub_packet->size());
-                if (!dirty_check[index])
-                {
+                if (!dirty_check[index]) {
                     dirty_check[index] = true;
                     count--;
                 }
@@ -159,10 +132,9 @@ void DepthAI::get_frames(std::unordered_map<std::string, CV_mat_ptr>& output_str
         }
     }
 
-    for (int i = 0; i < stream_names_.size(); ++i)
-    {
+    for (int i = 0; i < stream_names_.size(); ++i) {
         output_streams[stream_names_[i]] = image_streams_[i];
     }
 }
 
-}  // namespace DepthAI
+} // namespace DepthAI
