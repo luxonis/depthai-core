eeea37a25f9a6e22f7693d60e977fff34ecd7874
SzabolcsGergely <szabi@luxonis.com>
2020-10-05 19:50:05 +0300

    Update device side firmware hash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d90ebbfd9c529cdc2cb0ed7667e3188fb42b7ed5")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "34aab09c0a1826f0fc980a27ce7c86005832d614")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
