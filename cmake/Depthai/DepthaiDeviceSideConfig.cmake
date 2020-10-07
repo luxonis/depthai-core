173d43ccbb11398b84a04c6e85fddfaf901ec454
alex-luxonis <alex@luxonis.com>
2020-10-07 16:32:04 +0300

    Update depthai-shared (and device side with it)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2dc84f49e816a826a0a396a430c5588066643587")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a6547dca63c1705d09d7225020f101e53e94183a")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
