1488b7e723ac93fe3c5d5a3e66588b62669abcb4
alex-luxonis <alex@luxonis.com>
2020-10-15 16:01:23 +0300

    Receive metadata for all streams

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2f49dc5f43e668af0c70fe683367b390fc5a6abb")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cc7b6c4296bd31e30dae65e4d5f1552947750dba")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
