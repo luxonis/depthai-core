abc0f75a201001253fbcf6e1db246134fe9f3e6f
saching13 <sachin.guruswamy@gmail.com>
2020-10-21 14:42:51 -0700

    added develop merged depthai device side

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f9df8e9f3115e796682ff3499068b04f782f888f")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "24c8d75789be175cad5093acc3d93da45464bf0e")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
