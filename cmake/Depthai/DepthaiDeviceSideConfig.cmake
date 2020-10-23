f9611b2bb25997fcf66430d259a9408292b8a6b9
alex-luxonis <alex@luxonis.com>
2020-10-23 17:27:21 +0300

    Update device FW: fix X,Y,Z calc on NN bounding box

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f9df8e9f3115e796682ff3499068b04f782f888f")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "808326d10e913968d18316f6fb9c6d18175231ae")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
