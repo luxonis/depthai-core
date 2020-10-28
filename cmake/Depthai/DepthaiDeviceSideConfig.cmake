2f9c5438bffeefde84d0d116b1a18a3b76dd6193
alex-luxonis <alex@luxonis.com>
2020-10-28 02:14:43 +0200

    Update FW, bugfix for config_h2d handling after initial setup

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d8813bc1b6cbfbcca742c3e718d3f592a63efcc3")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "642a7b7db68a6ec7c94797bea942f8f0df906397")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
