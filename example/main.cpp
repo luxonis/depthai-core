eeb1e9fc7a5fd2d8a057e5202dfad6687d8b5081
alex-luxonis <alex@luxonis.com>
2020-11-09 14:06:38 +0200

    Add StereoDepth node and example. Run: ./myapp depth Device FW not yet updated, TODO

--- example/main.cpp
+++ example/main.cpp
@@ -14,7 +14,7 @@
 #include "depthai/pipeline/node/XLinkIn.hpp"
 #include "depthai/pipeline/node/MyProducer.hpp"
 #include "depthai/pipeline/node/VideoEncoder.hpp"
-
+#include "depthai/pipeline/node/StereoDepth.hpp"
 
 #include "depthai/pipeline/datatype/NNData.hpp"
 #include "depthai/pipeline/datatype/ImgFrame.hpp"
@@ -623,7 +623,7 @@ void startMjpegCam(){
 
 }
 
-void startMonoCam() {
+void startMonoCam(bool withDepth) {
     using namespace std;
 
     dai::Pipeline p;
@@ -632,10 +632,21 @@ void startMonoCam() {
     auto monoRight = p.create<dai::node::MonoCamera>();
     auto xoutLeft  = p.create<dai::node::XLinkOut>();
     auto xoutRight = p.create<dai::node::XLinkOut>();
+    auto stereo    = withDepth ? p.create<dai::node::StereoDepth>() : nullptr;
+    auto xoutDisp  = p.create<dai::node::XLinkOut>();
+    auto xoutDepth = p.create<dai::node::XLinkOut>();
+    auto xoutRectifL = p.create<dai::node::XLinkOut>();
+    auto xoutRectifR = p.create<dai::node::XLinkOut>();
 
     // XLinkOut
     xoutLeft->setStreamName("left");
     xoutRight->setStreamName("right");
+    if (withDepth) {
+        xoutDisp->setStreamName("disparity");
+        xoutDepth->setStreamName("depth");
+        xoutRectifL->setStreamName("rectified_left");
+        xoutRectifR->setStreamName("rectified_right");
+    }
 
     // MonoCamera
     monoLeft->setResolution(dai::MonoCameraProperties::SensorResolution::THE_720_P);
@@ -645,9 +656,47 @@ void startMonoCam() {
     monoRight->setCamId(2);
     //monoRight->setFps(5.0);
 
-    // Link plugins CAM -> XLINK
-    monoLeft->out.link(xoutLeft->input);
-    monoRight->out.link(xoutRight->input);
+    bool outputDepth = false;
+    bool outputRectified = false;
+    bool lrcheck  = true;
+    bool extended = false;
+    bool subpixel = true;
+
+    int maxDisp = 96;
+    if (extended) maxDisp *= 2;
+    if (subpixel) maxDisp *= 32; // 5 bits fractional disparity
+
+    if (withDepth) {
+        // StereoDepth
+        stereo->setOutputDepth(outputDepth);
+        stereo->setOutputRectified(outputRectified);
+        stereo->setConfidenceThreshold(200);
+        stereo->setRectifyEdgeFillColor(0); // black, to better see the cutout
+        //stereo->loadCalibrationFile("../../../../depthai/resources/depthai.calib");
+        //stereo->setInputResolution(1280, 720);
+        stereo->setMedianFilter(dai::StereoDepthProperties::MedianFilter::MEDIAN_OFF);
+        stereo->setLeftRightCheck(lrcheck);
+        stereo->setExtendedDisparity(extended);
+        stereo->setSubpixel(subpixel);
+
+        // Link plugins CAM -> STEREO -> XLINK
+        monoLeft->out.link(stereo->left);
+        monoRight->out.link(stereo->right);
+
+        stereo->syncedLeft.link(xoutLeft->input);
+        stereo->syncedRight.link(xoutRight->input);
+        stereo->rectifiedLeft.link(xoutRectifL->input);
+        stereo->rectifiedRight.link(xoutRectifR->input);
+
+        stereo->disparity.link(xoutDisp->input);
+        stereo->depth.link(xoutDepth->input);
+
+    } else {
+        // Link plugins CAM -> XLINK
+        monoLeft->out.link(xoutLeft->input);
+        monoRight->out.link(xoutRight->input);
+    }
+
 
     // CONNECT TO DEVICE
 
@@ -662,6 +711,10 @@ void startMonoCam() {
 
         auto leftQueue = d.getOutputQueue("left", 8, true);
         auto rightQueue = d.getOutputQueue("right", 8, true);
+        auto dispQueue = withDepth ? d.getOutputQueue("disparity", 8, true) : nullptr;
+        auto depthQueue = withDepth ? d.getOutputQueue("depth", 8, true) : nullptr;
+        auto rectifLeftQueue = withDepth ? d.getOutputQueue("rectified_left", 8, true) : nullptr;
+        auto rectifRightQueue = withDepth ? d.getOutputQueue("rectified_right", 8, true) : nullptr;
 
         while (1) {
             auto t1 = std::chrono::steady_clock::now();
@@ -673,6 +726,33 @@ void startMonoCam() {
             auto t4 = std::chrono::steady_clock::now();
             cv::imshow("right", cv::Mat(right->getHeight(), right->getWidth(), CV_8UC1, right->getData().data()));
             auto t5 = std::chrono::steady_clock::now();
+
+            if (withDepth) {
+                // Note: in some configurations (if depth is enabled), disparity may output garbage data
+                auto disparity = dispQueue->get<dai::ImgFrame>();
+                cv::Mat disp(disparity->getHeight(), disparity->getWidth(),
+                        subpixel ? CV_16UC1 : CV_8UC1, disparity->getData().data());
+                disp.convertTo(disp, CV_8UC1, 255.0 / maxDisp); // Extend disparity range
+                cv::imshow("disparity", disp);
+                cv::Mat disp_color;
+                cv::applyColorMap(disp, disp_color, cv::COLORMAP_JET);
+                cv::imshow("disparity_color", disp_color);
+
+                if (outputDepth) {
+                    auto depth = depthQueue->get<dai::ImgFrame>();
+                    cv::imshow("depth", cv::Mat(depth->getHeight(), depth->getWidth(),
+                            CV_16UC1, depth->getData().data()));
+                }
+
+                if (outputRectified) {
+                    auto rectifL = rectifLeftQueue->get<dai::ImgFrame>();
+                    cv::imshow("rectified_left", cv::Mat(rectifL->getHeight(), rectifL->getWidth(),
+                            CV_8UC1, rectifL->getData().data()));
+                    auto rectifR = rectifRightQueue->get<dai::ImgFrame>();
+                    cv::imshow("rectified_right", cv::Mat(rectifR->getHeight(), rectifR->getWidth(),
+                            CV_8UC1, rectifR->getData().data()));
+                }
+            }
 
             int ms1 = std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
             int ms2 = std::chrono::duration_cast<std::chrono::milliseconds>(t3-t2).count();
@@ -707,7 +787,9 @@ int main(int argc, char** argv){
         } else if(nnPath == "test2"){
             startTest(2);
         } else if(nnPath == "mono"){
-            startMonoCam();
+            startMonoCam(false);
+        } else if(nnPath == "depth"){
+            startMonoCam(true);
         } else {
             startWebcam(0, nnPath);
         }
