042329377c80184311fdb5799820478d110608cc
alex-luxonis <alex@luxonis.com>
2020-11-26 00:49:30 +0200

    Update depthai-shared and device FW after merging gen2_updates_from_develop

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a6f45f127c9a8f35eef10e72d8686f52857c2103")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "03aab74b5625cd95399dd02807c7bac81a5bbcb3")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
