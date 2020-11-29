1741031c5a9aa728f23085a01242cebd6e917923
Jon Ngai <jon.ngai@gmail.com>
2020-11-28 21:16:28 -0700

    Checking style changes from clang-format.

--- include/depthai/pipeline/node/SPIOut.hpp
+++ include/depthai/pipeline/node/SPIOut.hpp
@@ -9,44 +9,42 @@ namespace dai {
 namespace node {
     class SPIOut : public Node {
         dai::SPIOutProperties properties;
-    
-        std::string getName(){
+
+        std::string getName() {
             return "SPIOut";
         }
-    
-        std::vector<Input> getInputs(){
+
+        std::vector<Input> getInputs() {
             return {input};
         }
-        
-        std::vector<Output> getOutputs(){
+
+        std::vector<Output> getOutputs() {
             return {};
         }
 
-        nlohmann::json getProperties(){
+        nlohmann::json getProperties() {
             nlohmann::json j;
             nlohmann::to_json(j, properties);
             return j;
         }
 
-        std::shared_ptr<Node> clone(){
+        std::shared_ptr<Node> clone() {
             return std::make_shared<SPIOut>(*this);
         }
-    
-    public:
-        SPIOut(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId) : Node(par, nodeId) {
-        }
 
-        Input input{*this, "in", Input::Type::SReceiver, {{DatatypeEnum::Buffer, true}} };
+       public:
+        SPIOut(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId) : Node(par, nodeId) {}
 
-        void setStreamName(std::string name){
+        Input input{*this, "in", Input::Type::SReceiver, {{DatatypeEnum::Buffer, true}}};
+
+        void setStreamName(std::string name) {
             properties.streamName = name;
         }
 
-        void setBusId(int id){
+        void setBusId(int id) {
             properties.busId = id;
         }
-
     };
 
-} // namespace node
-} // namespace dai
+}  // namespace node
+}  // namespace dai
