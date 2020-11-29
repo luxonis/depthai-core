bad0ab2c07ba3915eed9b2b7fd21851862051012
Jon Ngai <jon.ngai@gmail.com>
2020-11-29 12:31:26 -0700

    Updating depthai.cmd reference.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5f883069a28733fbcabfb4d436e220f91019b494")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8cb1e08f8fc4df5f6ef1b6b5cdeacc1db34bf080")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
