fcba06a60e95b00c0f2d81a8aba3bc7e69b84902
alex-luxonis <alex@luxonis.com>
2020-12-01 04:09:56 +0200

    Update FW: report RGB VCM/focus position over meta_d2h

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6fc8c54e33b8aa6d16bf70ac5193d10090dcd0d8")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b9709758dc63de046eecce80632e48afd3746716")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
