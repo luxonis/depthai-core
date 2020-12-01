a2b88640486c6ee9ece95e6ca3a6742a6ad9c641
saching13 <sachin.guruswamy@gmail.com>
2020-11-30 17:13:01 -0800

    updated fw commit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "140e154fc21a851daeb1d2ecee29d5dfb16f6847")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6fc8c54e33b8aa6d16bf70ac5193d10090dcd0d8")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
