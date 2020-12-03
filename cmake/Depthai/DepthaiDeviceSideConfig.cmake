9cfdf1ca14c1178bbb9d2227cc31833a703bae61
alex-luxonis <alex@luxonis.com>
2020-12-03 02:45:48 +0200

    Update FW: try to fix IMU init failure

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "25ab9cca70980967df4c7ae92cef4de640628621")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b23259907fa63d89a7e65b8d85896da5743b9a72")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
