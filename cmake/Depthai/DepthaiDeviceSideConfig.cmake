e3a2e878114d9d5ce4c1ed8de816f79bbf1e0980
TheMarpe <martin.peterlin7@gmail.com>
2020-12-10 22:37:07 +0100

    Single mono camera fix, install 3rdparty public headers

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a112effa00f9870fc54bf98253af9037b25ae66c")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "55bdb161678f2d81c17f7893a5cd0dcc9761591d")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
