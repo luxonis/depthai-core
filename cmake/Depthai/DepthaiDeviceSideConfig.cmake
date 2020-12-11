8c846e5e645db3a2f328da5e0726b232832404be
TheMarpe <martin.peterlin7@gmail.com>
2020-12-11 22:14:52 +0100

    Improved install step for library and its dependecies

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0dd5b5d6ecc0eb546b3c6d8abdc3276a547a5179")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ad9a7e069c2a747a8aff7eb861f3cc3ae1b97c89")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
