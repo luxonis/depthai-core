3ecf161103f75d6b24036f5e6dc7507777e01dae
TheMarpe <martin.peterlin7@gmail.com>
2020-12-17 20:24:41 +0100

    Updated firmware - Fixed NN bug

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4901429e0ff330e9f58ec12bc7af67d1b652d174")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "017ac3ac79ccd9a7e6a6a2d09d96a726af22f1a9")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
