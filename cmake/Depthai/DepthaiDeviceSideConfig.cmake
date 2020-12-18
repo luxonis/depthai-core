d9ddab66bc7b666ac63b37d92090cc0a66857868
alex-luxonis <alex@luxonis.com>
2020-12-18 17:41:15 +0200

    Update FW: increase_manual_exposure_limits

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "140e154fc21a851daeb1d2ecee29d5dfb16f6847")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a0dff2324e17e367fc49d3e93a56d5085c300a59")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
