8c8f10834636d210239ef14d8323225d7ef5ac7a
TheMarpe <martin.peterlin7@gmail.com>
2020-12-29 22:48:18 +0100

    Updated device with VideoEncoder improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b263e5292bdb58d776d84a1a7bc1382d116f3488")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b911488dc709f79c2de6abd6352fc45db45ab5cf")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
