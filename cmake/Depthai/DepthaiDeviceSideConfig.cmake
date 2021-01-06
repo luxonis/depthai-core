b21a4570c5502f6c7bb303dcfda1382e62de7d3f
alex-luxonis <alex@luxonis.com>
2021-01-06 06:59:23 +0200

    Update FW: rotate RGB camera view 180deg on OAK-1 (Gen1)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a0dff2324e17e367fc49d3e93a56d5085c300a59")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1860863d0f10da90fc82098da935f4146a1a0d5b")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
