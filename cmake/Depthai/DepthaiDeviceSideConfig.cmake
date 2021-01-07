9f36b05c47a8b461882447f77e16f9a3251d4040
Jon Ngai <jon.ngai@gmail.com>
2021-01-07 10:39:46 -0700

    Updating subodules for clangformat and depthai.cmd.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d76513e33c5bbbd2267e5142d12fbc283ad32fbe")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f8c9ca2bef92c90e6b5a71fa73957a91eb462826")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
