8b11d20cf5ee8b5717ee8f09c755a680ade70e18
TheMarpe <martin.peterlin7@gmail.com>
2021-01-11 16:15:56 +0100

    Updated 'ExecuteTestTimeout' script

--- examples/cmake/ExecuteTestTimeout.cmake
+++ examples/cmake/ExecuteTestTimeout.cmake
@@ -1,7 +1,30 @@
+cmake_minimum_required(VERSION 3.2)
+
 # TODO (themarpe) - cross platform timeout
 
+# Parse out arguments
+foreach(_arg RANGE ${CMAKE_ARGC})
+    if(append)
+        list(APPEND arguments "${CMAKE_ARGV${_arg}}")
+    endif()
+
+    if("${CMAKE_ARGV${_arg}}" STREQUAL "-P")
+        set(append true)
+        message(status "found -P")
+    endif()
+endforeach()
+list(REMOVE_AT arguments 0)
+
+message(STATUS "arguments: ${arguments}")
+
+# Check if ENV variable TEST_TIMEOUT is set and use that rather than TIMEOUT_SECONDS
+if(DEFINED ENV{TEST_TIMEOUT})
+    message(STATUS "Overriding timeout: ${TIMEOUT_SECONDS} with $ENV{TEST_TIMEOUT}")
+    set(TIMEOUT_SECONDS $ENV{TEST_TIMEOUT})
+endif()
+
 # Execute the example
-execute_process(COMMAND timeout -s SIGINT -k 5 ${TIMEOUT_SECONDS} ${PATH_TO_TEST_EXECUTABLE} RESULT_VARIABLE error_variable)
+execute_process(COMMAND timeout -s SIGINT -k 5 ${TIMEOUT_SECONDS} ${arguments} RESULT_VARIABLE error_variable)
 message(STATUS "After timeout, ${PATH_TO_TEST_EXECUTABLE} produced the following exit code: ${error_variable}")
 
 # After that, wait for ~3 seconds
