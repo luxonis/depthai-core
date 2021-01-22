b126bfd52935df4fe657aea9e2e12895be0e59f1
jonngai <jon.ngai@gmail.com>
2021-01-22 14:00:49 -0700

    Updating firmware. (#44)
    Adding GET_MESSAGE_PART for SPI api..

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f8870d221dbac29efc8b4d66b051647ee2bd804b")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0450e1fd6b11c892d3c75b3794a7b9839796afd2")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
