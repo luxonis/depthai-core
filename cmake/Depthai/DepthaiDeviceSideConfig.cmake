3fa78250546e8a5e1aa52c0e2dcd51ac9ab7498b
Jon Ngai <jon.ngai@gmail.com>
2021-01-22 11:11:40 -0700

    Updating firmware.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2a7529b029e63c362eacb595c8e41df7259e3997")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a1c817c5e9e77830fb0ba47cc0b8fa6b0553e3b3")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
