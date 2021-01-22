4c9722d2e93750154eb5e4523f84fa4f17c42c37
SzabolcsGergely <szabi@luxonis.com>
2021-01-22 19:47:46 +0200

    Bump core version; include fixes for Xlink

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1860863d0f10da90fc82098da935f4146a1a0d5b")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "922551b76fe361cd60dd9d24b88ce11d4dfc3e70")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
