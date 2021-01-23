99db0099f5e76a96651cc8b8a6d68f6a1359c057
TheMarpe <martin.peterlin7@gmail.com>
2021-01-23 22:48:33 +0100

    Updated FW - NeuralNetwork hotfix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0450e1fd6b11c892d3c75b3794a7b9839796afd2")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "eef12eeeff0152ef565180bbe0e71e19da23aacf")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
