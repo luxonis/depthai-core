63cc87314577fad0691a466c1a43cb19dfbe79d0
alex-luxonis <alex@luxonis.com>
2021-01-29 10:12:25 +0200

    CameraControl: add initial set of ISP/3A controls. Update FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "891b6a4f202251d14fc3d9f01383633c17fc91d6")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a10606cda3fbb2a76043228d3cc96eec8b19ab54")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- include/depthai/pipeline/datatype/CameraControl.hpp
+++ include/depthai/pipeline/datatype/CameraControl.hpp
@@ -21,6 +21,38 @@ class CameraControl : public Buffer {
     // Functions to set properties
     void setCaptureStill(bool capture);
 
+    void setStartStreaming();
+    void setStopStreaming();
+
+    // Focus
+    void setAutoFocusMode(RawCameraControl::AutoFocusMode mode);
+    void setAutoFocusTrigger();
+    void setAutoFocusRegion(uint16_t startX, uint16_t startY, uint16_t width, uint16_t height);
+    void setManualFocus(uint8_t lensPosition);
+
+    // Exposure
+    void setAutoExposureEnable();
+    void setAutoExposureLock(bool lock);
+    void setAutoExposureRegion(uint16_t startX, uint16_t startY, uint16_t width, uint16_t height);
+    void setAutoExposureCompensation(int8_t compensation);
+    void setAntiBandingMode(RawCameraControl::AntiBandingMode mode);
+    void setManualExposure(uint32_t exposureTimeUs, uint32_t sensitivityIso);
+
+    // White Balance
+    void setAutoWhiteBalanceMode(RawCameraControl::AutoWhiteBalanceMode mode);
+    void setAutoWhiteBalanceLock(bool lock);
+
+    // Other image controls
+    void setBrightness(uint16_t value); // TODO move to AE?
+    void setContrast(uint16_t value);
+    void setSaturation(uint16_t value);
+    void setSharpness(uint16_t value);
+    void setNoiseReductionStrength(uint16_t value);
+    void setLumaDenoise(uint16_t value);
+    void setChromaDenoise(uint16_t value);
+    void setSceneMode(RawCameraControl::SceneMode mode);
+    void setEffectMode(RawCameraControl::EffectMode mode);
+
     // Functions to retrieve properties
     bool getCaptureStill() const;
 };
