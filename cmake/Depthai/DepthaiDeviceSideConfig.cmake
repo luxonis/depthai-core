e15f6aa53cb5fb7ff2c6ffc56f356efbc9eee15c
alex-luxonis <alex@luxonis.com>
2021-01-30 18:34:51 +0200

    Update FW: fix for devices without NOR flash, avoid meta_d2h stream lockup

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "83717273059158eb810559cf7cf9723c0983ad3d")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d7a02668cfffb71bd1c95dea5892b3500bc1cbc9")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
