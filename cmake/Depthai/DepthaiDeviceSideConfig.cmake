b4cdec5b46ec157e10de4054584ef6a337d3c4e0
alex-luxonis <alex@luxonis.com>
2021-01-30 17:10:47 +0200

    Update FW: add flash programming test, the image written is also used to test SPI flash boot - blinking the SoM LED after boot mode change

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "30768f242a5e8c382c559570676be4bdbd155a03")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "83717273059158eb810559cf7cf9723c0983ad3d")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
