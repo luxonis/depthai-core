1a904968b10481cc4f6b7f2ed2674b7d82ac4131
TheMarpe <martin.peterlin7@gmail.com>
2021-02-01 21:30:10 +0100

    Updated FW - ImageManip rounding fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eef12eeeff0152ef565180bbe0e71e19da23aacf")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5b6abbe617695d431568ab01fa27e3fa7468da69")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
