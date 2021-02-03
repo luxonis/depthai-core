77d1bc720bf989cd2afaf4ddae012c593497b940
TheMarpe <martin.peterlin7@gmail.com>
2021-02-03 02:47:00 +0100

    Updated FW and added extra checks when running examples

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5b6abbe617695d431568ab01fa27e3fa7468da69")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "86c1a4f6c8a92e23fe0324f8bd3e4fd42793dd3f")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
