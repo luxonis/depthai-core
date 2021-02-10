1d3b7ce9527e633462892bc6f9538c71bd95227e
SzabolcsGergely <szabi@luxonis.com>
2021-02-10 03:04:48 +0200

    Update FW with usb2 patch

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b8541a692c4bad67884849905a328229114504e8")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d83f43039de57e658f83813ebbf51290a2d54a71")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
