1905c3a3cad2d91a5f97176607ea15db4d27ddc8
alex-luxonis <alex@luxonis.com>
2021-02-11 22:19:29 +0200

    Update FW: log warnings for unimplemented `initialControl` commands, changes to shared

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c1d766103180bf0e8893b07314ec3382e054f6e8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bb7bda62770e68d9e6de69ce2801ab87d9fa1047")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
