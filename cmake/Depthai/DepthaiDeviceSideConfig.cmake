b58a8674603d9b41fae1ea72a9b94bec0c0d4bfd
alex-luxonis <alex@luxonis.com>
2021-02-12 23:07:57 +0200

    Update FW: fix for USB init while booting from flash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "af97e22ce7219227b82c20975ff7e7bddeb5b7ea")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3e5caef40f5c489b67eb4029ae5487cd163c6530")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
