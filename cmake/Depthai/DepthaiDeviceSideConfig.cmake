58033d63d88a9bb0903578320a534ed34ba975a0
alex-luxonis <alex@luxonis.com>
2021-02-12 19:50:15 +0200

    Update FW: fix for 4-point warp with normalized = false

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f8760df31f4d37efcd8d6fc210f1a4215221de3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d49ac887074adf18024c21e33ce071ea48b0c014")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
