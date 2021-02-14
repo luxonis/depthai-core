87c48150215acc8f1f03fda8ddfa6eb3ad41d312
alex-luxonis <alex@luxonis.com>
2021-02-14 02:07:26 +0200

    Update FW: change UVC format to NV12

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3e5caef40f5c489b67eb4029ae5487cd163c6530")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "86d080a5e151aa76598a44aed41f125f6007ccf6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
