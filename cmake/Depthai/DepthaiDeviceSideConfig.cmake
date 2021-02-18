c5cfcf32c91a0d7d22bbdab316afa41a82c682c7
SzabolcsGergely <szabi@luxonis.com>
2021-02-18 14:53:10 +0200

    Add CMX memory usage repor

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "be86d417a082248f6127d98d99e184d29db36aa0")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1815d38f94853dd0d077af2c57108c9f077b70d5")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
