c3c25654f136dfafa86450cff49ccef7e1597a14
SzabolcsGergely <szabi@luxonis.com>
2021-02-19 23:21:31 +0200

    Add USB2 fix for large blobs; add h264 cpp example

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c3f56ebb457f5bd8430ed4987becf1c96786454a")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f2d0313939538c4dfa40fa142e22a6d9ea227393")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
