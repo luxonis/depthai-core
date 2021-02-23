f108badedb03c945b43e356293af64c8403e015a
alex-luxonis <alex@luxonis.com>
2021-02-23 12:50:57 +0200

    Update FW: improve image resize mechanism (less cropping), for ColorCamera `preview` output and ImageManip setResize

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "37a2c71ec7b87b24fba7d6c018c701acf662c077")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4549a0e785f7e3a5d79481c05112e5f78ceb5e4e")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
