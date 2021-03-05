848440e6fb40bec0d00259eec48bb766222ba528
alex-luxonis <alex@luxonis.com>
2021-03-05 17:13:26 +0200

    Update FW/shared: formal switch to develop branch

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5e4974e930f4ecbe1bf718463f6a5da664e68788")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ae1c9ddcbd69ecbf9f6aeb720f3158a3003a8b3a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
