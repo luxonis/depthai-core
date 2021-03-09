2fe060375d7763681c4750741a2fe676d92163c7
SzabolcsGergely <szabi@luxonis.com>
2021-03-10 01:21:45 +0200

    Update FW with rectified fixes in stereo

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d059b917a6ae26b91473a8ae573b92567ae0a4da")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ba02275edca0a757c45c84fa7467a1baba450a1c")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
