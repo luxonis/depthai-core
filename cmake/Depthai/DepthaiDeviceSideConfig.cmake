87bf3c394adb0986f475c582aa0a0782c4090c59
SzabolcsGergely <szabi@luxonis.com>
2021-03-13 08:50:16 +0200

    Update FW with rgb-depth sync

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "58b9f5633728f6c3f6b36cc1d06c707d076f85a2")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6d99fda9ff265548d22f76e1d775f3a8936f0179")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
