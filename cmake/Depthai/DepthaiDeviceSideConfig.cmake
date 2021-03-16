fd4be4f8fbb0b8342977f976ff4e56f90cab53ed
SzabolcsGergely <szabi@luxonis.com>
2021-03-16 12:45:54 +0200

    Update submodules before merge

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2a237f8c8dd57134c37200cbc6ec84d64f8621fa")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1a02d5d1e684380dce117d0cb0c5fe83dc7bc8b2")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
