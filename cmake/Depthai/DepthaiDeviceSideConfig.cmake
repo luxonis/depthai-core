4b130dd60a19d6b1a41ea9eab057453f2b40bd16
TheMarpe <martin.peterlin7@gmail.com>
2021-03-17 00:59:29 +0100

    Updated FW - fixes depth calculator issues on devices without calibration

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1a02d5d1e684380dce117d0cb0c5fe83dc7bc8b2")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0149933b6f845f30b6d68defd59ca6f52bdaa78c")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
