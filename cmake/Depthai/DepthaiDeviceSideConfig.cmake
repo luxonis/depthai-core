b6c1293e6bd42e2532bb455c51352217c12d910b
alex-luxonis <alex@luxonis.com>
2021-03-17 19:40:31 +0200

    Update FW: gen2_uvc_custom

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6b48e357a99e46f8aeaea3d507926f17fed0fcf8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8ce6ddaf369aa668e1b02816a4eb69a7bfebbf0b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
