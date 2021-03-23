d4338e28e2699155faad5eee8f073333568638f3
alex-luxonis <alex@luxonis.com>
2021-03-23 03:17:12 +0200

    CameraControl: add ranges for extra controls, remove non-implemented setNoiseReductionStrength. Updated FW: all initial controls can be applied for Mono/ColorCamera (no longer limited to focus settings for Color)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "51da5cf23ce7f05ff8466193dbe76758c4668c69")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "80b63be3ce97dc177df17865da85403ff08348ca")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
