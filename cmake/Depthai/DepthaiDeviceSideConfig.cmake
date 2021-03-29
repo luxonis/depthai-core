ab96dd5ef34a3d6d2f23e6a5d870eb917ce99ee9
SzabolcsGergely <szabi@luxonis.com>
2021-03-29 22:23:16 +0300

    Hotfix for bounding box mapping

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "563f668041c5dd1d462a15ce1174a368ded1fbb8")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8d27dfe16cbb72fa2649b87385a5a726127c4cfa")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
