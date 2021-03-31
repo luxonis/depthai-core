69726b3c37598b31faa6016e677bb904b5960d95
alex-luxonis <alex@luxonis.com>
2021-03-31 14:08:24 +0300

    Update FW: disparity (U8) aligning to RGB works. TODO depth and subpixel (U16)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f1ac77644d6fd0ce75485e9fa20d7e34ae23ce7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "51da5cf23ce7f05ff8466193dbe76758c4668c69")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
