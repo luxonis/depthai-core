7df740240dac46d3c16187df8365040190eafc36
alex-luxonis <alex@luxonis.com>
2021-04-02 04:09:44 +0300

    Update FW: optimize depth align, make it work with subpixel/U16 (still not optimized)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "80b63be3ce97dc177df17865da85403ff08348ca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3c19d5edec86d1b7709e5a4224a9019f5e695d32")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
