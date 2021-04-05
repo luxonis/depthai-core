7ad8ca5eaaf8caf4b8d353e1101d372e804bc101
alex-luxonis <alex@luxonis.com>
2021-04-05 19:42:35 +0300

    Update FW: fix flash-restart after USB-run

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f75705aaabeade6362269fbfc119d857040cb699")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2fb0e4fef444e139e53aa45b5f85d6895b9adb9e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
