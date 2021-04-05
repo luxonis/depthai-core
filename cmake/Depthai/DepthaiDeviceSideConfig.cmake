456d65429e4289d3d9faec203c864d32ac50114c
alex-luxonis <alex@luxonis.com>
2021-04-05 18:01:47 +0300

    Update FW: boot mode 0x2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b1998128483aae74b9afb388195b5b202d22e5c5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f75705aaabeade6362269fbfc119d857040cb699")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
