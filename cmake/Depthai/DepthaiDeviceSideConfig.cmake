6e6391a781b8ecc58fcb77bf8a99b8c6c08c2a72
Sachin <sachin.guruswamy@gmail.com>
2021-04-08 12:40:02 -0700

    modified device side config

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0d77d6d052b43509262155430bbe71f067550b80")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "369b2bcca21c393b312328a1c3885c17b293dac6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
