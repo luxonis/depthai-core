46d0d912319e3e49bad14d6623574bbf032bbfb3
TheMarpe <martin.peterlin7@gmail.com>
2021-04-13 23:46:48 +0200

    Fixed scripting 'setCropRect' and added bounds

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "819c1c01771e2cfe36cfef4ee79d32d6f602c3b2")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "15e58e52c188720247e27fc2b04595cf954e9698")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
