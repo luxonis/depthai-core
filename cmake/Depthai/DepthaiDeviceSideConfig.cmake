c78e0013d579a9ec6310242e47df816d54775296
Jon Ngai <jon@luxonis.com>
2021-04-14 09:08:39 -0600

    Updating firmware (Fixing datetime on ImgFrame::getTimestamp)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "819c1c01771e2cfe36cfef4ee79d32d6f602c3b2")          
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a45d74937ea0e20230e36144fe123fa7e5aefce9")          
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
