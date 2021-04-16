f92242f8e79e6f4e4f9cda9acb1aac7b2d268bb1
Martin Peterlin <martin.peterlin7@gmail.com>
2021-04-16 11:09:57 +0200

    Updated FW - MobileNet parsing bugfix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "595330292c4bd1955e4e8046b0f42af2dba4b124")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f34e1ec1450b659bd8ff8383eaff5b7f988cb6fc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
