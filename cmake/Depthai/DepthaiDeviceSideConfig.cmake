02357b892e64764e2dd6b71810d5de1af8b6ee22
alex-luxonis <alex@luxonis.com>
2021-04-24 02:50:22 +0300

    Update FW: IMU upgrade fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0d4e935f183d3220107fdf004004d4de96a6bed1")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fcdefaf4c60afb4d712999e890c466139fa6b2a1")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
