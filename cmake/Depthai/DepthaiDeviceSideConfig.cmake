a1285bb66e2799644f0972b02130db656ba910c1
alex-luxonis <alex@luxonis.com>
2021-04-24 03:20:21 +0300

    Update FW: fix handling for no IMU / HW issues

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "fcdefaf4c60afb4d712999e890c466139fa6b2a1")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "18f22539f844a5d24118299f424e64156a9fa430")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
