8699d1535df7d2e1b26d1792691efe9f0bf41b4f
SzabolcsGergely <szabi@luxonis.com>
2021-04-26 13:40:53 +0300

    Update FW with fix for timesync

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3da45bce8f1178f33be211861a7848ab3993d9b9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a2efd50dde44e2d4e84e18b85d883d6421af68e2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
