f62304d8b37cc19aeb7b8f535c1bdd51ae60d528
alex-luxonis <alex@luxonis.com>
2021-04-28 01:26:50 +0300

    Update FW: fix still capture with scaling, add FPS capping (with warnings)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c03b9979b83941cdc9feb1a29f5a1452234a65f9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9a713b5a5e4422944a2cf98ba177738d465da19f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
