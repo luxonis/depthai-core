2cfe7453cbdd93fe0c4bbdd320bc2a88819e0e1d
SzabolcsGergely <szabi@luxonis.com>
2021-04-28 02:34:55 +0300

    Add new field: Removed to object tracker status

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b71584129b101b30e4632678c19d33b020c04c36")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3ff687833a5532b40e36e50db7d1ca49d65ec837")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
