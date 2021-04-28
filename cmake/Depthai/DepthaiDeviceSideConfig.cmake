bffa915bdaac684511c80c52f9fcc5c69f2271cd
alex-luxonis <alex@luxonis.com>
2021-04-28 13:37:42 +0300

    Update FW, fix CI build: depthai-shared PR merged

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "49ca8b9d035ef51c3442d860ebf2e9d701807e44")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3b51648b0d96c9391d878eede0fd5fd9b44084ca")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
