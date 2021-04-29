edc7f81bad04dc3cfde8a6225c9528a5420ff659
TheMarpe <martin.peterlin7@gmail.com>
2021-04-29 03:37:35 +0200

    Updated device side with 'getConnectedCameras'

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b71584129b101b30e4632678c19d33b020c04c36")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "024f66e3c54509867585c4079c949b57ac9d8bdf")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- src/device/Device.cpp
+++ src/device/Device.cpp
@@ -593,7 +593,9 @@ void Device::init2(bool embeddedMvcmd, bool usb2Mode, const std::string& pathToM
 
         // Starts pipeline if given
         if(pipeline) {
-            startPipeline(*pipeline);
+            if(!startPipeline(*pipeline)){
+                throw std::runtime_error("Couldn't start the pipeline");
+            }
         }
 
     } catch(const std::exception&) {
