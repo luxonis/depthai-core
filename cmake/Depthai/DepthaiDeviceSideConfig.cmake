e3b5ef38e954f1688b9c555852393e5e8f098d3b
alex-luxonis <alex@luxonis.com>
2021-04-30 09:20:46 +0300

    Update FW: initial ColorCamera support for OV9782

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b54b32bba941a6d343b29f870283787a487406bc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "694506bc17d036e03a5a69880a9ea79cc1e577ec")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
