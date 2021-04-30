f925d467a5beb6d5c7a280ae1317dcc028512711
alex-luxonis <alex@luxonis.com>
2021-05-01 02:51:16 +0300

    Update FW: ImageManip: allow YUV420p and NV12 to be processed to Gray (1 or 3 planes)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "30730bb1522e60ec4bdecf94534ec3134a1896d0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "36d4dc18adbc7c6f8da782359571307f6240e975")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
