75038999b6766ffede481b3e40c8e16a956153cd
alex-luxonis <alex@luxonis.com>
2021-05-03 17:03:35 +0300

    Update FW (properly set flipping with LRcheck enabled to spatial calculator), update StereoDepth docs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "59c6e4cc56a0cfc1342dadd525458f4ff41ddb06")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "16652fb049b5a9b47e3098d94fb9cedc353f82e9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
