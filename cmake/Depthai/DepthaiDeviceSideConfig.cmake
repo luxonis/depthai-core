074756205cbd99997b88f71c18efb4b8bb709612
alex-luxonis <alex@luxonis.com>
2021-05-08 01:08:55 +0300

    Update FW: fix IMU streaming after an interrupted upgrade (followed by a completed one) - add support for Arducam OV9282 with different I2C addr

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "18f22539f844a5d24118299f424e64156a9fa430")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "dea38dbff4ff38d6225c113587e42a5e4b25aa10")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
