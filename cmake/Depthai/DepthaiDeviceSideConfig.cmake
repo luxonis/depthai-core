92a55a29a614cf0b93bfdecfae510b8369430203
alex-luxonis <alex@luxonis.com>
2021-05-07 21:18:02 +0300

    Update FW: all OV9282 configs using single MIPI lane

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "16652fb049b5a9b47e3098d94fb9cedc353f82e9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8817227caea9094409c659670dfcfe66c4217d49")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
