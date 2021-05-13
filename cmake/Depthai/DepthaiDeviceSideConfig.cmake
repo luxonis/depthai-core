93a580991f899f165d2059314fb40b29d92a276e
Sachin <sachin.guruswamy@gmail.com>
2021-05-12 18:57:23 -0700

    fixed test and updated shated

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3c59e35b2c427e5c5a699fd487e455cabf738d3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6e40806fa64d0efa4fec3e522e705df3effcf119")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
