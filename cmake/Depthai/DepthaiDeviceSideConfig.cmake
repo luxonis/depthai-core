04a790bb6ac1b193249eff6a692ac7bae044423b
alex-luxonis <alex@luxonis.com>
2021-05-15 00:09:27 +0300

    Update FW: fix OV9782 bayer order

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c2bf875327990b95a84aaa0e5d5fe05fa68e0592")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "faf65d29a955cf5b375826437468db0dc33d0fb5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
