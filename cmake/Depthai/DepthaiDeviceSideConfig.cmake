916f9cd598cc4fc4f2c0673e594d9eee50ab9b4e
TheMarpe <martin.peterlin7@gmail.com>
2021-05-17 23:01:02 +0200

    Hotfix - Lossless encoding

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "37840fc261256c6e8a1e7d854838d6055000f195")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2ca9f51f803f2ebb587ffb7fdcea01d6638bdf6a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
