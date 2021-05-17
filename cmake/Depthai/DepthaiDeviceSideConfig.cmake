85c9b2f43eb52716c5a78d920956caddf455c63d
alex-luxonis <alex@luxonis.com>
2021-05-17 21:38:41 +0300

    OV9282: fix over-exposure outdoors, in sunlight

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "37840fc261256c6e8a1e7d854838d6055000f195")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6fcedbe811789bb36181f06639b84a0bf01d2172")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
