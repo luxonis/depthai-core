8378d6c3e4616bff35415e70fa547f50a1a3e571
alex-luxonis <alex@luxonis.com>
2021-05-19 10:58:54 +0300

    Update FW: stereo fixes: LR-check flip, `depth` align to RGB

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2ca9f51f803f2ebb587ffb7fdcea01d6638bdf6a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d26fdab6b0a66b09f0b3b6ca8d0dea50229278b7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
