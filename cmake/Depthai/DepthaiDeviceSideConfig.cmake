99ce505ec76c66bbcfd5ee4adef7e9e49b663084
alex-luxonis <alex@luxonis.com>
2021-05-20 02:50:37 +0300

    TMP: OV9782(color) working on Left and/or Right sockets

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d2b298e834f3cf746071059e2c1e1c4d37b724af")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4ceaec0afc1891083c8fb041e2788731ec699850")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
