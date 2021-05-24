1f3bed069159a966018ee2430c0b2ff519cbf27e
alex-luxonis <alex@luxonis.com>
2021-05-24 17:03:37 +0300

    Update FW, update `setRectifyMirrorFrame` functionaliy/description

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d26fdab6b0a66b09f0b3b6ca8d0dea50229278b7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "556a856e85570e0d874d19b305a0080a8f1f58bf")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
