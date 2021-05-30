64f63a7e3a97ab5b1a3bfb7eb02547be2a47bf74
Sachin <sachin.guruswamy@gmail.com>
2021-05-29 18:38:32 -0700

    Updated examples to create backup

--- examples/src/calibration_flash.cpp
+++ examples/src/calibration_flash.cpp
@@ -1,5 +1,6 @@
 
 #include <cstdio>
+#include <filesystem>
 #include <iostream>
 #include <string>
 
@@ -8,12 +9,15 @@
 
 int main(int argc, char** argv) {
     std::string calibPth(CALIB_PATH);
+    std::string calibBackUpPath("depthai_calib_backup.json");
     if(argc > 1) {
         calibPth = std::string(argv[1]);
     }
 
     dai::CalibrationHandler calibData(calibPth);
     dai::Device d;
+    dai::CalibrationHandler deviceCalib = d.readCalibration();
+    deviceCalib.eepromToJsonFile(calibBackUpPath);
     if(d.flashCalibration(calibData)) {
         std::cout << "Calibration flash Successful" << std::endl;
     } else {
--- examples/src/calibration_flash_v5.cpp
+++ examples/src/calibration_flash_v5.cpp
@@ -1,22 +1,24 @@
 
 #include <cstdio>
+#include <filesystem>
 #include <iostream>
 #include <string>
 
 // Inludes common necessary includes for development using depthai library
-#include "depthai-shared/common/CameraBoardSocket.hpp"
-#include "depthai-shared/common/EepromData.hpp"
 #include "depthai/depthai.hpp"
 
 int main(int argc, char** argv) {
     std::string calibBinaryFile(CALIB_PATH), boardConfigFile(BOARD_PATH);
+    std::string calibBackUpPath("depthai_calib_backup.json");
     if(argc > 2) {
         calibBinaryFile = std::string(argv[1]);
         boardConfigFile = std::string(argv[2]);
     }
 
-    dai::Device d;
     dai::CalibrationHandler calibData(calibBinaryFile, boardConfigFile);
+    dai::Device d;
+    dai::CalibrationHandler deviceCalib = d.readCalibration();
+    deviceCalib.eepromToJsonFile(calibBackUpPath);
     if(d.flashCalibration(calibData)) {
         std::cout << "Calibration flash Successful" << std::endl;
     } else {
