f7cc581f56b0d646d8b65c6c4d96e802d37c5d62
Sachin <sachin.guruswamy@gmail.com>
2021-05-29 20:35:41 -0700

    Updated device side

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c79af2518c8e8459a48ccfecaa5608ef2a6bf182")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "dcbd40fb85dd67b01e7a23134a223c7c1b361793")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
