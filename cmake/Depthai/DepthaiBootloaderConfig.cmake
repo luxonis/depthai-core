c5e45250e6697bf093eda7093ea4748beaa66005
TheMarpe <martin.peterlin7@gmail.com>
2021-06-02 18:32:34 +0200

    Updated bootloader and command to boot fw

--- cmake/Depthai/DepthaiBootloaderConfig.cmake
+++ cmake/Depthai/DepthaiBootloaderConfig.cmake
@@ -1,5 +1,5 @@
 # Maturity level "snapshot" / "release"
-set(DEPTHAI_BOOTLOADER_MATURITY "release")  
+set(DEPTHAI_BOOTLOADER_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_BOOTLOADER_VERSION "0.0.11")
+set(DEPTHAI_BOOTLOADER_VERSION "3866a3293952f69f219f5767009d1a7ac221ad81")
