dc960d2ccaa91c3ad199019d975dc18f2284dd37
SzabolcsGergely <szabi@luxonis.com>
2021-06-03 18:53:10 +0300

    Rename RAW_* to *_RAW in ImuSensors

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "24ce35f81d6b8ea556f680a257cc5d82a840fa0f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "40af742fb6b4c9cb33e246f2436ade1fbaa4c3d9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
