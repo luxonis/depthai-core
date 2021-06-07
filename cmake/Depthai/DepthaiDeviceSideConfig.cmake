6a04a4120d7133d375b5efd85123bffc4c127b9d
alex-luxonis <alex@luxonis.com>
2021-06-07 21:42:35 +0300

    Update depthai-shared: make clangformat

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4d2e72e667d1449b755413658440a1d48bc59d91")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3941335896a9ff2b83b3d20bd1d62fed82a543d7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
