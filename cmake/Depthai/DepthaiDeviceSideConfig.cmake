2398e35ece9f98482cb0c2a91600e206411a0f7d
alex-luxonis <alex@luxonis.com>
2021-06-09 20:38:58 +0300

    CameraControl: add `setManualWhiteBalance(colorTemperatureK)`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bfee6dc9c8b294e87c3f14cca347bc53ae85e8be")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a9d8a52ed16a703b7df8ec263102153a9f01da0c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
