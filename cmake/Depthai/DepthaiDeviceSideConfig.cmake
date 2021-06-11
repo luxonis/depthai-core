37fd24f3beddb0bbf904acb09f7966a240ba67e8
TheMarpe <martin.peterlin7@gmail.com>
2021-06-11 17:47:58 +0200

    Fixed NN bug

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e944a7737a6209e91d9cadaf8897dabc93fcf255")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "97b0219c954de22149a28fe31c373ee2bb0655a8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
