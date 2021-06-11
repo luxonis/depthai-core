e7d6fae6d71669826ad30658bf7d2e23d7c57f37
Kunal Tyagi <tyagi.kunal@live.com>
2021-06-11 13:19:56 +0900

    Adding convenience headers

--- include/depthai/depthai.hpp
+++ include/depthai/depthai.hpp
@@ -14,34 +14,10 @@
 #include "pipeline/Pipeline.hpp"
 
 // Include common nodes
-#include "pipeline/node/ColorCamera.hpp"
-#include "pipeline/node/DetectionNetwork.hpp"
-#include "pipeline/node/IMU.hpp"
-#include "pipeline/node/ImageManip.hpp"
-#include "pipeline/node/MonoCamera.hpp"
-#include "pipeline/node/NeuralNetwork.hpp"
-#include "pipeline/node/ObjectTracker.hpp"
-#include "pipeline/node/SPIOut.hpp"
-#include "pipeline/node/SpatialDetectionNetwork.hpp"
-#include "pipeline/node/SpatialLocationCalculator.hpp"
-#include "pipeline/node/StereoDepth.hpp"
-#include "pipeline/node/SystemLogger.hpp"
-#include "pipeline/node/VideoEncoder.hpp"
-#include "pipeline/node/XLinkIn.hpp"
-#include "pipeline/node/XLinkOut.hpp"
+#include "pipeline/Nodes.hpp"
 
 // Include common datatypes
-#include "pipeline/datatype/Buffer.hpp"
-#include "pipeline/datatype/CameraControl.hpp"
-#include "pipeline/datatype/IMUData.hpp"
-#include "pipeline/datatype/ImageManipConfig.hpp"
-#include "pipeline/datatype/ImgDetections.hpp"
-#include "pipeline/datatype/ImgFrame.hpp"
-#include "pipeline/datatype/NNData.hpp"
-#include "pipeline/datatype/SpatialImgDetections.hpp"
-#include "pipeline/datatype/SpatialLocationCalculatorData.hpp"
-#include "pipeline/datatype/SystemInformation.hpp"
-#include "pipeline/datatype/Tracklets.hpp"
+#include "pipeline/Datatypes.hpp"
 
 // namespace dai {
 // namespace{
