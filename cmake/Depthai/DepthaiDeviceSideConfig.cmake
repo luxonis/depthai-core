9713a08f9739c4a3b80ad1b2a213ee88e4b2545e
TheMarpe <martin.peterlin7@gmail.com>
2021-06-11 18:56:30 +0200

    Fixed NN deadlock edge case

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9628a4701a703d4654d2e965bbfcb74b6cee4915")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d98e1b5b0d8681533916eb4733f27096ed6000b9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
