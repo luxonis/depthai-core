aff7240eed0c46aa82aee0b8e0d794f8e060976d
alex-luxonis <alex@luxonis.com>
2021-06-12 21:03:05 +0300

    Update FW: depthai-shared PR merged, also included FW changes from https://github.com/luxonis/depthai-core/pull/118 : fixes for new boards with 0x2 boot mode (not switching back to bootloader after app reset)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cd125785287b9416c5c31afb3790e30c92d855b0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cae5765151ebd8dbf24c6dbd772a9bf67d935a48")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
