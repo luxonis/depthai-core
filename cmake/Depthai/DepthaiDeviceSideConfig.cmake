5bd57d8f8aee7c862f13baf15972ccc314861f98
SzabolcsGergely <szabi@luxonis.com>
2021-06-17 20:51:28 +0300

    Update FW with resource allocation fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e67dee8bba18786b8fabe8fe212455481ca5c8b3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "169d63f7c4360e0a1ad9de6917ce734d97113a75")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
