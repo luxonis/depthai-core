7cba0507cf7579e314ef7effede34dc24ca599b8
TheMarpe <martin.peterlin7@gmail.com>
2021-06-18 22:58:32 +0200

    Applied formatting

--- examples/src/flash_bootloader.cpp
+++ examples/src/flash_bootloader.cpp
@@ -1,5 +1,5 @@
-#include <string>
 #include <chrono>
+#include <string>
 
 #include "depthai/depthai.hpp"
 
@@ -7,10 +7,10 @@ int main(int argc, char** argv) {
     using namespace std::chrono;
 
     dai::DeviceBootloader::Type blType = dai::DeviceBootloader::Type::USB;
-    if(argc > 1){
-        if(std::string(argv[1]) == "usb"){
+    if(argc > 1) {
+        if(std::string(argv[1]) == "usb") {
             blType = dai::DeviceBootloader::Type::USB;
-        } else if(std::string(argv[1]) == "eth"){
+        } else if(std::string(argv[1]) == "eth") {
             blType = dai::DeviceBootloader::Type::ETHERNET;
         } else {
             std::cout << "Specify either 'usb' or 'eth' bootloader type\n";
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
     std::string message;
     auto t1 = steady_clock::now();
     std::tie(res, message) = bl.flashBootloader(blType, progress);
-    if(res){
+    if(res) {
         std::cout << "Flashing successful. Took " << duration_cast<milliseconds>(steady_clock::now() - t1).count() << "ms" << std::endl;
     } else {
         std::cout << "Flashing failed: " << message << std::endl;
