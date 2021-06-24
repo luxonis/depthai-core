18a09bbe0db000e87880a003772202b737672d7e
SzabolcsGergely <szabi@luxonis.com>
2021-06-24 23:41:42 +0300

    Hotfix: Update FW with fix for crash w/ depth-rgb aligment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a365ab1f9d9b9a44afb00bb648e3bceb730fe797")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1b93fa5bce981ec0624688db407231cf8e822b6d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
