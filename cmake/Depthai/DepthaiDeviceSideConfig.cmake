4f6b2ab0a0eff648a54a932c14f543d6502f5d98
alex-luxonis <alex@luxonis.com>
2021-06-25 03:22:04 +0300

    Update bootloader, FW: fix flash boot

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4c1f99aca11c76bca125e3a537df60125b70a552")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0b98d6bd62d991e93fe1c97d37cec7d0847f956f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
