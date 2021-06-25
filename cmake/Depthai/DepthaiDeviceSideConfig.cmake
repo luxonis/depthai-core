41fef7e25db6d386a9f1b5a6e96b08c18a842865
Sachin <sachin.guruswamy@gmail.com>
2021-06-25 16:17:57 -0700

    updated FW to develop:

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6838c4ee7dbf680f7924a85d63b019555bf693e1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d604d6076067e2136049ed3e9fbb1d0aa340aa50")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
