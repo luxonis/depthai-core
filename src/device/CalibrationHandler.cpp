330d74d8c9b4ca99e1b6c403a0ab359c69090bfe
Sachin <sachin.guruswamy@gmail.com>
2021-06-25 14:21:23 -0700

    Additinal checks

--- src/device/CalibrationHandler.cpp
+++ src/device/CalibrationHandler.cpp
@@ -343,7 +343,7 @@ std::vector<std::vector<float>> CalibrationHandler::getImuToCameraExtrinsics(Cam
 
     std::vector<std::vector<float>> transformationMatrix = eepromData.imuExtrinsics.rotationMatrix;
     if(useSpecTranslation) {
-        transformationMatrix4[0].push_back(eepromData.cameraData[cameraId].extrinsics.specTranslation.x);
+        transformationMatrix[0].push_back(eepromData.cameraData[cameraId].extrinsics.specTranslation.x);
         transformationMatrix[1].push_back(eepromData.cameraData[cameraId].extrinsics.specTranslation.y);
         transformationMatrix[2].push_back(eepromData.cameraData[cameraId].extrinsics.specTranslation.z);
     } else {
@@ -399,7 +399,7 @@ std::vector<std::vector<float>> CalibrationHandler::computeExtrinsicMatrix(Camer
         throw std::runtime_error("Invalid cameraId input..");
     }
     if(eepromData.cameraData[srcCamera].extrinsics.toCameraSocket == dstCamera) {
-        if(eepromData.cameraData[srcCamera].rotationMatrix.size() == 0 || eepromData.cameraData[srcCamera].toCameraSocket == CameraBoardSocket::AUTO) {
+        if(eepromData.cameraData[srcCamera].extrinsics.rotationMatrix.size() == 0 || eepromData.cameraData[srcCamera].extrinsics.toCameraSocket == CameraBoardSocket::AUTO) {
             throw std::runtime_error(
                 "Defined Extrinsic conenction but rotation matrix is not available. Please cross check your calibration data configuration.");
         }
