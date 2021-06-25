6f21c8976b8601eaa4205ffffde7683098ccfa1f
SzabolcsGergely <szabi@luxonis.com>
2021-06-25 15:12:23 +0300

    Merge FW with latest develop

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a7b77e897e1967661cd58410316f6f9d1d62246d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f67626f89e6becc3bbdb7e21015012ac33a834e2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
