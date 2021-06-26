3ddc45f8ded07c774001ad9ef32d891a3ecf119f
SzabolcsGergely <szabi@luxonis.com>
2021-06-26 03:56:33 +0300

    Add edge detector node using HW sobel edge filter

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a365ab1f9d9b9a44afb00bb648e3bceb730fe797")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f1e762e07e82cbe991b74404c00c365cab6f72db")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
