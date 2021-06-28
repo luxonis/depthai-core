c76be5256aa133a61dd0337616ad8be0ee5ca12a
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2021-06-28 09:13:59 -0700

    Calib fix (#163)
    * Bug fix
    
    * Fixing negative
    
    * Updated device side fix for signs
    
    * Additinal checks
    
    * Fixed styling
    
    * updated FW to develop:

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1b93fa5bce981ec0624688db407231cf8e822b6d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d604d6076067e2136049ed3e9fbb1d0aa340aa50")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
