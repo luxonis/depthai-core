5be4fab0af2c4f2270f52f0bb4d4145d561ee8b9
alex-luxonis <alex@luxonis.com>
2021-07-02 17:53:41 +0300

    Update FW: improved focus for IMX214, lowered range

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8ebbc98374f6970e227804c235e87e8801043bf1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "28e8a2ae69b9c6cebbb6b4cf920f21d5b3d7a8fe")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
