897e2a72125a4991d977cc8e28dfa1afaa0d3c13
alex-luxonis <alex@luxonis.com>
2021-07-02 06:36:46 +0300

    Update FW: fix some OV7251 flickering, change stereo res to 480p, improve IMX214 focusing

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9cbd496b930bb28a3d6642c156c60c6f158943ca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8ebbc98374f6970e227804c235e87e8801043bf1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
