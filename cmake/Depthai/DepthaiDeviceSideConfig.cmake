da37369e770d15754cebfa9371879b5561722389
SzabolcsGergely <szabi@luxonis.com>
2021-07-06 03:13:05 +0300

    Release v2.6.0

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "33ff0b2b5287df25573690438374f44a59529429")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cd9d9c66a6000f9cedd16d64ae243aa56ab119d1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
