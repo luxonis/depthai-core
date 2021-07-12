fdb11cb9da6eff50e293a10d7ab81f0f0792453a
SzabolcsGergely <szabi@luxonis.com>
2021-07-12 17:46:52 +0300

    Hotfix FW: revert increased memory consumption

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "33ff0b2b5287df25573690438374f44a59529429")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6c7d5206dca68fde22314bc97c0e437b00930b70")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
