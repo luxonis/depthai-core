2f3dc1c3f4f4d575eed44cb7a12d23b8ba1997b1
SzabolcsGergely <szabi@luxonis.com>
2021-07-13 06:36:50 +0300

    Update FW with multi instance support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bf0a337613d82ad97f13b1cf2b6e99e4d8ae7032")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "99b34ec1ca0635bb9e55c570b907a67eafa5fe19")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
