66f7f4276578584dade92e777e45b920de8fc31b
alex-luxonis <alex@luxonis.com>
2021-07-15 02:41:44 +0300

    Update FW: implement RGB - Mono sync: capture time and sequence numbers

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "04b3c29581c258502fb50540f3954bd49aac7f90")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "dd9a974d93b0951de921c9223c434f8ce1960699")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
