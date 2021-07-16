6ae815c044fbf76be62ebdfacb7eceea4a5bda8b
SzabolcsGergely <szabi@luxonis.com>
2021-07-16 09:46:44 +0300

    Hotfix: fix NN memory allocation regression

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "04b3c29581c258502fb50540f3954bd49aac7f90")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c02b60f1b735fedef3b52b838e5b3c65affd6c61")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
