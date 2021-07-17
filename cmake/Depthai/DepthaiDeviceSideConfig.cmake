d8cd2f7cec48e2066b0714273119decc4f194e15
alex-luxonis <alex@luxonis.com>
2021-07-17 11:04:45 +0300

    Update FW: report lens position, exposure, iso in ImgFrame for RGB, (note: lens position may be reported one frame in advance), fix black image when initial manual exposure was set with other controls, fix switching from auto to manual exposure causing the lens to jump to zero (note: the lens is directly set now with manual focus, bypassing 3A)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0ca2bf5859abefbf4a85e614b5ef07ca8fa93788")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "99c462de2e5a19816686c7b5cb88107f682205f8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
