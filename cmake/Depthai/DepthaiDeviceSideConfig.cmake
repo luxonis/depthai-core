e8d077e4a3e537ff55f48504dd49efb8144a6e70
SzabolcsGergely <szabi@luxonis.com>
2021-07-19 06:16:21 +0300

    Update FW with OpenVino FW fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1087403e3407a40a3342d27b3169b9a8e042b2ad")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5e256e60d6c3552f9ce24a1df364abd3a11045ef")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
