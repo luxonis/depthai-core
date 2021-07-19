1991185f7c7a8885bf6ab8ef2382910670133146
TheMarpe <martin.peterlin7@gmail.com>
2021-07-19 21:56:18 +0200

    Fixed a binding issue in FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f7161557bbeb0d491841c8d0146141d610d91535")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "af62791b09e10086a13e1c1805176bf1db3e2802")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
