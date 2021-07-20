53c63320be462d35202bdb667cf11e23f655df0d
alex-luxonis <alex@luxonis.com>
2021-07-20 03:45:25 +0300

    UVC: configure GPIO control for stream on/off

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0b98d6bd62d991e93fe1c97d37cec7d0847f956f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "01d27cf47e20f7eae2e14a3798284bb8fc0d6237")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- include/depthai/pipeline/node/UVC.hpp
+++ include/depthai/pipeline/node/UVC.hpp
@@ -3,12 +3,12 @@
 #include <depthai/pipeline/Node.hpp>
 
 // shared
-// TODO #include <depthai-shared/pb/properties/XLinkOutProperties.hpp>
+#include <depthai-shared/properties/UVCProperties.hpp>
 
 namespace dai {
 namespace node {
 class UVC : public Node {
-    // dai::XLinkOutProperties properties;
+    dai::UVCProperties properties;
 
     std::string getName() const override;
     std::vector<Input> getInputs() override;
@@ -20,6 +20,15 @@ class UVC : public Node {
     UVC(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId);
 
     Input input{*this, "in", Input::Type::SReceiver, {{DatatypeEnum::Buffer, true}}};
+
+    /// Set GPIO list <gpio_number, value> for GPIOs to set (on/off) at init
+    void setGpiosOnInit(std::unordered_map<int, int> list);
+
+    /// Set GPIO list <gpio_number, value> for GPIOs to set when streaming is enabled
+    void setGpiosOnStreamOn(std::unordered_map<int, int> list);
+
+    /// Set GPIO list <gpio_number, value> for GPIOs to set when streaming is disabled
+    void setGpiosOnStreamOff(std::unordered_map<int, int> list);
 };
 
 }  // namespace node
