f486feb117da67fd24ec191d11db9b88949c4e59
alex-luxonis <alex@luxonis.com>
2021-07-21 02:32:12 +0300

    Remove UAC dummy speaker

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "01d27cf47e20f7eae2e14a3798284bb8fc0d6237")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d5c0c6c2a52a5a753c5469c56de443d262351f3d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
