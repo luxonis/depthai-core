4293e76ac68bd69f6bc44ebd54f70cd37e07604b
alex-luxonis <alex@luxonis.com>
2021-07-24 04:26:39 +0300

    CameraControl: add setAutoFocusLensRange

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "90da733dd5519e805ac5563d13cc4b0516ddb5c3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0c389abe5377d13dbaae412d54922cfc940ac1db")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
