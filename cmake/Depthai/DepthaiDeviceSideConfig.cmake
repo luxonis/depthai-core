a4bc106cedbb9b95bee6a3e207f90452d400d6c7
SzabolcsGergely <szabi@luxonis.com>
2021-07-26 10:22:32 +0300

    Update FW with memory optimizations

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d66b4da7255c5b436b48987d176b2552c3bd97fa")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "16efec0dcbe538570c7dd4e4f4fab40ed0549a6c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
