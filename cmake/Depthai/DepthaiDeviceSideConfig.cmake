28034e0b5b43cdee4c519af5a2c26b89a2b631a1
Martin Peterlin <martin.peterlin7@gmail.com>
2021-07-28 15:15:46 +0200

    Added an XLink 221 fix in FW and a default confidence threshold

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e88cdb4afa8be3cbbf8c739601f1aa1b2fd5395b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6487c7c2a1bc8f2d88431179e51db97771d842d7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
