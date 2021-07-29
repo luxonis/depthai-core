4cac8173dc49dd844d71c860e49562d74dc8978f
Martin Peterlin <martin.peterlin7@gmail.com>
2021-07-29 04:31:56 +0200

    Updated flash_bootloader to be a bit more verbose

--- examples/src/flash_bootloader.cpp
+++ examples/src/flash_bootloader.cpp
@@ -6,32 +6,67 @@
 int main(int argc, char** argv) {
     using namespace std::chrono;
 
+    char confirmation;
+
     dai::DeviceBootloader::Type blType = dai::DeviceBootloader::Type::USB;
+    std::string blTypeStr;
     if(argc > 1) {
-        if(std::string(argv[1]) == "usb") {
+        blTypeStr = std::string(argv[1]);
+        if(blTypeStr == "usb") {
             blType = dai::DeviceBootloader::Type::USB;
-        } else if(std::string(argv[1]) == "eth") {
+        } else if(blTypeStr == "network") {
             blType = dai::DeviceBootloader::Type::NETWORK;
         } else {
-            std::cout << "Specify either 'usb' or 'eth' bootloader type\n";
+            std::cout << "Specify either 'usb' or 'network' bootloader type\n";
             return 0;
         }
     } else {
-        std::cout << "Usage: " << argv[0] << " <usb/eth>\n";
+        std::cout << "Usage: " << argv[0] << " <usb/network>\n";
         return 0;
     }
 
-    bool res = false;
-    dai::DeviceInfo info;
-    std::tie(res, info) = dai::DeviceBootloader::getFirstAvailableDevice();
+    std::cout << "Warning! Flashing bootloader can potentially soft brick your device and should be done with caution." << std::endl;
+    std::cout << "Do not unplug your device in while the bootloader is flashing." << std::endl;
+    std::cout << "Type 'y' and press enter to proceed, otherwise exits: ";
+    confirmation = std::cin.get();
+    if(confirmation != 'y') {
+        std::cout << "Prompt declined, exiting..." << std::endl;
+        return -1;
+    }
 
-    dai::DeviceBootloader bl(info);
+    bool found = false;
+    dai::DeviceInfo info;
+    std::tie(found, info) = dai::DeviceBootloader::getFirstAvailableDevice();
+    if(!found) {
+        std::cout << "No device found to flash. Exiting." << std::endl;
+        return -1;
+    }
+
+    // Open DeviceBootloader and allow flashing bootloader
+    dai::DeviceBootloader bl(info, true);
+    auto currentBlType = bl.getType();
+    std::cout << "Connected to already running bootloader: " << !bl.isEmbeddedVersion() << std::endl;
+
+    // Check if bootloader type is the same
+    if(currentBlType != blType) {
+        std::cout << "Are you sure you want to flash '" << blType << "' bootloader over current '" << currentBlType << "' bootloader?" << std::endl;
+        std::cout << "Type 'y' and press enter to proceed, otherwise exits: ";
+        std::cin.ignore();
+        confirmation = std::cin.get();
+        if(confirmation != 'y') {
+            std::cout << "Prompt declined, exiting..." << std::endl;
+            return -1;
+        }
+    }
+
+    // Create a progress callback lambda
     auto progress = [](float p) { std::cout << "Flashing Progress..." << p * 100 << "%" << std::endl; };
 
-    std::string message;
     auto t1 = steady_clock::now();
-    std::tie(res, message) = bl.flashBootloader(dai::DeviceBootloader::Memory::FLASH, blType, progress);
-    if(res) {
+    bool success = false;
+    std::string message;
+    std::tie(success, message) = bl.flashBootloader(dai::DeviceBootloader::Memory::FLASH, blType, progress);
+    if(success) {
         std::cout << "Flashing successful. Took " << duration_cast<milliseconds>(steady_clock::now() - t1).count() << "ms" << std::endl;
     } else {
         std::cout << "Flashing failed: " << message << std::endl;
