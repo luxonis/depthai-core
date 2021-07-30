25cff55677c54fe64a0b2b93ff6bc67fa8f9963b
alex-luxonis <alex@luxonis.com>
2021-07-30 21:42:14 +0300

    Update FW: UVC: fix USB2 crash, 720p with USB2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "76f6546d8a094915ba168f312f9bc43b05d97bd8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d874e7adddac44165f1fb2061f765931ed78d924")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
