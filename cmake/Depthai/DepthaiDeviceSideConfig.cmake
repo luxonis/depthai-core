9ad4d9559bc416d312498a10b9f22b2afc62d30c
alex-luxonis <alex@luxonis.com>
2021-08-10 06:19:39 +0300

    Update FW: ToF on RGB port, OV9282 mono on Left, OV9782 color on Right

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1f0e5f648dc7c7c101d293b026080bb6b4b92a44")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4688ec3ff168c6c4c81c8568fac258fadef53367")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
