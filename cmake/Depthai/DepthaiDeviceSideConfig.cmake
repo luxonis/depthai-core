2cefcaf905e1a7b380ded01dd30845e3ef7200f6
alex-luxonis <alex@luxonis.com>
2021-08-11 05:23:20 +0300

    Update FW: disable RGB-mono AE lock

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3317375ebd54c2d661ef0905af58c88aa44f3714")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "83b311e5754a06441ad2996cee184bf59866aff4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
