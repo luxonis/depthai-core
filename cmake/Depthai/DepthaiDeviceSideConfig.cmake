3c56cb5613362a368383dbc99789ced4e109c607
SzabolcsGergely <szabi@luxonis.com>
2021-08-11 16:55:46 +0300

    Add workaround for stereo subpixel/extended mode crash at the expense of system performance

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a858d35dbe1574fc02e590ae04524ade7b5bf0ba")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a2baa42ba41f7b79dcbee987b34f24da075de196")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
