cb2894d53e430083d60fd0a67d4d6838c78ae8bd
alex-luxonis <alex@luxonis.com>
2021-08-13 12:40:33 +0300

    Update FW: reduce ColorCamera still pool: 4 -> 1 frame

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "fe17fa5db71454e10b9745ec70a2ce09f17f45d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2d4eac697a1c0f12d07b82188378fb15d984a1b9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
