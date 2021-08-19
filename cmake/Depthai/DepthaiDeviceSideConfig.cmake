aaf66c57a4dee656f910f100d08949f18668f58e
SzabolcsGergely <szabi@luxonis.com>
2021-08-19 03:34:41 +0300

    Update FW with stereo LR-check, subpixel fixes; extended mode is not available

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eb3b3cfd94b306ae38f114c1c98b090cb3fe2671")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "06a2d8b03e7903024ed58e3a74161440438a6da3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
