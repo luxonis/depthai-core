5dd1ec3e8d06c2857bcf03c09650a758467a4599
TheMarpe <martin.peterlin7@gmail.com>
2021-08-23 20:37:54 +0200

    Updated FW for UsbSpeed handling

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b5d2239124f9b66d056222676e9e845824329928")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "02668d20837db9d90cd66a9f47b5a4a8f359f9e7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
