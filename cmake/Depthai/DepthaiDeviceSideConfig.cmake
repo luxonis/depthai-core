0abc44fe007549292e46b35f0c45953d7b1a3d53
alex-luxonis <alex@luxonis.com>
2021-08-23 18:21:53 +0300

    DeviceBase/Device: add {set/get}XLinkChunkSize RPC calls

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "82c6726ce9e296908763395bcd49a6631d33de08")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c582aef4d821c9a664b503699706ed44e7da718c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
