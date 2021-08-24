79b6f4544115f42f6e9ea9b9c30a1c182ccc580a
alex-luxonis <alex@luxonis.com>
2021-08-24 19:19:14 +0300

    Update BL/FW: LED init

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e5f802b6685ba7e3a890123587b5d13802cb509b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "42776bc2ddd35dea6d5e3b5c3fce286ad2d1b75f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
