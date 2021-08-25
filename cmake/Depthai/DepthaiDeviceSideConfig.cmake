5a361217d81bad0af2ac170078f23c64890164af
TheMarpe <martin.peterlin7@gmail.com>
2021-08-26 00:31:22 +0200

    Updated FW with GPIO and SPI improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2d05346034bd91fccc22974d70aa05651513624b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "231cdecf84a2f1977a0d00bcee2ce6fa2ae8ce85")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
