15c3d8d5be34cc8801f305cffaccebbd92bade90
alex-luxonis <alex@luxonis.com>
2021-09-03 14:36:59 +0300

    Update FW with ETH bootloader support for more flash chips

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "28d9f187770707584909711e5bac2d477f2b0fa4")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0b8278a0e40107ede5f57165c5b46b7a6baa63a5")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
