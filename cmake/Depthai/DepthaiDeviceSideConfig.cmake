0f0c15cd6b28b76b1a397c0e4423093465956972
alex-luxonis <alex@luxonis.com>
2021-09-04 04:54:04 +0300

    Update FW: support for up to 3x IMX378/IMX477 or 3x OV9782 color cameras. Note: configs for Left and Right sockets must be identical for now

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "000fc415c51bd7838432bcfc79d43dcc611d084b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f6926cb5bca9962ba690cea0f371b13baf2f18ec")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
