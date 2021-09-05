823e39e6deb8041f79d062a9f47a4f888c78a165
alex-luxonis <alex@luxonis.com>
2021-09-06 02:39:37 +0300

    Update FW: fix default camera orientation for OAK-1-PoE, was rotated

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a78b518e8a08f2a6b55b7b2e926685b6539d7a71")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c5303ecec4c30a752a7f362589239b9d1784a0d3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
