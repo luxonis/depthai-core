35e163a9951badae16541f012f37bc5e2fe2c02c
TheMarpe <martin.peterlin7@gmail.com>
2021-09-06 18:57:01 +0200

    Updated FW - fixed cache coherency issue

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6a2abd52e513272993a6f8140702407e5c6ea251")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6793bd877ae334ac468b884ad1c9db22667da5b8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
