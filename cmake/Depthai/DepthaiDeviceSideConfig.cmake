fe5fce7703bf0ef797b2090d3567de4b5fd170cd
TheMarpe <martin.peterlin7@gmail.com>
2021-09-09 01:07:02 +0300

    FW - Updated ColorCamera 1080P resolution config

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c2462c39ab83d97b673c19b37c3e957ed13da0fb")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b8692a26d1806d778713b8f01caf07554c8f50a8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
