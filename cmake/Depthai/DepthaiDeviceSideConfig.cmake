706bce77fd0ecc2136026a1dafc8a95ef33f5623
SzabolcsGergely <szabi@luxonis.com>
2021-09-09 19:19:59 +0300

    Update FW with stereo performance improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ff3310c9e34043ddaf1f7c18bcfc64a22b9305c9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c4b3d00943fc95edac6e51602e74ce506f72b191")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
