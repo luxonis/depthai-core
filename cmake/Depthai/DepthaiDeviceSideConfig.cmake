0e44c3893ba8dafbfa238de5aaa3b3531990782c
SzabolcsGergely <szabi@luxonis.com>
2021-09-10 00:19:24 +0300

    Update FW with stereo confidence runtime config fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c4b3d00943fc95edac6e51602e74ce506f72b191")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "998500e9b6cd25679bfacf54c085e4d6c494d944")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
