44bae80965baefbad6faf20ce0298ddcf3bca947
alex-luxonis <alex@luxonis.com>
2021-09-10 03:43:12 +0300

    Update FW: upgraded ETH bootloader to be flashed, more flash info

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0b8278a0e40107ede5f57165c5b46b7a6baa63a5")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "29f6828876bb918eeb8b82504653c5a4a7ca1a94")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
