8f7c5023fc851ff8a8fa1b1b9d449d8e3eb02a01
alex-luxonis <alex@luxonis.com>
2021-09-11 01:58:38 +0300

    Update FW (IMX214 multiple resolutions), add more camera sockets

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "411501ab173fb66b453695bfce555695f4359c8b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0f9a0ec2012a7cfe61c9eb945fbdd52a557d357d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
