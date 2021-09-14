6e8bcaa75ed8688a1fdacf5879b369ca2f9b6d82
alex-luxonis <alex@luxonis.com>
2021-09-14 19:38:49 +0300

    Update FW: CAM-D (4th socket) tested, AR0234 support. Note: if AR0234 sensors are used, cannot be combined with other types at the moment (due to tuning blobs in use)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f9a0ec2012a7cfe61c9eb945fbdd52a557d357d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a452469b565b39417056d50e20d97a978cda7f29")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
