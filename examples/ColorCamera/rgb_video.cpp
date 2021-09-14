369284d57dcb26d32a3f50558b279c1f85a536bf
alex-luxonis <60824841+alex-luxonis@users.noreply.github.com>
2021-09-14 03:51:02 +0300

    rgb_video: 4K -> 1080p, non-blocking, qsize 1, show FPS

--- examples/ColorCamera/rgb_video.cpp
+++ examples/ColorCamera/rgb_video.cpp
@@ -15,8 +15,12 @@ int main() {
 
     // Properties
     camRgb->setBoardSocket(dai::CameraBoardSocket::RGB);
-    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+//    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_4_K);
+    camRgb->setIspScale(1, 2);
     camRgb->setVideoSize(1920, 1080);
+    camRgb->initialControl.setAntiBandingMode(dai::CameraControl::AntiBandingMode::MAINS_60_HZ);
+    camRgb->setFps(38);
 
     xoutVideo->input.setBlocking(false);
     xoutVideo->input.setQueueSize(1);
@@ -27,10 +31,28 @@ int main() {
     // Connect to device and start pipeline
     dai::Device device(pipeline);
 
-    auto video = device.getOutputQueue("video");
+    int qsize = 1;
+    bool blocking = false;
+    auto video = device.getOutputQueue("video", qsize, blocking);
+
+    using namespace std::chrono;
+    auto tprev = steady_clock::now();
+    int count = 0;
 
     while(true) {
         auto videoIn = video->get<dai::ImgFrame>();
+
+        if (1) { // FPS calc
+            auto tnow = steady_clock::now();
+            count++;
+            auto tdiff = duration<double>(tnow - tprev).count();
+            if (tdiff >= 1) {
+                double fps = count / tdiff;
+                printf("FPS: %.3f\n", fps);
+                count = 0;
+                tprev = tnow;
+            }
+        }
 
         // Get BGR frame from NV12 encoded video frame to show with opencv
         // Visualizing the frame on slower hosts might have overhead
