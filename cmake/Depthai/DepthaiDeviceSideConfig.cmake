b8ccbad529957921da62de6bebf765edc9c21f03
SzabolcsGergely <szabi@luxonis.com>
2021-09-18 02:51:40 +0300

    Add option to pick spatial calculation algorithm : average,min,max of selected ROI

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b8692a26d1806d778713b8f01caf07554c8f50a8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3068e8bd9bba9e46d565c92fe2c2d2d663a20bd2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
