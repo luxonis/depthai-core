2a87322ff0e55ac0f63e92a72004b8795c3d4df2
SzabolcsGergely <szabi@luxonis.com>
2021-09-17 20:50:38 +0300

    Update FW with subpixel fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "71bfc84bfc06df57a826b0924b2ec0d388565b09")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f1e86083efeaf88f9dc6342e51b88cc3eb41a14b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
