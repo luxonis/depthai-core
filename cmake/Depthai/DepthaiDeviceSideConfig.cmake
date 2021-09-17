6c5070ffa25171e0882a837bde8d638da35b67b4
alex-luxonis <alex@luxonis.com>
2021-09-17 18:03:45 +0300

    Update FW: changes for IMX214/OV7251, other fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a452469b565b39417056d50e20d97a978cda7f29")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "70c0b8e0e8c47a63a0991cccd2088ac5ce5e825f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
