8c85d31c14e03462df267221884de541a0f8866b
SzabolcsGergely <szabi@luxonis.com>
2021-09-18 18:34:10 +0300

    Update FW to match depthai-shared

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f1e86083efeaf88f9dc6342e51b88cc3eb41a14b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "484b9952039f243d7fe57ee2d58d62a797ca784e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
