baaa938d22a72d6aa7b3d4f7ad7c3d4d8164e0db
alex-luxonis <alex@luxonis.com>
2021-09-21 02:02:50 +0300

    Update FW: fix ColorCamera `preview`/`video`/`still` when multiple cameras use these outputs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "70c0b8e0e8c47a63a0991cccd2088ac5ce5e825f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c6f9eda4f47b4aa26538b7e79743d1940db74bb5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
