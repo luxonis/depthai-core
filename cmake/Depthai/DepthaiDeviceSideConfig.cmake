0776684af309df781d4e76a2ed150bef3667c31c
SzabolcsGergely <szabi@luxonis.com>
2021-09-21 22:23:48 +0300

    Update FW with runtime disparity range fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "238b43b7fbd0af171e301e764699776538594b8d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "db62f875ea8aca53e22b4095daff67a26e1cfdd2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
