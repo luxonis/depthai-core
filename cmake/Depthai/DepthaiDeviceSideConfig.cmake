21067b812b11ee45776c49b90c6a33f775cf21e3
alex-luxonis <alex@luxonis.com>
2021-09-22 00:31:12 +0300

    Update FW: change SIPP mem alloc to allow 4x ColorCamera to operate at the same time

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c6f9eda4f47b4aa26538b7e79743d1940db74bb5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4959c559cf7979e1a1a5cbdd0f9a5cbd2299ecb2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
