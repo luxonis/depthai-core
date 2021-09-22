55a758d1e3e9096579d3abad4aa878b6a59f197c
alex-luxonis <alex@luxonis.com>
2021-09-23 00:09:39 +0300

    Update FW: StereoDepth: fix RGB alignment when running at calib resolution (OAK-D with 800_P or OAK-D-Lite)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4959c559cf7979e1a1a5cbdd0f9a5cbd2299ecb2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "32581ad0d1d86e743d011ef747d57a3f39bbc6d9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
