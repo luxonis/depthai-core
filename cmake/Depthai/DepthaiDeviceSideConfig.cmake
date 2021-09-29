91d91e52e083f4c9ef58394a78749109d7b6161f
alex-luxonis <alex@luxonis.com>
2021-09-29 04:05:22 +0300

    Update bootloader/FW: early turn off LEDs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "42776bc2ddd35dea6d5e3b5c3fce286ad2d1b75f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "10a1203c673dca6f0dfa5643c45dafc2506af3ee")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
