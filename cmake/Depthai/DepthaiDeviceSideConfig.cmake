f8140a92d2d41e58035027cab919ce88d6e7e54c
alex-luxonis <alex@luxonis.com>
2021-10-01 19:22:16 +0300

    Add RPC for LM3644 IR projector registers read/write on OAK-D-Pro

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "48fca4a443d841221c94c70d5c05e7e946168636")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b78c8707a68e19a06a83b5e8eb7b75f836fefaf5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
