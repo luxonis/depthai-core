9056d786e4553a108e57f30e4d8674206763fc5a
SzabolcsGergely <szabi@luxonis.com>
2021-10-03 18:24:21 +0300

    Hotfix for FW message cache coherency

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b81c795238f5ccf667829ae270c1c35876cdd206")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8e63c65cbb74103573a7bc5dbfcf765fd391ad0b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
