9b2de20527d82e2f77a09b8ecdff47ff39c5ff9b
alex-luxonis <alex@luxonis.com>
2021-10-06 23:43:44 +0300

    Update FW: IMX577 proper support (0577 sensor ID)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "25e22d27838d599121d1b6d9fc131456efc49a46")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "90da733dd5519e805ac5563d13cc4b0516ddb5c3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
