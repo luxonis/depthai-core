d4936ebf2034c257301928fb4d69d34cdd7057bc
SzabolcsGergely <szabi@luxonis.com>
2021-10-15 23:23:02 +0300

    Internal firmware sdk fixes: for ISP/Stereo/Sipp filter crashes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9820d2403580961a0e8457ee722f748e3bddec18")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bdcb5732313d56ae83cf2e1ca3148f647df7383d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
