0e323f3de87738806c483d087c0b904c6dfaebc9
alex-luxonis <alex@luxonis.com>
2021-11-03 20:59:08 +0200

    Update FW: configurable FPS and fix enumeration issues with AR0234

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "09389b417b6a358f866e9619a6e937641ba006ac")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b7a1378f5f3b5c93d281f39659a8bfb3982340ff")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
