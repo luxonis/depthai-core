12fdc525acc227f54c3d4eb588cc9a1642604131
alex-luxonis <alex@luxonis.com>
2021-11-05 19:03:13 +0200

    Update FW, fix support for USB pre-boot config (VID/PID, speed)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c487960a2b777c5d37649f31ce36a9e398d9760b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d3278e93a071525fb1bafa309327c943ab360304")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
