1b427f21bf26504426efa0437aabb2b5103bdd06
SzabolcsGergely <szabi@luxonis.com>
2021-11-05 19:40:43 +0200

    Add fine tuned stereo settings, configurable P1/P2 cost aggregation parameters

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "58371c8407f617c12958b4bd84b7905b725135da")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "74d46aedb0c09356b4a8ff7fced6f0c1cd4763a7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
