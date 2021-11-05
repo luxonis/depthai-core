abc5ddb21d495d3dbd50e29e14dcaf029c91a874
SzabolcsGergely <szabi@luxonis.com>
2021-11-05 23:13:07 +0200

    Increase LR-check threshold to 10; disparity confidence threshold to 245 by default

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "74d46aedb0c09356b4a8ff7fced6f0c1cd4763a7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "921d45693a28dc472548e12df037d088111e66a5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
