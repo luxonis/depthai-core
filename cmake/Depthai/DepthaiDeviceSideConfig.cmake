3837fee4b2ec2253ec8429b4362d31fb430e6607
SzabolcsGergely <szabi@luxonis.com>
2021-11-07 23:38:27 +0200

    Add support for non-square YOLO output

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cdef6e16a44f4655f9bce4d852e1c324b0e40f4b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "73f5dfe7a318e713481dd00d134c2c597112afff")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
