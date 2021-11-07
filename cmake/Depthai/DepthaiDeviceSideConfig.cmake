630b461b5c14085862c020020e352ce49c98e56a
SzabolcsGergely <szabi@luxonis.com>
2021-11-07 23:46:19 +0200

    Update FW with error reporting for DetectionNetwork

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "73f5dfe7a318e713481dd00d134c2c597112afff")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bfb509700962dc4d8d78de9be30a49efa98e25f8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
