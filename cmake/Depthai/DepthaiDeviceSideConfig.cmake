6754cbb282144302ee05dfd27091814846e7b9f1
SzabolcsGergely <szabi@luxonis.com>
2021-11-07 18:52:43 +0200

    Update FW with Script node (DynamicPool) related fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "921d45693a28dc472548e12df037d088111e66a5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cdef6e16a44f4655f9bce4d852e1c324b0e40f4b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
