4df2c68f6bd79a9a0a5e09096e272d1634cc1ec7
SzabolcsGergely <szabi@luxonis.com>
2021-11-11 08:38:45 +0200

    Fixes for object tracker; support for KCF and imageless short term tracking algorithms

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b2f48737e039cc1179e1a51e51ca3c3a4d4b5c13")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b95b1375ccdccdb233ed4463a74abe3ad62241a3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
