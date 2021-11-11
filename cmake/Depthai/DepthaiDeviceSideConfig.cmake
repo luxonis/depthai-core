e4766c4ccb339126d24b740e09629f10e63bdf2a
alex-luxonis <alex@luxonis.com>
2021-11-11 04:02:19 +0200

    Update FW: populate `CameraProperties::hasAutofocus`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b7a1378f5f3b5c93d281f39659a8bfb3982340ff")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8df967b036f6a45fcd65c06377019aca9e5227fd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
