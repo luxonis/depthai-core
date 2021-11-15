6620089062d9660da8f896e9e61b4d0ae34e71f7
alex-luxonis <alex@luxonis.com>
2021-11-15 16:46:43 +0200

    Color/MonoCamera: handle more resolutions for OAK-D-Lite cameras: IMX214 (13MP) and OV7251 (480P)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3bb469fa08b7b0abc4353cd27219521dfd763a9d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4354b366954d3c743e387b058e4bc369a6d0e758")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
