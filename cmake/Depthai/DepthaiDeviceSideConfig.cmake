e134f6e72a363e259fd46ca029d82a10793f4225
SzabolcsGergely <szabi@luxonis.com>
2021-11-17 17:40:08 +0200

    Fix styling

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "208babd8dc6e9532be6386091b54802a6e4b93d3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cda4f8b601a555bd6a6726f1b1388dc72dfa8c9e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
