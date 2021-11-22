b987b5590a67e23680abb4819c0c52803578ed03
SzabolcsGergely <szabi@luxonis.com>
2021-11-22 11:03:59 +0200

    Update firmware SDK to r17.5

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7699a5479cd35532e5c6443301491541088d5549")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9bf030ae0575bcd87b362cd2804c1d8d5e37d251")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -8,8 +8,8 @@ hunter_config(
 hunter_config(
     XLink
     VERSION "luxonis-2021.4-master"
-    URL "https://github.com/luxonis/XLink/archive/9e2c90d0f68f2c32434b4f5d450d837b884cef1d.tar.gz"
-    SHA1 "eb18f8a5726d205e7b6e7972155e43c1fd173712"
+    URL "https://github.com/luxonis/XLink/archive/a59434f819f45908b77efc6ddc13a50c67f43dbc.tar.gz"
+    SHA1 "2c4cec9672b7b6a0b7b841493fee672765ba0ec5"
 )
 
 hunter_config(
