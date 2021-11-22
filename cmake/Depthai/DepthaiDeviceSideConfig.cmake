0ba0825d259e0b3a7aaedcf85a1a9b11b1233f3b
SzabolcsGergely <szabi@luxonis.com>
2021-11-22 11:50:59 +0200

    Update FW with xlink thread priority changes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b5960e1d9054fea932bd74bb200cda710362fdec")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "28c625387fce2d731328cb072f959435ab29458b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
