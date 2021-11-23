c9eeb155a3a926c43e9acbe40126b32920ffc89d
SzabolcsGergely <szabi@luxonis.com>
2021-11-23 09:03:56 +0200

    Initial version of temporal + thresholding filter

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d492416efbfe1961620697df8a4470b122bd3cf3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "289e452a6ddfe03466a7ded65972f33f10527824")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
