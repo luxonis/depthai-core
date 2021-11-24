487b2ab98c83970e3ebf66399a26a7543f396ca5
SzabolcsGergely <szabi@luxonis.com>
2021-11-24 06:43:51 +0200

    Add specle filter

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "289e452a6ddfe03466a7ded65972f33f10527824")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "67f976f93639a60e4d4fac52efbd1a0813b3c6d3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
