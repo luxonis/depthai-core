ba2bdc62ce1284c1e90f7b0e07c7c12fcb5bfeef
SzabolcsGergely <szabi@luxonis.com>
2021-11-25 21:47:03 +0200

    FW fix for resource allocation issues when setRuntimeModeSwitch is used

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d492416efbfe1961620697df8a4470b122bd3cf3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "29036665ba3c8416048f602f0bfc82c17d26fc2e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
