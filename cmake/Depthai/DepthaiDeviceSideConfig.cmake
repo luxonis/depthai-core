290e8a6e81bcf95b0e263afc1d39e05ce4a2b962
SzabolcsGergely <szabi@luxonis.com>
2021-11-30 01:27:44 +0200

    Add spatial filter

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "67f976f93639a60e4d4fac52efbd1a0813b3c6d3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bcf25ebe7c46b08083432f427879726f3cc1f105")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
