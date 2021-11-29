f838fb25a1ed506d090754e72718b255f9503f99
alex-luxonis <alex@luxonis.com>
2021-11-29 23:34:51 +0200

    Update FW: fix VideoEncoder potential crash (after power-cycle), instability introduced in 2.13.0

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "efb86d01cfead07c7978bde3d8f82bc059f4ac51")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a6ed5f116c68dfd4514533867411b479a2fdfea1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
