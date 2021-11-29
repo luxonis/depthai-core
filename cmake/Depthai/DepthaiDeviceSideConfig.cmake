51d97a96fab8907586c94d1ffd7b8a1dd61797e1
alex-luxonis <alex@luxonis.com>
2021-11-29 17:21:25 +0200

    Update shared/FW: manual_white_balance merged, other fixes: - fixes a crash with more than 4x VideoEncoder instances, now up to around 8 should work - StereoDepth fix crash with RGB-depth align and missing RGB calib (calibrated with -drgb) - StereoDepth fix RGB alignment when running at calib resolution (OAK-D with 800_P or OAK-D-Lite) - an error is thrown if multiple cameras have the same socket assigned

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4f7c0c94b0d70b502c20e55c14eadff875485a06")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "efb86d01cfead07c7978bde3d8f82bc059f4ac51")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
