9d09f40e958424f91aa016ee6d8936a8182281c3
alex-luxonis <alex@luxonis.com>
2021-11-30 23:46:12 +0200

    Update FW: zero out uninitialized DDR memory

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a6ed5f116c68dfd4514533867411b479a2fdfea1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "48fca4a443d841221c94c70d5c05e7e946168636")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
