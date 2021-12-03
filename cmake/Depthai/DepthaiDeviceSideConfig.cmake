b5794f3915bcadb3f723414cc19030df8b0b68a7
SzabolcsGergely <szabi@luxonis.com>
2021-12-03 10:57:58 +0200

    Add configurable decimation filter modes: pixel skipping/non zero median/non zero mean

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "50194b9c574d770d519313a1f80b80e55c053c51")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8119caa45450c7db8bb49250a84a6e1d80a720c9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
