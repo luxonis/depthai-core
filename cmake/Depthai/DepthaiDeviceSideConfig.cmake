b1f967c7bbb666c5e91ee58a482e6f27bc0e95ec
alex-luxonis <alex@luxonis.com>
2021-12-03 05:31:52 +0200

    Update FW: add SDcard support, accessible from Script nodes usually at: /media/mmcsd-0-0    -- first partition /media/mmcsd-0-1    -- second partition ... The partition(s) should be pre-formatted as FAT32. Note the Script node must set: .setProcessor(dai::ProcessorType::LEON_CSS)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "48fca4a443d841221c94c70d5c05e7e946168636")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8811678636629201aeca81fee8eaf0d9255fa03e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
