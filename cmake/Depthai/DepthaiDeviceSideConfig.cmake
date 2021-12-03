2bdb1834508488a5d718defa4ef161ea3ccc1c9f
SzabolcsGergely <szabi@luxonis.com>
2021-12-03 12:11:13 +0200

    Apply thresholding filter on disparity map if depth is not enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8119caa45450c7db8bb49250a84a6e1d80a720c9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b6ecb61a590d42a1707a940cd12bfcb95a1ed5e2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
