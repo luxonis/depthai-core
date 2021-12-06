97e3c9b4d2146344e36df19975af39e45e175d2f
TheMarpe <martin.peterlin7@gmail.com>
2021-12-06 12:03:28 +0100

    Updated FW with YoloV5 support and stability improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "48fca4a443d841221c94c70d5c05e7e946168636")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7cff595514999a71475eee47141532c193a476f9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
