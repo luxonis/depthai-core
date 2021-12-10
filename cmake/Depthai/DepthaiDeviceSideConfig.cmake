c7033ae911e7eb1c75d75b95f5a4d120d2489c0a
alex-luxonis <alex@luxonis.com>
2021-12-10 18:22:08 +0200

    Update FW: fixes for multiple IMX378/477

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7647ffe6f3a71d1d9ea2fa09319801bac11f6b6d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "094e7070491a3cd17b98ac87efb87ace15c1f6b5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
