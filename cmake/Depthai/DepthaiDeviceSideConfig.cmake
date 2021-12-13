1cdf23a7fe7f96cfa01fab6fa2e665960acb2e53
SzabolcsGergely <szabi@luxonis.com>
2021-12-13 14:09:40 +0200

    FW: Edge case fix for RGB aligment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "78b9e769d74a08356339cbfcdf9a0979ff7ebb56")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5354297a8efbab91ef7fd2bacf74cee3fc34311b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
