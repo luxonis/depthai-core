06d240965e09fad1064fd72ba44d67de014c1675
SzabolcsGergely <szabi@luxonis.com>
2021-12-13 10:41:12 +0200

    FW update: don't apply threshold filtering on confidence map

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d6dcfcffe55822364c1a4d261e6a53048fdc800f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "78b9e769d74a08356339cbfcdf9a0979ff7ebb56")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
