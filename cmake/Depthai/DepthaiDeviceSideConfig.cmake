bf2e531587c8abd84587d7e5bc8372fabb59a942
TheMarpe <martin.peterlin7@gmail.com>
2021-12-14 17:33:01 +0100

    Exposed max serialized metadata size

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3bcb7a8078f2a6a7b6e95c811432822588978050")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "37e4206ac8fb977d00db63fd48ef6f888ffae6bc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
