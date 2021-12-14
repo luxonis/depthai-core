99649a5690880ce497e97ce286c6ec00d4158f17
SzabolcsGergely <szabi@luxonis.com>
2021-12-14 16:50:00 +0200

    Update FW with improved resource allocation for RGB aligment; improved error handling when out of resources

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c5ce9d3e5342f41be4311e752bde29e1cf305166")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3b360d38cca54826a799f76d609086376bf43d18")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
