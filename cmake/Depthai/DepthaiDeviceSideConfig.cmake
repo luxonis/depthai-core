97a9c63eb6a180b9534fa7bc0e4d3e2995fdfcb5
SzabolcsGergely <szabi@luxonis.com>
2021-12-17 10:14:17 +0200

    Update FW with ipv6 disabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d1649317d08c52e603e6c2999c676f646f1ca38b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c331d88b13bf939fcf125cabbd20a2425cd08fe6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
