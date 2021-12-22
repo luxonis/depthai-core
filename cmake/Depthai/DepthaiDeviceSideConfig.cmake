e44bb6cc61091c5868c391d15fb37cb36b3fb087
SzabolcsGergely <szabi@luxonis.com>
2021-12-23 01:40:48 +0200

    FeatureTracker: Add support for 4k/12MP inputs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "165ade159396e09389e2c66572b15edc1770d1f8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "28471d4d80ffe0dcd9c251544e5da2aecfc2ec48")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
