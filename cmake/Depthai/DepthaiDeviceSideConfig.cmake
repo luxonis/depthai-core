77ac1fedcc929ea5b6c7c4b2030cf41664002e5f
SzabolcsGergely <szabi@luxonis.com>
2021-12-23 21:39:36 +0200

    Update FW; wakeup driven high

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "33287b3eaf62cb0d12b98454459f2f09d22200a3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4f0c4537a7414cd9bb3361bc3779777e165bf6b7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
