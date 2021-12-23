bba5961c37936633187d74f39c23166df42cb617
SzabolcsGergely <szabi@luxonis.com>
2021-12-23 16:56:16 +0200

    Set pullup for IMU wakeup pin

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "28471d4d80ffe0dcd9c251544e5da2aecfc2ec48")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "33287b3eaf62cb0d12b98454459f2f09d22200a3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
