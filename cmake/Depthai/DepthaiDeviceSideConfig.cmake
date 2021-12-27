7bc4d3a2e7ab0b796ee5962b559002114a61d487
SzabolcsGergely <szabi@luxonis.com>
2021-12-28 00:57:09 +0200

    Fix serialization of spatial img data

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4f0c4537a7414cd9bb3361bc3779777e165bf6b7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7921527d974ec618fa87ad2ea6e66427eeb0be72")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
