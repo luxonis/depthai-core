8f26e2e85fbb255a2a37340ea3a743da943bb5ca
alex-luxonis <alex@luxonis.com>
2022-01-03 23:12:49 +0200

    Updates for multi-ToF (on cam ports A and D)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3422ec414413846ac63a82bb539dd38a3ac75c98")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9c72a9f3a9ad365f72f16ce542d5dcf91567b123")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
