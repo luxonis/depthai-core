1d11e1dca8112bdd27642030c2f212d4dcbd3fc3
alex-luxonis <alex@luxonis.com>
2022-01-05 06:22:39 +0200

    Update FW: fixes for OV9282 on CAM-D

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3b61dca56990c54eec39f9e8d99d937b12df9dd4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "79d5dbddc890f1d998da57ffa651221cb3e5c083")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
