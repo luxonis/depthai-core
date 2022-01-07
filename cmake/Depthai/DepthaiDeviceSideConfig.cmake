8524117ae37400882f2bf2f7c31933b6398e1c58
SzabolcsGergely <szabi@luxonis.com>
2022-01-06 23:11:20 -0800

    Add spatialLocationCalculator output message to spatial detection network

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2ae062fb9a446d23914256315b718f33a01f3de8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e23227b70384c20565c9cc1b32b923f4cfc7eadf")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
