529b34239d719441d48855d9e760cac69ab6b4f0
alex-luxonis <alex@luxonis.com>
2022-01-07 21:23:43 +0200

    Update FW: fixes for OV9282/9782 on CAM-D, fix stereo rectified frame type for color

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "094e7070491a3cd17b98ac87efb87ace15c1f6b5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e3345e17ac4cc49677480452154985c2682be334")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
