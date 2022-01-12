fef43fdb0cd7cfa2753b2931d3d5357dc685ba32
SzabolcsGergely <szabi@luxonis.com>
2022-01-11 22:38:22 -0800

    StereoDepth: check if input/output messages are connected

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2ae062fb9a446d23914256315b718f33a01f3de8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f503b7adfd44b696187b7825a12a134ded0ca3f3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
