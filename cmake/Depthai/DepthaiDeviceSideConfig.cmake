8e07627edfe76615f803eb75d59e1ddc3992d8fa
alex-luxonis <alex@luxonis.com>
2022-01-13 20:25:52 +0200

    Configurable FPS for IMX214: 0.735 .. 35 for 1080p, 1.4 .. 30 (28.5 actually, TODO) for 4K/12MP/13MP

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f503b7adfd44b696187b7825a12a134ded0ca3f3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4fae48c5d938fb0fb7463116f1eb3a247813dbc6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
