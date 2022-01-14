d2e4a8a9ba1ebc4b0de1e4330a1591745b0aa868
alex-luxonis <alex@luxonis.com>
2022-01-14 19:27:51 +0200

    Update FW: allow `StereoDepth` to work with `ColorCamera`s, without having to call `StereoDepth::setInputResolution`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e3345e17ac4cc49677480452154985c2682be334")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5b4675066b28847d2ffc59f9117263df3fa55fcb")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
