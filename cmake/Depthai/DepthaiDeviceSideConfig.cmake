8eaeea07ad32180dc74d7cf7a7dc8c2ceb4cfb8e
alex-luxonis <alex@luxonis.com>
2022-01-14 10:20:54 +0200

    Update FW: flash USB boot header when config specifies it

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ad5f95bc1d3b70302ca1c4a586f62db257b3bdfc")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "44bbdc39abb71cd13e6ae13afbeefd8410cda444")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
