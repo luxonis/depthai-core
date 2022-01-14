2bf10c6f8bb1ae31e919222256f38f3bacba4756
alex-luxonis <alex@luxonis.com>
2022-01-14 16:13:16 +0200

    Update FW: IMX577 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "46336e387ab34ed0c7e517f9294ea4881024d643")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "acc7bf7e5d6647fa54400e3fd5830a1f15b0b5a9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
