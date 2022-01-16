c50c19108f646d5a4f3144a5b4ae3daa68a09a39
SzabolcsGergely <szabi@luxonis.com>
2022-01-16 14:33:25 -0800

    Add support for confidence map RGB alignment; fix bounding box remapping for RGB aligned depth frames

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2d72ac3bbbec5227f59873ea0b14b890a4fbf9bb")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "86351e75c31587864362505ec5b56714ee987fe4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
