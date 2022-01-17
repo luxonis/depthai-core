7171f75c96eeb6c02510351fa918dbcfdac63bbc
SzabolcsGergely <szabi@luxonis.com>
2022-01-17 11:15:33 -0800

    Re-enable feature tracker metadata

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2d72ac3bbbec5227f59873ea0b14b890a4fbf9bb")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "06f177db0eb89f788f55c055ac1f2e01167998f0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
