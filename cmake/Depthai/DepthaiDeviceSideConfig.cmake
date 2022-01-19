80d244b50f28b941313e4ef19011e588f3eb7bff
TheMarpe <martin.peterlin7@gmail.com>
2022-01-19 10:35:28 +0100

    Updated libnop library

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ed64d0ee71188906faca2e89d199e25a0a7dfd49")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "064b31cfdee20fbc93ca33733afc9cd1ff78a237")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
