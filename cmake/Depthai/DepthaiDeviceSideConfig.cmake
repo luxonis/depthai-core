4e31c0abbc524d32c8230ad5beb31f462b1e24f4
alex-luxonis <alex@luxonis.com>
2022-01-20 01:00:11 +0200

    Update flashed ETH bootloader to 0.0.16

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")  
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "44bbdc39abb71cd13e6ae13afbeefd8410cda444")            
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5bf84fd82e13d5bec12cafdc421100f4bfdeb50b")            
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
