b917a644a393c7c5a5df665383aef48b27e41366
SzabolcsGergely <szabi@luxonis.com>
2022-01-25 16:36:21 +0200

    Get focal length from calibration intrinsics

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9349b07517306b353cfbfeeb1be2c9746f9eda8e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a367a99082def6e82292ea1b18c3c5e7f730d302")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
