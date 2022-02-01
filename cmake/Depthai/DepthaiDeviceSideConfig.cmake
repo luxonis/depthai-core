52d82791d203ad27dc5096c3182685d808996a8c
OanaMariaVatavu <oana@luxonis.com>
2022-02-02 00:16:22 +0200

    Update FW: optimized RGB-depth alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1c3271f30b6bf60cc9f2000ebf2948e5fc0cf416")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ddb631475f65a34a32e3e717e9d8862fb97bb749")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
