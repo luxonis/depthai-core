bb69f2c81cf38685d1ea2a6ca48886a7d6f56a93
SzabolcsGergely <szabi@luxonis.com>
2022-02-02 07:10:00 +0200

    Update FW with stereo fixes for instance number; RGB depth alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1c3271f30b6bf60cc9f2000ebf2948e5fc0cf416")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3632be26e225b8078b2ce91f02910e32f478f7a2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
