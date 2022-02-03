de201c41d22c3156c452a7796ab11737162886d9
SzabolcsGergely <szabi@luxonis.com>
2022-02-03 03:35:20 +0200

    Update shared/FW w/ build fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3632be26e225b8078b2ce91f02910e32f478f7a2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4e63bce52514acfb7e808ffd6b9134b6868652be")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
