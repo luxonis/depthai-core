04d5149e7cfddc4dc871d55d1a3d9ee270b76835
SzabolcsGergely <szabi@luxonis.com>
2022-02-03 07:43:29 +0200

    Update FW with clock related tweaks for feature tracker

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4e63bce52514acfb7e808ffd6b9134b6868652be")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "22f00aeeec005de80c99084b23291a9f154bb84f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
