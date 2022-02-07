3f14de2f5a14a25a2606ad291103684c36dbc06a
alex-luxonis <alex@luxonis.com>
2022-02-07 23:38:49 +0200

    Allow using AR0234/OV5645 (but at degraded image quality) with other sensors enabled at the same time

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b037f059199f80493e3e081aab4220a5222e218a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e3baa61d440ca6964dc74768140971aac5dc7fca")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
