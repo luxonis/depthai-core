db35ee2db68d8fcb79b58d0f74835b0b89a2a8d8
TheMarpe <martin.peterlin7@gmail.com>
2022-02-11 14:12:37 +0100

    Updated shared with some modified data types

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "29a2561b7db37f53681e93b8d5e2129467e71667")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "eae5a213a44d0f59f719d800a575f9ac7a1ef150")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
