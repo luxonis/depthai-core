8deb3ae69bca790dd538894e697367f979162181
alex-luxonis <alex@luxonis.com>
2022-02-11 17:35:03 +0200

    Update FW with multiple fixes: - properly handle manual/auto focus with multiple cameras - fix potential init failure with same sensors (IMX378/477/214 / AR0234 / OV5645) on Left and Right sockets - fix sensor name reporting for AR0234 / IMX378/380/477/577

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e3baa61d440ca6964dc74768140971aac5dc7fca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e89359595f669e066ac690799f4fb20f6af16754")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
