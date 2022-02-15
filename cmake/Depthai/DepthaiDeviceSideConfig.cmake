f5ded1289662118cf222fbb77cdf60a95f068475
OanaMariaVatavu <oana@luxonis.com>
2022-02-16 01:29:32 +0200

    Update FW - adding the RGB scaling factor for the RGB-depth center alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e42bf9d072c4f97f8c75d9ef8779e9dff919ee72")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "823893894f2ee5d5bc4d1ebf6bb920a58fa1e231")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
