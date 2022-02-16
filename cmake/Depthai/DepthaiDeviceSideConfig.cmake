2bc97ffdef715c472740bf6c5226fde8242b1cd9
OanaMariaVatavu <oana@luxonis.com>
2022-02-16 15:13:48 +0200

    Update mdk - RGB-depth alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "823893894f2ee5d5bc4d1ebf6bb920a58fa1e231")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f81a69c4d259004213234511738af9df1142c2c7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
