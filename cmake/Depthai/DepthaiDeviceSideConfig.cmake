e03be4c7b43307b148fd1e4da4306e7166ded2a4
alex-luxonis <alex@luxonis.com>
2022-02-22 15:36:12 +0200

    Update FW: ColorCamera memory optimization when `still` is not connected, properly handle width-multiple-of-32 (only needed for VideoEncoder), don't enforce it on `video` by default, allow `still` size smaller than `video`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f2876f23bebca25cc5db0889c06376572f5227d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bfc09cbb3a84193709558b60fbd4adc3d744f361")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
