6cfa2eebda762efa67a0d8e1393159a0d0e2fdff
alex-luxonis <alex@luxonis.com>
2022-02-23 12:01:01 +0200

    Update FW: RGB orientation fix for OAK-1 (new versions) and OAK-1-Lite

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4fae48c5d938fb0fb7463116f1eb3a247813dbc6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bfc908b0cbaa949254b8ae1c1b68f51d8ba7cb01")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
