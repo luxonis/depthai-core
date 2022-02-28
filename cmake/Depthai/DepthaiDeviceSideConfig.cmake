66413a7127f141899f1dafc746e89877ced7f2f2
alex-luxonis <alex@luxonis.com>
2022-03-01 01:54:18 +0200

    Update FW: internal clock for audio, could work without refclk1 osc

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a3efdc7e27a3d967fa38988e4c169ad03dac00e4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0da7e93a7095e41ca661ea3ea307a606e4b3a519")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
