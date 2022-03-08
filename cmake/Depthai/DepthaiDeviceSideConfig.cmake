bab13440310ce8d4d963c99b03394c4b92efcbc3
TheMarpe <martin.peterlin7@gmail.com>
2022-03-09 00:40:16 +0100

    Updated FW with thermal protection mechanism

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "456246cec6c17358f9eda61a41f45059c6ec7a2b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0db59ca9b81e4901f8bc49023fb8c0121be821a0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
