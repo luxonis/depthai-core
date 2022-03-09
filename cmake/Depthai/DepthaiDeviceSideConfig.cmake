3028a1f09a83715a7efc219f14dd00344fb6a59c
alex-luxonis <alex@luxonis.com>
2022-03-09 19:49:21 +0200

    Update FW: allow SDcard working without Ethernet. Note: doesn't work on PoE boards, running with Ethernet

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "67c9c554e0d44d1ddd2d243aef7a1fbc831eac72")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b1f969060f6c9b2df130d510dd33f2adea9a9fee")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
