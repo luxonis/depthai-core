77a05648cf0e88736b21368669e022cd5f59e367
OanaMariaVatavu <oana@luxonis.com>
2022-03-10 14:53:28 +0200

    Update FW/mdk - Optimizing the confidence map alignment(CleanUp logs)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cab716a0c2de42fb3df1dc7298e65995af1367e4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e21bfb96a88d244757aa113ebaf039fa28f3eec5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
