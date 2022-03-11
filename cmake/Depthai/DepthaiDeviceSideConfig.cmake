486ca22c33f37119a2d1ead115874dc8a191dce3
alex-luxonis <alex@luxonis.com>
2022-03-11 17:29:03 +0200

    Update FW: fix UART0 ('/dev/ttyS0') init failure

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b1f969060f6c9b2df130d510dd33f2adea9a9fee")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a37ba48636fe84cc32bd743ae6ab81be673bd2ed")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
