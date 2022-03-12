3da7a29579ba3ef1cbc285897698218630cc9010
alex-luxonis <alex@luxonis.com>
2022-03-12 04:25:30 +0200

    Update FW: patch EEPROM data for incorrectly programmed RGB spec translation, for manually ran calibration on CM3/CM4 boards. Was leading to wrong RGB-depth alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0db59ca9b81e4901f8bc49023fb8c0121be821a0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5813806b6f2de7aaa4c211e5d8aa8e98774da8c8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
