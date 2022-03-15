ff88d6493b5f7b298f8155b56969c2f69598e882
SzabolcsGergely <szabi@luxonis.com>
2022-03-15 02:15:39 +0200

    Initial BMI270 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0db59ca9b81e4901f8bc49023fb8c0121be821a0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3dd9d30abaf960071b0b2e387386dd5f320af800")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
