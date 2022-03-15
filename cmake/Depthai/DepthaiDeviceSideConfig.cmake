0d4aa70a9c326a5f8cce7160a6a0dca826ec42de
alex-luxonis <alex@luxonis.com>
2022-03-15 23:52:03 +0200

    Update FW: fix StereoDepth crash with missing EEPROM, report error if missing, do not rotate RGB (AUTO orientation) on OAK-D(-Lite) if EEPROM is missing

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5813806b6f2de7aaa4c211e5d8aa8e98774da8c8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2bebad51c15c2e8961899a29b93b93f2409d8464")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
