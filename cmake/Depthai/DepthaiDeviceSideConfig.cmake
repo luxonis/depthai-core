804a3d6a00cd4624ecb66bef214d1d4bb46ddf12
SzabolcsGergely <szabi@luxonis.com>
2022-03-16 15:38:32 +0200

    Warp engine improvements for RGB alignment/stereo rectification

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2bebad51c15c2e8961899a29b93b93f2409d8464")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9215cfc78468345cf226e92984ff3ed7ad3cfe2b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
