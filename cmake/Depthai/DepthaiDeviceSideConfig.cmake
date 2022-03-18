6e29e4625858d991a759e24c8ac85dbfcd02acd1
alex-luxonis <alex@luxonis.com>
2022-03-18 19:59:56 +0200

    Update FW: improve PoE throughput and latency (set net.inet.tcp.delayed_ack=0), add config for MTU (not advised to change for now) and other sysctl params

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3bbd4d5a9a8108b654e6e63e0beb21520c2beca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0c743791925013732fbbe1048875785407c1bf10")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
