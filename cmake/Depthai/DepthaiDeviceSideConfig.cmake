db81d8d0051b3b8e1bb23c7ad45c31edf1087a48
alex-luxonis <alex@luxonis.com>
2022-03-19 07:05:41 +0200

    Update FW: fix Device(pipeline) or startPipeline(p) getting stuck on macOS/Windows until an UVC client connected (if the UVC node was enabled)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3154af275df03e61f69deeb71aa3c2b1f923e93")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c8ece34f6e1afd0a0830996f378719c8ffd92086")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
