b3cd426005c0814f1f786eece206e39187df2ca2
SzabolcsGergely <szabi@luxonis.com>
2022-03-21 23:30:22 +0200

    Add customizable depth unit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3bbd4d5a9a8108b654e6e63e0beb21520c2beca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "309cba28a6d73b83a0ac9365d41bd02a65e03090")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
