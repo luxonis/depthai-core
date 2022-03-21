dae14123d991be68c166d14ef1bcd3ff076b26c7
SzabolcsGergely <szabi@luxonis.com>
2022-03-21 23:58:12 +0200

    Change millimetre to depth unit where it's applicable in docs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "309cba28a6d73b83a0ac9365d41bd02a65e03090")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "86b4e1837b2ba14cd73dcc0968fcd25f02a62a71")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
