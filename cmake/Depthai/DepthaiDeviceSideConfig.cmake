d0bb322114c4ad4113232a727e00de43a0efbcbf
SzabolcsGergely <szabi@luxonis.com>
2022-03-22 23:01:28 +0200

    Openvino: improve error logging for out of memory cases

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "df5e50e79532ddaa879a6ccf2928fbbe27adeef7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1d690f5ace17b6ab80b835f364e4182005b32b3e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
