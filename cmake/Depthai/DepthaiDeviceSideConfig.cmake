a367ef0ec84504b14a5219262cec0bd8fd9f0c41
TheMarpe <martin.peterlin7@gmail.com>
2022-03-28 18:16:05 +0200

    Fixes #436 - removes temporary warning log in StereoDepth

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1d690f5ace17b6ab80b835f364e4182005b32b3e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "afe8d00c868344c73ee897e26278f80a444b9763")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
