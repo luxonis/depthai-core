950d1167972fe8f8dc6b44cf211bbd911e1fa9c6
TheMarpe <martin.peterlin7@gmail.com>
2022-04-06 18:52:02 +0200

    Updated bootloader and device FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "afe8d00c868344c73ee897e26278f80a444b9763")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "eaaa7b2234e07961225f1e259150bb3b5894c1d8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
