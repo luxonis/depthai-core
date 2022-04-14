b5e05bae81abc35f6365a92c7d95b541eaf5e25e
OanaMariaVatavu <oana@luxonis.com>
2022-04-14 22:53:19 +0300

    Update FW - [ImageManip] Adding support for input/output GRAY8 format

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "391ede37d1b55aa3dac727f58ff99aac7d2865dc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "53c09072ddcc23361a37026486e668196695a790")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
