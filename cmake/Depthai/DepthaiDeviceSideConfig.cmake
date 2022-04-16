7de8b69f2ac143bf86c7e8502a2290cd8b30c9e5
TheMarpe <martin.peterlin7@gmail.com>
2022-04-16 03:31:29 +0200

    Fixed device Clock.now in Script node to match messages timestamps

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "47e3396c6a6d65a3223ff56f8c9215f8b98e93f8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4c4318254a27b6776a065bd1a75773653fb10724")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
