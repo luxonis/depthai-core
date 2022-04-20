08be0a71b315c8fe4dbb58bf14bb0fe57e826e51
SzabolcsGergely <szabi@luxonis.com>
2022-04-20 17:44:53 +0300

    Disable mesh rectification by default; fix error reporting when RGB alignment is enabled and left-right check disabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bbfbbbc322744d745a90305f1f77a36c0af02bfa")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8b3cc8c5b516969e7cd9b6ef570bfbff82118779")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
