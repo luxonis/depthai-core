a2703b5a73c9106d58063dc256351e4bfcd769fc
alex-luxonis <alex@luxonis.com>
2022-04-21 22:27:31 +0300

    Update FW: configurable FPS for OV7251: max 99 for 480p, 117 for 400p

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "921b8a6a29fa445ea1c250d76dbe8694b5768584")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "af9024d1b2f023f80171f7a36fbeca4718a7b808")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
