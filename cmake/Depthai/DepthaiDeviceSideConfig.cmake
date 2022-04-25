1ee7cedee3899ecc670ce94944ced8928474da8c
SzabolcsGergely <szabi@luxonis.com>
2022-04-25 23:03:40 +0300

    Fix RGB alignment remapping when configured color camera resolution is different from calibration one

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "921b8a6a29fa445ea1c250d76dbe8694b5768584")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7f7e0e7aa2d705d8e618605800fd3c2447a866dd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
