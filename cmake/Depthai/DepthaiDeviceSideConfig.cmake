5fdd462feabc6e7e5582e8b026912745660b09cd
TheMarpe <martin.peterlin7@gmail.com>
2022-04-25 17:39:41 +0200

    Updated FW with device EEPROM handling fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2531b4af4fc4724a9bd5cfa0f8aed6441c96ac7a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7279adec35030e586dc8b81390f58178b4b34ae9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
