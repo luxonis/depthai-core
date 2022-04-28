70d4796c94c6741af73c09731c3786cb68c1d15f
SzabolcsGergely <szabi@luxonis.com>
2022-04-28 19:38:24 +0300

    Update FW: fix overriding useHomographyRectification behaviour specified in docs when custom mesh is provided

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7f7e0e7aa2d705d8e618605800fd3c2447a866dd")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d1086bf51f6aa5a75c9afcda34c74c62e4e9a53b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
