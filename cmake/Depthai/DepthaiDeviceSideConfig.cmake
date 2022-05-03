b7b14782a813cf742a488286d5500dc76a9727b7
SzabolcsGergely <szabi@luxonis.com>
2022-05-03 21:26:08 +0300

    Stereo: Add support for extended and subpixel together

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ade89030cc99e5fb7045bd4eda5705b134631af2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6d55842132d2ef37b6a6aeaaf810a8e745cd457c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
