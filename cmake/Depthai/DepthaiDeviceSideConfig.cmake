f3c9870d0c44c233841ab6b7f4bcd7c300f3ed25
TheMarpe <martin.peterlin7@gmail.com>
2022-05-03 18:15:22 +0200

    Lowered minimum required nlohmann json version to 3.6.0

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ade89030cc99e5fb7045bd4eda5705b134631af2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5abca1ab9c58948b07c04738880c779abf4fdac8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
