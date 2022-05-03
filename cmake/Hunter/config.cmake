9c864d0f1903782fa792f5d7fdeffbcc68bc6052
Martin Peterlin <martin.peterlin7@gmail.com>
2022-05-03 13:19:03 +0200

    Updated libusb with some integration fixes

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -8,8 +8,8 @@ hunter_config(
 hunter_config(
     XLink
     VERSION "luxonis-2021.4.2-develop"
-    URL "https://github.com/luxonis/XLink/archive/5701f7a686e33646edef54971cd9caf73bc4516d.tar.gz"
-    SHA1 "20526f51eb2929e6063afb8b5e6d9564dc577326"
+    URL "https://github.com/luxonis/XLink/archive/56bbbc9bf6c5bff0e618c02ade81fb77aa352002.tar.gz"
+    SHA1 "69ae5b6d2522ae8b94ac66963255efe0f3a00e65"
 )
 
 hunter_config(
@@ -117,8 +117,8 @@ hunter_config(
 hunter_config(
     libusb-luxonis
     VERSION "1.0.24-cmake"
-    URL "https://github.com/luxonis/libusb/archive/4a5f684d4b1ddfb462468056b31290d674b340b1.tar.gz"
-    SHA1 "fd741a5d27ac954a4a7e4876fddc0d98bac83d01"
+    URL "https://github.com/luxonis/libusb/archive/b7e4548958325b18feb73977163ad44398099534.tar.gz"
+    SHA1 "2d79573d57628fe56d2868d2f6ce756d40906cf4"
     CMAKE_ARGS
         WITH_UDEV=OFF
         # Build shared libs by default to not cause licensing issues
