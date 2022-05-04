524d2f1bf3fc2180954b599a758ba1cb251b0fcb
alex-luxonis <alex@luxonis.com>
2022-05-04 17:10:54 +0300

    rgb_video: add audio mic streaming (XLink), saving to "audio.raw"

--- examples/ColorCamera/rgb_video.cpp
+++ examples/ColorCamera/rgb_video.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 // Includes common necessary includes for development using depthai library
 #include "depthai/depthai.hpp"
@@ -8,11 +9,14 @@
 int main(int argc, char** argv) {
     bool enableUVC = 0;
     bool enableToF = 0;
+    bool enableMic = 0;
     if(argc > 1) {
         if (std::string(argv[1]) == "uvc") {
             enableUVC = 1;
         } else if (std::string(argv[1]) == "tof") {
             enableToF = 1;
+        } else if (std::string(argv[1]) == "mic") {
+            enableMic = 1;
         } else {
             printf("Unrecognized argument: %s\n", argv[1]);
         }
@@ -59,6 +63,19 @@ int main(int argc, char** argv) {
         tof->out.link(xoutToF->input);
     }
 
+    std::ofstream fAudio;
+    int audioSampleSize = 3; // 2, 3, 4
+    if (enableMic) {
+        auto uac = pipeline.create<dai::node::UAC>();
+        auto xoutMic = pipeline.create<dai::node::XLinkOut>();
+        uac->initialConfig.setMicGainDecibels(30);
+        uac->setXlinkSampleSizeBytes(audioSampleSize);
+        xoutMic->setStreamName("mic");
+        uac->out.link(xoutMic->input);
+
+        fAudio.open("audio.raw", std::ios::trunc | std::ios::binary);
+    }
+
     // Connect to device and start pipeline
     auto config = dai::Device::Config();
     config.board.uvcEnable = enableUVC;
@@ -78,6 +95,7 @@ int main(int argc, char** argv) {
     auto video = device.getOutputQueue("video", qsize, blocking);
 
     auto depth = enableToF ? device.getOutputQueue("tof", qsize, blocking) : nullptr;
+    auto audio = enableMic ? device.getOutputQueue("mic", qsize, blocking) : nullptr;
 
     using namespace std::chrono;
     auto tprev = steady_clock::now();
@@ -110,6 +128,31 @@ int main(int argc, char** argv) {
                 cv::normalize(depthFrm, norm, 255, 0, cv::NORM_MINMAX, CV_8UC1);
                 cv::applyColorMap(norm, norm, cv::COLORMAP_JET);
                 cv::imshow("tof-depth", norm);
+            }
+        }
+
+        if (enableMic) {
+            auto audioIn = audio->tryGet<dai::ImgFrame>();
+            if (audioIn) {
+#if 0 // works without zero-copy
+                auto audioData = audioIn->getData();
+                printf("MIC seq %ld, data size %lu, samples %d, mics %d\n",
+                        audioIn->getSequenceNum(),
+                        audioData.size(),
+                        audioIn->getHeight(),
+                        audioIn->getWidth() );
+                fAudio.write((char*)&audioData[0], audioData.size());
+#else // with zero-copy
+                uint8_t *audioData = audioIn->packet->data;
+                // uint32_t size = audioIn->packet->length; // not ok, includes metadata
+                uint32_t size = audioSampleSize * audioIn->getHeight() * audioIn->getWidth();
+                printf("MIC seq %ld, data size %u, samples %d, mics %d\n",
+                        audioIn->getSequenceNum(),
+                        size,
+                        audioIn->getHeight(),
+                        audioIn->getWidth() );
+                fAudio.write((char*)audioData, size);
+#endif
             }
         }
 
