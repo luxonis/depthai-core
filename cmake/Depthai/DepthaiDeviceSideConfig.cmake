396bc6f6aa66dbcb002ebeb0ce072489f853ed06
SzabolcsGergely <szabi@luxonis.com>
2022-05-06 22:26:07 +0300

    Update FW; change behavior of stereo rectification based on stereo camera FOV

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b9914d084f5fa91233d53352f7f5abc56ef8412c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "dcf4526f11cf88c88a0d3a3fbaba9152e96bf2c9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
