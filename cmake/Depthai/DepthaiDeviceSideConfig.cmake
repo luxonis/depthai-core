869afdbcf4d4e9a0fd9d4e42a32e0b3e77609ddd
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2022-05-10 10:23:50 -0700

    Json fix (#478)
    * Fixed nlohmann json < v3.9.0 compat and toolchain generation
    
    * turn off clang format
    
    Co-authored-by: Martin Peterlin <martin.peterlin7@gmail.com>
    Co-authored-by: TheMarpe <martin@luxonis.com>

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "12ab42f2923f30cedbf129fbb765ae7ca973bdba")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e094fda1e352bfaabfc6e25f75ba648008b8437e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
