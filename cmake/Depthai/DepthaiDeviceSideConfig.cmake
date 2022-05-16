c74aa861bee9d11a7bd5b12b4280a476ba68d150
OanaMariaVatavu <oana@luxonis.com>
2022-05-16 07:47:34 +0300

    [ImageManip] - FW update:     [API maintained]         [setKeepAspectRatio]             - resize with keeping aspect ratio + crop the input field of view to match the output aspect ratio             - using the "setKeepAspectRatio" API in conjunction with the "setResizeThumbnail" API, the "setKeepAspectRatio" API will not have any effect because the "setResizeThumbnail" API has priority             - keepAspectRatio config is True by default
        [bugs fixed]
            [artifacts/memory coruption]
                Properly extract the chroma rect
                Properly compute the luma/chroma cached memory size
                Input interleaved -> planar conversion + rect rotation
            [hang]
                input rectangle outside frame

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "53c09072ddcc23361a37026486e668196695a790")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ababbe18bad449d5a3db0e238a6bad7d43a377fc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
