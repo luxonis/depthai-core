1942ec46f3217c87d165ea80da9e6948dbeee699
TheMarpe <martin.peterlin7@gmail.com>
2022-05-16 13:23:29 +0200

    Added DetectionParser node and exposed network output of DetectionNetworks

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e094fda1e352bfaabfc6e25f75ba648008b8437e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8e93a5a88be9000853b407eb5863b2c043d605ed")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
