8ecc668c945ff99795e3c7932ae12133f6f8153e
OanaMariaVatavu <oana@luxonis.com>
2022-05-17 11:12:50 +0300

    [ImageManip] - update FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2041e04ea0a021f4534d630baf14813696f56311")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e483189a46ece011aca33e693eddddbce0236f50")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
