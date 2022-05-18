a20d06d361ca19123e846f1641dc35297d187466
alex-luxonis <alex@luxonis.com>
2022-05-18 17:40:24 +0300

    Update FW, add initial support for IMX582/IMX586

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0647981afd0f2f366666641f51b51494ea0b62b1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b15c025a88fadb8901e99faf37ad14834b268c0b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
