e1a17c29c469cad69267a24f34a4186cc6adac74
TheMarpe <martin.peterlin7@gmail.com>
2022-05-19 17:29:30 +0200

    Fixed VideoEncoder crash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1023338b5a0bee222d45c024cdaeb05fa08248d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1b0bd47c6e66b28f9f66cd7e0e9ecac8ac900eb0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
