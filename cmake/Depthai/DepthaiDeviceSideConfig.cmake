1f14176ead0294921ccba9509da34b9a006f1c22
TheMarpe <martin.peterlin7@gmail.com>
2022-05-19 19:42:41 +0200

    Fixed FW lazyloading issue

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "132e29eaa80632b4fb5686d31f75e0cd5076cada")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4b438da366d67c4375973acfa6934e9ce6055a4d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
