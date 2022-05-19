18d9d752e7f4c248ab2cea56e44efc8a6d190be7
Martin Peterlin <martin.peterlin7@gmail.com>
2022-05-19 04:20:49 +0200

    Updated VideoEncoder to lift 16 frames limitation and capability to specify their size explicitly

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "de2b6d9eb29118e7920e6943b93aba6bd5ee9b07")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3d152a9b2f6caf564d12421e86bcafc6c09b6811")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
