5fe7b34b1070be1f0100210f5d53cb9f8aa65178
alex-luxonis <alex@luxonis.com>
2022-05-20 02:51:50 +0300

    Update FW: allow larger pools (internal allocators bumped 512 -> 1024)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4b438da366d67c4375973acfa6934e9ce6055a4d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5439e4d4cdf0f0853e3434160994e46b8cd592d2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
