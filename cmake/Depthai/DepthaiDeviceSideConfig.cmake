055a74b2ee372e76baaae32e3f535789002f759e
SzabolcsGergely <szabi@luxonis.com>
2022-05-21 01:34:01 +0300

    Add IMU firmware update example

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "85bd7ae41e6f09f62d167a142c04a238a2ae9a69")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8c8ee74873f3a0caa83009c7ff00353fa1c91e97")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- examples/IMU/imu_firmware_update.cpp
+++ examples/IMU/imu_firmware_update.cpp
@@ -20,8 +20,10 @@ int main() {
 
     xlinkOut->setStreamName("imu");
 
-    // enable ROTATION_VECTOR at 400 hz rate
-    imu->enableIMUSensor(dai::IMUSensor::ROTATION_VECTOR, 400);
+    // enable ACCELEROMETER_RAW at 500 hz rate
+    imu->enableIMUSensor(dai::IMUSensor::ACCELEROMETER_RAW, 500);
+    // enable GYROSCOPE_RAW at 400 hz rate
+    imu->enableIMUSensor(dai::IMUSensor::GYROSCOPE_RAW, 400);
     // it's recommended to set both setBatchReportThreshold and setMaxBatchReports to 20 when integrating in a pipeline with a lot of input/output connections
     // above this threshold packets will be sent in batch of X, if the host is not blocked and USB bandwidth is available
     imu->setBatchReportThreshold(1);
@@ -33,29 +35,38 @@ int main() {
     // Link plugins IMU -> XLINK
     imu->out.link(xlinkOut->input);
 
+    imu->enableFirmwareUpdate(true);
+
+    // Pipeline is defined, now we can connect to the device
     dai::Device d(pipeline);
 
+    bool firstTs = false;
+
     auto imuQueue = d.getOutputQueue("imu", 50, false);
-    auto baseTs = steady_clock::now();
+    auto baseTs = std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration>();
 
     while(true) {
         auto imuData = imuQueue->get<dai::IMUData>();
 
         auto imuPackets = imuData->packets;
         for(auto& imuPacket : imuPackets) {
-            auto& rVvalues = imuPacket.rotationVector;
+            auto& acceleroValues = imuPacket.acceleroMeter;
+            auto& gyroValues = imuPacket.gyroscope;
 
-            auto rvTs = rVvalues.timestamp.get() - baseTs;
-            printf("Rotation vector timestamp: %ld ms\n", static_cast<long>(duration_cast<milliseconds>(rvTs).count()));
+            auto acceleroTs1 = acceleroValues.timestamp.get();
+            auto gyroTs1 = gyroValues.timestamp.get();
+            if(!firstTs) {
+                baseTs = std::min(acceleroTs1, gyroTs1);
+                firstTs = true;
+            }
 
-            printf(
-                "Quaternion: i: %.3f j: %.3f k: %.3f real: %.3f\n"
-                "Accuracy (rad): %.3f \n",
-                rVvalues.i,
-                rVvalues.j,
-                rVvalues.k,
-                rVvalues.real,
-                rVvalues.rotationVectorAccuracy);
+            auto acceleroTs = acceleroTs1 - baseTs;
+            auto gyroTs = gyroTs1 - baseTs;
+
+            printf("Accelerometer timestamp: %ld ms\n", static_cast<long>(duration_cast<milliseconds>(acceleroTs).count()));
+            printf("Accelerometer [m/s^2]: x: %.3f y: %.3f z: %.3f \n", acceleroValues.x, acceleroValues.y, acceleroValues.z);
+            printf("Gyroscope timestamp: %ld ms\n", static_cast<long>(duration_cast<milliseconds>(gyroTs).count()));
+            printf("Gyroscope [rad/s]: x: %.3f y: %.3f z: %.3f \n", gyroValues.x, gyroValues.y, gyroValues.z);
         }
 
         int key = cv::waitKey(1);
