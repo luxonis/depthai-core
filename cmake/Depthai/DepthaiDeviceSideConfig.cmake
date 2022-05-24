d18f9797e0ecd81fe370b5b739b50ee7b7cc35da
alex-luxonis <alex@luxonis.com>
2022-05-24 03:11:05 +0200

    Update FW: IMX582 4K center crop, fix 12MP crash with video/preview

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e58fc1794ce7f1f225f350cf35d3587ede5303e7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3f834f665ef33a8d8b60831c7a57e208dc786659")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
