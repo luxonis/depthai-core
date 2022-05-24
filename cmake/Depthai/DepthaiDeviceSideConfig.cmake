2307e9ca464eb904d812f6cc3b1a68547862c96b
SzabolcsGergely <szabi@luxonis.com>
2022-05-24 18:40:31 +0300

    OpenVINO add more explicit error logging

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c1b1c5fe00931df3c9408eb64933d6b195deb7be")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "672d67f85d534f4864bd13e5bc5b81ceb7a63aed")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
