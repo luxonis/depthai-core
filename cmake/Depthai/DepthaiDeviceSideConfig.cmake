9a9d89c0f52a2ee964901a2502e0b4d3de308633
SzabolcsGergely <szabi@luxonis.com>
2022-05-26 13:42:48 +0200

    Update IMU driver for CM4-POE

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "672d67f85d534f4864bd13e5bc5b81ceb7a63aed")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8bed558dd19e25bc4b07f716c0c6a494050e0f81")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
