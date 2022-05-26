08d9d2ee2192bead651e43a557bb0bdb683efcb4
alex-luxonis <alex@luxonis.com>
2022-05-26 17:38:47 +0200

    TMP FW: enumerate only IMX378/582 on RGB socket, for faster bootup

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3f834f665ef33a8d8b60831c7a57e208dc786659")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d03e4773cbfb51e5d43a2cee9db4956497ff4a1c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
