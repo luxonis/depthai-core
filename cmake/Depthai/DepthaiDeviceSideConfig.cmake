88ba5ce2c758a67264c045c046a632859da8cfb2
alex-luxonis <alex@luxonis.com>
2022-05-27 17:07:46 +0200

    Fix UVC streaming over USB2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3f834f665ef33a8d8b60831c7a57e208dc786659")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c8bf84106911d9a24b780b1d98a79f1a4aadf0f1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
