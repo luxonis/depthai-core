08da5a9a7084b86d8f1a63e30e53d9dff4dd8005
TheMarpe <martin.peterlin7@gmail.com>
2022-06-03 11:04:10 +0200

    Fixed fast boot issue

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5439e4d4cdf0f0853e3434160994e46b8cd592d2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d8704f5b0d46360baed01895812a394a5612e70b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
