03e47ad07bc4de8efe78e2673d6cd904ee473d59
SzabolcsGergely <szabi@luxonis.com>
2022-06-03 16:37:02 +0300

    Update FW, add openvino 2022.1 test

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2872593946d8096f7085a7fc07b2bce419659f67")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "077fa75ba35a83d8c42156c40718d412a5825b77")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
