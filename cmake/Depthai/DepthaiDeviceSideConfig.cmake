2855b4a5c3771ac9c55048637317f08fe2f5927e
alex-luxonis <alex@luxonis.com>
2022-06-07 01:14:42 +0300

    Update FW: support for new devices with 3x 4-lane cam ports, TMP: enable RGB-depth alignment for 12MP full FoV (hardcoded)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "077fa75ba35a83d8c42156c40718d412a5825b77")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "47e069cc7f16d29b37e17b2923322bc9824ebed4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
