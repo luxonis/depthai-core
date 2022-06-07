c4eab23369f0a0797ddd379c4a9a8a7aa9b5d8e2
alex-luxonis <alex@luxonis.com>
2022-06-07 19:46:37 +0300

    Update FW: fix enabling MIPI clocks for 4-lane ports

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "47e069cc7f16d29b37e17b2923322bc9824ebed4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "35d45ad291a6f988becc9663b6e8b2d58d4de24c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
