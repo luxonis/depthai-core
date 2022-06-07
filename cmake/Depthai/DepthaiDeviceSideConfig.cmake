c033718742a289612097ce13ede6f155d6995a70
TheMarpe <martin.peterlin7@gmail.com>
2022-06-07 20:35:55 +0200

    Updated BL & FW for small density Micron NOR chips

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "63f3da42f123d5dd0e7069c29b6abbbbc920ca43")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b475422afdfcb7da6c49828654f2de8add6dd276")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
