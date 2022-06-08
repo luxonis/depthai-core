e13e4d7e7dca4ff4d02a1e101bd5148baf87b396
SzabolcsGergely <szabi@luxonis.com>
2022-06-08 04:36:58 +0300

    Update FW; do subpixel interpolation for extended mode too

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "13c36e636dd00e83b89a5cc1ee76049567657c4b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d616e5354de2d63602fdcbda434628ab071b56a3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
