f5831ccee376fe33957663cd71a4d144e6766e12
TheMarpe <martin.peterlin7@gmail.com>
2022-06-08 03:51:44 +0200

    FW - Fixed lazy loading of Script node & added GPIO8 to HIGH on first ColorCamera frame

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d8704f5b0d46360baed01895812a394a5612e70b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5da990fa78ba35303784f8a5fda5730f3155a2ad")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
