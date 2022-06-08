283347024d75049a736e04f297dd64eb326938fa
alex-luxonis <alex@luxonis.com>
2022-06-08 15:52:29 +0300

    Update FW, extra logs for tuning

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3154af275df03e61f69deeb71aa3c2b1f923e93")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e239abbe4aeb14463c8721ff5c8cf149a1d26b17")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
