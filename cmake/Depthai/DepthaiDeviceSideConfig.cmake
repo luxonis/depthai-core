9017a779d2e6ac55aff39cce3f7b22578e58a236
alex-luxonis <alex@luxonis.com>
2022-06-15 23:55:16 +0300

    Update FW: remove GPIO8 set high, VideoEncoder forward CameraSettings metadata

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a69e9867e42c5fbeb13025d6db679fbba0cf2422")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5efe7408bf5cb0c443e5fdf6abe0402ecbef7480")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
