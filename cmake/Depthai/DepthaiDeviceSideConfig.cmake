59700a1e3aef41030e9059db1a75f762244ad915
TheMarpe <martin.peterlin7@gmail.com>
2022-06-17 00:30:46 +0200

    Added capability to read raw EEPROM areas

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b475422afdfcb7da6c49828654f2de8add6dd276")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b6ecdc6f5a272abbf8404f0afd5f921620256d97")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
