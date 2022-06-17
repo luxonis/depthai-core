7a2d988c9d5ab76224c46918936d42f6e3dd498d
TheMarpe <martin.peterlin7@gmail.com>
2022-06-17 22:22:41 +0200

    Updated FW with Script node improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b6106cd20ab26e590aa74ce12b010d28e0b20c9a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "31353742540a3e3d61021c4ea70f542c528336cc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
