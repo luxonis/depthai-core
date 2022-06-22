0139f912fa4e246b86a8bbaefaf2acb9250c5fd3
TheMarpe <martin.peterlin7@gmail.com>
2022-06-22 17:45:35 +0200

    EEPROMv7 fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b6ecdc6f5a272abbf8404f0afd5f921620256d97")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a35d3a30f927ba29486d2aad2242bc2e756b249a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
