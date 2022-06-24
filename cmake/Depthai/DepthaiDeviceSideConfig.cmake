c676cfa4f0b898c523e1e4da980dc6963af3f1db
alex-luxonis <alex@luxonis.com>
2022-06-24 05:28:06 +0300

    FW: support for new PMIC on S2 R6

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0451ba4cae06bb3796e7a8fb28467944c7bf8691")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0c0f0d2fd9284a092b927e99672a55d5cb28653d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
