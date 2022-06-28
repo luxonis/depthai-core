fe80049044cc06c00c2517ca482b03dc39970dc1
alex-luxonis <alex@luxonis.com>
2022-06-28 06:29:52 +0300

    FW: support for flashing GD25LQ16

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e692e202c6c2f83467b5bac029e7bbb904d9a7af")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "aaf6d8b7bea9abe3fc4e1127be4218b021b7370b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
