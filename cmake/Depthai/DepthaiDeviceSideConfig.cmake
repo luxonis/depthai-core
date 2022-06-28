ffdfcc1d164e04f7dc41fd7cf92d2919b01145bb
SzabolcsGergely <szabi@luxonis.com>
2022-06-28 17:18:02 +0300

    Add support for Yolo v6 decoding

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "796eb3f58b24447286ee88ba2278c52709133591")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "52f7331e81fc872f2191fd473adc96032675f518")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
