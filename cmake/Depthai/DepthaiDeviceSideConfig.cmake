8be8888f172c5f7065d08fc5f74294a9ea240d48
alex-luxonis <alex@luxonis.com>
2022-06-29 13:51:24 +0300

    FW: add EEPROM handler for generic OAK-D S2 R6

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "aaf6d8b7bea9abe3fc4e1127be4218b021b7370b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fb8ea9d43243bae7f6b106e4a64b09c3de3c7a69")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
