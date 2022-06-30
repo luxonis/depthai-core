5458342a3509a9bfa054b026ae041898b3d7df44
SzabolcsGergely <szabi@luxonis.com>
2022-07-01 01:54:30 +0300

    Stereo: Add pixel shift option for input frame

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "52f7331e81fc872f2191fd473adc96032675f518")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b9b9152331a82b3a962d6cdfa226b248fd5206f1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
