046765fe129aa7e6f33f8a0de9136f1cd4ec1d35
SzabolcsGergely <szabi@luxonis.com>
2022-07-01 18:14:12 +0300

    Stereo: improve pixel shifting performance

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b9b9152331a82b3a962d6cdfa226b248fd5206f1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5fb3c74512ab06b5c01a58a251d5e9c6318a01e8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
