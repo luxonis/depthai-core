6c9e0af9ee7845bee5773b17894400bd271a4ddb
TheMarpe <martin.peterlin7@gmail.com>
2022-07-06 02:37:10 +0200

    TMP - updated shared & fw with capability to specify imx214 as the only enumerated sensor

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a68e78f1f94ee212f22c5eb8994a28a14a694901")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2238b17a56c662882fc2242fe7ce57d0e84f4e38")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
