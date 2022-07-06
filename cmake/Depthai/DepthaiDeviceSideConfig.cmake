55daf871130c25bcb6aa73335ea119b813be9bd0
SzabolcsGergely <szabi@luxonis.com>
2022-07-06 20:33:41 +0300

    Update FW: Fix static frame input from host with RGB alignment and stereo

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6821a65c99033d806e42472985c6e66985f05ca9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "33cc407af320f00d4c0ccc3f49eda8289342105d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
