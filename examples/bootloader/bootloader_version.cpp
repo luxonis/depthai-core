1589735f5f8a808d355d88672f20a3963f39fb1c
TheMarpe <martin.peterlin7@gmail.com>
2022-07-11 15:30:23 +0200

    Updated bootloader_version example

--- examples/bootloader/bootloader_version.cpp
+++ examples/bootloader/bootloader_version.cpp
@@ -10,18 +10,30 @@ int main(int argc, char** argv) {
         dai::DeviceBootloader bl(info);
         std::cout << "Version: " << bl.getVersion().toString() << std::endl;
 
-        for(const auto& mem : {dai::DeviceBootloader::Memory::FLASH, dai::DeviceBootloader::Memory::EMMC}) {
-            std::cout << std::endl;
-            auto memoryInfo = bl.getMemoryInfo(mem);
-            if(memoryInfo.available) {
-                std::cout << "Memory '" << mem << "' size: " << memoryInfo.size << ", info: " << memoryInfo.info << std::endl;
+        auto supportedMemTypes = {dai::DeviceBootloader::Memory::FLASH, dai::DeviceBootloader::Memory::EMMC};
+        if(bl.getType() == dai::DeviceBootloader::Type::USB){
+            std::cout << "USB Bootloader - supports only Flash memory" << std::endl;
+            supportedMemTypes = {dai::DeviceBootloader::Memory::FLASH};
+        } else {
+            std::cout << "NETWORK Bootloader" << std::endl;
+        }
 
-                auto appInfo = bl.readApplicationInfo(mem);
-                std::cout << "Application, flashed: " << appInfo.hasApplication << " firmware version: " << appInfo.firmwareVersion
-                          << " application name: " << appInfo.applicationName << std::endl;
-            } else {
-                std::cout << "Memory '" << mem << "' not available..." << std::endl;
+        try {
+            for(const auto& mem : supportedMemTypes) {
+                std::cout << std::endl;
+                auto memoryInfo = bl.getMemoryInfo(mem);
+                if(memoryInfo.available) {
+                    std::cout << "Memory '" << mem << "' size: " << memoryInfo.size << ", info: " << memoryInfo.info << std::endl;
+
+                    auto appInfo = bl.readApplicationInfo(mem);
+                    std::cout << "Application, flashed: " << appInfo.hasApplication << " firmware version: " << appInfo.firmwareVersion
+                            << " application name: " << appInfo.applicationName << std::endl;
+                } else {
+                    std::cout << "Memory '" << mem << "' not available..." << std::endl;
+                }
             }
+        } catch(const std::exception& ex) {
+            std::cout << "Couldn't retrieve memory details: " << ex.what() << std::endl;
         }
     } else {
         std::cout << "No devices found" << std::endl;
