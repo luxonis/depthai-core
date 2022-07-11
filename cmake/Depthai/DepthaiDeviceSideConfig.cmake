faaaeff8f69106ddf1787d99943c08c920d8728e
alex-luxonis <alex@luxonis.com>
2022-07-11 17:20:57 +0300

    FW: support for the new S2/Pro devices, including OV9782 on RGB socket

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "adc0375d7ec059f253627e076edc4aab7f7a1a6b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "87b278f6d491234baae352f7cdf3dd0dc7791bba")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
