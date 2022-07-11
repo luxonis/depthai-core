5136a452dac64624cfd51723494657783a7d6ea1
SzabolcsGergely <szabi@luxonis.com>
2022-07-11 12:48:52 +0300

    Update FW: yolov7 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c9cb313f127e628d213a4d830a78250b8eb57a15")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "adc0375d7ec059f253627e076edc4aab7f7a1a6b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
