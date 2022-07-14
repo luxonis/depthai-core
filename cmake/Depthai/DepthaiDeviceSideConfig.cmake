14304b0220493542a2357663acb44c3af69acb44
TheMarpe <martin.peterlin7@gmail.com>
2022-07-14 20:50:42 +0200

    Updated FW with added Python bindings for timestamp and sequence numbers

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a65fe39adf0174e0a9671f7118cd3707bcaa64c3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a3c3514fb2da2a0af03744de0b8226475be9bafb")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
