1f0be51f467eeb12631bf19fea078cabc4ef56c3
SzabolcsGergely <szabi@luxonis.com>
2022-07-14 13:04:57 +0300

    Update FW: bugfix for https://github.com/luxonis/depthai-core/issues/521

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4829d3c015e229da5cad125ccc80758095160910")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a65fe39adf0174e0a9671f7118cd3707bcaa64c3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
