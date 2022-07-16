baf77403b6ed5c39ae072131867e9006d076d17c
TheMarpe <martin.peterlin7@gmail.com>
2022-07-16 05:06:02 +0200

    Updated FW with a fix for stuck PoE devices that required to be power cycled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a3c3514fb2da2a0af03744de0b8226475be9bafb")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7dd0007f2083d194228210b095c5cbf3440f0871")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
