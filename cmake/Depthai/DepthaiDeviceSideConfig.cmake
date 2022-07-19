ef0304bd40bb7e58834c24c30f437511d4cbd746
Martin Peterlin <martin.peterlin7@gmail.com>
2022-07-19 16:27:48 +0200

    Updated bootloader to 0.0.20 and FW with PoE device discovery fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "06e791bb9f6cefc968889a7ae7918ed0ff49e897")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b49f83c86e129b4de41981a306a26dbcd25509ea")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
