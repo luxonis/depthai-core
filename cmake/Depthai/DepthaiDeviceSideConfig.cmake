b2ca0dedebb2776cca66a330b3f90e734b4c361e
Martin Peterlin <martin.peterlin7@gmail.com>
2022-07-19 18:35:43 +0200

    FW - fixed bindings for some functions

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b49f83c86e129b4de41981a306a26dbcd25509ea")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "352b763bb4c862ceca49ed9a864aa7dd78711a1e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
