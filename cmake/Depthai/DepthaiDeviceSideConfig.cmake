fa9d57a63c47919549507dda1d7fd11b593d8f9f
alex-luxonis <alex@luxonis.com>
2022-07-20 15:19:48 +0300

    FW: BNO IMU FW update to 3.9.9, generic board handler for OAK-D S2 PoE (custom/Pro)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "fb8ea9d43243bae7f6b106e4a64b09c3de3c7a69")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "971b3a55501d1d05362f0d8ecfc5e5a2768cb1ea")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
