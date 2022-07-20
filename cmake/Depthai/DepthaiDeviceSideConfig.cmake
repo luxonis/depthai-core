7bf60b1c561522048e65a2c5b0ffbe56c9332ce3
alex-luxonis <alex@luxonis.com>
2022-07-20 04:31:17 +0300

    Fix IMX582 4K center-crop artifacts when flipping/rotating

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "839e9ca044499c8d664b8259b21d87b49129132c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "63102f8690662e5c7646f07a61d0fc3165ab118f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
