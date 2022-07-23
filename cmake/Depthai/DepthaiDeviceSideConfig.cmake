2407efed14c4b6a5badb470968ab36b8d11e8df9
alex-luxonis <alex@luxonis.com>
2022-07-23 08:19:12 +0300

    FW: handle 4-lane MIPI and explicit handler for NG9097 R4M2E3

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "971b3a55501d1d05362f0d8ecfc5e5a2768cb1ea")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d95fced7fc069635f4707771771d251ede4013a4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
