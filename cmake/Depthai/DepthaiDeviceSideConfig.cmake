4b66f8d49c990396e2293d561df166d065b1f2d4
SzabolcsGergely <szabi@luxonis.com>
2022-07-26 04:36:33 +0300

    Update FW: option to skip preview in ColorCamera

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "839115e7f597165faf26eb77e05adeda857c515e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f17bb425eb74855b2e10ab98466cebfaf979882b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
