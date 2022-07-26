14702569a943c3e9f03f1fdc896f3eb7d4975e58
TheMarpe <martin.peterlin7@gmail.com>
2022-07-26 17:00:46 +0200

    Reverted FW allocation modifications

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "352b763bb4c862ceca49ed9a864aa7dd78711a1e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1cfa51d59471f57c43cfef2b69205c227c272958")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
