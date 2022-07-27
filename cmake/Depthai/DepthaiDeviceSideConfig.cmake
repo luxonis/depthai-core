fdea9577c176062cc3a806a70f156b367ee52a5f
SzabolcsGergely <szabi@luxonis.com>
2022-07-27 22:49:11 +0300

    Update FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f17bb425eb74855b2e10ab98466cebfaf979882b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8f3d55d4039b28c9d85693d131aaf1d7ad61c313")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
