74f24f61ad02528f1d84f5c6d9d9d789f3e0e1ed
SzabolcsGergely <szabi@luxonis.com>
2022-07-28 14:41:39 +0300

    Update FW: StereoDepth: Fix ImgFrame metadata width and height when decimation filter is enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7a7ea51a555cf4c5ff6fd453ea4701ee7e2d470d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f734632a981d4b918fbb6b52f077e729b1a74d45")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
