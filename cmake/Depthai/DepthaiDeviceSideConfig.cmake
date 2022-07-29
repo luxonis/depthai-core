f7e0586e1f0f97c64895e5bd71bd4834f3569993
alex-luxonis <alex@luxonis.com>
2022-07-30 00:56:39 +0300

    CameraControl: add `setControlMode`, `setCaptureIntent`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8f3d55d4039b28c9d85693d131aaf1d7ad61c313")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "270f1cb79a4d1e23606920f0ab4283dff2025130")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- include/depthai/pipeline/datatype/CameraControl.hpp
+++ include/depthai/pipeline/datatype/CameraControl.hpp
@@ -36,6 +36,8 @@ class CameraControl : public Buffer {
     using AutoWhiteBalanceMode = RawCameraControl::AutoWhiteBalanceMode;
     using SceneMode = RawCameraControl::SceneMode;
     using EffectMode = RawCameraControl::EffectMode;
+    using ControlMode = RawCameraControl::ControlMode;
+    using CaptureIntent = RawCameraControl::CaptureIntent;
     using FrameSyncMode = RawCameraControl::FrameSyncMode;
 
     /// Construct CameraControl message
@@ -218,6 +220,18 @@ class CameraControl : public Buffer {
      * @param mode Effect mode
      */
     CameraControl& setEffectMode(EffectMode mode);
+
+    /**
+     * Set a command to specify control mode
+     * @param mode Control mode
+     */
+    CameraControl& setControlMode(ControlMode mode);
+
+    /**
+     * Set a command to specify capture intent mode
+     * @param mode Capture intent mode
+     */
+    CameraControl& setCaptureIntent(CaptureIntent mode);
 
     // Functions to retrieve properties
     /**
