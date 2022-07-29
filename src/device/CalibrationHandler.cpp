483b66f9842177641ff248efffc584a87d88ab37
Sachin <sachin.guruswamy@gmail.com>
2022-07-29 08:20:40 -0700

    Fixing type issues

--- src/device/CalibrationHandler.cpp
+++ src/device/CalibrationHandler.cpp
@@ -221,7 +221,7 @@ std::vector<std::vector<float>> CalibrationHandler::getCameraIntrinsics(
 
         std::vector<std::vector<float>> scaleMat;
         if(keepAspectRatio) {
-            float originalRatio = eepromData.cameraData[cameraId].width / eepromData.cameraData[cameraId].height
+            double originalRatio = eepromData.cameraData[cameraId].width / eepromData.cameraData[cameraId].height;
             if(resizeWidth / resizeHeight <= 1.34 && originalRatio <= 1.778 && originalRatio > 1.5) {
                 float scaleW = resizeWidth / static_cast<float>(eepromData.cameraData[cameraId].width);
                 float scaleH = resizeHeight / static_cast<float>(eepromData.cameraData[cameraId].height);
@@ -230,9 +230,9 @@ std::vector<std::vector<float>> CalibrationHandler::getCameraIntrinsics(
                 scaleMat = {{scaleW, 0, 0}, {0, scaleW, 0}, {0, 0, 1}};
                 intrinsicMatrix = matMul(scaleMat, intrinsicMatrix);
 
-                if(scaleW * height < resizeHeight) {
+                if(scaleW * eepromData.cameraData[cameraId].height < resizeHeight) {
                     intrinsicMatrix[1][2] += static_cast<float>(resizeHeight - eepromData.cameraData[cameraId].height * scaleW) / 2.0f;
-                } else if(scaleW * width > destWidth) {
+                } else if(scaleW * eepromData.cameraData[cameraId].width > resizeWidth) {
                     intrinsicMatrix[0][2] += static_cast<float>(resizeWidth - eepromData.cameraData[cameraId].width * scaleW) / 2.0f;
                 }
             } else {
