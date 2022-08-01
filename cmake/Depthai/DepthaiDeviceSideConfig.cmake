d41aecc589a748cf2079c7863e85520c77138a9e
TheMarpe <martin.peterlin7@gmail.com>
2022-08-01 23:26:21 +0200

    Exposed access to Node structures

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f734632a981d4b918fbb6b52f077e729b1a74d45")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "85e7fbf0bcad3fc8b72402596eb55644106f16c9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
