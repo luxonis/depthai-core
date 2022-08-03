fd5de3cc621981174e6de4bdab2ba9fb858c9078
alex-luxonis <alex@luxonis.com>
2022-08-03 09:26:14 +0300

    Fix for USB/recovery boot on DM2088 OAK-D-PoE

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d95fced7fc069635f4707771771d251ede4013a4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "71ce457fab22345f1e065cc4558223009b82fb27")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
