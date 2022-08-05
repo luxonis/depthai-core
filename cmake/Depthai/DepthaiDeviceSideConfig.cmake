361bca4fe8002d222f16d66ae6112eae1b4f6e95
TheMarpe <martin.peterlin7@gmail.com>
2022-08-05 03:26:26 +0200

    Updated FW to handle new board revisions

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "85e7fbf0bcad3fc8b72402596eb55644106f16c9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "89ec5d6cd261b354a09915267fb751a294d14ec4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
