0c71697cc8ea7d65c39227b0c46655bdbea44076
alex-luxonis <alex@luxonis.com>
2022-08-09 22:40:38 +0300

    FW: fix UVC with USB2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "305f51d9ae45ca62cc4d67df7180eb4e2d83c901")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "45a0a8604e5d4b9623d286a40d99c4ba89085320")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
