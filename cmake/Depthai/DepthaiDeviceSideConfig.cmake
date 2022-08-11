42b329d4352fc2c82f31bd181ede1c0678a39f3d
alex-luxonis <alex@luxonis.com>
2022-08-11 03:48:00 +0300

    FW: fix auto-exposure with large frame sizes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "45a0a8604e5d4b9623d286a40d99c4ba89085320")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8a763b206e139fb52376c7f2a2f6adbc0e027a83")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
