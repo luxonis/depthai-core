245fb573da7702a0dc9eb4ab85e21c14d0a6fc86
alex-luxonis <alex@luxonis.com>
2022-08-24 18:28:06 +0300

    Fix a potential crash with `DEPTHAI_WATCHDOG=0`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f02e98b07c988a0bcf95da20046b87f3fa8df7a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "31be3c3f138f11050b6a3d6ab6ad7f686f86e18f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
