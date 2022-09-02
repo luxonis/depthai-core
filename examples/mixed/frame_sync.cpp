896397bae66643635e1d663186db0de509fc4238
Matevz Morato <matevz.morato@gmail.com>
2022-09-02 16:48:49 +0200

    Formating

--- examples/mixed/frame_sync.cpp
+++ examples/mixed/frame_sync.cpp
@@ -34,7 +34,6 @@ int main() {
     left->out.link(stereo->left);
     right->out.link(stereo->right);
 
-
     // Script node will sync high-res frames
     auto script = pipeline.create<dai::node::Script>();
 
@@ -43,55 +42,55 @@ int main() {
     camRgb->preview.link(script->inputs["rgb_in"]);
 
     script->setScript(R"(
-                FPS=15
-                import time
-                from datetime import timedelta
-                import math
+        FPS=15
+        import time
+        from datetime import timedelta
+        import math
 
-                MS_THRESHOL=math.ceil(500 / FPS)  # Timestamp threshold (in miliseconds) under which frames will be considered synced
+        MS_THRESHOL=math.ceil(500 / FPS)  # Timestamp threshold (in miliseconds) under which frames will be considered synced
 
-                def check_sync(queues, timestamp):
-                    matching_frames = []
-                    for name, list in queues.items(): # Go through each available stream
-                        # node.warn(f"List {name}, len {str(len(list))}")
-                        for i, msg in enumerate(list): # Go through each frame of this stream
-                            time_diff = abs(msg.getTimestamp() - timestamp)
-                            if time_diff <= timedelta(milliseconds=MS_THRESHOL): # If time diff is below threshold, this frame is considered in-sync
-                                matching_frames.append(i) # Append the position of the synced frame, so we can later remove all older frames
-                                break
+        def check_sync(queues, timestamp):
+            matching_frames = []
+            for name, list in queues.items(): # Go through each available stream
+                # node.warn(f"List {name}, len {str(len(list))}")
+                for i, msg in enumerate(list): # Go through each frame of this stream
+                    time_diff = abs(msg.getTimestamp() - timestamp)
+                    if time_diff <= timedelta(milliseconds=MS_THRESHOL): # If time diff is below threshold, this frame is considered in-sync
+                        matching_frames.append(i) # Append the position of the synced frame, so we can later remove all older frames
+                        break
 
-                    if len(matching_frames) == len(queues):
-                        # We have all frames synced. Remove the excess ones
-                        i = 0
-                        for name, list in queues.items():
-                            queues[name] = queues[name][matching_frames[i]:] # Remove older (excess) frames
-                            i+=1
-                        return True
-                    else:
-                        return False # We don't have synced frames yet
+            if len(matching_frames) == len(queues):
+                # We have all frames synced. Remove the excess ones
+                i = 0
+                for name, list in queues.items():
+                    queues[name] = queues[name][matching_frames[i]:] # Remove older (excess) frames
+                    i+=1
+                return True
+            else:
+                return False # We don't have synced frames yet
 
-                names = ['disp', 'rgb']
-                frames = dict() # Dict where we store all received frames
-                for name in names:
-                    frames[name] = []
+        names = ['disp', 'rgb']
+        frames = dict() # Dict where we store all received frames
+        for name in names:
+            frames[name] = []
 
-                while True:
-                    for name in names:
-                        f = node.io[name+"_in"].tryGet()
-                        if f is not None:
-                            frames[name].append(f) # Save received frame
+        while True:
+            for name in names:
+                f = node.io[name+"_in"].tryGet()
+                if f is not None:
+                    frames[name].append(f) # Save received frame
 
-                            if check_sync(frames, f.getTimestamp()): # Check if we have any synced frames
-                                # Frames synced!
-                                node.warn(f"Synced frame!")
-                                for name, list in frames.items():
-                                    syncedF = list.pop(0) # We have removed older (excess) frames, so at positions 0 in dict we have synced frames
-                                    node.warn(f"{name}, ts: {str(syncedF.getTimestamp())}, seq {str(syncedF.getSequenceNum())}")
-                                    node.io[name+'_out'].send(syncedF) # Send synced frames to the host
+                    if check_sync(frames, f.getTimestamp()): # Check if we have any synced frames
+                        # Frames synced!
+                        node.warn(f"Synced frame!")
+                        for name, list in frames.items():
+                            syncedF = list.pop(0) # We have removed older (excess) frames, so at positions 0 in dict we have synced frames
+                            node.warn(f"{name}, ts: {str(syncedF.getTimestamp())}, seq {str(syncedF.getSequenceNum())}")
+                            node.io[name+'_out'].send(syncedF) # Send synced frames to the host
 
 
-                    time.sleep(0.001)  # Avoid lazy looping
-                )");
+            time.sleep(0.001)  # Avoid lazy looping
+        )");
 
     std::vector<std::string> scriptOut{"disp", "rgb"};
     for(auto& name : scriptOut) {
