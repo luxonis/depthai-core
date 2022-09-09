92f53321887bff8c823ec65a351197b2a05053b4
Teunis Peters <teunis.peters@gmail.com>
2022-09-09 14:13:06 -0600

    Update to use aligned build

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2470bb3fb3855de1808cf3cc94509ee771bae7d5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9e5cac5b6c0b4a018bdfc92ae1d88ca5e53cfc15")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
