d1b49b3d66fb7923ba8e23a14ed33b9b7325f61a
Matevz Morato <matevz.morato@gmail.com>
2022-09-16 18:50:35 +0200

    Update device side

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bde814c917d27dd0e3b11fffe7c21bb44b1650cf")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1a59c80266cd7a30ba874aa8d4a8277e0bf161ec")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
