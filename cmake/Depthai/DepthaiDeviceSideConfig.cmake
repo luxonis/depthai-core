b88ca03d9e1e0f20d8c082de9a53da22bc86ec07
TheMarpe <martin.peterlin7@gmail.com>
2022-09-22 17:00:54 +0200

    Added Warp node

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1a59c80266cd7a30ba874aa8d4a8277e0bf161ec")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1b1fbd6333603479636fa57e28c33e3d69d422eb")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
