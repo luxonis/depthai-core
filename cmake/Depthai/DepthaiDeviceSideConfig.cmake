2352cd45f06d059269d4b76bee3f7a45f0b13713
alex-luxonis <alex@luxonis.com>
2022-09-29 04:29:56 +0300

    Update FW with a fix for OAK-D-SR

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2e6929c0c75351019c43d317c43d2fb7e9b8d0ad")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c6f3745522c2a513cecf204c8fc09cc82841609e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
