a17f33e1b2bb255e9515a5100391a17325c03ef9
Matevz Morato <matevz.morato@gmail.com>
2022-10-03 12:50:29 +0200

    Fix typo and add calibration_factory_reset example

--- examples/calibration/calibration_factory_reset.cpp
+++ examples/calibration/calibration_factory_reset.cpp
@@ -9,7 +9,7 @@ int main() {
     dai::Device device(dai::OpenVINO::VERSION_2021_4, dai::UsbSpeed::HIGH);
 
     try {
-        device.factoryResetCalibration()
+        device.factoryResetCalibration();
         std::cout << "Factory reset calibration OK" << std::endl;
     } catch (const std::exception& ex) {
         std::cout << "Factory reset calibration FAIL: " << ex.what() << std::endl;
