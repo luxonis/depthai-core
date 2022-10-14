2a1ed06a270c8090f7f572341b5336235fd51111
SzabolcsGergely <szabi@luxonis.com>
2022-10-14 18:04:07 +0300

    Add support for yolov6r2 parsing

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d8e8462c9c218005c25f2423f88b5ca5d9589e04")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a85bb00e45bdf306ae6654058ba45b1fd17f08f8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
