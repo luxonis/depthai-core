2077d0b86bb3b943f6138f1a27f1e339572a35df
SzabolcsGergely <szabi@luxonis.com>
2022-10-14 18:33:56 +0300

    Expand spatial image detections with original config

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a85bb00e45bdf306ae6654058ba45b1fd17f08f8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b4ec492d7e4c20138e52868c200f6c2453bfa8fc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
