fdf008ae579da6e309f24cef6a6093b62f3381fd
alex-luxonis <alex@luxonis.com>
2022-10-18 00:10:02 +0300

    Several FW updates: - !!! For all sockets/cameras to be scanned, the EEPROM calib data boardName/productName should contain "FFC" or an FFC-related board name: 1090/2090/2088. Otherwise the behavior is to scan only for standard OAK devices sockets/cameras - Some more checks for the LCM48 (on MAX) supported resolutions - Fixes for RGB-mono SW-sync for standard OAK-D / OAK-D-Lite camera configs - AR0234 monochrome support, however needs an OV9282 enabled at the same time for now

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d00b3c9fdcc5664c56dcdada6c448797a4b7ff63")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6e74f146c72e1db77fd7d22cad16cc50592a75e8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
