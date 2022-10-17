93e08e515a0d29e8b090328579041beeaa3e5cef
alex-luxonis <alex@luxonis.com>
2022-10-17 21:21:53 +0300

    Add THE_1200_P for AR0234 MonoCamera

--- src/pipeline/node/MonoCamera.cpp
+++ src/pipeline/node/MonoCamera.cpp
@@ -107,6 +107,10 @@ std::tuple<int, int> MonoCamera::getResolutionSize() const {
         case MonoCameraProperties::SensorResolution::THE_480_P:
             return {640, 480};
             break;
+
+        case MonoCameraProperties::SensorResolution::THE_1200_P:
+            return {1920, 1200};
+            break;
     }
     return {1280, 720};
 }
