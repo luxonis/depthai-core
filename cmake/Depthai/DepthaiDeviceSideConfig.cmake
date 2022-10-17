d955edb929f961888adcc8171324ea87e9f08ae7
SzabolcsGergely <szabi@luxonis.com>
2022-10-17 20:35:41 +0300

    Add getTimestamp and getTimestampDevice getter to IMU packets

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b4ec492d7e4c20138e52868c200f6c2453bfa8fc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d00b3c9fdcc5664c56dcdada6c448797a4b7ff63")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
