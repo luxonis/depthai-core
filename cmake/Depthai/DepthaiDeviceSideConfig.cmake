15850668e9ef6df6c367bd59ee6b248a5ccc00c9
Matevz Morato <matevz.morato@gmail.com>
2022-10-17 23:13:13 +0200

    Update RVC2 firmware

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1bd31df536026261ee2b826bfe05efbc20004827")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "47ed2ec8807bf90f3133b8f25deda109e8ebfce4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
