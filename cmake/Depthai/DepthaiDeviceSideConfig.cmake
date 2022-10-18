dc9bc3dff2f419728443352e3087ba0d39763b2b
alex-luxonis <alex@luxonis.com>
2022-10-18 14:04:55 +0300

    Update shared/FW after merge, fix error log printed for MonoCamera AUTO socket

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6e74f146c72e1db77fd7d22cad16cc50592a75e8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "79a4ff3b53cc13eaa7f9bf73e74eb03b19c3fca3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
