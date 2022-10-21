4890c05a1d333598abaf0b89e02a92245e720894
SzabolcsGergely <szabi@luxonis.com>
2022-10-21 18:13:01 +0300

    stability test

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a555263a9dbff1166088bcb758ab4306dca9ae1a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1324b27d4e69a633d66812e71c1944b053e90433")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
