3ecbb1e8f28f1b2e1a1e227b735526afabd87c05
alex-luxonis <alex@luxonis.com>
2022-10-25 19:01:02 +0300

    FW: OV9282 720p/800p max FPS: 60 -> 120

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c62955ce3e726f06e81eb5b0b868c3cb0866a5bf")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3c6f948fc05b4fe3d442c578f4ea4d492ae314b5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
