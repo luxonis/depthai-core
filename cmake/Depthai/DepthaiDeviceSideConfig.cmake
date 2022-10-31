2f4e16caa70e5c41e5f6b91c52c494d9233f7b52
SzabolcsGergely <szabi@luxonis.com>
2022-10-31 15:30:19 +0200

    Fix yolo v5 decoding when there is a single class

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6d514c337c5f893f0a63bfd1781af6bcffb72bca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "313a7065c10a61c2651105b0e3cde7b4d159ebd8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
