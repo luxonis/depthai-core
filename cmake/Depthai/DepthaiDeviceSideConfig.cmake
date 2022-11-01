ad4a64f2a6f85488de05de98fdbf96777bad7e27
alex-luxonis <alex@luxonis.com>
2022-11-01 13:47:03 +0200

    FW: fixes for certain OV9782 and OV9282 permutations/configs, fix for OV9782/OV9282 alone failing on RGB/CAM_A socket

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "313a7065c10a61c2651105b0e3cde7b4d159ebd8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "29ed0ecddecc052d23200a56b0a717eacf5e18cc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
