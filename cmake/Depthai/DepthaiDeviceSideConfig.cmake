d38eb3bda395b7161cbfd094c8c5afccf1e149c5
TheMarpe <martin.peterlin7@gmail.com>
2022-11-08 12:23:12 +0100

    Modified pipeline schema dump to happen on trace log level as well

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3aeaf23ff5857fc8f79d412ceefc08da23e7aad")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a852e70f93f5923ef4b392f4b6abde4d91bd250f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
