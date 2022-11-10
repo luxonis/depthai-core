5a0600fd867dbb8fcd9210ce8b855f12c64750ff
TheMarpe <martin.peterlin7@gmail.com>
2022-11-10 22:38:27 +0100

    FW: bugfix for device bootup with default static IP

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8ab8a798b27c69154bbd64fcf81f7aac7ee940d1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1333912662dc304ee757c94bdd34f20c4072995b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
