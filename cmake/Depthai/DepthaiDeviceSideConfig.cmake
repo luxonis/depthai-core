4572b3e3721358cb9b66620fa62893872af2a598
TheMarpe <martin.peterlin7@gmail.com>
2022-11-10 18:11:49 +0100

    Added IR driver support for new OAK-FFC 4P

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "dd3f378864da91f09a9da6e364c5ec7f032dd18f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8ab8a798b27c69154bbd64fcf81f7aac7ee940d1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
