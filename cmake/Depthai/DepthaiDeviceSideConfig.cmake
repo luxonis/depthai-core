e941101bc1b3d2e29f49549dd8ecf3e316f041a3
alex-luxonis <alex@luxonis.com>
2022-12-01 13:51:39 +0200

    FW: support for IMX296 custom

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8fcc6446d9c2f7fe2069aaf19e0169ad0141803a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6b12e9bb6f9e5cb794279ec9a68a72ed45bc5729")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
