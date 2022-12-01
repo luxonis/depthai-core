48f0214fa6c50b10dd1982ddaf014803cf088645
TheMarpe <martin.peterlin7@gmail.com>
2022-12-01 17:35:13 +0100

    [FW] Fixed ImageManip + Subpixel issues and added FFC camera naming

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b511dc1a23aa9016007f7d6e9979a5a322bcefbe")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c3891fbe461a569a21fd0dc713d2dced535fbd8d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
