de8136f02865f15c01fbf10e3bfe2de7d2c4dfc2
alex-luxonis <alex@luxonis.com>
2022-12-13 05:37:40 +0200

    FW: IMX296 support, added THE_1440x1080 resolution

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6402439a9867ea3f672ce2cf6629759ea72356f7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7f0ccc2bf420aa47c6cbecd88bc0f048b14d3cc0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
