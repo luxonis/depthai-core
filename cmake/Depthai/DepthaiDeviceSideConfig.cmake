74467452363fa90fe9354135e2e6ced0bba7e5b8
Martin Peterlin <martin.peterlin7@gmail.com>
2022-12-17 02:47:09 +0100

    [FW] Fixed a bug in board downselection. OAK-D S2/Pro camera enumeration fix.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4ea30b84fc77078c88e1ddb4bb61a24c6dea6ed9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b6040a0abd70095a5737aa1ad6ab58964e417d8a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
