de4db9aa54fb5f17fc37eef2930efc8105661990
Martin Peterlin <martin.peterlin7@gmail.com>
2022-12-17 12:34:52 +0100

    [FW] Added support for Mono video/preview in Camera node

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b9624e2c0bf79f5f2a0bd63e124d159a6acec672")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fd084422d7b210d3ed44a0404a29e4b49f644cac")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
