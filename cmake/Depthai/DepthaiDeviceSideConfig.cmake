e30a8b9b8bf4e278a3ff0f0c7e0ac528641177f9
TheMarpe <martin.peterlin7@gmail.com>
2022-12-19 22:13:21 +0100

    [FW/XLink] Explicitly limited to single connection

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c9261358b24009c73ca56af6a62d3ff635379a36")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8202718a1ca77df044e7fbb86e60e4dd4fa213b8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
