9e199d526914d7b7cdad39c29684feb73b749d45
SzabolcsGergely <szabi@luxonis.com>
2022-12-22 20:24:19 +0200

    Add option to override baseline and/or focal length for disparity to depth conversion

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "11f4b3583a1b92e06ffcd41367d0ca7998c9c058")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cfa6ae1779c61282d2fd010ec07af6fe79142847")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
