b2da50897224ca1bcd5d6ad8ae0d04b798904fad
TheMarpe <martin.peterlin7@gmail.com>
2023-01-05 14:38:46 +0100

    FW - Modifed watchdog to do a graceful reset instead

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "44d9ea0145b0ac29ca2a8e5b9abc6c2b77e75a9f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "efa8bc81a8d5048fc369a740decc87ba494046ee")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
