efb3f4ddc8bc378974853e1dd341a8b3251553a4
Martin Peterlin <martin.peterlin7@gmail.com>
2023-01-12 03:21:12 +0100

    [FW] Fix for CAM_C not being detected

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3bc719a7f3887d9eaf4a196ec66827b19480ef9c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5bb379177fe3b03950a09922204767d868fa501a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
