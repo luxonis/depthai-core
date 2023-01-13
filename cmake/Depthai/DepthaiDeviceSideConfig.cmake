aa500ac482d0f4895fc6ea46b09852af2d2ca8b0
alex-luxonis <alex@luxonis.com>
2023-01-13 05:12:02 +0200

    FW: IMX296 updates for FPS, exposure

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7987f0720f799c596f3e7d67f52502ce8fa45caa")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f0d35d2c29ae4a8c1bd5329006e4e19438a40617")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
