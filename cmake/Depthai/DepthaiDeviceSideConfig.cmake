490be97d0deb976afcb7b9078ea84c61470e247c
alex-luxonis <alex@luxonis.com>
2023-01-15 14:26:55 +0200

    FW: IMX296 exposure/gain sync

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f0d35d2c29ae4a8c1bd5329006e4e19438a40617")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a919869dca18c6f1d2f211805c73c251651cbab5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
