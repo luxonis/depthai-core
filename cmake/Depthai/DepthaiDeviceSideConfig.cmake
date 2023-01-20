34c4b2fe6d497f70dd3d4551b5e20abd0a3ae466
alex-luxonis <alex@luxonis.com>
2023-01-20 19:04:31 +0200

    FW: IMX296 Camera node, IMX378 1080p limited to 60fps

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7f0ccc2bf420aa47c6cbecd88bc0f048b14d3cc0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7271eb450f49e21300089fe76ca0b5b6081f1902")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
