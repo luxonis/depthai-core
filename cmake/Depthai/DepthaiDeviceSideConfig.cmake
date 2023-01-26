4b2e9da8d97029403b9a4272a28d3d07f35d5a43
SzabolcsGergely <szabi@luxonis.com>
2023-01-27 00:13:17 +0200

    Implement multi stereo support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f0a97b2759374d783a846a7c2f5741c21b47fc98")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d72f34bac860aae1f14ae9dfd4ce06e49eda0f7b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
