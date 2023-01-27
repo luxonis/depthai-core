e73cf4cf5d06183cd291eac75f3693b0a330bfbf
alex-luxonis <alex@luxonis.com>
2023-01-27 23:30:58 +0200

    FW: fix OV9282 SW-sync on devices with OV9782 RGB camera

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "298d2545faae6763390a4320533e6b5d16c8b0f2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d32f768aa9ea5e96db17c612f4a682738cea7748")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
