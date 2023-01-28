d17527faafde36afd639aff9922a8473f896c5df
SzabolcsGergely <szabi@luxonis.com>
2023-01-28 16:18:44 +0200

    Update FW: Fix OOM due to too many SIPP pipelines

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "93ae098b13ba7de17d21b8f038dfa9eb62085ccc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b061395bb872b10748793191e547e240500a1e76")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
