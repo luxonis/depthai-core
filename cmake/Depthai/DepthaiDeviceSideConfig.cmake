80727e4b5d1f8f4c6ce9d2c25f429aa6aba92a94
SzabolcsGergely <szabi@luxonis.com>
2023-02-01 02:52:32 +0200

    Update FW: support for stereo alignment to original left or right inputs; OOM fix for 4 stereo nodes with median filter

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7d93c101945b03db76114a362af353379c7d9a74")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8a0afa76294ffa520238efb460e65babcb6cc23c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
