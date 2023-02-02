66420ce9ef2fa884267280630e76c6e3c9603540
SzabolcsGergely <szabi@luxonis.com>
2023-02-02 17:30:46 +0200

    Update FW w/ syncing stall fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0c60cec397ace4c61e63e8ee6518ec58f0c2259f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3e54d31182b0f91933e375354e08e0be97c66a02")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
