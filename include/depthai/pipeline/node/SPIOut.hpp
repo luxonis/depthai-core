fed2d616cf9793e9fc23df9f3534adec1078c099
anonymous-1000 <124404305+anonymous-1000@users.noreply.github.com>
2023-02-03 13:53:28 -0800

    Update SPIOut.hpp
    to address error C4458: declaration of 'id' hides class member

--- include/depthai/pipeline/node/SPIOut.hpp
+++ include/depthai/pipeline/node/SPIOut.hpp
@@ -43,8 +43,8 @@ class SPIOut : public NodeCRTP<Node, SPIOut, SPIOutProperties> {
      * Specifies SPI Bus number to use
      * @param id SPI Bus id
      */
-    void setBusId(int id) {
-        properties.busId = id;
+    void setBusId(int newId) {
+        properties.busId = newId;
     }
 };
 
