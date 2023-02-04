f9d9534e3884543ec101a9e5b1a4b403030e4379
SzabolcsGergely <szabi@luxonis.com>
2023-02-05 00:06:27 +0200

    Expose SIPP mempool configurable sizes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4d70a8d0549e8105cda4901ddb180343ea254c48")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d141bec706eb9b01d8ae1b28f338abee1aae938a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
