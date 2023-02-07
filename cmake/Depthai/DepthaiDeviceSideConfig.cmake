ec72ea8272734ad0b59ecaaaa4c425e2c622e496
SzabolcsGergely <szabi@luxonis.com>
2023-02-07 17:18:08 +0200

    Update FW with performance metrics when DEPTHAI_LEVEL=info is enabled; enable brightness filter for 0 intensity pixels by default

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7ae1cd76b204199f9116930d5de2c7e187825827")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "78ba554b106d7eb2baca2f7a2770ed1f312811aa")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
