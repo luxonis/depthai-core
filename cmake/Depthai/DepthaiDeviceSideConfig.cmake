8001c2a8ac0587b484194183b28b59137b84e88d
alex-luxonis <alex@luxonis.com>
2023-02-07 17:08:25 +0200

    FW: fix UART0 usage in Script node

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "78ba554b106d7eb2baca2f7a2770ed1f312811aa")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f0a15ea706119e4c52d0ce7025146f422fc618e9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
