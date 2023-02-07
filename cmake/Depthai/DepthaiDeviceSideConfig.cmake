9f1de695786b01c5313c617b0d50f55de7836a68
SzabolcsGergely <szabi@luxonis.com>
2023-02-07 14:27:42 +0200

    Update FW with RGB alignment fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "353473583404240dd2f15dabaffd42abcc833d68")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7ae1cd76b204199f9116930d5de2c7e187825827")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
