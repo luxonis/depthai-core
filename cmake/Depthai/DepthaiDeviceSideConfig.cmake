25bacff1a11f2d0fcde1412b0d30f888571dae46
SzabolcsGergely <szabi@luxonis.com>
2023-02-08 19:28:38 +0200

    Improve spatial calculation X and Y accuracy; fix RGB alignment when custom output depth size is specified

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "78ba554b106d7eb2baca2f7a2770ed1f312811aa")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "81701b1fe003b50e9bd17862a7f9c0b1ffdaf53a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
