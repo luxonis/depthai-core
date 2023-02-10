b7a4cb7c5d77873c46077032ba91f525712efe02
SzabolcsGergely <szabi@luxonis.com>
2023-02-10 20:33:58 +0200

    Update FW with commit hash attached to crash dump

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1ec2c463fef44d4bf30062e160b8e283db62a0ee")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d5195cfdbe8e0c024d6763c227a2e3dc3c38248d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
