fe5a566c7bdd912189c5fdbab14b57cdecba0fd3
SzabolcsGergely <szabi@luxonis.com>
2023-02-14 21:53:06 +0200

    Update FW; default median for SpatialDetectionNetwork

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ff550421ac5b2f333e58735f3c5e1d0085d9f0c9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6115bb387fda64ea7747a387288b06d48517ca85")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
