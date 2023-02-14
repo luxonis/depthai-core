de15c271571d60c2a53dc9a5d44ecdb3cf8d6e4d
SzabolcsGergely <szabi@luxonis.com>
2023-02-14 17:56:28 +0200

    Update FW, crash dump contains device ID

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "da7739bf402427b2d2d38316beedb0e1a1cf7304")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "143e162f9adb251d0875d91ed6835ce52663ef54")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- examples/CrashReport/crash_report.cpp
+++ examples/CrashReport/crash_report.cpp
@@ -1,7 +1,13 @@
+#include <fstream>
 #include <iostream>
 
 // Includes common necessary includes for development using depthai library
 #include "depthai/depthai.hpp"
+
+static bool fileExists(dai::Path path) {
+    std::ifstream file(path);
+    return file.is_open();
+}
 
 int main() {
     using namespace std;
@@ -10,13 +16,23 @@ int main() {
     dai::Device device;
     if(device.hasCrashDump()) {
         auto crashDump = device.getCrashDump();
+        std::string commitHash = crashDump.depthaiCommitHash;
+        std::string deviceId = crashDump.deviceId;
 
         auto json = crashDump.serializeToJson();
-        std::cout << json << std::endl;
 
-        dai::Path destPath = "crashDump.json";
-        std::ofstream ob(destPath);
-        ob << std::setw(4) << json << std::endl;
+        for(int i = 0;; i++) {
+            dai::Path destPath = "crashDump_" + to_string(i) + "_" + deviceId + "_" + commitHash + ".json";
+            if(fileExists(destPath)) continue;
+
+            std::ofstream ob(destPath);
+            ob << std::setw(4) << json << std::endl;
+
+            std::cout << "Crash dump found on your device!" << std::endl;
+            std::cout << "Saved to " << std::string(destPath) << std::endl;
+            std::cout << "Please report to developers!" << std::endl;
+            break;
+        }
     } else {
         std::cout << "There was no crash dump found on your device!" << std::endl;
     }
