47d6fffa42dfa3a4d32b4389addaedbf92f70c23
alex-luxonis <alex@luxonis.com>
2023-02-19 23:04:01 +0200

    FW: HW sync (trigger mode) enabled for OAK-D-LR, for cameras with matching FPS

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7e7f544cc4ebcf96d9818792f838f3b365c0c36a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "53c4570f02cb46186fdbad60aeff078723d56abd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
