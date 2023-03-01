37b1cc599161b7f35d86c4a6d4b4c0df2b3d94a8
SzabolcsGergely <szabi@luxonis.com>
2023-03-01 15:46:26 +0200

    Update FW: Add workaround for BNO sequence number limit of 256 (sensors sends uint8)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6e693ae5d97e56f4ea29231ca7e346091f6785d1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "27d38955ce5e7953e8e19e1de0e35ff62f776f2c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
