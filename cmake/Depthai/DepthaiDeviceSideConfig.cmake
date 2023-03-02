ae75d72701344eecfc047ebb6117af20244d37ea
alex-luxonis <alex@luxonis.com>
2023-03-02 04:47:38 +0200

    FW: camera sensor improvements: - AR0234 improved AE smoothness, increased max gain to 400x (first 25.6x analog), - OV9782 on RGB/CAM-A socket max FPS: 120 (previously was only for OV9282), also improves image quality in very bright light, - OV9782/9282 minimum exposure time decreased: 20us -> 10us, helps in very bright light. TODO update tuning to make use of it, currently only possible to use with manual exposure

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "27d38955ce5e7953e8e19e1de0e35ff62f776f2c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "115765d4d9f70a9f983a7824b3b38f06c6464267")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
