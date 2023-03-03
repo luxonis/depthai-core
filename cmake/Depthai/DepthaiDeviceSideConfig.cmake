c8cb7ca754b69964c6a6f259b46392cc294f03c9
SzabolcsGergely <szabi@luxonis.com>
2023-03-03 23:27:48 +0200

    Update ObjectTracker with improvements from rvc3

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "115765d4d9f70a9f983a7824b3b38f06c6464267")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "095e25b1b7564afe0371b1575eeb54d674deab56")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
