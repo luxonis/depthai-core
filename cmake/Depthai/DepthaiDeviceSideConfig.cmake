0a7956bc705c3ebea305a9ba1559ff0e023fad77
SzabolcsGergely <szabi@luxonis.com>
2023-03-03 23:19:56 +0200

    Enable interrupt mode: Update BMI driver with fixes from: https://github.com/boschsensortec/BMI270-Sensor-API/pull/16

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "115765d4d9f70a9f983a7824b3b38f06c6464267")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8e9d4424a97060cb4383896b76e131400d03893e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
