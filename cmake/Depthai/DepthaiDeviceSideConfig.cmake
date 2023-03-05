14fed3c118cff2f663838056731f707f7c7b039c
SzabolcsGergely <szabi@luxonis.com>
2023-03-06 01:51:04 +0200

    Update FW with IMU fix for BNO

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a69259a0e38deca9d77ea246b0c33251cdf0b6cc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3e50182fc9c6e7b8ae86b6bc38fe2997ceca77d3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
