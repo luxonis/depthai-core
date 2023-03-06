e3164680e5612523b8a10079cf91dab93d9b0caf
SzabolcsGergely <szabi@luxonis.com>
2023-03-07 01:09:02 +0200

    Update FW: IMU support for OAK-D-SR

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "877941d3193136414b666c2cebd221c0d467a23b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c1b1e481341664a7c6bc64074e0815ed0cbe4dbd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
