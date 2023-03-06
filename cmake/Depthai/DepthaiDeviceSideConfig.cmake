0655df12801f45436c9d1119339ea1b4cc1ca1cf
SzabolcsGergely <szabi@luxonis.com>
2023-03-06 18:45:45 +0200

    Update FW with fix for BMI timestamp

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3e50182fc9c6e7b8ae86b6bc38fe2997ceca77d3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "877941d3193136414b666c2cebd221c0d467a23b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
