12b198a67f9c7a56747666679f00763c35cf64e3
SzabolcsGergely <szabi@luxonis.com>
2023-03-08 16:22:43 +0200

    Update FW with deprecation warning for enableFirmwareUpdate

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "86a4983d07f5d3cbe7bbdc108548629902149cdb")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4dc50bbc4988c1fd8b27ec65b9a84f1a1a8e643e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
