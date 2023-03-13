e5dae8fd2f9e543bd99f9d1a73959398ef5bd0b4
TheMarpe <martin.peterlin7@gmail.com>
2023-03-13 18:03:09 +0100

    [FW] Added missing bindings for CameraControl and ImgFrame

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bd54decd22f0913323b95b547dbd435ec12d4fc7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "25a664f9f2371135504b12705583f0c2a692f03a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
