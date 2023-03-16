b4d5baa2a47874ffb6cfb6ec60867b5825860037
Florin Buica <florin.buica@luxonis.com>
2023-03-16 20:12:40 +0200

    add tof node

---
+++ include/depthai/pipeline/node/TofCamera.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <depthai/pipeline/DeviceNode.hpp>
+
+// standard
+#include <fstream>
+
+// shared
+#include <depthai-shared/properties/TofCameraProperties.hpp>
+
+#include "depthai/pipeline/datatype/TofCameraConfig.hpp"
+
+namespace dai {
+namespace node {
+
+/**
+ * @brief TofCamera node.
+ * Performs feature tracking and reidentification using motion estimation between 2 consecutive frames.
+ */
+class TofCamera : public NodeCRTP<DeviceNode, TofCamera, TofCameraProperties> {
+   public:
+    constexpr static const char* NAME = "TofCamera";
+    using NodeCRTP::NodeCRTP;
+
+   protected:
+    Properties& getProperties();
+
+   private:
+    std::shared_ptr<RawTofCameraConfig> rawConfig;
+
+   public:
+    TofCamera();
+    TofCamera(std::unique_ptr<Properties> props);
+
+    /**
+     * Initial config to use for feature tracking.
+     */
+    TofCameraConfig initialConfig;
+
+    /**
+     * Input TofCameraConfig message with ability to modify parameters in runtime.
+     * Default queue is non-blocking with size 4.
+     */
+    Input inputConfig{true, *this, "inputConfig", Input::Type::SReceiver, false, 4, {{DatatypeEnum::TofCameraConfig, false}}};
+    /**
+     * Input message with frame data on which feature tracking is performed.
+     * Default queue is non-blocking with size 4.
+     */
+    Input inputRaw{true, *this, "inputRaw", Input::Type::SReceiver, false, 4, true, {{DatatypeEnum::ImgFrame, false}}};
+
+    /**
+     * Outputs TrackedFeatures message that carries tracked features results.
+     */
+    Output depth{true, *this, "depth", Output::Type::MSender, {{DatatypeEnum::ImgFrame, false}}};
+
+    /**
+     * Passthrough message on which the calculation was performed.
+     * Suitable for when input queue is set to non-blocking behavior.
+     */
+    Output passthroughInputRaw{true, *this, "passthroughInputRaw", Output::Type::MSender, {{DatatypeEnum::ImgFrame, false}}};
+
+    // Functions to set properties
+    /**
+     * Specify whether or not wait until configuration message arrives to inputConfig Input.
+     * @param wait True to wait for configuration message, false otherwise.
+     */
+    [[deprecated("Use 'inputConfig.setWaitForMessage()' instead")]] void setWaitForConfigInput(bool wait);
+
+    /**
+     * @see setWaitForConfigInput
+     * @returns True if wait for inputConfig message, false otherwise
+     */
+    [[deprecated("Use 'inputConfig.setWaitForMessage()' instead")]] bool getWaitForConfigInput() const;
+};
+
+}  // namespace node
+}  // namespace dai
---
+++ src/pipeline/node/TofCamera.cpp
@@ -0,0 +1,32 @@
+#include "depthai/pipeline/node/TofCamera.hpp"
+
+#include "spdlog/fmt/fmt.h"
+
+namespace dai {
+namespace node {
+
+TofCamera::TofCamera()
+    : NodeCRTP<DeviceNode, TofCamera, TofCameraProperties>(), rawConfig(std::make_shared<RawTofCameraConfig>()), initialConfig(rawConfig) {}
+
+TofCamera::TofCamera(std::unique_ptr<Properties> props)
+    : NodeCRTP<DeviceNode, TofCamera, TofCameraProperties>(std::move(props)),
+      rawConfig(std::make_shared<RawTofCameraConfig>(properties.initialConfig)),
+      initialConfig(rawConfig) {}
+
+TofCamera::Properties& TofCamera::getProperties() {
+    properties.initialConfig = *rawConfig;
+    return properties;
+}
+
+// Node properties configuration
+void TofCamera::setWaitForConfigInput(bool wait) {
+    inputConfig.setWaitForMessage(wait);
+}
+
+bool TofCamera::getWaitForConfigInput() const {
+    return inputConfig.getWaitForMessage();
+}
+
+
+}  // namespace node
+}  // namespace dai
