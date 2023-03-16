0c3d6be3b3574867cc99d1ad85c3f75eade40f58
SzabolcsGergely <szabi@luxonis.com>
2023-03-16 15:41:41 +0200

    Update FW with fix for calibration load example

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "25a664f9f2371135504b12705583f0c2a692f03a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e0b55845f96c3e554718a7acd8e82e915259a758")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
