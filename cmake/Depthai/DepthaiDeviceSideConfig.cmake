1c12f43b5c4c734efabea8e08707d38335089910
alex-luxonis <alex@luxonis.com>
2023-03-17 05:33:59 +0200

    Color/Mono/Camera: add `setRawOutputPacked` config

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "094290889f57942077d4032e649ebe5343c5d8c7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cd9f1c5df79127de65d3e0bd06326c8eefe475ed")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
