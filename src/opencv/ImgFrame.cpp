3e5303f78cb72a8fbbf2192927d397eb18a012f4
alex-luxonis <alex@luxonis.com>
2023-03-17 05:35:20 +0200

    ImgFrame: handle RAW10/12/14 (unpacked) like RAW16

--- src/opencv/ImgFrame.cpp
+++ src/opencv/ImgFrame.cpp
@@ -46,6 +46,9 @@ cv::Mat ImgFrame::getFrame(bool deepCopy) {
             break;
 
         case Type::RAW16:
+        case Type::RAW14:
+        case Type::RAW12:
+        case Type::RAW10:
             size = cv::Size(getWidth(), getHeight());
             type = CV_16UC1;
             break;
@@ -139,6 +142,9 @@ cv::Mat ImgFrame::getCvFrame() {
 
         case Type::RAW8:
         case Type::RAW16:
+        case Type::RAW14:
+        case Type::RAW12:
+        case Type::RAW10:
         case Type::GRAY8:
         case Type::GRAYF16:
             output = frame.clone();
