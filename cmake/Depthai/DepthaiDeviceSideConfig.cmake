992905e9b07826f98a9b2a053b67bcab2fc35f5e
alex-luxonis <alex@luxonis.com>
2023-03-17 16:23:29 +0200

    FW: initial IMX219 support, tested with RPi Camera V2, FPS fixed to 21.19 (gets lowered to about 16 under low light with increased exposure), using 2 MIPI lanes, has tuning but currently only supports in parallel other IMX378/477/577/582, only full resolution 3280x2464 but can be ISP-downscaled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "094290889f57942077d4032e649ebe5343c5d8c7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fc00a6131311f47f6c27cc286aa2e59b201949f9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
