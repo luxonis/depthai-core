fc81e1b7d0c66813b9e75dbe83191d8c9e1db66e
TheMarpe <martin.peterlin7@gmail.com>
2023-03-19 20:09:09 +0100

    [FW] Removed UTF-8 degree sign from temperature prints. Closes: #773

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "094290889f57942077d4032e649ebe5343c5d8c7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cbab456e3481d680f7f125fbc3b5ec0435323a83")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
