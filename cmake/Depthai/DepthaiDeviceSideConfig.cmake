49791323407d9a667ad20c52ea1a56847dd8d119
TheMarpe <martin.peterlin7@gmail.com>
2023-03-22 09:43:34 +0100

    [FW] Power cycle stuck fix 2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7ac2b5891426d03b977fac1e7c8b1258638700ca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9461a07677e7fb03946f54508fb2852052150ca5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
