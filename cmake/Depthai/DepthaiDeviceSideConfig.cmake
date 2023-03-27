26e65a338f5db1309010d617ea948f03c5ca235b
SzabolcsGergely <szabi@luxonis.com>
2023-03-27 22:05:58 +0300

    Update FW: handle disparity flipping

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "413372f7c9951a95d703246df78d6e126de2d733")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1b0f251903548edc3b29685e7ec004826b1fe465")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
