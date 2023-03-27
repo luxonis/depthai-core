9ad71097fb00494ae0a562cc8a1c9edfb1771767
SzabolcsGergely <szabi@luxonis.com>
2023-03-27 21:24:53 +0300

    [Stereo] Add option to invalidate edge pixels on disparity/depth frame

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7ac2b5891426d03b977fac1e7c8b1258638700ca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "413372f7c9951a95d703246df78d6e126de2d733")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
