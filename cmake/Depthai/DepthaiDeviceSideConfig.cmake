2097673b5489c6e15148e9b5db54d9733330a011
SzabolcsGergely <szabi@luxonis.com>
2023-03-28 16:36:11 +0300

    Update FW: support for stereo.setOutputSize when LEFT or RIGHT alignment is set

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7ac2b5891426d03b977fac1e7c8b1258638700ca")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "791ee24fb05561a56c2b72ebd9e2937b5beeb693")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
