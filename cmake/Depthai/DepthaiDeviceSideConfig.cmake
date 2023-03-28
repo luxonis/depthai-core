b3a1c3bee3a51235120bfb592d2c0aceadbefdad
SzabolcsGergely <szabi@luxonis.com>
2023-03-28 04:00:00 +0300

    Update FW: support for configurable ImageManip interpolation type

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0ac7686c9827bbca3e35b62437c8dd1a9a64b9e7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "45a89f8644568e03fbfa92f9262bc2f926bd5701")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
