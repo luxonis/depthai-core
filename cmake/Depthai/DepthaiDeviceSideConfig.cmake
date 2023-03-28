4c2035bbdf9dbe9000e55da111f68fda79504a52
SzabolcsGergely <szabi@luxonis.com>
2023-03-28 19:38:35 +0300

    Update FW: support for stereo between RGB and LEFT/RIGHT

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "791ee24fb05561a56c2b72ebd9e2937b5beeb693")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7f88e89b122b144dd68951d8faf1ac5d5a2bbb43")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
