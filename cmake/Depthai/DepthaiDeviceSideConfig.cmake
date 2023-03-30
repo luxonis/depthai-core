998740645690867b2146dc44bbf87ef2743c7777
TheMarpe <martin.peterlin7@gmail.com>
2023-03-30 22:34:02 +0200

    [FW] ImageManip CSC improvements, New boards and power cycle fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7f88e89b122b144dd68951d8faf1ac5d5a2bbb43")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e667166e4f55814f0f1f32bef2ef0be070875ce9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
