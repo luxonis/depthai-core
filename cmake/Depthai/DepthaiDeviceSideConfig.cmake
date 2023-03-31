8ed944b91a0d6b1d2773d6457b0ebaaeed18ae8b
SzabolcsGergely <szabi@luxonis.com>
2023-03-31 21:31:48 +0300

    Update FW with fix for USB devices stuck after reboot

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e26721b204c893ce655993789af39342d1086eb8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "57ca2c171f6ef53cb83fbbc3aabfe512c67796ce")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
