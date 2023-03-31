e67e1c3ff901ba58fa1a5c11ae63cafa6c54d9e8
alex-luxonis <alex@luxonis.com>
2023-03-31 19:11:18 +0300

    FW: fix for UART0 / '/dev/ttyS0' init failure in Script node

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0ac7686c9827bbca3e35b62437c8dd1a9a64b9e7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e26721b204c893ce655993789af39342d1086eb8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
