b21dafae1de6c48c9927f3d56b41c28d1ec10c1c
SzabolcsGergely <szabi@luxonis.com>
2023-04-05 01:10:47 +0300

    Update FW: fix spatial location calculator for 400p/480p resolution

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "57ca2c171f6ef53cb83fbbc3aabfe512c67796ce")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3e3d5738e8e83d090e34bfb06740b0baa0fad195")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
