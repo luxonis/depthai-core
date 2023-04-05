ffd0546d426dd2870898b6b8c8dc1ad1615815f8
SzabolcsGergely <szabi@luxonis.com>
2023-04-05 18:58:01 +0300

    FW: Fix camera intrinsics when RGB alignment is used

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3e3d5738e8e83d090e34bfb06740b0baa0fad195")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3575b77f20e796b4e79953bf3d2ba22f0416ee8b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
