0b8c5d374efc08b4bbb6f62b491aea167f1c89ba
alex-luxonis <alex@luxonis.com>
2023-04-06 05:45:22 +0300

    FW: support for IMX283, at almost full resolution: 5312x3692, FPS max 20, note: exposure control not working properly, and lower FPS would apply longer exposure

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3575b77f20e796b4e79953bf3d2ba22f0416ee8b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b3e328001232436c27573f9d8d072e14f0624874")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
