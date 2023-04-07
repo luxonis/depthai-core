dc0cac4195119323429cf162aad58245d9f42b2b
Florin Buica <florin.buica@luxonis.com>
2023-04-07 15:47:07 +0300

    rename to tof

--- include/depthai/pipeline/node/ToF.hpp
+++ include/depthai/pipeline/node/ToF.hpp
@@ -6,42 +6,42 @@
 #include <fstream>
 
 // shared
-#include <depthai-shared/properties/TofCameraProperties.hpp>
+#include <depthai-shared/properties/ToFProperties.hpp>
 
-#include "depthai/pipeline/datatype/TofCameraConfig.hpp"
+#include "depthai/pipeline/datatype/ToFConfig.hpp"
 
 namespace dai {
 namespace node {
 
 /**
- * @brief TofCamera node.
+ * @brief ToF node.
  * Performs feature tracking and reidentification using motion estimation between 2 consecutive frames.
  */
-class TofCamera : public NodeCRTP<DeviceNode, TofCamera, TofCameraProperties> {
+class ToF : public NodeCRTP<DeviceNode, ToF, ToFProperties> {
    public:
-    constexpr static const char* NAME = "TofCamera";
+    constexpr static const char* NAME = "ToF";
     using NodeCRTP::NodeCRTP;
 
    protected:
     Properties& getProperties();
 
    private:
-    std::shared_ptr<RawTofCameraConfig> rawConfig;
+    std::shared_ptr<RawToFConfig> rawConfig;
 
    public:
-    TofCamera();
-    TofCamera(std::unique_ptr<Properties> props);
+    ToF();
+    ToF(std::unique_ptr<Properties> props);
 
     /**
      * Initial config to use for feature tracking.
      */
-    TofCameraConfig initialConfig;
+    ToFConfig initialConfig;
 
     /**
-     * Input TofCameraConfig message with ability to modify parameters in runtime.
+     * Input ToFConfig message with ability to modify parameters in runtime.
      * Default queue is non-blocking with size 4.
      */
-    Input inputConfig{true, *this, "inputConfig", Input::Type::SReceiver, false, 4, {{DatatypeEnum::TofCameraConfig, false}}};
+    Input inputConfig{true, *this, "inputConfig", Input::Type::SReceiver, false, 4, {{DatatypeEnum::ToFConfig, false}}};
     /**
      * Input message with frame data on which feature tracking is performed.
      * Default queue is non-blocking with size 4.
---
+++ src/pipeline/datatype/ToFConfig.cpp
@@ -0,0 +1,22 @@
+#include "depthai/pipeline/datatype/ToFConfig.hpp"
+
+namespace dai {
+
+ToFConfig::Serialized ToFConfig::serialize() const {
+    return {data, raw};
+}
+
+ToFConfig::ToFConfig() : Buffer(std::make_shared<RawToFConfig>()), cfg(*dynamic_cast<RawToFConfig*>(raw.get())) {}
+ToFConfig::ToFConfig(std::shared_ptr<RawToFConfig> ptr)
+    : Buffer(std::move(ptr)), cfg(*dynamic_cast<RawToFConfig*>(raw.get())) {}
+
+dai::RawToFConfig ToFConfig::get() const {
+    return cfg;
+}
+
+ToFConfig& ToFConfig::set(dai::RawToFConfig config) {
+    cfg = config;
+    return *this;
+}
+
+}  // namespace dai
