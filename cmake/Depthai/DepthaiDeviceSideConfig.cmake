c4af66caaf544ba1b409bee9ead30cd8ba3deda4
alex-luxonis <alex@luxonis.com>
2023-04-13 20:12:21 +0300

    FW: fix running 4 cameras with Ethernet, 4th cam enabled didn't stream due to device memory allocation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3575b77f20e796b4e79953bf3d2ba22f0416ee8b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f69a5fc562685b0b5b1993de8aa8f17468bbcaea")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
