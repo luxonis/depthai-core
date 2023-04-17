0eca08e9b25c26e71abcee3a959de6de2a4f6529
alex-luxonis <alex@luxonis.com>
2023-04-17 16:20:55 +0300

    Update FW: IMX283 exposure and ISO setting fixed, added 4K res (but has issues at higher FPS, about `> 19`), fixed a depthai crash with camera FPS set `< 0.5`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b3e328001232436c27573f9d8d072e14f0624874")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1cc4a536f38b35697d31a3eac16ecee44ec73bac")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
