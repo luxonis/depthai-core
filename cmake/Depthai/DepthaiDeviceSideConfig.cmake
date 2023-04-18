8d35bac4ac5cc7618cd89d6a2a1629eeb8cc5c74
TheMarpe <martin.peterlin7@gmail.com>
2023-04-18 03:23:04 +0200

    [FW] Fix for OAK-D-SR camera enumeration

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f69a5fc562685b0b5b1993de8aa8f17468bbcaea")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "49524b99e3f484d486127905b2d0d57bababe237")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
