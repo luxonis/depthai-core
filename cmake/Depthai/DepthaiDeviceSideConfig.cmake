c03bfeab670ab4e325fec5ab8c23fb5804759a90
TheMarpe <martin.peterlin7@gmail.com>
2023-04-19 00:25:26 +0200

    [FW / BL] Updated both FW & BL for OAK-D-LR R1. ETH fixes and moved to BNO086 IMU.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "64f53ba8b53a5ebe94ab529472e413928d518d71")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "97fc2b14eb0eb9ffdc3a941d47e8cb4d1412cdc6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
