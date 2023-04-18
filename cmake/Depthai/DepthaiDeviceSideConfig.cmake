53198b31067c97448051c64a39dd49cf5bc35647
TheMarpe <martin.peterlin7@gmail.com>
2023-04-18 03:23:16 +0200

    [FW] OAK-D-LR R1 preparation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "49524b99e3f484d486127905b2d0d57bababe237")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "64f53ba8b53a5ebe94ab529472e413928d518d71")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
