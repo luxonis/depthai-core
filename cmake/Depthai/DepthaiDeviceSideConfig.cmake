e92d9882f615ed0ff4daa62147181025e8789266
alex-luxonis <alex@luxonis.com>
2023-04-19 17:55:07 +0300

    ToF decoding, custom, on standard FFC devices only works fine with RGB/CAM-A

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c54ad8710de77f65a9f7bc542d4cfef2d53353d9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "083671e910429c775b0eb77a569ad8ba8642b8b8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
