9000b2594ea7620df581f1c1c859b32114c9604a
Florin Buica <florin.buica@luxonis.com>
2023-04-21 14:50:24 +0300

    adding bindings for samsung33d

--- include/depthai/pipeline/node/ToF.hpp
+++ include/depthai/pipeline/node/ToF.hpp
@@ -21,6 +21,17 @@ class ToF : public NodeCRTP<Node, ToF, ToFProperties> {
     ToF(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId);
     ToF(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId, std::unique_ptr<Properties> props);
 
+    /**
+     * Initial config to use for depth calculation.
+     */
+    RawToFConfig initialConfig;
+
+    /**
+     * Input ToF message with ability to modify parameters in runtime.
+     * Default queue is non-blocking with size 4.
+     */
+    Input inputConfig{*this, "inputConfig", Input::Type::SReceiver, false, 4, {{DatatypeEnum::ToFConfig, false}}};
+ 
     Input input{*this, "input", Input::Type::SReceiver, true, 8, {{DatatypeEnum::ImgFrame, true}}};
 
     /**
--- src/pipeline/node/ToF.cpp
+++ src/pipeline/node/ToF.cpp
@@ -7,7 +7,7 @@ namespace node {
 ToF::ToF(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId) : ToF(par, nodeId, std::make_unique<ToF::Properties>()) {}
 ToF::ToF(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId, std::unique_ptr<Properties> props)
     : NodeCRTP<Node, ToF, ToFProperties>(par, nodeId, std::move(props)) {
-    setInputRefs({&input});
+    setInputRefs({&input, &inputConfig});
     setOutputRefs({&depth, &amplitude, &error});
 }
 
