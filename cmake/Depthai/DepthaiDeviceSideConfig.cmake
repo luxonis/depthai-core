a8935bf7facd29d4ac34e843ae0c32a76ffd78c8
TheMarpe <martin.peterlin7@gmail.com>
2023-04-23 15:21:13 +0200

    [FW] Match shared

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "97fc2b14eb0eb9ffdc3a941d47e8cb4d1412cdc6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e148c4ba300f23a6c57bb44055c897eebe60ce0f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
