ceac0c62184165e71ddbc83cb135700edfb3c3c8
Martin Peterlin <martin.peterlin7@gmail.com>
2023-04-27 03:28:29 +0200

    [FW] OAK-D-LR R1 back to BMI270

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0a9c944afd1462d40cdc63c2f6c899b16795b7b1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fff16efe7f9e5b729a48cc9a49b99eb10d1f09de")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
