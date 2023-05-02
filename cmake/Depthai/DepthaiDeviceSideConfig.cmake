9096d62e3598afbc005f20d7d9f0420470da7234
SzabolcsGergely <szabi@luxonis.com>
2023-05-03 01:33:27 +0200

    [Stereo] Fix temporal filter crash on startup

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "24a8a36272d6f6affb7e9a9321c64b116ca5d70f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f8e6996f0a3cf933ee64b0c2a89560be88bc6dba")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
