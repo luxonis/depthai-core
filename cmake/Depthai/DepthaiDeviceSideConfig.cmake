3a288536dd28ad836680cfa832358e94d26e221c
Florin Buica <florin.buica@luxonis.com>
2023-05-03 14:58:30 +0300

    update device firmware

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d9feac0a433acaa772e358c6a499f41179f0c0d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c505eb67b7bd9e0694c61b14aec1e82e846149bd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
