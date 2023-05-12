3a202c711c836344dc2e623d781660cc5ea08788
alex-luxonis <alex@luxonis.com>
2023-05-12 04:35:07 +0300

    FW: IMX378/IMX582 manual ISO up to 3200, handle miscellaneous controls: .initialControl.setMisc("downsampling-mode", "binning")  # default: "scaling" .initialControl.setMisc("binning-mode", "sum")  # default: "avg"
    Max FPS for IMX378 1080p back to 118 (from 60) with binning enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a30f307db9c7879e660037697e18bf6c90456847")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d6d87845b7705581f934f645a3cd587ed8e1f794")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
