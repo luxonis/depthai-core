75b93d617bf97b3fcd57504438592c294ab64478
SzabolcsGergely <szabi@luxonis.com>
2023-05-19 00:05:41 +0300

    FW: Stereo: handle queue blocking settings

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eff5c5290845fc60f240355187caae5809fa51d2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2610badb46c61639065377b46bfc8490f30ed733")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
