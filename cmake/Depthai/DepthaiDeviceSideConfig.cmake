fe99967ad39fce19107e2ad1f4a25ced57efc407
Florin Buica <florin.buica@luxonis.com>
2023-05-18 18:39:59 +0300

    FW: tof fps improvements and amplitude as RAW8

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eff5c5290845fc60f240355187caae5809fa51d2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e4f8ccf6e391b0587ad6562a03abd419a0e2a692")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
