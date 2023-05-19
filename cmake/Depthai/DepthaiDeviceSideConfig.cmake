81b014331245865cb25bfca481d647cc78be37cd
TheMarpe <martin.peterlin7@gmail.com>
2023-05-19 12:16:28 +0200

    Added new boards support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "fff16efe7f9e5b729a48cc9a49b99eb10d1f09de")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "924fa2f41e73914a9b585936467534f97f982dd7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
