b7de3709dbeee18a9552ee057db905956f1e331e
alex-luxonis <alex@luxonis.com>
2023-05-23 08:05:57 +0300

    FW: ToF sync

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e4f8ccf6e391b0587ad6562a03abd419a0e2a692")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "97c99629d1be9a9df3911abb2493846a03e64ed5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
