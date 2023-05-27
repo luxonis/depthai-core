f02126fd6075cd8210fe009081f1034532cfa7ab
alex-luxonis <alex@luxonis.com>
2023-05-27 16:52:55 +0300

    FW: IMX586 on-sensor HDR, with new misc controls: .initialControl.setMisc("hdr-exposure-ratio", 4) # enables HDR when set `> 1`, current options: 2, 4, 8 .initialControl.setMisc("hdr-local-tone-weight", 75) # default 75, range 0..100

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0ff8a1fe6601ec41218ede6a892975147b369cc1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cf38dd36af81bd02474a647966e439a292943969")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
