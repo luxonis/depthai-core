be9c9d9fa379fdf6f433a8bf758a194529a8b510
TheMarpe <martin.peterlin7@gmail.com>
2023-05-30 18:04:23 +0200

    [FW] WIP for new SoMs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b4a6abc71d4c3735b41ae2c9a584e8aa12861844")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "523872ba26994fc7b50647c7e65bb80819b10395")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
