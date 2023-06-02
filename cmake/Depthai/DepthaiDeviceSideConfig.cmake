72b05d8044b2e80720d847e3a61c447e45b20b00
TheMarpe <martin.peterlin7@gmail.com>
2023-06-02 19:19:49 +0200

    [FW] Fixed camera orientation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "002a76463f0d7a606a2cff458b2530bd80041aa0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "93af344aa4fafe974590f1e8413d2e9b1a2a9ae4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
