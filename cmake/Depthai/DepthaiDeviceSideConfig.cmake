0435b503c65c7bdd280d2b641747118c0f72c865
alex-luxonis <alex@luxonis.com>
2023-06-12 17:59:29 +0300

    FW: update IMX296 tuning

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1f1f66862e38df587cabdc24f2c8ce12071b998b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "18eef1cc81427262f72745e0b2dcc307b4609bce")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
