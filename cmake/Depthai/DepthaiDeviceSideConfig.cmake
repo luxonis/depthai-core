1702d31345357cdd9a856badd3d27dd76cc27dfc
alex-luxonis <alex@luxonis.com>
2023-07-04 15:51:46 +0300

    FW: fix CAM_D enumeration on OAK-FFC-4P R7

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "900949c33d6b683d78ad8084828b6ba9c2979d93")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6ff42a5c5a99cde04f015f6694d5e611f7dc2939")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
