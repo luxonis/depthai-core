e52f10f2d9933f4e4a543bc02d70683147080f3a
SzabolcsGergely <szabi@luxonis.com>
2023-07-04 16:03:39 +0300

    FW: Stereo: Apply alpha scaling parameter to RGB intrinsics when RGB alignment is enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1c4ac419c70d4baddb7afd78a3446e28286067bc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "900949c33d6b683d78ad8084828b6ba9c2979d93")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
