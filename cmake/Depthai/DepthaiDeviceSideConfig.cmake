3ef1175944637c928ac571c9b1f5cff92161bfde
SzabolcsGergely <szabi@luxonis.com>
2023-07-04 19:38:49 +0300

    Camera: move alpha parameter as optional to be consistent with stereo

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "900949c33d6b683d78ad8084828b6ba9c2979d93")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "91fd63cb4e50b716bbd22e667d86fcc904bee92c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
