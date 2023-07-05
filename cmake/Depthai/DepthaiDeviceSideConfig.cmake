ca3de8621761d91489f43e92bd042a70237c6823
SzabolcsGergely <szabi@luxonis.com>
2023-07-05 20:24:33 +0300

    BMI: Fix accumulating latency due to slow xlink read

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "78f6ce2dff2aa2edb9c8ea06106c46d268da8160")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8a540fc8d3f116faaeddec8bb95864e6cb483786")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
