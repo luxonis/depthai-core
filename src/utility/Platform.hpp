4cb9b855d9b0c6d5395670f5c8453c9f4cc86c1c
Matevz Morato <matevz.morato@gmail.com>
2023-07-17 17:17:44 +0000

    Add platfrom independed code for a temporary directory

--- src/utility/Platform.hpp
+++ src/utility/Platform.hpp
@@ -13,5 +13,7 @@ std::string getIPv4AddressAsString(std::uint32_t binary);
 // TODO change this to std::thread
 void setThreadName(JoiningThread& thread, const std::string& name);
 
+// Get the path to the temporary directory // TODO change this to std::filesystem::path if the project switches to C++ 17
+std::string getTempPath();
 }
 }
