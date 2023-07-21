2e5e71f152088a9e4dac5cc7875cc782559b233c
alex-luxonis <alex@luxonis.com>
2023-07-21 03:22:52 +0300

    FW: ToF custom HW sync, add RPC `readCcmEepromRaw`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f8866caab491a1c327762c1b2969098453abd90f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9b1d3f21fbc3a3fa44fdaf0dcc54b764cf8f5888")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
