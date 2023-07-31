a26232ffd0e7b214724fdd2b0241536b1111250a
TheMarpe <martin.peterlin7@gmail.com>
2023-07-31 22:12:18 +0200

    Fixed OAK-D-SR R3&R4 IMU

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0e55265bea300ec47b8a97874acce0ee7c80365b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "51474806227bd76345fcd2e8c891eb57dfd0152f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
