142af0c41cb7c7b935c2ff1f90d5ba35a042a0e0
TheMarpe <martin.peterlin7@gmail.com>
2023-07-31 22:00:21 +0200

    Added OAK-D-LR R2&R3 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c0cdc33038f6673d8903e6c41806cd3f023636b6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0e55265bea300ec47b8a97874acce0ee7c80365b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
