98d6b8bb31d0bad64df02372adf6939496acf2ca
alex-luxonis <alex@luxonis.com>
2023-08-07 08:41:47 +0300

    Fix IMU (wake pin) on OAK-D-LR R3 and OAK-D-SR R4

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "51474806227bd76345fcd2e8c891eb57dfd0152f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b5739a058126af595e4bddff422c80bfd511e938")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
