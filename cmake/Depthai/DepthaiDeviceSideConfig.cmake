131d92de9c2ef07d5fb57327af9ac9890c4eaf57
Martin Peterlin <martin.peterlin7@gmail.com>
2023-08-25 21:36:55 +0200

    Updated FW with potential powercycle errata fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "aab183746ea27261c159fd71681b55f65232e2db")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2d9211e9d1bb860792060dc2290fc19dc0da941b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
