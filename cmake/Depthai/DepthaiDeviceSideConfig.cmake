091bc64964b8188d465a356205ccd6dd3db43bf8
zrezke <jeretina.filip@gmail.com>
2023-08-31 16:42:00 +0200

    Update FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "205beb65aadba482443334067754ce38e6734937")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8c4bd1cdde202d59a4cb80abf65dc07100fa0f5e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
