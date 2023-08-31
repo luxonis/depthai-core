47055d5270b13adbb740cb1a897b7f9fe7a98aa5
alex-luxonis <alex@luxonis.com>
2023-08-31 21:08:45 +0300

    FW: fix OAK-D-SR camera enum. IMX296 updates: - support for RPi GS Cam paired with Luxonis SL6996 RPi camera adapter - support for external trigger on XTR/XTRIG pin: active low, pulse width determines exposure time. Limitation: needs an initial trigger shortly after start to avoid a crash, or `DEPTHAI_WATCHDOG=0` can be a workaround

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1d4f2c5443b1ae2dd3d7d394da7487158c75246c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7bb1b76021f46a599ffda2bbe9443027a88242df")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
