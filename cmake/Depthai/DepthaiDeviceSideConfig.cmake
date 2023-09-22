b928542294770e42c1561202cefa0e8a0afc338c
Martin Peterlin <martin.peterlin7@gmail.com>
2023-09-23 00:23:47 +0200

    [FW] Improved OV9782 fps handling

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f6d67f68b93a105814f28627aabe843c17ca6ea0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3b164d7220ce82c187cfb100b27e86f269cd882b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
