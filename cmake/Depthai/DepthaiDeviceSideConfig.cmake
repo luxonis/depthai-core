052603c088b4268c108f6a2bf7a8012accc599eb
Martin Peterlin <martin.peterlin7@gmail.com>
2023-09-22 22:51:20 +0200

    [FW] OAK-D SR PoE and ToF improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5c0832b9936055085e902098afbcfd3c33a6aba3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f6d67f68b93a105814f28627aabe843c17ca6ea0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
