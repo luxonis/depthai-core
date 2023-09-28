66ec090801ff02b94c8289c9462a78c7eb87eeea
alex-luxonis <alex@luxonis.com>
2023-09-28 06:17:09 +0300

    FW: fixes for AR0234 fsync, AR0234 AE max exposure: 16->33 ms, more robust camera handling (especially in sync modes)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e8417f185496f8c9f21597bfb8613465eb2daad9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5c8d51b3a9e7687f113beda652036043eb530506")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
