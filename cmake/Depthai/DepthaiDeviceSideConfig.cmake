e4084bb14cc6102d695761c26419d96f0ab6f049
Matevz Morato <matevz.morato@gmail.com>
2023-10-06 22:18:47 +0200

    [RVC2 FW] Update device side with added indices

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "39a9d271a9ed0172f6481877723fca96d41b54c6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "60185a20f022e5f2bcce6af7abe2dec71541199f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
