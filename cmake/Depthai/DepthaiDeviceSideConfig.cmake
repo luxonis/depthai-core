f59efaaed8c5cf20d8314e5cacb1877ce616ab7c
alex-luxonis <alex@luxonis.com>
2023-10-09 18:44:40 +0300

    FW: OAK-D-SR-PoE R1 fixes for fsync detect and status LED (green when running), StereoDepth fix for RGB-depth alignment with Left-Right-RGB layout

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "39a9d271a9ed0172f6481877723fca96d41b54c6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ae8f3bcc5e17f57c8686ada8e103587437a54570")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
