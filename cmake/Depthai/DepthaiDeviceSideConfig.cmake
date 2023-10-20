0be55ac9ea8fc9e8b3f47bdcb80af8d01e484073
SzabolcsGergely <szabi@luxonis.com>
2023-10-20 17:52:01 +0300

    Add vertical mesh support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "50c60a492e518edbfac8843d8c98f7490a0944a1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ba1783993c9b37dc8592a10a667d242f652a6fce")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
