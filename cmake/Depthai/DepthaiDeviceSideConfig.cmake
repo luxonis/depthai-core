e02889cadcac042a2ebe34e439a0f9283bb7c252
SzabolcsGergely <szabi@luxonis.com>
2023-11-01 17:30:33 +0200

    Update FW for vertical extended disparity

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6110275f9c095228c0cc80764a252aeed5121a38")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9aca152f1ee399011e158e8041919ea47bea913b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
