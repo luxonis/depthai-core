0abf5fd859b51d9b431c6bcc80dccc6914fa5e81
SzabolcsGergely <szabi@luxonis.com>
2023-11-05 00:46:42 +0200

    Fix multi stereo node race condition when depth output is enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c9b6a2e0a4bdbf635914b80baa06efc2f76fa2bc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d088dd017f964a3b51ecb1165b5e625e35d78af4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
