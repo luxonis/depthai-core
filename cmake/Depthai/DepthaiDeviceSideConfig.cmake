060e8d0a7afc8d217e1fcf5ecb73efb3857ee1fd
alex-luxonis <alex@luxonis.com>
2023-11-15 05:53:49 +0200

    Add CameraControl `setAutoExposureLimit`. FW: improve OV9282/OV9782 image quality in low-light / under high ISO (1300+)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3bba9ec3238bfb254174402253c0ecbe7dd9a71a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fdaa38eebe7698b23bc608cb30eba06e9472a285")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
