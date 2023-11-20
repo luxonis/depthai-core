693c57f9715a197199cafe29cbfdbc8887c9850c
zrezke <jeretina.filip@gmail.com>
2023-11-20 14:14:49 +0100

    Update shared on device and host

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "76ff45773ebf994abbdb42e78ab1cfe08ac73d58")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e22e0a628020d5e90c50751d51f2a27c36ecf1ef")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
