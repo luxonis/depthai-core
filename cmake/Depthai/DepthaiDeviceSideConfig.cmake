c43615074b9202636516a5fa037203aecc550a53
TheMarpe <martin.peterlin7@gmail.com>
2023-11-21 01:48:55 +0100

    Updated FW - powercycle stuck fix due to IMU CS pin

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3181f130742ed4f18341970a936afbdcfeaddbc9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f55cd8909ae23daaf386e3d6f8d6549fa929e23b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
