fad0a3106f3b984d3a731677fdbea82158586fda
asahtik <asahtik@gmail.com>
2023-11-24 08:40:33 +0100

    Rename connectivity enum

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "793b15e062bd44cba8ec9729bec3c51bfa9b8943")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "498ef58688d388b1ff7fe12a7cdec2d04c335098")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
