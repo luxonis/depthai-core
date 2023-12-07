ec0c72033e57cff335d7a1973f6e6da45376c305
Matevz Morato <matevz.morato@gmail.com>
2023-12-07 15:40:50 +0100

    Update FW with a stability fix for timesync

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2db1bac8b771b7718ed97692caa91f56bb7b5c89")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c9f8d0a3fbaa8170c8357b57f82ca44f7b2ca5f6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
