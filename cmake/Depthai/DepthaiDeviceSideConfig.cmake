a25a3dc7a91e6f321e1bbb5832012e3dbed27673
Matevz Morato <matevz.morato@gmail.com>
2023-12-11 15:47:32 +0100

    Fix the xlink profiling

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "afcf303d4bee6a0e313fc763ecb390fcf1a4886e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a95f582a61ec9bdbd0f72dec84822455872ffaf7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
