5ca1e14cafa22f975b29398ebd13416bd69ddc47
Matevz Morato <matevz.morato@gmail.com>
2023-12-11 01:44:47 +0100

    Revert the XLink profiling to uint64_t change

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c9f8d0a3fbaa8170c8357b57f82ca44f7b2ca5f6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6021368f44a6300fa700d8ad1bdc0bddc01f2453")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
