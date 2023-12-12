679a33da6eebabe266cce6bec5d6fe28bee32090
Matevz Morato <matevz.morato@gmail.com>
2023-12-12 21:15:56 +0100

    [Update FW with extra params]

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "60185a20f022e5f2bcce6af7abe2dec71541199f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "56e99a7f70d0268b98b8d6fb663cdf8cbda9c5a4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
