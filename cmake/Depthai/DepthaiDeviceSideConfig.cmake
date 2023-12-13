9ad83dae4fb5ecfcd24d733984a77c914a49c4ba
zrezke <jeretina.filip@gmail.com>
2023-12-13 14:03:45 +0100

    Updated device side shared

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6f4c58056952605eaed9e18bbaa3c939712cea2d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5e397fa51910f1f5c282463643b6fc86bbec227d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
