bd70e8512b403dec3519eb69c94cc0fe1a08b7d9
alex-luxonis <alex@luxonis.com>
2024-01-17 19:05:59 +0200

    Fix AE limit not being applied during AF lens movement

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a4c08c837e1637552fde7c9300001e0cf89d4a0d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "45383ea283a284afe6d928c5ff3e751a0b8618bb")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
