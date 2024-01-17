9a9bc8bbcba15e3ad223f98e9b06fbd59847c042
zrezke <jeretina.filip@gmail.com>
2024-01-17 18:41:49 +0100

    Uncomment mockIsp Camera node input and bump device side commit to fixed version.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a4c08c837e1637552fde7c9300001e0cf89d4a0d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fa7b9cc02936d3831af21dd2ba193132fbb35558")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
