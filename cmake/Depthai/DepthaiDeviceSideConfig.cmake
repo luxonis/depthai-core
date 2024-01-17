669cb604a9c4cf280551981381a35aa750b1e7da
zrezke <jeretina.filip@gmail.com>
2024-01-17 15:37:42 +0100

    Sync shared with device side, remove thermal nnet example for now, will add later when it works. Update device side commit.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f25ef24d99810280a5148126f27430db5359670e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3a7a29c839b4bb95a1573125c57cce8270b34ced")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
