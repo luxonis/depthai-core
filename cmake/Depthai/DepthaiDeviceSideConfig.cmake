dda2719e9f3bce7ade826cd152c73504ce5f22b6
zrezke <jeretina.filip@gmail.com>
2024-01-18 16:31:12 +0100

    Fix  fw build.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3a7a29c839b4bb95a1573125c57cce8270b34ced")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "95b473f2d6bef8885fa7c4dcb7f90cdd49486bcc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
