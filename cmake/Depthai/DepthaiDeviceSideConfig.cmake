e1abcce16bf0e2836556bdfaddea059f2fcd1736
zrezke <jeretina.filip@gmail.com>
2024-01-19 17:58:54 +0100

    Added new OAK-D-SR revisions.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "876c4ac79a6aa7ead15dc5e7bb3f4202f305022c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "409332a77f484a1cb9ac68b67d37dcfaca4155b9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
