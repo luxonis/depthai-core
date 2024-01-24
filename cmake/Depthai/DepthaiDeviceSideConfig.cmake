3e08b71c0498428888e4324d7412e39fdafcf1fc
Filip Jeretina <59307111+zrezke@users.noreply.github.com>
2024-01-24 15:22:02 +0100

    [RVC2] getStereoPairs (#941)
    * getStereoPairs. Skip getAvailableStereoPairs for now, since it may give weird results on RAE, or similar setups...
    
    * Formatting
    
    * Added isVertical to serialize
    
    * Updated device side shared
    
    * Impl << operator for StereoPair and vector<StereoPair>
    
    * getAvailableStereoPairs
    
    * Address https://github.com/luxonis/depthai-core/pull/941#discussion_r1460988267
    
    * Bump device side to develop.
    
    * Point shared to develop
    
    * [FW] point to same depthai-shared (ci fix)
    
    * Fix windows build.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a4c08c837e1637552fde7c9300001e0cf89d4a0d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f089593893c1b1ccb29c99ccdc9a0e723db57a3b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
