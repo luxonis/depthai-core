cbe742816810165fdc6a3f1dd4a26bfe785cf14f
zrezke <jeretina.filip@gmail.com>
2024-01-24 14:27:05 +0100

    [FW] point to same depthai-shared (ci fix)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8e44ce193c4c8d1666e1a0a08409c0015c2fc4c1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f089593893c1b1ccb29c99ccdc9a0e723db57a3b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
