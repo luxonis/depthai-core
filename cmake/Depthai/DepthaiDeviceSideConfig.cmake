5179c2e9b8e37e8b5fe8b4c527bbe9907efb50c4
zrezke <jeretina.filip@gmail.com>
2024-02-05 12:54:11 +0100

    Sync shared across host and device.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8d1e07eb96001916af03ef603d4e67f305da223b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fdd7f9e70a4ce94924fb5342b6c3b9ec4712727a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
