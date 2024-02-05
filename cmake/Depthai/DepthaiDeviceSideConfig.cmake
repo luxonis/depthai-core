943936cfe8a3c9d167a27e250159dd5ef12475ff
zrezke <jeretina.filip@gmail.com>
2024-02-05 19:41:58 +0100

    early OAK-T production version

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "409332a77f484a1cb9ac68b67d37dcfaca4155b9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d9ca71415a4bbf875181285c4116d2e0655be387")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
