198c6bc26c8e77acb247065646cb7ba370f41733
alex-luxonis <alex@luxonis.com>
2024-02-08 13:08:20 +0200

    FW: initial support for IMX715, 4lane, 4K, max FPS 9.7 for now

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "66e35b2bde14b638ef39a140848aec7a5422c7fe")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "98ba7fd36749051e345bc25cb8369af61c2dd7a4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
