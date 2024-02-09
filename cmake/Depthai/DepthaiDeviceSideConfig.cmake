15bd8c6bae2a97ec3bc53f8ed37b433364278f60
Tommy <tomprochazka0@gmail.com>
2024-02-09 11:33:29 +0100

    Add fw commit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f089593893c1b1ccb29c99ccdc9a0e723db57a3b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "73735aa6a3198ecc7097228194506d5b24c6962f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
