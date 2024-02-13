85711afda633e4c9ce8c896a54e89a1df85b3ed8
zrezke <jeretina.filip@gmail.com>
2024-02-13 14:48:29 +0100

    Fix signal integrity issues at high ambient temperatures.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "18d524de230144c6a13f643cb29ca61eb4f9e714")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "18139ef4deeae7fdd5836fe454ae2339acd10bf1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
