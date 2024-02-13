eb51d419f0616c90b17e2a13c09ee7f59764f8ea
alex-luxonis <alex@luxonis.com>
2024-02-13 23:18:23 +0200

    FW: fix CAM_C failing to stream in certain cases, with same sensor type on CAM_B

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "73735aa6a3198ecc7097228194506d5b24c6962f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9a26c8a19c1dbd741f10c7dce498561f4843db48")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
