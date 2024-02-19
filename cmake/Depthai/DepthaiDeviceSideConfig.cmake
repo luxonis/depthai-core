b6383d9540d519c0450547250ce9d5bb85c4995f
zrezke <jeretina.filip@gmail.com>
2024-02-19 12:55:09 +0100

    Merged device side.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "18139ef4deeae7fdd5836fe454ae2339acd10bf1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d86f2efa04753e0e47c153db678753afa9a630a0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
