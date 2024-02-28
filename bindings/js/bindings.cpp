7a5bcdd9a8b8251dcf2e57e93188cbc3df5b2ee1
Jakob Socan <10517617+jakgra@users.noreply.github.com>
2024-02-28 17:24:03 +0100

    Deserialize whole packets with raw data included from js and add type check function

--- bindings/js/bindings.cpp
+++ bindings/js/bindings.cpp
@@ -10,33 +10,93 @@
 
 // include depthai library
 #include <depthai/depthai.hpp>
+#include <depthai/pipeline/datatype/DatatypeEnum.hpp>
+#include <depthai/pipeline/datatype/StreamMessageParser.hpp>
+
+EMSCRIPTEN_DECLARE_VAL_TYPE(Uint8Array);
 
 template <typename T>
-T daiDeserializeFromJS(std::vector<std::uint8_t> data) {
-    T imgFrame;
-    std::cout << "input bytes array length: " << data.size() << std::endl;
-    std::cout << "starting deserialize" << std::endl;
-    const auto rc = dai::utility::deserialize(data, imgFrame);
-    std::cout << "after deserialize" << std::endl;
-    if(!rc) {
-        // this should never happen as exceptions are enabled !!!
-        std::cout << "Deserialization failed" << std::endl;
+std::shared_ptr<T> daiDeserializeFromJS(std::vector<std::uint8_t> data) {
+    // T daiDeserializeFromJS(Uint8Array data) {
+    streamPacketDesc_t packet;
+    packet.data = data.data();
+    packet.length = data.size();
+    const auto res = dai::StreamMessageParser::parseMessage(&packet);
+    const auto typedRes = std::dynamic_pointer_cast<T>(res);
+    if(typedRes == nullptr) {
+        throw std::invalid_argument(
+            "Deserialization error. Passed in message data is for message of other type. "
+            "Please use getMessageType() first and call the correct decoding function for the returned type");
     }
-    return imgFrame;
+    return typedRes;
+}
+
+dai::DatatypeEnum daiGetMessageType(std::vector<std::uint8_t> data) {
+    streamPacketDesc_t packet;
+    packet.data = data.data();
+    packet.length = data.size();
+    dai::DatatypeEnum objectType;
+    size_t serializedObjectSize;
+    size_t bufferLength;
+    std::tie(objectType, serializedObjectSize, bufferLength) = dai::StreamMessageParser::parseHeader(&packet);
+    return objectType;
 }
 
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(depthai_js) {
-    class_<dai::ImgFrame>("ImgFrame").property("width", &dai::ImgFrame::getWidth).property("height", &dai::ImgFrame::getHeight);
+    // emscripten::val types using EMSCRIPTEN_DECLARE_VAL_TYPE() macro
+    register_type<Uint8Array>("Uint8Array");
+
+    // vector types
+    register_vector<std::uint8_t>("Uint8Vector");
+    register_vector<dai::ImgDetection>("ImgDetectionVector");
+
+    // enums
+    enum_<dai::DatatypeEnum>("DatatypeEnum")
+        .value("ADatatype", dai::DatatypeEnum::ADatatype)
+        .value("Buffer", dai::DatatypeEnum::Buffer)
+        .value("ImgFrame", dai::DatatypeEnum::ImgFrame)
+        .value("EncodedFrame", dai::DatatypeEnum::EncodedFrame)
+        .value("NNData", dai::DatatypeEnum::NNData)
+        .value("ImageManipConfig", dai::DatatypeEnum::ImageManipConfig)
+        .value("CameraControl", dai::DatatypeEnum::CameraControl)
+        .value("ImgDetections", dai::DatatypeEnum::ImgDetections)
+        .value("SpatialImgDetections", dai::DatatypeEnum::SpatialImgDetections)
+        .value("SystemInformation", dai::DatatypeEnum::SystemInformation)
+        .value("SystemInformationS3", dai::DatatypeEnum::SystemInformationS3)
+        .value("SpatialLocationCalculatorConfig", dai::DatatypeEnum::SpatialLocationCalculatorConfig)
+        .value("SpatialLocationCalculatorData", dai::DatatypeEnum::SpatialLocationCalculatorData)
+        .value("EdgeDetectorConfig", dai::DatatypeEnum::EdgeDetectorConfig)
+        .value("AprilTagConfig", dai::DatatypeEnum::AprilTagConfig)
+        .value("AprilTags", dai::DatatypeEnum::AprilTags)
+        .value("Tracklets", dai::DatatypeEnum::Tracklets)
+        .value("IMUData", dai::DatatypeEnum::IMUData)
+        .value("StereoDepthConfig", dai::DatatypeEnum::StereoDepthConfig)
+        .value("FeatureTrackerConfig", dai::DatatypeEnum::FeatureTrackerConfig)
+        .value("ToFConfig", dai::DatatypeEnum::ToFConfig)
+        .value("TrackedFeatures", dai::DatatypeEnum::TrackedFeatures)
+        .value("BenchmarkReport", dai::DatatypeEnum::BenchmarkReport)
+        .value("MessageGroup", dai::DatatypeEnum::MessageGroup);
+
+    // structs
     value_object<dai::ImgDetection>("ImgDetection")
         .field("xmin", &dai::ImgDetection::xmin)
         .field("xmax", &dai::ImgDetection::xmax)
         .field("label", &dai::ImgDetection::label);
-    class_<dai::ImgDetections>("ImgDetections").property("detections", &dai::ImgDetections::detections);
+
+    // classes
+    class_<dai::ImgFrame>("ImgFrame")
+        .smart_ptr_constructor("ImgFrame", &std::make_shared<dai::ImgFrame>)
+        .property("width", &dai::ImgFrame::getWidth)
+        .property("height", &dai::ImgFrame::getHeight);
+    class_<dai::ImgDetections>("ImgDetections")
+        .smart_ptr_constructor("ImgDetections", &std::make_shared<dai::ImgDetections>)
+        .property("detections", &dai::ImgDetections::detections);
+
+    // module global functions
     function("deserializeImgFrame", &daiDeserializeFromJS<dai::ImgFrame>);
     function("deserializeImgDetections", &daiDeserializeFromJS<dai::ImgDetections>);
-    register_vector<std::uint8_t>("Uint8Vector");
-    register_vector<dai::ImgDetection>("ImgDetectionVector");
+    function("getMessageType", &daiGetMessageType);
 }
 
--- bindings/js/example/serialize.cpp
+++ bindings/js/example/serialize.cpp
@@ -2,16 +2,21 @@
 
 // include depthai library
 #include <depthai/depthai.hpp>
+#include <depthai/pipeline/datatype/StreamMessageParser.hpp>
 
 // include opencv library (Optional, used only for the following example)
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 
-void writeDataToFile(const char* filename, const std::vector<std::uint8_t>& data) {
+template <typename T>
+void writeMessageToFile(const T& message, const char* filename) {
+    const auto metadata = dai::StreamMessageParser::serializeMetadata(message);
+    const auto data = message->getData();
     ofstream myfile;
     myfile.open(filename, ios::out | ios::binary);
     myfile.write(reinterpret_cast<const char*>(data.data()), data.size() * sizeof(data.front()));
+    myfile.write(reinterpret_cast<const char*>(metadata.data()), metadata.size() * sizeof(metadata.front()));
     myfile.close();
 }
 
@@ -36,15 +41,7 @@ int main() {
             // Receive 'preview' frame from device
             auto imgFrame = preview->get<dai::ImgFrame>();
             std::vector<std::uint8_t> data;
-            dai::utility::serialize(*imgFrame, data);
-
-            dai::ImgFrame imgFrame2;
-            const auto rc = dai::utility::deserialize(data, imgFrame2);
-            if(!rc) {
-                std::cout << "Deserialization failed" << std::endl;
-            }
-
-            writeDataToFile("frame.dump", data);
+            writeMessageToFile(imgFrame, "img_frame.dump");
 
             // Show the received 'preview' frame
             cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
