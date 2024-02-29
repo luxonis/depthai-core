207e779db7982d1573e1791043034e172314d424
zrezke <jeretina.filip@gmail.com>
2024-02-29 13:43:49 +0100

    [FW] R3 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d86f2efa04753e0e47c153db678753afa9a630a0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f8c77feccd4698b9217f119ddce3d9d2afd77f1b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
