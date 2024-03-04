991144d36b7b712c7a885c3b87af8c8bb312330f
alex-luxonis <alex@luxonis.com>
2024-03-05 00:55:39 +0200

    FW: -fix `setAutoExposureLimit` flicker during AF lens move, - IMX214 4K resolution timings were slightly off, also bump max FPS for it (up to 34), - VideoEncoder fix artifacts for multiple monochrome streams with different resolutions (OV9282 and AR0234), - fix potential crash with Script GPIO.setInterrupt with too high priority (<= 15, low value = high prio)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "04f4eb23f73013d5dcbec036e936f6193fd119c0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2a663257d5e41ad42f26003873b1980cf271806d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
