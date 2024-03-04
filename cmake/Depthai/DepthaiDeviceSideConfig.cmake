f2c83a13f0fed5b34dcc907c49da080fea675776
Filip Jeretina <59307111+zrezke@users.noreply.github.com>
2024-03-04 21:49:15 +0100

    Enhance camera features (#973)
    * Improve reporting of camera features. Added calibrationResolution and added fov rect to CameraSensorConfig

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cc5f1c3599470968a8dfecc47cca2689f73a0729")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "04f4eb23f73013d5dcbec036e936f6193fd119c0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
