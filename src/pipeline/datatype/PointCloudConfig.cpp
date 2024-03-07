045b53d3daaa14b6190c30d4de5144171a4e2689
Matevz Morato <matevz.morato@gmail.com>
2024-03-07 14:50:16 +0100

    Fix compiler warning

--- src/pipeline/datatype/PointCloudConfig.cpp
+++ src/pipeline/datatype/PointCloudConfig.cpp
@@ -37,22 +37,10 @@ PointCloudConfig& PointCloudConfig::setTransformationMatrix(const std::array<std
 }
 
 PointCloudConfig& PointCloudConfig::setTransformationMatrix(const std::array<std::array<float, 3>, 3>& transformationMatrix) {
-    cfg.transformationMatrix = {transformationMatrix[0][0],
-                                transformationMatrix[0][1],
-                                transformationMatrix[0][2],
-                                0,
-                                transformationMatrix[1][0],
-                                transformationMatrix[1][1],
-                                transformationMatrix[1][2],
-                                0,
-                                transformationMatrix[2][0],
-                                transformationMatrix[2][1],
-                                transformationMatrix[2][2],
-                                0,
-                                0,
-                                0,
-                                0,
-                                1};
+    cfg.transformationMatrix = {{{transformationMatrix[0][0], transformationMatrix[0][1], transformationMatrix[0][2], 0},
+                                 {transformationMatrix[1][0], transformationMatrix[1][1], transformationMatrix[1][2], 0},
+                                 {transformationMatrix[2][0], transformationMatrix[2][1], transformationMatrix[2][2], 0},
+                                 {0, 0, 0, 1}}};
     return *this;
 }
 
