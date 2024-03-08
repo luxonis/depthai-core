84e8a9c7cce036d9cc55191ddd6b442cd7ee3262
Matevz Morato <matevz.morato@gmail.com>
2024-03-08 11:45:45 +0100

    Update FW to develop

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "16a281fb85eb543c9ecf70d661fbbe01e4516113")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7665aed8712bb941958660a0103868908726eb56")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
