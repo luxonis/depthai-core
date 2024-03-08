272be36cd56101cb02719f6525615dc79d853e0e
alex-luxonis <alex@luxonis.com>
2024-03-08 14:14:04 +0200

    TMP FW with more logging for bad-packet debugging. Fix ImageManip potential OsDrvCpr crash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d86f2efa04753e0e47c153db678753afa9a630a0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "00911c9808f8036c260149049010a29abc46d692")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
