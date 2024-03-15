2972c50cdaa7b7ddef99e2925d4b93eddaf3e6d3
Jakob Socan <10517617+jakgra@users.noreply.github.com>
2024-03-15 18:46:09 +0100

    Added advanced NNArchive example

--- examples/NNArchive/nn_archive.cpp
+++ examples/NNArchive/nn_archive.cpp
@@ -22,9 +22,43 @@ static const std::vector<std::string> labelMap = {
 
 static const std::atomic<bool> syncNN{true};
 
-int main(int argc, char** argv) {
-    using namespace std;
-    using namespace std::chrono;
+void initializeNNArchive(const std::string& exampleType,    // NOLINT(bugprone-easily-swappable-parameters)
+                         const std::string& nnArchivePath,  // NOLINT(bugprone-easily-swappable-parameters)
+                         const std::shared_ptr<dai::node::ColorCamera>& camRgb,
+                         const std::shared_ptr<dai::node::DetectionNetwork>& detectionNetwork) {
+    if(exampleType != "advanced") {
+        camRgb->setPreviewSize(640, 640);
+    }
+
+    if(exampleType == "fs") {
+        detectionNetwork->setNNArchive(dai::NNArchive(nnArchivePath));
+    } else if(exampleType == "memory") {
+        std::ifstream archiveFile(nnArchivePath, std::ios::binary);
+        std::vector<uint8_t> fileContents((std::istreambuf_iterator<char>(archiveFile)), std::istreambuf_iterator<char>());
+        detectionNetwork->setNNArchive(dai::NNArchive(fileContents));
+    } else if(exampleType == "advanced") {
+        const dai::NNArchiveConfig config(nnArchivePath);
+        const auto& configV1 = config.getConfigV1();
+        if(!configV1) {
+            throw std::runtime_error("Wrong config version");
+        }
+        const auto width = (*configV1).model.inputs[0].shape[2];
+        const auto height = (*configV1).model.inputs[0].shape[3];
+        if(width > 1920 || height > 1080) {
+            // We could decide to load another NNArchive that has a smaller size instead of throwing ...
+            // All without loading / reading to memory the whole blob
+            throw std::runtime_error("Sorry that's to big");
+        }
+        camRgb->setPreviewSize(static_cast<int>(width), static_cast<int>(height));
+        detectionNetwork->setNNArchive(dai::NNArchive(config, dai::NNArchiveBlob(config, nnArchivePath)));
+    } else {
+        throw std::runtime_error("Not implemented yet");
+    }
+}
+
+int main(int argc, char** argv) {  // NOLINT
+    using namespace std;           // NOLINT
+    using namespace std::chrono;   // NOLINT
 
     auto args = dai::span(argv, static_cast<size_t>(argc));
     if(args.size() < 3) {
@@ -54,25 +88,12 @@ int main(int argc, char** argv) {
     xoutRgb->setStreamName("rgb");
     nnOut->setStreamName("detections");
 
-    // Properties
-    // TODO(jakgra) warn / fail fast if inputs don't match the ones specified in the NNArchive .json file???
-    // camRgb->setPreviewSize(416, 416);
-    camRgb->setPreviewSize(640, 640);
     camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
-    camRgb->setInterleaved(false);
-    camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);
+    camRgb->setInterleaved(false);                                       // NOLINT
+    camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);  // NOLINT
     camRgb->setFps(40);
 
-    if(exampleType == "fs") {
-        detectionNetwork->setNNArchive(dai::NNArchive(nnArchivePath));
-    } else if(exampleType == "memory") {
-        std::ifstream archiveFile(nnArchivePath, std::ios::binary);
-        std::vector<uint8_t> fileContents((std::istreambuf_iterator<char>(archiveFile)), std::istreambuf_iterator<char>());
-        detectionNetwork->setNNArchive(dai::NNArchive(fileContents));
-    } else {
-        throw std::runtime_error("Not implemented yet");
-    }
-
+    initializeNNArchive(exampleType, nnArchivePath, camRgb, detectionNetwork);
     // TODO(jakgra)
     // Will we get this from NN Archive also?
     detectionNetwork->setNumInferenceThreads(2);
@@ -103,14 +124,14 @@ int main(int argc, char** argv) {
     auto color2 = cv::Scalar(255, 255, 255);
 
     // Add bounding boxes and text to the frame and show it to the user
-    auto displayFrame = [](std::string name, cv::Mat frame, std::vector<dai::ImgDetection>& detections) {
+    auto displayFrame = [](const std::string& name, cv::Mat frame, std::vector<dai::ImgDetection>& detections) {
         auto color = cv::Scalar(255, 0, 0);
         // nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
         for(auto& detection : detections) {
-            int x1 = detection.xmin * frame.cols;
-            int y1 = detection.ymin * frame.rows;
-            int x2 = detection.xmax * frame.cols;
-            int y2 = detection.ymax * frame.rows;
+            int x1 = detection.xmin * frame.cols;  // NOLINT
+            int y1 = detection.ymin * frame.rows;  // NOLINT
+            int x2 = detection.xmax * frame.cols;  // NOLINT
+            int y2 = detection.ymax * frame.rows;  // NOLINT
 
             uint32_t labelIndex = detection.label;
             std::string labelStr = to_string(labelIndex);
@@ -143,7 +164,7 @@ int main(int argc, char** argv) {
         auto currentTime = steady_clock::now();
         auto elapsed = duration_cast<duration<float>>(currentTime - startTime);
         if(elapsed > seconds(1)) {
-            fps = counter / elapsed.count();
+            fps = static_cast<float>(counter) / elapsed.count();
             counter = 0;
             startTime = currentTime;
         }
@@ -152,7 +173,7 @@ int main(int argc, char** argv) {
             frame = inRgb->getCvFrame();
             std::stringstream fpsStr;
             fpsStr << "NN fps: " << std::fixed << std::setprecision(2) << fps;
-            cv::putText(frame, fpsStr.str(), cv::Point(2, inRgb->getHeight() - 4), cv::FONT_HERSHEY_TRIPLEX, 0.4, color2);
+            cv::putText(frame, fpsStr.str(), cv::Point(2, static_cast<int>(inRgb->getHeight()) - 4), cv::FONT_HERSHEY_TRIPLEX, 0.4, color2);
         }
 
         if(inDet) {
