12766a70b0e02bf599fa97096163996af06b5418
alex-luxonis <alex@luxonis.com>
2024-03-19 19:13:52 +0200

    FW: fix fsync for OAK-D-SR

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7665aed8712bb941958660a0103868908726eb56")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e9f7c1cde3be3471cf7185af2bcb5a66eba57227")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
