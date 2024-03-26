a8ea8d25b2620bfa42a942cc6ad80d1cc9ecc21e
SzabolcsGergely <szabi@luxonis.com>
2024-03-26 19:55:08 +0100

    Update Cast node with fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f8762f81bf92bec6aed5446b70eba133d385e45b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "605494049fdc0660b900ea660e60753e24ddc8d6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
