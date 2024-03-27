b1dafed9c013938178b991fb5f2d900bf20df4aa
Jakob Socan <10517617+jakgra@users.noreply.github.com>
2024-03-27 15:21:20 +0100

    Started test for different resolutions

---
+++ tests/src/resolutions_test.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <iterator>
+#include <stdexcept>
+
+// Libraries
+#include <catch2/catch_all.hpp>
+
+// Includes common necessary includes for development using depthai library
+#include "depthai/common/CameraBoardSocket.hpp"
+#include "depthai/common/CameraSensorType.hpp"
+#include "depthai/depthai.hpp"
+#include "depthai/pipeline/Pipeline.hpp"
+#include "depthai/properties/ColorCameraProperties.hpp"
+
+/*
+The code is the same as for Tiny-yolo-V3, the only difference is the blob file.
+The blob was compiled following this tutorial: https://github.com/TNTWEN/OpenVINO-YOLOV4
+*/
+
+// TODO fix so all resolutions work
+struct Res {
+    int32_t width;
+    int32_t height;
+};
+static const std::vector<Res> notWorkingResolutions = {
+    {.width = 1352, .height = 1012}  // dai::ColorCameraProperties::SensorResolution::THE_1352X1012
+};
+
+dai::ColorCameraProperties::SensorResolution resolutionFromSensorConfig(const dai::CameraSensorConfig& config) {
+    const auto width = config.width;
+    const auto height = config.height;
+    using R = dai::ColorCameraProperties::SensorResolution;
+    if(width == 1920 && height == 1080) {
+        return R::THE_1080_P;
+    } else if(width == 3840 && height == 2160) {
+        return R::THE_4_K;
+    } else if(width == 4056 && height == 3040) {
+        return R::THE_12_MP;
+    } else if(width == 4208 && height == 3120) {
+        return R::THE_13_MP;
+    } else if(width == 1280 && height == 720) {
+        return R::THE_720_P;
+    } else if(width == 1280 && height == 800) {
+        return R::THE_800_P;
+    } else if(width == 1920 && height == 1200) {
+        return R::THE_1200_P;
+    } else if(width == 2592 && height == 1944) {
+        return R::THE_5_MP;
+    } else if(width == 4000 && height == 3000) {
+        return R::THE_4000X3000;
+    } else if(width == 5312 && height == 6000) {
+        return R::THE_5312X6000;
+    } else if(width == 8000 && height == 6000) {
+        return R::THE_48_MP;
+    } else if(width == 240 && height == 180) {
+        return R::THE_240X180;
+    } else if(width == 1280 && height == 962) {
+        return R::THE_1280X962;
+    } else if(width == 2000 && height == 1500) {
+        return R::THE_2000X1500;
+    } else if(width == 2028 && height == 1520) {
+        return R::THE_2028X1520;
+    } else if(width == 2104 && height == 1560) {
+        return R::THE_2104X1560;
+    } else if(width == 1440 && height == 1080) {
+        return R::THE_1440X1080;
+    } else if(width == 1352 && height == 1012) {
+        return R::THE_1352X1012;
+    } else if(width == 2024 && height == 1520) {
+        return R::THE_2024X1520;
+    } else {
+        throw std::runtime_error("Unknown resolution " + std::to_string(width) + "x" + std::to_string(height) + " requested");
+    }
+}
+
+dai::CameraSensorConfig getClosestCameraConfig(dai::Device& device, int64_t width, int64_t height) {
+    const auto& cameraFeatures = device.getConnectedCameraFeatures();
+    const auto& camera = std::find_if(
+        cameraFeatures.begin(), cameraFeatures.end(), [](const dai::CameraFeatures& itr) -> bool { return itr.socket == dai::CameraBoardSocket::CAM_A; });
+    if(camera == cameraFeatures.end()) {
+        throw std::runtime_error("Device doesn't support ColorCamera");
+    }
+    std::vector<dai::CameraSensorConfig> colorCameraModes;
+    std::copy_if(camera->configs.begin(), camera->configs.end(), std::back_inserter(colorCameraModes), [](const auto& itr) {
+        return itr.type == dai::CameraSensorType::COLOR;
+    });
+    int64_t minAdditionalPixels = -1;
+    ssize_t foundIndex = -1;
+    ssize_t index = 0;
+    for(const auto& mode : colorCameraModes) {
+        if(mode.width >= width && mode.height >= height) {
+            if(std::find_if(notWorkingResolutions.begin(),
+                            notWorkingResolutions.end(),
+                            [&mode = std::as_const(mode)](const auto& itr) { return itr.width == mode.width && itr.height == mode.height; })
+               != notWorkingResolutions.end()) {
+                std::cout << "Warning: ignoring possible best resolution " << mode.width << "x" << mode.height << " because of possible firmware bugs"
+                          << std::endl;
+            } else {
+                int64_t additionalPixels = (mode.width - width) * mode.height + (mode.height - height) * mode.width;
+                if(minAdditionalPixels == -1 || additionalPixels < minAdditionalPixels) {
+                    foundIndex = index;
+                    minAdditionalPixels = additionalPixels;
+                }
+            }
+        }
+        ++index;
+    }
+    if(minAdditionalPixels == -1 || foundIndex == -1) {
+        throw std::runtime_error("This camera can't provide the wanted resolution" + std::to_string(width) + "x" + std::to_string(height));
+    }
+    return colorCameraModes[foundIndex];
+}
+
+TEST_CASE("resolutions") {
+    dai::Pipeline pipeline;
+
+    auto camRgb = pipeline.create<dai::node::ColorCamera>();
+
+    camRgb->setInterleaved(false);                                       // NOLINT
+    camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::BGR);  // NOLINT
+    camRgb->setFps(40);
+
+    const long width = 444;
+    const auto height = 888;
+
+    const auto device = pipeline.getDefaultDevice();
+    if(!device) {
+        throw std::runtime_error("The pipeline was without device");
+    }
+    const auto& mode = getClosestCameraConfig(*device, width, height);
+    std::cout << "FOUND CLOSEST RESOLUTION: " << mode.width << "x" << mode.height << std::endl;
+    std::cout << "SETTING PREVIEW SIZE TO: " << width << "x" << height << std::endl;
+    camRgb->setResolution(resolutionFromSensorConfig(mode));
+    const auto useWidth = (double)width / (double)mode.width > (double)height / (double)mode.height;
+    int numerator = useWidth ? width : height;
+    int denominator = useWidth ? mode.width : mode.height;
+    const auto div = std::gcd(numerator, denominator);
+    numerator = numerator / div;
+    denominator = denominator / div;
+    // 16 is the max numerator and 63 the max denominator
+    if(numerator > 16 || denominator > 63) {
+        const double scale = (double)numerator / (double)denominator;
+        if(scale > 16.0 / 63.0) {
+            numerator = 16;
+            denominator = std::floor(scale / 16.0);
+        } else {
+            numerator = std::ceil(scale * 63.0);
+            denominator = 63;
+        }
+    }
+    std::cout << "USING ISP SCALE " << numerator << "/" << denominator << std::endl;
+    camRgb->setIspScale(numerator, denominator);
+    camRgb->setPreviewSize(static_cast<int>(width), static_cast<int>(height));
+
+    const auto queueFrames = camRgb->raw.getQueue();
+
+    // Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    const auto& qRgb = queueFrames;
+    pipeline.start();
+
+    for(int counter = 0; counter < 10; ++counter) {
+        const auto inRgb = qRgb->get<dai::ImgFrame>();
+        if(inRgb) {
+            REQUIRE(inRgb->getWidth() == width);
+            REQUIRE(inRgb->getHeight() == height);
+        }
+        counter++;
+    }
+    pipeline.stop();
+    pipeline.wait();
+}
