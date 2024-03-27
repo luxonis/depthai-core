10e6107cbfd61c205e0eb7820e020410ca29960a
SzabolcsGergely <szabi@luxonis.com>
2024-03-27 12:26:13 +0100

    Handle multi input NN

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "605494049fdc0660b900ea660e60753e24ddc8d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f134c967666e57000a11ba9dfffc6ddea3c5ef97")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
