1e57cb6ea83bd77d4efbd04a1b50b767199a1b46
SzabolcsGergely <szabi@luxonis.com>
2024-04-01 20:43:02 +0300

    Handle edge cases; unsupported distortion coefficients

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bdc411045d1ca178466971c9624a5122870f16d0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8002bb03e3176d8f94f69aceb9e227804818d969")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
