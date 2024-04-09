361d5d6f97b3d0a323c041cc6ca91ef378114562
SzabolcsGergely <szabi@luxonis.com>
2024-04-10 01:03:42 +0300

    Update phase unwrapping, add configurable levels

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "afc2923cc0d951335c56d1944a02a00daa41157e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e0739d876920b06c2d30f5f2d72e500eed8791e2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
