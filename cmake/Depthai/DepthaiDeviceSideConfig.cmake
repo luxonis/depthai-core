40721447193748b6214546e7e2429d98feffaaea
SzabolcsGergely <szabi@luxonis.com>
2024-04-09 22:56:15 +0300

    Add phase unwrapping

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "76606927e89c7b6eaa317d9b7fc80bc26376aaa6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "afc2923cc0d951335c56d1944a02a00daa41157e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
