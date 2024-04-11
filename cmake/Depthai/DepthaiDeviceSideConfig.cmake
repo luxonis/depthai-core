00306cdb1329e63151609554239c02a2f56460e0
alex-luxonis <alex@luxonis.com>
2024-04-11 17:23:38 +0300

    FW: fixes for IMX378 and IMX582: fix concurrent run, fix scaling with IMX378 THE_1352X1012 resolution, change Camera node best sensor config select to prioritize matching aspect ratio

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c58546bf837efe2ffc1befb3dffc7d2c0a69fdec")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "72cee6bc0a4c984cfe7eede3f482269568f6fe2c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
