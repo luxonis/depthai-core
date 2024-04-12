a9d80ac259bbd01d3786c476addaae6a722ec0ef
SzabolcsGergely <szabi@luxonis.com>
2024-04-12 20:54:59 +0300

    Optimized decoding pipeline: 60->27 ms

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e0739d876920b06c2d30f5f2d72e500eed8791e2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d9ff8cb9ba4031e2f54f789e363894c23c5804b1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
