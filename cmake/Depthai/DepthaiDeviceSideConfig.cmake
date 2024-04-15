9a4892e8c27f0abd82f95e666e6b08ead1c9e621
SzabolcsGergely <szabi@luxonis.com>
2024-04-15 20:05:55 +0300

    Add multi shave support for ToF decoding

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d9ff8cb9ba4031e2f54f789e363894c23c5804b1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d48ee8b20f9372712cb945761ef9b522789e00fe")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- src/pipeline/node/ToF.cpp
+++ src/pipeline/node/ToF.cpp
@@ -7,12 +7,22 @@ ToF::ToF(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId) : ToF(par, no
 ToF::ToF(const std::shared_ptr<PipelineImpl>& par, int64_t nodeId, std::unique_ptr<Properties> props)
     : NodeCRTP<Node, ToF, ToFProperties>(par, nodeId, std::move(props)), rawConfig(std::make_shared<RawToFConfig>()), initialConfig(rawConfig) {
     setInputRefs({&inputConfig, &input});
-    setOutputRefs({&depth, &amplitude, &intensity, &phase, &error});
+    setOutputRefs({&depth, &amplitude, &intensity, &phase});
 }
 
 ToF::Properties& ToF::getProperties() {
     properties.initialConfig = *rawConfig;
     return properties;
+}
+
+ToF& ToF::setNumShaves(int numShaves) {
+    properties.numShaves = numShaves;
+    return *this;
+}
+
+ToF& ToF::setNumFramesPool(int numFramesPool) {
+    properties.numFramesPool = numFramesPool;
+    return *this;
 }
 
 }  // namespace node
