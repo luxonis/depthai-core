e9816712817b2181e6eeb332faf08f7e227d67a1
asahtik <asahtik@gmail.com>
2024-04-15 13:07:30 +0200

    Make PCL include optional

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8ca0de8717a3fdf1cc5a295b1610b06a032c0691")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2219cc49d51590e0e2de79a5b3ff22d0c5844869")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
