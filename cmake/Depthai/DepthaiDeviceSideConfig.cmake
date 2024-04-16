c32ffd1c29d7148f657c01c5a50fae4c4c9dd43c
alex-luxonis <alex@luxonis.com>
2024-04-16 03:01:58 +0300

    FW: some fixes for OAK-D-SR-PoE testing

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d9ca71415a4bbf875181285c4116d2e0655be387")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "369288cddb7ce04fc530c2cce9468b40168657a4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
