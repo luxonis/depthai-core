a09c3ea1391c0ec89e79aa7e21ef067e6a34c2e6
SzabolcsGergely <szabi@luxonis.com>
2024-04-16 18:07:50 +0300

    Update ToF decoding: handle arbitrary sensor rotation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4babf2323b442e69acc3941da04008c429fd5e33")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a9d9eab89011c71321cc63667d042367b3933bee")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
