5a2db83df063f8b12363f6b31d291cb4df77e293
alex-luxonis <alex@luxonis.com>
2024-04-16 18:09:25 +0300

    FW: fix default fsync GPIO state for OAK-FFC-4P R7, FSIN_4LANE GPIO42 = input, pull-down

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0e15bb23f924c195fd8a891043265a9b20ec4531")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "729be280c478a321676b457aa8514179f66bb2e3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
