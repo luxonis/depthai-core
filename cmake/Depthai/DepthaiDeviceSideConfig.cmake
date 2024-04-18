a588da9f9263b0a92498ba61b5fa97648b7a959c
SzabolcsGergely <szabi@luxonis.com>
2024-04-18 17:29:16 +0300

    Add phaseUnwrappingErrorThreshold config

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a9d9eab89011c71321cc63667d042367b3933bee")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c17f369d4abd81df2ec75c35ae28f21d995b9bae")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
