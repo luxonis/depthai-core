db9cf639b9e10b5492c86142fbd38d83b8bb5cad
Zimamazim (Martin Zimen) <zimenm@seznam.cz>
2024-04-23 13:54:52 +0200

    Host node sugaring

--- bindings/python/src/pipeline/node/HostNodeBindings.cpp
+++ bindings/python/src/pipeline/node/HostNodeBindings.cpp
@@ -3,6 +3,8 @@
 #include "depthai/pipeline/ThreadedHostNode.hpp"
 #include "depthai/pipeline/node/host/HostNode.hpp"
 
+#include <pybind11/eval.h>
+
 extern py::handle daiNodeModule;
 
 using namespace dai;
@@ -17,8 +19,8 @@ class PyThreadedHostNode : public NodeCRTP<ThreadedHostNode, PyThreadedHostNode>
 
 class PyHostNode : public NodeCRTP<HostNode, PyHostNode> {
    public:
-    std::shared_ptr<Buffer> runOnce(std::shared_ptr<dai::MessageGroup> in) override {
-        PYBIND11_OVERRIDE_PURE(std::shared_ptr<Buffer>, HostNode, runOnce, in);
+    std::shared_ptr<Buffer> _process(std::shared_ptr<dai::MessageGroup> in) override {
+        PYBIND11_OVERRIDE_PURE(std::shared_ptr<Buffer>, HostNode, _process, in);
     }
 };
 
@@ -44,9 +46,56 @@ void bind_hostnode(pybind11::module& m, void* pCallstack){
     threadedHostNode.def(py::init<>()).def("run", &ThreadedHostNode::run);
 
     hostNode.def(py::init<>())
-        .def("runOnce", &HostNode::runOnce)
+        .def("_process", &HostNode::_process)
         .def_property_readonly(
             "inputs", [](HostNode& node) { return &node.inputs; }, py::return_value_policy::reference_internal)
         .def("runSyncingOnHost", &HostNode::runSyncingOnHost, DOC(dai, node, HostNode, runSyncingOnHost))
         .def("runSyncingOnDevice", &HostNode::runSyncingOnDevice, DOC(dai, node, HostNode, runSyncingOnDevice));
+
+    py::exec(R"(
+        import inspect
+        import typing
+        #import depthai
+        def __init_subclass__(cls):
+            members = dict(inspect.getmembers(cls))
+            # TODO Should this be an assert or return?
+            assert "process" in members, "Subclass of HostNode must define method 'process'"
+            sig = inspect.signature(members["process"])
+            assert list(sig.parameters.keys())[0] == "self", \
+                'Please use "self" as the first parameter for process method'
+
+            cls.input_desc = {}
+            for name, param in sig.parameters.items():
+                if name == "self": continue
+                annotation = param.annotation
+                if annotation == inspect.Parameter.empty:
+                    annotation = typing.Any
+                cls.input_desc[name] = annotation
+
+            cls.output_desc = sig.return_annotation
+            if cls.output_desc == inspect.Signature.empty:
+                cls.output_desc = None
+
+            def _process(self, messageGroup):
+                rv = members["process"](self, 
+                    *(messageGroup[argname] for argname in cls.input_desc.keys()))
+                if cls.output_desc == None:
+                    assert rv == None, "Expected non-None return value from 'process'"
+                    return
+                if isinstance(cls.output_desc, dict):
+                    assert isinstance(rv, dict), "Expected return value from 'process' to be a dictionary"
+                    raise NotImplementedError()
+                else:
+                    #assert isinstance(rv, #TODO assert
+                    return rv
+            cls._process = _process
+
+            def build(self, *args):
+                for name, arg in zip(cls.input_desc, args):
+                    arg.link(self.inputs[name])
+                return self
+            cls.build = build
+
+        HostNode.__init_subclass__ = classmethod(__init_subclass__)
+    )", daiNodeModule.attr("__dict__"));
 }
