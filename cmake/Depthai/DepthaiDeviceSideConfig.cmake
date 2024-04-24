99e5c2bb90b499f8c1fe3c56952c5d295a7617a6
alex-luxonis <alex@luxonis.com>
2024-04-24 12:40:03 +0300

    FW: fix for SR-PoE R3 detection

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "683108f71048e601f0dbf554dbcd2a68b760ed91")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2194e30b2e51b3229b50fcd0f14720d65be898c5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
