8b326422b876d393bacfc6cb1aeb5183d99050c0
alex-luxonis <alex@luxonis.com>
2024-04-26 12:58:56 +0300

    FW: fsync shift on custom board

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4daf300cb102ae50ee4b4e14b6e6998cf6c4a382")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2acb11cd583f02b48b84fa6b4db6a85f3171f82a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
