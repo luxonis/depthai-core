6a0c468ac019d161b04800354473cdb7465e2c73
Jakob Socan <jakob@reeba.io>
2024-04-29 18:11:58 +0200

    Added resizeMode to image capabilities

--- include/depthai/capabilities/ImgFrameCapability.hpp
+++ include/depthai/capabilities/ImgFrameCapability.hpp
@@ -11,6 +11,24 @@
 
 namespace dai {
 
+enum struct ImgResizeMode {
+    /**
+     * Doesn't keep aspect ratio.
+     * Squishes or streches the image to fill the required pixel area.
+     */
+    FILL,
+    /**
+     * Keeps aspect ratio.
+     * Envelops the image with a background color to get the corect output aspect ratio.
+     */
+    CONTAIN,
+    /**
+     * Keeps aspect ratio.
+     * Crops the image to get the correct output aspect ratio.
+     */
+    COVER
+};
+
 class ImgFrameCapability : public CapabilityCRTP<Capability, ImgFrameCapability> {
    public:
     constexpr static const char* NAME = "dai/img-frame";
@@ -19,7 +37,7 @@ class ImgFrameCapability : public CapabilityCRTP<Capability, ImgFrameCapability>
     CapabilityRange<std::tuple<uint32_t, uint32_t>> size;
     CapabilityRange<uint32_t> fps;
     std::optional<ImgFrame::Type> encoding;
-    // envelope / background around
+    ImgResizeMode resizeMode{ImgResizeMode::COVER};
     // TODO(jakgra) add optional CapabilityRange fov / max-min horiz. / vertical crop;
 
    private:
