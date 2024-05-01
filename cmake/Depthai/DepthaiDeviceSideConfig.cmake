f429fa42877741b66892600aa5d55e69d53596d5
SzabolcsGergely <szabi@luxonis.com>
2024-05-01 17:33:06 +0300

    Decode TOF on burst of 4 frames

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2acb11cd583f02b48b84fa6b4db6a85f3171f82a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2b51e435062462ed2ae12cdc8cbe0633d569aa3a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
