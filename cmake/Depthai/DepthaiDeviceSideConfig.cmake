a1773216e42163a9eb050887dab43095e82d29fb
SzabolcsGergely <szabi@luxonis.com>
2024-05-02 22:09:32 +0300

    Update FW: DepthAlign node implementeD

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cc5f1c3599470968a8dfecc47cca2689f73a0729")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4363aedd777aed9f28cd9b062a96e24c63ad823d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
