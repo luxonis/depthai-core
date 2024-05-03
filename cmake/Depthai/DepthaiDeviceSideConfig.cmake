35274e7961c0f40be0ebe431b39a5215800cb9b6
SzabolcsGergely <szabi@luxonis.com>
2024-05-03 10:33:06 +0300

    Update TOF decoding/eeprom content

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1ba40bb61b4aff6db210c9d0973b0f6e54ff9bdb")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d3d9488154b2031cf6832deb4b2bbe15f08772ad")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
