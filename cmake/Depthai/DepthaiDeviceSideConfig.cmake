241ec13300eb59a296c66ee8916612ea9164ec3f
SzabolcsGergely <szabi@luxonis.com>
2024-05-07 18:12:31 +0300

    Added enableBurstMode=false and enablePhaseShuffleTemporalFilter=true options.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d3d9488154b2031cf6832deb4b2bbe15f08772ad")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e31506a9b036ed7e863fdc62b4117f362381fac5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
