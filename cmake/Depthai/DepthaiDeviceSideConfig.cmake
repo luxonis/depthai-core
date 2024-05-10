dc84019a334e1732bb7a81d4395626ac68b0b7f5
SzabolcsGergely <szabi@luxonis.com>
2024-05-10 16:03:53 +0300

    Update FW; 10 bit amplitude, uint16; temperature calibration version check

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "75b99b2d6a429329eb3adebcb1e40f4ef9cc6957")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3aa696f6491871aed67d3076446ddf7e44347898")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
