cca0a8e4a3686098dbd2b56db146cbd57711eb36
asahtik <asahtik@gmail.com>
2024-05-13 14:13:17 +0200

    Removed unnecessary code

--- include/depthai/utility/HolisticRecordReplay.hpp
+++ include/depthai/utility/HolisticRecordReplay.hpp
@@ -17,30 +17,6 @@ bool setupHolisticReplay(Pipeline& pipeline,
                          const std::string& mxId,
                          RecordConfig& recordConfig,
                          std::unordered_map<std::string, std::string>& outFilenames);
-#else
-template <typename... T>
-struct dependent_false {
-    static constexpr bool value = false;
-};
-template <typename... T>
-bool __attribute__((weak)) setupHolisticRecord(Pipeline& pipeline,
-                                               const std::string& mxId,
-                                               RecordConfig& recordConfig,
-                                               std::unordered_map<std::string, std::string>& outFilenames) {
-    // static_assert(dependent_false<T...>::value, "Library not configured with OpenCV support");
-    throw std::runtime_error("Library not configured with OpenCV support");
-    return false;
-}
-template <typename... T>
-bool __attribute__((weak)) setupHolisticReplay(Pipeline& pipeline,
-                                               std::string replayPath,
-                                               const std::string& mxId,
-                                               RecordConfig& recordConfig,
-                                               std::unordered_map<std::string, std::string>& outFilenames) {
-    // static_assert(dependent_false<T...>::value, "Library not configured with OpenCV support");
-    throw std::runtime_error("Library not configured with OpenCV support");
-    return false;
-}
 #endif
 
 }  // namespace utility
