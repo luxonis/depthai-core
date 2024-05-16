5b4b8b8a6e68e764a9c0b765b238e9cfa5b52d94
alex-luxonis <alex@luxonis.com>
2024-05-16 04:50:14 +0300

    FW: fix for MessageGroup parsing after the added end-of-packet marker

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1b395d76effe968b32b44a4019df4cf1892597be")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "90c8ad77377abed19ee1099a80ec3c00f8d7e9e4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
