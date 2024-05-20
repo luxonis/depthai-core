7e50521acf56400fad3444b3db1774f918a674d9
SzabolcsGergely <szabi@luxonis.com>
2024-05-20 20:07:04 +0300

    Update FW; handle edge cases

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ea5692bca917c3b09a4e4381663492b488297c33")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0aec7e2c3c53a18677b339a89fa27233fc48b45d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
