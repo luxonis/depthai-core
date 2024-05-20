acc1ad6f8c5cd4de2578ca4068ca9083f4b3c4f3
alex-luxonis <alex@luxonis.com>
2024-05-20 04:17:48 +0300

    FW: ToF double raw stream FPS internally to get depth at configured FPS, for the default case (non-burst mode). Other ToF changes: - fix timestamps and sequence numbers - reduce device CPU load when ToF streams, depending on FPS - set the proper ToF exposure time in ImgFrame metadata, 796us max - warnings for FPS capping, 80 max recommended, 88 max possible (with reduced depth quality far away)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "29e2341ec6fe1a64ea1d6996493aed2498aefa53")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ea5692bca917c3b09a4e4381663492b488297c33")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
