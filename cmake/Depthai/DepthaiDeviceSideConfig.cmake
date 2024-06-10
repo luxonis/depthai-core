120a6758e510e072f6931f741f79a6c871af5ff8
zrezke <jeretina.filip@gmail.com>
2024-06-10 20:03:29 +0200

    Fix OAK-D-SR stream fail when eeprom is not flashed.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "46df683dbd3d332ce739626810ac57e60dddf922")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3356c229ce7f65f1ba8154391af4dd11d1c54b86")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
