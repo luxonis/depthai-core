08ac4eab3e3c031dfb89a85d33f4c228894179da
alex-luxonis <alex@luxonis.com>
2024-06-12 18:51:40 +0300

    FW: fix BNO086 IMU init failure (not detected) with heavier pipelines, possibly improve IMU performance (FPS) as well - FW thread priority increased

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "afbcd8ce64e3fadb9c981890b31575e635a64322")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "02c904d5d038b6325e67252d2a4a102309f03d2b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
