380b82b82ce276d8ff4f20bee1420a10e16b99cf
Matevz Morato <matevz.morato@gmail.com>
2024-06-17 19:54:58 +0200

    [RVC2 FW] Update stereo with working inputConfig

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5210eb74d850b630cb47f23e36f2a37c00a7feb2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d8dec5be0a408eba67cc9312931a78c2f34002e3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
