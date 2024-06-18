da4bb3c8d7c17affcdfd42ccaf9419c153d33a16
alex-luxonis <alex@luxonis.com>
2024-06-18 05:00:59 +0300

    FW: fix potential restart failure with IMX214 (VCM I2C stuck), fix setRawOutputPacked(False) for MonoCamera

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6981eb7108e5b8802017f1140db01b51655567c0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9822af7082d67b87c5307b9c37ec7f6ab8e1bbce")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
