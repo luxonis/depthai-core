ce570c1229fa7731ecb1985e48fd4efcf7d96dbf
SzabolcsGergely <szabi@luxonis.com>
2024-06-26 17:49:30 +0300

    Update FW: fix Camera node when undistortion is applied

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9822af7082d67b87c5307b9c37ec7f6ab8e1bbce")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2b14e0eb41ef5d76746900fca40edf7ddb0124c7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
