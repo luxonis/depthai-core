fef460a2e9ebec7372b7b5920866079c2b0280e4
SzabolcsGergely <szabi@luxonis.com>
2024-06-27 04:13:29 +0300

    Fix decimation filter + depth alignment to specific socket

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2b14e0eb41ef5d76746900fca40edf7ddb0124c7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e1895d1903cb4bff46698f407966b25ab349ef16")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
