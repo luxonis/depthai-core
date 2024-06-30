4f9d1d79a18b7e4b70f099bb5341bfeea975c52c
kuba <lnotspotl@gmail.com>
2024-06-30 17:22:10 +0200

    Add examples, both C++ and python

--- examples/v3/ModelZoo/model_zoo.cpp
+++ examples/v3/ModelZoo/model_zoo.cpp
@@ -1,14 +1,49 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 
 #include "depthai/depthai.hpp"
 
 int main(int argc, char* argv[]) {
-    dai::Device device;  // Or pipeline.getDefaultDevice() if you have a pipeline
-    dai::NNModelDescription modelDescription = dai::NNModelDescription::fromParameters("ales-test", "rvc2");
 
-    std::cout << "Using model description:\n" << modelDescription << std::endl;
+    dai::Pipeline pipeline(true);
 
-    NNArchive model = dai::getModelFromZoo(modelDescription, true);  // True means use cached model if available
+    // Download model from zoo
+    dai::NNModelDescription modelDescription;
+    modelDescription.modelSlug = "ales-test";
+    modelDescription.platform = pipeline.getDefaultDevice()->getPlatformAsString();
+    std::string modelPath = dai::getModelFromZoo(modelDescription, true);  // True means use cached model if available
+
+    // Color camera node
+    auto camRgb = pipeline.create<dai::node::ColorCamera>();
+    camRgb->setPreviewSize(256, 256);
+    camRgb->setColorOrder(dai::ColorCameraProperties::ColorOrder::RGB);
+    camRgb->setInterleaved(false);
+
+    // Neural network node
+    auto neuralNetwork = pipeline.create<dai::node::NeuralNetwork>();
+    neuralNetwork->setBlobPath(modelPath);
+    neuralNetwork->setNumInferenceThreads(2);
+
+    // Linking
+    camRgb->preview.link(neuralNetwork->input);
+
+    auto nnDetectionQueue = neuralNetwork->out.createQueue();
+    auto nnPassthroughQueue = neuralNetwork->passthrough.createQueue();
+
+    pipeline.start();
+
+    while(pipeline.isRunning()) {
+
+        auto detection = nnDetectionQueue->get<dai::NNData>();
+        auto passthrough = nnPassthroughQueue->get<dai::ImgFrame>();
+
+        // Do something with the data
+        // ...
+
+        std::cout << "Detected" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
 
     return EXIT_SUCCESS;
 }
