9bb6b69656ce61284fc8fc75d982ef686542b20c
Matevz Morato <matevz.morato@gmail.com>
2024-07-01 17:24:22 +0200

    Add holistic semver support

--- cmake/depthaiDependencies.cmake
+++ cmake/depthaiDependencies.cmake
@@ -29,6 +29,7 @@ else()
         hunter_add_package(Backward)
     endif()
     hunter_add_package(libnop)
+    hunter_add_package(semver)
 endif()
 
 # If library was build as static, find all dependencies
@@ -66,6 +67,7 @@ if(NOT CONFIG_MODE OR (CONFIG_MODE AND NOT DEPTHAI_SHARED_LIBS))
         find_package(Backward ${_QUIET} CONFIG REQUIRED)
         unset(STACK_DETAILS_AUTO_DETECT)
     endif()
+    find_package(semver ${_QUIET} CONFIG REQUIRED)
 endif()
 
 # Xtensor
