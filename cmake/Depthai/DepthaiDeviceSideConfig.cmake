92d959756e953055509bfae9a463e31313d7ffaf
SzabolcsGergely <szabi@luxonis.com>
2024-07-01 15:57:27 +0300

    FW: fix watchdog timeout on init

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f67bb3217e39fdf959247ac7d60e22a81911e77")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d5016767c23997b8cfa6f35fb5d3d878cd4211f6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
