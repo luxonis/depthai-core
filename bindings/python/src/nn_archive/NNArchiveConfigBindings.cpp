415cb07a3940856559475f8cdc20dd474628518e
kuba <lnotspotl@gmail.com>
2024-07-02 07:37:43 +0200

    Add NNArchive python bindings

--- bindings/python/src/nn_archive/NNArchiveConfigBindings.cpp
+++ bindings/python/src/nn_archive/NNArchiveConfigBindings.cpp
@@ -1,18 +1,18 @@
-#include "NNArchiveBindings.hpp"
+#include "NNArchiveConfigBindings.hpp"
 
+#include <pybind11/functional.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-#include <pybind11/functional.h>
 
 // depthai
-#include "depthai/nn_archive/NNArchive.hpp"
+#include "depthai/nn_archive/NNArchiveConfig.hpp"
 
 PYBIND11_MAKE_OPAQUE(std::vector<uint8_t>);
 
-void NNArchiveBindings::bind(pybind11::module& m, void* pCallstack) {
+void NNArchiveConfigBindings::bind(pybind11::module& m, void* pCallstack) {
     using namespace dai;
 
-    py::class_<NNArchive> nnArchive(m, "NNArchive", DOC(dai, NNArchive));
+    py::class_<NNArchiveConfig> nnArchiveConfig(m, "NNArchiveConfig", DOC(dai, NNArchiveConfig));
 
     ///////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////
@@ -27,22 +27,19 @@ void NNArchiveBindings::bind(pybind11::module& m, void* pCallstack) {
     ///////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////
 
-    // Bind NNArchive
-    nnArchive
+    nnArchiveConfig
         .def(py::init<const dai::Path&, NNArchiveEntry::Compression>(),
              py::arg("path"),
              py::arg("compression") = NNArchiveEntry::Compression::AUTO,
-             DOC(dai, NNArchive, NNArchive))
+             DOC(dai, NNArchiveConfig, NNArchiveConfig))
         .def(py::init<const std::vector<uint8_t>&, NNArchiveEntry::Compression>(),
              py::arg("data"),
              py::arg("compression") = NNArchiveEntry::Compression::AUTO,
-             DOC(dai, NNArchive, NNArchive))
-        .def(py::init<
-                 const std::function<int()>&,
-                 const std::function<std::shared_ptr<std::vector<uint8_t>>()>&,
-                 const std::function<int64_t(int64_t, NNArchiveEntry::Seek)>&,
-                 const std::function<int64_t(int64_t)>&,
-                 const std::function<int()>&,
-                 NNArchiveEntry::Compression>())
-        ;
+             DOC(dai, NNArchiveConfig, NNArchiveConfig))
+        .def(py::init<const std::function<int()>&,
+                      const std::function<std::shared_ptr<std::vector<uint8_t>>()>&,
+                      const std::function<int64_t(int64_t, NNArchiveEntry::Seek)>&,
+                      const std::function<int64_t(int64_t)>&,
+                      const std::function<int()>&,
+                      NNArchiveEntry::Compression>());
 }
