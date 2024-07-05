9ba92fe1a12675d74fddcac42252ef28289d7b1f
lnotspotl <lnotspotl@gmail.com>
2024-07-05 02:37:23 +0200

    Add test descriptions

--- tests/src/superblob_test.cpp
+++ tests/src/superblob_test.cpp
@@ -1,8 +1,7 @@
 #include <catch2/catch_all.hpp>
-
 #include <depthai/openvino/OpenVINO.hpp>
 
-TEST_CASE("Pipeline node creation, link, unlink and removal") {
+TEST_CASE("Superblob can generate blobs with different number of shaves") {
     dai::OpenVINO::SuperBlob superblob(SUPERBLOB_PATH);
 
     // Generate blobs for all possible number of shaves
@@ -10,8 +9,14 @@ TEST_CASE("Pipeline node creation, link, unlink and removal") {
         dai::OpenVINO::Blob blob = superblob.getBlobWithNumShaves(static_cast<int>(numShaves));
         REQUIRE(blob.numShaves == numShaves);
     }
+}
 
-    // Out of bounds
+TEST_CASE("Superblob throws when invalid number of shaves are requested") {
+    dai::OpenVINO::SuperBlob superblob(SUPERBLOB_PATH);
     REQUIRE_THROWS(superblob.getBlobWithNumShaves(-1));
     REQUIRE_THROWS(superblob.getBlobWithNumShaves(dai::OpenVINO::SuperBlob::NUMBER_OF_PATCHES + 1));
+}
+
+TEST_CASE("Superblob throws when file does not exist") {
+    REQUIRE_THROWS(dai::OpenVINO::SuperBlob("totallyRandomSuperblobName.superblob"));
 }
