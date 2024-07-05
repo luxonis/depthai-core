bf3c90bb433c46346889042bc6f0c367b2bd1cf7
Serafadam <serafadam@gmail.com>
2024-07-05 18:56:21 +0200

    windows fix again

--- src/pipeline/datatype/TransformData.cpp
+++ src/pipeline/datatype/TransformData.cpp
@@ -1,6 +1,10 @@
 #include "depthai/pipeline/datatype/TransformData.hpp"
 #define _USE_MATH_DEFINES
-#include <cmath>
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 namespace dai {
 
