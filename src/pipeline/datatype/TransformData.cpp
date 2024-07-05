3f1bdeffd22795ddb1f9ffe349f12e29963dc544
Serafadam <serafadam@gmail.com>
2024-07-05 23:39:03 +0200

    fix float warnings

--- src/pipeline/datatype/TransformData.cpp
+++ src/pipeline/datatype/TransformData.cpp
@@ -13,16 +13,16 @@ TransformData::TransformData(const Transform& transform):transform(transform) {}
 TransformData::TransformData(const std::array<std::array<double, 4>, 4>& data) :transform({data}) {}
 TransformData::TransformData(double x, double y, double z, double qx, double qy, double qz, double qw) {
     // x,y,z,qx,qy,qz,qw to homography matrix
-    double n = 1.0f/sqrt(qx*qx+qy*qy+qz*qz+qw*qw);
+    double n = 1.0d/sqrt(qx*qx+qy*qy+qz*qz+qw*qw);
     qx *= n;
     qy *= n;
     qz *= n;
     qw *= n;
     transform.matrix = {{
-        {1.0f - 2.0f*qy*qy - 2.0f*qz*qz, 2.0f*qx*qy - 2.0f*qz*qw, 2.0f*qx*qz + 2.0f*qy*qw, x},
-        {2.0f*qx*qy + 2.0f*qz*qw, 1.0f - 2.0f*qx*qx - 2.0f*qz*qz, 2.0f*qy*qz - 2.0f*qx*qw, y},
-        {2.0f*qx*qz - 2.0f*qy*qw, 2.0f*qy*qz + 2.0f*qx*qw, 1.0f - 2.0f*qx*qx - 2.0f*qy*qy, z},
-        {0.0f, 0.0f, 0.0f, 1.0f}
+        {1.0d - 2.0d*qy*qy - 2.0d*qz*qz, 2.0d*qx*qy - 2.0d*qz*qw, 2.0d*qx*qz + 2.0d*qy*qw, x},
+        {2.0d*qx*qy + 2.0d*qz*qw, 1.0d - 2.0d*qx*qx - 2.0d*qz*qz, 2.0d*qy*qz - 2.0d*qx*qw, y},
+        {2.0d*qx*qz - 2.0d*qy*qw, 2.0d*qy*qz + 2.0d*qx*qw, 1.0d - 2.0d*qx*qx - 2.0d*qy*qy, z},
+        {0.0d, 0.0d, 0.0d, 1.0d}
     }};
 
     
@@ -36,7 +36,7 @@ TransformData::TransformData(double x, double y, double z, double roll, double p
         {cy*cp, cy*sp*sr - sy*cr, cy*sp*cr + sy*sr, x},
         {sy*cp, sy*sp*sr + cy*cr, sy*sp*cr - cy*sr, y},
         {-sp, cp*sr, cp*cr, z},
-        {0.0f, 0.0f, 0.0f, 1.0f}
+        {0.0d, 0.0d, 0.0d, 1.0d}
     }};
 }
 
@@ -62,29 +62,29 @@ Quaterniond TransformData::getQuaternion() const {
     double qx, qy, qz, qw;
     double tr = transform.matrix[0][0] + transform.matrix[1][1] + transform.matrix[2][2];
     if(tr > 0){
-        double S = sqrt(tr+1.0f) * 2;
-        qw = 0.25f * S;
+        double S = sqrt(tr+1.0d) * 2;
+        qw = 0.25d * S;
         qx = (transform.matrix[2][1] - transform.matrix[1][2]) / S;
         qy = (transform.matrix[0][2] - transform.matrix[2][0]) / S;
         qz = (transform.matrix[1][0] - transform.matrix[0][1]) / S;
     } else if(transform.matrix[0][0] > transform.matrix[1][1] && transform.matrix[0][0] > transform.matrix[2][2]){
-        double S = sqrt(1.0f + transform.matrix[0][0] - transform.matrix[1][1] - transform.matrix[2][2]) * 2; 
+        double S = sqrt(1.0d + transform.matrix[0][0] - transform.matrix[1][1] - transform.matrix[2][2]) * 2; 
         qw = (transform.matrix[2][1] - transform.matrix[1][2]) / S;
-        qx = 0.25f * S;
+        qx = 0.25d * S;
         qy = (transform.matrix[0][1] + transform.matrix[1][0]) / S;
         qz = (transform.matrix[0][2] + transform.matrix[2][0]) / S;
     } else if(transform.matrix[1][1] > transform.matrix[2][2]){
-        double S = sqrt(1.0f + transform.matrix[1][1] - transform.matrix[0][0] - transform.matrix[2][2]) * 2; 
+        double S = sqrt(1.0d + transform.matrix[1][1] - transform.matrix[0][0] - transform.matrix[2][2]) * 2; 
         qw = (transform.matrix[0][2] - transform.matrix[2][0]) / S;
         qx = (transform.matrix[0][1] + transform.matrix[1][0]) / S;
-        qy = 0.25f * S;
+        qy = 0.25d * S;
         qz = (transform.matrix[1][2] + transform.matrix[2][1]) / S;
     } else {
-        double S = sqrt(1.0f + transform.matrix[2][2] - transform.matrix[0][0] - transform.matrix[1][1]) * 2;
+        double S = sqrt(1.0d + transform.matrix[2][2] - transform.matrix[0][0] - transform.matrix[1][1]) * 2;
         qw = (transform.matrix[1][0] - transform.matrix[0][1]) / S;
         qx = (transform.matrix[0][2] + transform.matrix[2][0]) / S;
         qy = (transform.matrix[1][2] + transform.matrix[2][1]) / S;
-        qz = 0.25f * S;
+        qz = 0.25d * S;
     }
     return Quaterniond(qx, qy, qz, qw);
 }
