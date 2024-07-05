de09a226bcfb5ffd7bd5d51b2c89d80242d11dda
Matevz Morato <matevz.morato@gmail.com>
2024-07-05 17:08:57 +0200

    Fix the version bug for the copy constructor

--- src/device/Version.cpp
+++ src/device/Version.cpp
@@ -19,24 +19,29 @@ semver::prerelease convertPreReleaseToSemver(Version::PreReleaseType type) {
 }
 
 class Version::Impl {
-public:
+   public:
     explicit Impl(const std::string& v) {
         auto posBuild = v.find('+');
 
-        if (posBuild != std::string::npos) {
+        if(posBuild != std::string::npos) {
             buildInfo = v.substr(posBuild + 1);
         }
 
         auto semverStr = v.substr(0, posBuild);
 
-        if (semverStr.empty() || !semver::valid(semverStr)) {
+        if(semverStr.empty() || !semver::valid(semverStr)) {
             throw std::invalid_argument("Invalid version string");
         }
 
         version = semver::version(semverStr);
     }
 
-    Impl(unsigned major, unsigned minor, unsigned patch, const PreReleaseType& type, const std::optional<uint16_t>& preReleaseVersion, const std::string& buildInfo)
+    Impl(unsigned major,
+         unsigned minor,
+         unsigned patch,
+         const PreReleaseType& type,
+         const std::optional<uint16_t>& preReleaseVersion,
+         const std::string& buildInfo)
         : version(major, minor, patch, convertPreReleaseToSemver(type), preReleaseVersion), buildInfo(buildInfo) {}
 
     bool operator==(const Impl& other) const {
@@ -63,17 +68,17 @@ public:
         return buildInfo;
     }
 
-private:
+   private:
     semver::version version;
     std::string buildInfo;
 };
 
 // Definitions of Version member functions
-Version::Version(const std::string& v) : pimpl(std::make_unique<Impl>(v)) {}
+Version::Version(const std::string& v) : pimpl(spimpl::make_impl<Impl>(v)) {}
 
-Version::Version(unsigned major, unsigned minor, unsigned patch, const PreReleaseType& type, const std::optional<uint16_t>& preReleaseVersion, const std::string& buildInfo)
-    : pimpl(std::make_unique<Impl>(major, minor, patch, type, preReleaseVersion, buildInfo)) {}
-
+Version::Version(
+    unsigned major, unsigned minor, unsigned patch, const PreReleaseType& type, const std::optional<uint16_t>& preReleaseVersion, const std::string& buildInfo)
+    : pimpl(spimpl::make_impl<Impl>(major, minor, patch, type, preReleaseVersion, buildInfo)) {}
 
 bool Version::operator==(const Version& other) const {
     return *pimpl == *other.pimpl;
