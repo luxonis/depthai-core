e0e1317a9b6700ca464939e78953e7117d23c0fe
lnotspotl <lnotspotl@gmail.com>
2024-07-05 20:35:31 +0200

    Add python constructor with kwargs for NNModelDescription

--- bindings/python/src/modelzoo/NNModelDescriptionBindings.cpp
+++ bindings/python/src/modelzoo/NNModelDescriptionBindings.cpp
@@ -22,8 +22,24 @@ void NNModelDescriptionBindings::bind(pybind11::module& m, void* pCallstack) {
     ///////////////////////////////////////////////////////////////////////
 
     // Bind DeviceBootloader
-    modelDescription
-        .def(py::init<>(), DOC(dai, NNModelDescription, NNModelDescription))
+    modelDescription.def(py::init<>(), DOC(dai, NNModelDescription, NNModelDescription))
+        .def(py::init([](const std::string& modelSlug,
+                         const std::string& platform,
+                         const std::string& modelVersionSlug,
+                         const std::string& modelInstanceSlug,
+                         const std::string& modelInstanceHash,
+                         const std::string& optimizationLevel,
+                         const std::string& compressionLevel) {
+                 return NNModelDescription{modelSlug, platform, modelVersionSlug, modelInstanceSlug, modelInstanceHash, optimizationLevel, compressionLevel};
+             }),
+             py::arg("modelSlug"),
+             py::arg("platform"),
+             py::arg("modelVersionSlug") = "",
+             py::arg("modelInstanceSlug") = "",
+             py::arg("modelInstanceHash") = "",
+             py::arg("optimizationLevel") = "",
+             py::arg("compressionLevel") = "",
+             DOC(dai, NNModelDescription, NNModelDescription))
         .def_static("fromYamlFile", &NNModelDescription::fromYamlFile, py::arg("yamlPath"), DOC(dai, NNModelDescription, fromYamlFile))
         .def("saveToYamlFile", &NNModelDescription::saveToYamlFile, py::arg("yamlPath"), DOC(dai, NNModelDescription, saveToYamlFile))
         .def("toString", &NNModelDescription::toString, DOC(dai, NNModelDescription, toString))
@@ -34,6 +50,5 @@ void NNModelDescriptionBindings::bind(pybind11::module& m, void* pCallstack) {
         .def_readwrite("modelInstanceSlug", &NNModelDescription::modelInstanceSlug, DOC(dai, NNModelDescription, modelInstanceSlug))
         .def_readwrite("modelInstanceHash", &NNModelDescription::modelInstanceHash, DOC(dai, NNModelDescription, modelInstanceHash))
         .def_readwrite("optimizationLevel", &NNModelDescription::optimizationLevel, DOC(dai, NNModelDescription, optimizationLevel))
-        .def_readwrite("compressionLevel", &NNModelDescription::compressionLevel, DOC(dai, NNModelDescription, compressionLevel))
-        ;
+        .def_readwrite("compressionLevel", &NNModelDescription::compressionLevel, DOC(dai, NNModelDescription, compressionLevel));
 }
