5f5d8033a45c297f67b1261df868e3c22db740a5
SzabolcsGergely <szabi@luxonis.com>
2024-07-08 17:39:56 +0300

    ToF: fix intrinsics/dist coeffs from sensor EEPROM

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d5016767c23997b8cfa6f35fb5d3d878cd4211f6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c20b8936cbde61879a6ad81666cee95eefae9a19")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
