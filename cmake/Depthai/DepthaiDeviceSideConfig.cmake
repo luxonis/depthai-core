acc12f44ea4418153510b40313f71cda65e07630
zrezke <jeretina.filip@gmail.com>
2024-07-09 15:27:03 +0200

    Device side

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5e317d6fec6097b1fa0465b5af8fbb91f3ead0b6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ee5434902333362a142460f231ba17080fc0e46f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
