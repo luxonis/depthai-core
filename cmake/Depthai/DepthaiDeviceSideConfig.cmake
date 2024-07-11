bc9b9db61c33f5073c03f98dfe1ae1f8504da69e
SzabolcsGergely <szabi@luxonis.com>
2024-07-11 19:39:03 +0300

    ToF: add enableDistortionCorrection field

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "65b21bce40bad8ac9b8744122d476629adbd7215")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a1833fbc749bcc680aad34c7d8978c9b0162c035")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
