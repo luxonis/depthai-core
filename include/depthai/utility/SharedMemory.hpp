50d9eb5bb85b2476bc96c302dfc40108105cf6a8
Filippo Mutta <filippo.mutta@gmail.com>
2024-07-12 18:07:50 +0200

    Added wrapper for memfd_create for glibc < 2.27

--- include/depthai/utility/SharedMemory.hpp
+++ include/depthai/utility/SharedMemory.hpp
@@ -16,6 +16,19 @@
 // project
 #include "depthai/utility/Memory.hpp"
 
+// memfd_create wrapper for glibc < 2.27
+#if defined(__unix__) && !defined(__APPLE__)
+#if (__GLIBC__ <= 2) && (__GLIBC_MINOR__ < 27)
+#include <sys/syscall.hpp>
+#define __NR_memfd_create 319
+#define SYS_memfd_create __NR_memfd_create
+
+int memfd_create(const char *name, unsigned int flags) {
+    return syscall(SYS_memfd_create, name, flags);
+}
+#endif
+#endif
+
 namespace dai {
 
 // memory as interface
