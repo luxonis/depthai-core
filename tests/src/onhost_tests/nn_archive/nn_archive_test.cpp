5feef3567a0db4a92f3279676f5677674192b798
kuba <lnotspotl@gmail.com>
2024-07-16 12:10:38 +0200

    Update tests

--- tests/src/onhost_tests/nn_archive/nn_archive_test.cpp
+++ tests/src/onhost_tests/nn_archive/nn_archive_test.cpp
@@ -1,8 +1,12 @@
 #include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <depthai/nn_archive/NNArchive.hpp>
+#include <filesystem>
+#include <memory>
+#include <string>
 
 TEST_CASE("NNArchive loads a BLOB properly") {
-    dai::NNArchive nnArchive(BLOB_PATH);
+    dai::NNArchive nnArchive(BLOB_ARCHIVE_PATH);
 
     // Loaded archive is BLOB
     REQUIRE(nnArchive.getArchiveType() == dai::NNArchiveType::BLOB);
@@ -16,7 +20,7 @@ TEST_CASE("NNArchive loads a BLOB properly") {
 }
 
 TEST_CASE("NNArchive loads a SUPERBLOB properly") {
-    dai::NNArchive nnArchive(SUPERBLOB_PATH);
+    dai::NNArchive nnArchive(SUPERBLOB_ARCHIVE_PATH);
 
     // Loaded archive is SUPERBLOB
     REQUIRE(nnArchive.getArchiveType() == dai::NNArchiveType::SUPERBLOB);
@@ -29,6 +33,43 @@ TEST_CASE("NNArchive loads a SUPERBLOB properly") {
     REQUIRE(!nnArchive.getModelPath().has_value());
 }
 
+namespace {
+class OnnxTestHelper {
+   public:
+    OnnxTestHelper() {
+        srand(time(nullptr));
+        extractFolder = "/tmp/depthai_test_" + std::to_string(rand());
+    }
+
+    ~OnnxTestHelper() {
+        std::filesystem::remove_all(extractFolder);
+    }
+
+    std::string extractFolder;
+};
+}  // namespace
+
 TEST_CASE("NNArchive loads other formats properly") {
-    // TODO: (lnotspot) Implement this once there's a model in artifactory
+    // Used for initialization and cleanup (even if the test fails)
+    std::unique_ptr<OnnxTestHelper> helper = std::make_unique<OnnxTestHelper>();
+    std::string extractFolder = helper->extractFolder;
+
+    dai::NNArchive nnArchive(ONNX_ARCHIVE_PATH, dai::NNArchiveOptions().extractFolder(extractFolder));
+
+    // Loaded archive is ONNX
+    REQUIRE(nnArchive.getArchiveType() == dai::NNArchiveType::OTHER);
+
+    // Returns model path
+    REQUIRE(nnArchive.getModelPath().has_value());
+
+    // Check that the returned path exists
+    std::string modelPath = nnArchive.getModelPath().value();
+    REQUIRE(std::filesystem::exists(modelPath));
+
+    // Check that it has the .onnx extension
+    REQUIRE(modelPath.substr(modelPath.size() - 5) == ".onnx");
+
+    // Returns nothing for other types
+    REQUIRE(!nnArchive.getBlob().has_value());
+    REQUIRE(!nnArchive.getSuperBlob().has_value());
 }
