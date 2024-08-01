9aedbd97c1fe66c6914d609391f464a12658d4f0
Matevz Morato <matevz.morato@gmail.com>
2024-08-01 13:04:40 +0200

    Update the stereo example

--- examples/python/RVC4/StereoDepth/stereo.py
+++ examples/python/RVC4/StereoDepth/stereo.py
@@ -3,52 +3,26 @@
 import cv2
 import depthai as dai
 import numpy as np
-from pathlib import Path
 
-
-# Temporary toy example until the RVC4 devices are calibrated
-examplePath = (Path(__file__).parent / ".." / "..").resolve().absolute()
-calibJsonFile = examplePath / "models" / "depthai_calib.json"
-# Check if file exists otherwise provoke the user to run `python3 examples/python/install_requirements.py`
-if not calibJsonFile.exists():
-    import sys
-
-    print(
-        f"Calibration file not found at: {calibJsonFile}. Please run {sys.executable} {examplePath}/install_requirements.py to get it downloaded."
-    )
-    exit(1)
-
-calibData = dai.CalibrationHandler(calibJsonFile)
-
-# Create pipeline
 pipeline = dai.Pipeline()
-pipeline.setCalibrationData(calibData)
-
-monoLeft = pipeline.create(dai.node.Camera)
-monoRight = pipeline.create(dai.node.Camera)
+monoLeft = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
+monoRight = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_C)
 stereo = pipeline.create(dai.node.StereoDepth)
 
-# Define sources and outputs
-monoLeft.setBoardSocket(dai.CameraBoardSocket.CAM_A)
-monoRight.setBoardSocket(dai.CameraBoardSocket.CAM_C)
-
 # Linking
-monoLeftOut = monoLeft.requestOutput((640, 400), type=dai.ImgFrame.Type.NV12)
-monoRightOut = monoRight.requestOutput((640, 400), type=dai.ImgFrame.Type.NV12)
+monoLeftOut = monoLeft.requestFullResolutionOutput(type=dai.ImgFrame.Type.NV12)
+monoRightOut = monoRight.requestFullResolutionOutput(type=dai.ImgFrame.Type.NV12)
 monoLeftOut.link(stereo.left)
 monoRightOut.link(stereo.right)
 
-
-stereo.setInputResolution(640, 400)
 stereo.setRectification(True)
 stereo.setExtendedDisparity(True)
-stereo.setNumFramesPool(10)
+
 syncedLeftQueue = stereo.syncedLeft.createOutputQueue()
 syncedRightQueue = stereo.syncedRight.createOutputQueue()
 disparityQueue = stereo.disparity.createOutputQueue()
 
 with pipeline:
-    pipeline.setCalibrationData(calibData)
     pipeline.start()
     while pipeline.isRunning():
         leftSynced = syncedLeftQueue.get()
@@ -59,7 +33,10 @@ with pipeline:
         assert isinstance(disparity, dai.ImgFrame)
         cv2.imshow("left", leftSynced.getCvFrame())
         cv2.imshow("right", rightSynced.getCvFrame())
-        cv2.imshow("disparity", (disparity.getFrame()).astype(np.uint8))
+        npDisparity = disparity.getFrame()
+        maxDisparity = np.max(npDisparity)
+        colorizedDisparity = cv2.applyColorMap(((npDisparity / maxDisparity) * 255).astype(np.uint8), cv2.COLORMAP_JET)
+        cv2.imshow("disparity", colorizedDisparity)
         key = cv2.waitKey(1)
         if key == ord('q'):
             pipeline.stop()
