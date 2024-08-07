f81b48ff0bb5b6223378b85e60b229ff72c04df4
Matevz Morato <matevz.morato@gmail.com>
2024-08-07 17:29:05 +0200

    Add a simple april tag example

--- bindings/python/src/XLinkBindings.cpp
+++ bindings/python/src/XLinkBindings.cpp
@@ -125,11 +125,11 @@ void XLinkBindings::bind(pybind11::module &m, void *pCallstack)
         .export_values()
         ;
 
-    xLinkPlatform
-        .value("X_LINK_ANY_PLATFORM", X_LINK_ANY_PLATFORM)
+    xLinkPlatform.value("X_LINK_ANY_PLATFORM", X_LINK_ANY_PLATFORM)
         .value("X_LINK_MYRIAD_2", X_LINK_MYRIAD_2)
         .value("X_LINK_MYRIAD_X", X_LINK_MYRIAD_X)
         .value("X_LINK_RVC3", X_LINK_RVC3)
+        .value("X_LINK_RVC4", X_LINK_RVC4)
         .export_values()
         ;
 
---
+++ examples/python/AprilTags/april_tags.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+import time
+from pathlib import Path
+
+examplesRoot = Path(__file__).parent / Path('../').resolve()
+models = examplesRoot / Path('models')
+tagImage = models / Path('april_tags.jpg')
+
+class HostCamera(dai.node.ThreadedHostNode):
+    def __init__(self):
+        dai.node.ThreadedHostNode.__init__(self)
+        self.output = dai.Node.Output(self)
+    def run(self):
+        while self.isRunning():
+            frame = cv2.imread(str(tagImage.resolve()))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            imgFrame = dai.ImgFrame()
+            imgFrame.setData(frame)
+            imgFrame.setWidth(frame.shape[1])
+            imgFrame.setHeight(frame.shape[0])
+            imgFrame.setType(dai.ImgFrame.Type.GRAY8)
+            # Send the message
+            self.output.send(imgFrame)
+            # Wait for the next frame
+            time.sleep(0.1)
+
+info = dai.DeviceInfo("127.0.0.1")
+info.protocol = dai.X_LINK_TCP_IP
+info.state = dai.X_LINK_GATE
+info.platform = dai.X_LINK_RVC4
+device = dai.Device(info)
+with dai.Pipeline(device) as pipeline:
+    hostCamera = pipeline.create(HostCamera)
+    camQueue = hostCamera.output.createOutputQueue()
+
+    aprilTag = pipeline.create(dai.node.AprilTag)
+
+    hostCamera.output.link(aprilTag.inputImage)
+
+    aprilTag.initialConfig.setFamily(dai.AprilTagConfig.Family.TAG_16H5)
+
+
+    outputFeaturePassthroughQueue = aprilTag.passthroughInputImage.createOutputQueue()
+    outQueue = aprilTag.out.createOutputQueue()
+
+
+
+    color = (0, 255, 0)
+
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
+
+    pipeline.start()
+    while pipeline.isRunning():
+        image : dai.ImgFrame = camQueue.get()
+        aprilTagData = outQueue.get().aprilTags
+
+        counter+=1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1 :
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        outputPassthroughImage : dai.ImgFrame = outputFeaturePassthroughQueue.get()
+
+        passthroughImage = outputPassthroughImage.getCvFrame()
+        passthroughImage = cv2.cvtColor(passthroughImage, cv2.COLOR_GRAY2BGR)
+
+        frame = passthroughImage.copy()
+
+        for aprilTag in aprilTagData:
+            topLeft = aprilTag.topLeft
+            topRight = aprilTag.topRight
+            bottomRight = aprilTag.bottomRight
+            bottomLeft = aprilTag.bottomLeft
+
+            center = (int((topLeft.x + bottomRight.x) / 2), int((topLeft.y + bottomRight.y) / 2))
+
+            cv2.line(frame, (int(topLeft.x), int(topLeft.y)), (int(topRight.x), int(topRight.y)), color, 2, cv2.LINE_AA, 0)
+            cv2.line(frame, (int(topRight.x), int(topRight.y)), (int(bottomRight.x), int(bottomRight.y)), color, 2, cv2.LINE_AA, 0)
+            cv2.line(frame, (int(bottomRight.x), int(bottomRight.y)), (int(bottomLeft.x), int(bottomLeft.y)), color, 2, cv2.LINE_AA, 0)
+            cv2.line(frame, (int(bottomLeft.x), int(bottomLeft.y)), (int(topLeft.x), int(topLeft.y)), color, 2, cv2.LINE_AA, 0)
+
+            idStr = "ID: " + str(aprilTag.id)
+            cv2.putText(frame, idStr, center, cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+
+        cv2.imshow("detections", frame)
+        cv2.imshow("passthrough", passthroughImage)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
