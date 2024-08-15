35275353fc263bfef5c84f4bb88b79ca5f107d28
alex-luxonis <alex@luxonis.com>
2024-08-16 00:02:08 +0300

    FW: 3A sync for all cameras on OAK-D-LR, main camera driving AE/AWB selected in the order `center > left > right`

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1e402e4cc2eb05363a41e5b6679457ec3a41f641")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9ed7c9ae4c232ff93a3500a585a6b1c00650e22c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
