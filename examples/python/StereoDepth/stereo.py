b69a6e0abc301ec3ce90af68e311187ef13dec2d
SzabolcsGergely <szabi@luxonis.com>
2024-08-16 17:24:56 +0300

    Update stereo example

--- examples/python/StereoDepth/stereo.py
+++ examples/python/StereoDepth/stereo.py
@@ -17,10 +17,16 @@ monoRightOut.link(stereo.right)
 
 stereo.setRectification(True)
 stereo.setExtendedDisparity(True)
+stereo.setLeftRightCheck(True)
+stereo.initialConfig.setConfidenceThreshold(220)
+stereo.initialConfig.setLeftRightCheckThreshold(2*16)
 
 syncedLeftQueue = stereo.syncedLeft.createOutputQueue()
 syncedRightQueue = stereo.syncedRight.createOutputQueue()
 disparityQueue = stereo.disparity.createOutputQueue()
+
+colorMap = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_JET)
+colorMap[0] = [0, 0, 0]  # to make zero-disparity pixels black
 
 with pipeline:
     pipeline.start()
@@ -35,7 +41,7 @@ with pipeline:
         cv2.imshow("right", rightSynced.getCvFrame())
         npDisparity = disparity.getFrame()
         maxDisparity = np.max(npDisparity)
-        colorizedDisparity = cv2.applyColorMap(((npDisparity / maxDisparity) * 255).astype(np.uint8), cv2.COLORMAP_JET)
+        colorizedDisparity = cv2.applyColorMap(((npDisparity / maxDisparity) * 255).astype(np.uint8), colorMap)
         cv2.imshow("disparity", colorizedDisparity)
         key = cv2.waitKey(1)
         if key == ord('q'):
