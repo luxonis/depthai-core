dc03563e1d3de072390bdf30a127a99be99f3f6f
alex-luxonis <alex@luxonis.com>
2024-08-20 02:00:50 +0300

    FW: AR0234 tuning increase max exposure: 33 -> 890 ms

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9ed7c9ae4c232ff93a3500a585a6b1c00650e22c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2c85c946a036ee2caa6c1219917201ab7a1dcdd3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
