1ec6075ca24ca3d6e8fd57e26aac6e2c75747b86
SzabolcsGergely <szabi@luxonis.com>
2024-08-20 19:15:53 +0300

    RVC2 FW: fix reverse camera output order

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f66d6ad470e50bed723d6b8efe307fe2a5e712c8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "330b59c4dcd3bdf317e3ad678d8b38f7ddfb10b1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
