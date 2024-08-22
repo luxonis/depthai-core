c17a80e3102aae7896d58e3a47aec7a38eef15ab
alex-luxonis <alex@luxonis.com>
2024-08-22 04:03:05 +0300

    FW: fix ToF EEPROM reading for certain FFC camera permutations

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2c85c946a036ee2caa6c1219917201ab7a1dcdd3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cee0e0be66ae6b1cce48fcbb75a83676aea5c5fc")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
