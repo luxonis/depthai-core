e2ba24b042685c6018b01050ac0a01d21a73fdc1
SzabolcsGergely <szabi@luxonis.com>
2024-08-23 20:45:57 +0300

    Add TOF example

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "330b59c4dcd3bdf317e3ad678d8b38f7ddfb10b1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e1fe196981f0727f5e9a7355735265508377d3e3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
