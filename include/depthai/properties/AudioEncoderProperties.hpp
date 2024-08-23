f40443f1160c2708111fde443e429f09a0d65685
Filippo Mutta <filippo.mutta@gmail.com>
2024-08-23 15:34:35 +0200

    Finalized properties for audio encoder.

--- include/depthai/properties/AudioEncoderProperties.hpp
+++ include/depthai/properties/AudioEncoderProperties.hpp
@@ -4,35 +4,11 @@
 
 namespace dai {
 
-enum AudioEncoderFormat {
-	AUDIO_ENCODER_RAW,
-	AUDIO_ENCODER_WAV,
-};
-
-
 /**
  * Specify properties for VideoEncoder such as profile, bitrate, ...
  */
 struct AudioEncoderProperties : PropertiesSerializable<Properties, AudioEncoderProperties> {
     static constexpr int AUTO = -1;
-
-    /**
-     *  Input 
-     */
-    AudioEncoderFormat inputEncoderFormat;
-    
-    /**
-     *  Output
-     */
-    AudioEncoderFormat outputEncoderFormat;
-
-    /**
-     *  Number of buffered audio frames before encoding
-     *  More frames = longer delay, but less load
-     */
-    unsigned int framesPerSecond = 10;
-
-    unsigned int frameSize;
     /**
      *  Bitrate for the output
      */
@@ -44,14 +20,13 @@ struct AudioEncoderProperties : PropertiesSerializable<Properties, AudioEncoderP
      */
     unsigned int channels = 2;
 
+    int format;
+
 };
 
 DEPTHAI_SERIALIZE_EXT(AudioEncoderProperties,
-		      inputEncoderFormat,
-		      outputEncoderFormat,
-		      framesPerSecond,
-		      frameSize,
 		      bitrate,
-		      channels);
+		      channels,
+		      format);
 
 }  // namespace dai
