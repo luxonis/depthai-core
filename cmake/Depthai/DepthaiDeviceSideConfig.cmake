73a77d44abe99e67585408e29d79a241d2dd213b
alex-luxonis <alex@luxonis.com>
2024-08-26 14:55:14 +0300

    FW: update after merge, add "3a-follow" misc control: ``` cam["cama"].initialControl.setMisc("3a-follow", dai.CameraBoardSocket.CAM_A) cam["camb"].initialControl.setMisc("3a-follow", dai.CameraBoardSocket.CAM_B) ```
    generic fast manual exposure (for all sensors)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cee0e0be66ae6b1cce48fcbb75a83676aea5c5fc")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "01b465eceaef943349270c59fbc1633ba9b95024")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
