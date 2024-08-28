7b39bd320db9e5029a347f6f87d3366dae05fe49
SzabolcsGergely <szabi@luxonis.com>
2024-08-28 19:30:19 +0300

    Update RVC2 FW; move ToF example to RVC2 folder

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "54cda21d5d44db3051e702368fabda6e14bfc39b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8a62e9908f7affefe4dad194930f3a5a0d80da45")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
