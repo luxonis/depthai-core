1eea87d41ac51ea6498880bca9c64f0de6b028e8
Matevz Morato <matevz.morato@gmail.com>
2024-08-28 18:39:10 +0200

    Improve visualization of the tof example

--- examples/python/RVC2/ToF/tof.py
+++ examples/python/RVC2/ToF/tof.py
@@ -2,6 +2,34 @@
 
 import cv2
 import depthai as dai
+import numpy as np
+
+def colorizeDepth(frameDepth):
+    invalidMask = frameDepth == 0
+    # Log the depth, minDepth and maxDepth
+    try:
+        minDepth = np.percentile(frameDepth[frameDepth != 0], 3)
+        maxDepth = np.percentile(frameDepth[frameDepth != 0], 95)
+        logDepth = np.log(frameDepth, where=frameDepth != 0)
+        logMinDepth = np.log(minDepth)
+        logMaxDepth = np.log(maxDepth)
+        np.nan_to_num(logDepth, copy=False, nan=logMinDepth)
+        # Clip the values to be in the 0-255 range
+        logDepth = np.clip(logDepth, logMinDepth, logMaxDepth)
+
+        # Interpolate only valid logDepth values, setting the rest based on the mask
+        depthFrameColor = np.interp(logDepth, (logMinDepth, logMaxDepth), (0, 255))
+        depthFrameColor = np.nan_to_num(depthFrameColor)
+        depthFrameColor = depthFrameColor.astype(np.uint8)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_JET)
+        # Set invalid depth pixels to black
+        depthFrameColor[invalidMask] = 0
+    except IndexError:
+        # Frame is likely empty
+        depthFrameColor = np.zeros((frameDepth.shape[0], frameDepth.shape[1], 3), dtype=np.uint8)
+    except Exception as e:
+        raise e
+    return depthFrameColor
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -16,7 +44,8 @@ with pipeline:
     while pipeline.isRunning():
         depth = depthQueue.get()
         assert isinstance(depth, dai.ImgFrame)
-        cv2.imshow("depth", depth.getCvFrame())
+        visualizedDepth = colorizeDepth(depth.getFrame())
+        cv2.imshow("depth", visualizedDepth)
 
         if cv2.waitKey(1) == ord("q"):
             break
