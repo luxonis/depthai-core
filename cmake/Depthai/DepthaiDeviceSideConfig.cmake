10b95c0276947dba3f2843cff184f39efd2a86d3
Matevz Morato <matevz.morato@gmail.com>
2024-08-29 13:23:39 +0200

    [RVC4&2] Update FW to latest implementation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c26ee202de4561360d7b08ce2966a506d08b8c89")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "48b7c8eab9a4ae1de7d7087dbe890beb2dfbe439")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
