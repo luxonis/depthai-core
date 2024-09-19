44a630b44247647c993e42f43f79c3222bdd9a34
Filip Jeretina <filip.jeretina@luxonis.com>
2024-09-19 13:03:02 +0200

    Device side

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "87979e2e8b5ff0353757fa7dfa6050ff65d9f017")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f772f742fb68481fc6565003b34fca43248e814a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
