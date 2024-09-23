e9d5a92b83fab2a8487130a88c47bbaacb27a7e5
Filip Jeretina <filip.jeretina@luxonis.com>
2024-09-23 17:24:54 +0200

    Fixed image brightness level going out of control in thermal example. Fixed autoFFC switch

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7fdba63eb1432bcc6f52dab582223b096e93ebc4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a559e6019925479c91772e3803e2c6732014b157")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
--- examples/python/RVC2/Thermal/thermal.py
+++ examples/python/RVC2/Thermal/thermal.py
@@ -20,30 +20,50 @@ with dai.Pipeline(True) as pipeline:
         elif key == ord("n"):
             if thermalConf.imageParams.brightnessLevel is None:
                 thermalConf.imageParams.brightnessLevel = THERMAL_IMAGE_BRIGHTNESS_STEP
-            if thermalConf.imageParams.brightnessLevel <= THERMAL_IMAGE_BRIGHTNESS_STEP:
+            if (
+                thermalConf.imageParams.brightnessLevel - THERMAL_IMAGE_BRIGHTNESS_STEP
+            ) <= THERMAL_IMAGE_BRIGHTNESS_STEP:
                 thermalConf.imageParams.brightnessLevel = THERMAL_IMAGE_BRIGHTNESS_STEP
             thermalConf.imageParams.brightnessLevel -= THERMAL_IMAGE_BRIGHTNESS_STEP
-            print("Set image brightness level to ", thermalConf.imageParams.brightnessLevel)
+            print(
+                "Set image brightness level to ",
+                thermalConf.imageParams.brightnessLevel,
+            )
             changed = True
         elif key == ord("m"):
             if thermalConf.imageParams.brightnessLevel is None:
                 thermalConf.imageParams.brightnessLevel = 0
-            if thermalConf.imageParams.brightnessLevel >= 255:
-                thermalConf.imageParams.brightnessLevel = 255 - THERMAL_IMAGE_BRIGHTNESS_STEP
+            if (
+                thermalConf.imageParams.brightnessLevel + THERMAL_IMAGE_BRIGHTNESS_STEP
+            ) >= 255:
+                thermalConf.imageParams.brightnessLevel = (
+                    255 - THERMAL_IMAGE_BRIGHTNESS_STEP
+                )
             thermalConf.imageParams.brightnessLevel += THERMAL_IMAGE_BRIGHTNESS_STEP
-            print("Set image brightness level to ", thermalConf.imageParams.brightnessLevel)
+            print(
+                "Set image brightness level to ",
+                thermalConf.imageParams.brightnessLevel,
+            )
             changed = True
         elif key == ord("a"):
             if thermalConf.ffcParams.autoFFC is None:
                 thermalConf.ffcParams.autoFFC = True
             thermalConf.ffcParams.autoFFC = not thermalConf.ffcParams.autoFFC
-            print("Set auto shutter to", "on." if thermalConf.ffcParams.autoFFC else "off.")
+            print(
+                "Set auto shutter to",
+                "on." if thermalConf.ffcParams.autoFFC else "off.",
+            )
             changed = True
         elif key == ord("s"):
             if thermalConf.ffcParams.closeManualShutter is None:
                 thermalConf.ffcParams.closeManualShutter = False
-            thermalConf.ffcParams.closeManualShutter = not thermalConf.ffcParams.closeManualShutter
-            print("Closing" if thermalConf.ffcParams.closeManualShutter else "Opening", "manual shutter.")
+            thermalConf.ffcParams.closeManualShutter = (
+                not thermalConf.ffcParams.closeManualShutter
+            )
+            print(
+                "Closing" if thermalConf.ffcParams.closeManualShutter else "Opening",
+                "manual shutter.",
+            )
             changed = True
 
         if changed:
