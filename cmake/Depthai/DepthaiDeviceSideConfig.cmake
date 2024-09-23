56e8fe38a5590d11ef93d7bc67c8bd1f6ee58f6f
Filip Jeretina <filip.jeretina@luxonis.com>
2024-09-23 15:09:39 +0200

    Fixed Thermal node not working when inputConfig is not connected

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1961a0a91d822c003b1861fb6da7bd8de7fac4c4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7fdba63eb1432bcc6f52dab582223b096e93ebc4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
