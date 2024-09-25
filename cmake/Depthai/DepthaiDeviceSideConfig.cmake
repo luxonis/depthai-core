2e824de00f76478034f797877ef4bf344f8809e9
alex-luxonis <alex@luxonis.com>
2024-09-26 01:08:08 +0300

    FW: misc controls for multi-ToF staggering: frame-sync-id, frame-sync-delay-us

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ad289e1220d3f007efc52187082722db77e9ab20")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0adb9bf58e0b3c80f8669e71d2840ecc7852e3dd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
