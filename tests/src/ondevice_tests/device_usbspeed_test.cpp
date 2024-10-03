b4dd2f13ef75f2e1a2e7dd7e51c96aadee801393
Matevz Morato <matevz.morato@gmail.com>
2024-10-03 17:47:42 +0200

    Update tests

--- tests/src/ondevice_tests/device_usbspeed_test.cpp
+++ tests/src/ondevice_tests/device_usbspeed_test.cpp
@@ -13,39 +13,40 @@
 #include "depthai/common/UsbSpeed.hpp"
 #include "depthai/depthai.hpp"
 
-TEST_CASE("Usb modes") {
-    using namespace std;
-    using namespace std::chrono;
-    using namespace std::chrono_literals;
+// TODO - port to v3
+// TEST_CASE("Usb modes") {
+//     using namespace std;
+//     using namespace std::chrono;
+//     using namespace std::chrono_literals;
 
-    dai::Pipeline p;
+//     dai::Pipeline p(false);
 
-    std::unique_ptr<dai::Device> device;
-    SECTION("UsbSpeed::HIGH") {
-        device.reset(new dai::Device(p, dai::UsbSpeed::HIGH));
-    }
+//     std::unique_ptr<dai::Device> device;
+//     SECTION("UsbSpeed::HIGH") {
+//         device.reset(new dai::Device(p, dai::UsbSpeed::HIGH));
+//     }
 
-    SECTION("UsbSpeed::SUPER") {
-        device.reset(new dai::Device(p, dai::UsbSpeed::SUPER));
-    }
+//     SECTION("UsbSpeed::SUPER") {
+//         device.reset(new dai::Device(p, dai::UsbSpeed::SUPER));
+//     }
 
-    SECTION("UsbSpeed::SUPER_PLUS") {
-        device.reset(new dai::Device(p, dai::UsbSpeed::SUPER_PLUS));
-    }
+//     SECTION("UsbSpeed::SUPER_PLUS") {
+//         device.reset(new dai::Device(p, dai::UsbSpeed::SUPER_PLUS));
+//     }
 
-    auto infoNode = p.create<dai::node::SystemLogger>();
-    auto outputQueue = infoNode->out.createOutputQueue();
-    p.start();
+//     auto infoNode = p.create<dai::node::SystemLogger>();
+//     auto outputQueue = infoNode->out.createOutputQueue();
+//     p.start();
 
-    const auto timeout = 1s;
-    bool timeoutOccurred = false;
-    auto infoFrame = outputQueue->get<dai::SystemInformation>(timeout, timeoutOccurred);
+//     const auto timeout = 1s;
+//     bool timeoutOccurred = false;
+//     auto infoFrame = outputQueue->get<dai::SystemInformation>(timeout, timeoutOccurred);
 
-    REQUIRE(!timeoutOccurred);
-    REQUIRE(infoFrame != nullptr);
-    REQUIRE(infoFrame->leonCssCpuUsage.average >= 0.0f);
-    REQUIRE(infoFrame->leonCssCpuUsage.average <= 1.0f);
-}
+//     REQUIRE(!timeoutOccurred);
+//     REQUIRE(infoFrame != nullptr);
+//     REQUIRE(infoFrame->leonCssCpuUsage.average >= 0.0f);
+//     REQUIRE(infoFrame->leonCssCpuUsage.average <= 1.0f);
+// }
 
 TEST_CASE("Usb config modes") {
     using namespace std;
