76e5cbd15309fbdb259267120e38a93c72a65cb2
SzabolcsGergely <szabi@luxonis.com>
2024-10-04 19:38:32 +0300

    Implement configurable filtering order

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1498234108ce6d900c7c80437ea997df3b0ffd65")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1d380e6aef6c14ea6b499f1b08dbb8a1fddc2eb3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
