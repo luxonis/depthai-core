850be5359f7eac8b35dfa29a7d51ca204a853863
Matevz Morato <matevz.morato@gmail.com>
2024-10-07 17:48:46 +0200

    Increase timeout as some tests take quite some time

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -47,7 +47,7 @@ def run_ctest(env_vars, labels, blocking=True, name=""):
     env = os.environ.copy()
     env.update(env_vars)
 
-    cmd = ["ctest", "--no-tests=error", "-VV", "-L", "^ci$", "--timeout", "300"]
+    cmd = ["ctest", "--no-tests=error", "-VV", "-L", "^ci$", "--timeout", "1000"]
     for label in labels:
         # Encapsulate label in ^label$ to match exactly
         label = f"^{label}$"
