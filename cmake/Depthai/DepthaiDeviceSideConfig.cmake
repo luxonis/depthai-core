04045092e99c4995e059f0c75531f3e444d4dbc9
Matevz Morato <matevz.morato@gmail.com>
2024-10-07 17:43:24 +0200

    Update device side to lower warn to info for shaves in NN

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "81837009128b7fae8f7c632eeacff7ef59a054c8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e24ecb45d75aeed9554a60acccf51e0753b0652f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
