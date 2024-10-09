c1b2d4af6b2f162819f9417984b44bde95c47cf1
Serafadam <adam.serafin@luxonis.com>
2024-10-09 15:24:52 +0200

    minor tags change

--- examples/cpp/HostNodes/events.cpp
+++ examples/cpp/HostNodes/events.cpp
@@ -28,21 +28,21 @@ int main(int argc, char* argv[]) {
     std::this_thread::sleep_for(std::chrono::milliseconds(7000));
 
     auto fileData = std::make_shared<dai::utility::EventData>("abc", "test.txt", "text/plain");
-	std::vector<std::shared_ptr<dai::utility::EventData>> data;
-	data.emplace_back(fileData);
-    eventsManager->sendSnap("testdata", nullptr, data, {"tag3, tag4"}, {{"key8", "value8"}});
+    std::vector<std::shared_ptr<dai::utility::EventData>> data;
+    data.emplace_back(fileData);
+    eventsManager->sendSnap("testdata", nullptr, data, {"tag3", " ag4"}, {{"key8", "value8"}});
     auto fileData2 = std::make_shared<dai::utility::EventData>("/test.txt");
-	std::vector<std::shared_ptr<dai::utility::EventData>> data2;
-	data2.push_back(fileData2);
-    eventsManager->sendSnap("testdata2", nullptr, data2, {"tag5, tag6"}, {{"key8", "value8"}});
+    std::vector<std::shared_ptr<dai::utility::EventData>> data2;
+    data2.push_back(fileData2);
+    eventsManager->sendSnap("testdata2", nullptr, data2, {"tag5", "tag6"}, {{"key8", "value8"}});
     auto fileData3 = std::make_shared<dai::utility::EventData>("/test.jpg");
-	std::vector<std::shared_ptr<dai::utility::EventData>> data3;
-	data3.push_back(fileData3);
-    eventsManager->sendSnap("testdata3", nullptr, data3, {"tag5, tag6"}, {{"key8", "value8"}});
-	std::vector<std::shared_ptr<dai::utility::EventData>> data4;
-	data4.push_back(fileData);
-	data4.push_back(fileData2);
-	eventsManager->sendSnap("testdata4", nullptr, data4, {"tag5, tag6"}, {{"key8", "value8"}});
+    std::vector<std::shared_ptr<dai::utility::EventData>> data3;
+    data3.push_back(fileData3);
+    eventsManager->sendSnap("testdata3", nullptr, data3, {"tag5", "tag6"}, {{"key8", "value8"}});
+    std::vector<std::shared_ptr<dai::utility::EventData>> data4;
+    data4.push_back(fileData);
+    data4.push_back(fileData2);
+    eventsManager->sendSnap("testdata4", nullptr, data4, {"tag5", "tag6"}, {{"key8", "value8"}});
     while(pipeline.isRunning()) {
         auto rgb = previewQ->get<dai::ImgFrame>();
 
