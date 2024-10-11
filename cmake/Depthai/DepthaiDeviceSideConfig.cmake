bc7c7cc202e16fb8f1b5f89c45679e657929feb0
SzabolcsGergely <szabi@luxonis.com>
2024-10-11 13:36:50 +0300

    StereoDepth: postprocessing: handle edge case when post-processing filter is enabled, disparity output disabled, median enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4280f8084e08f54d9e0f8c2e011aa27f1321a442")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9f1412b723dd7c6dbcbdb46c0ce0a46647a851e4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
