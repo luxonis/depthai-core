69f7669b43b1a45777f5e02f203e3ab8e2146d01
SzabolcsGergely <szabi@luxonis.com>
2024-10-17 15:21:45 +0300

    StereoDepth postprocessing: improve SpatialFilt/er and TemporalFilter delta parameter handling

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9f1412b723dd7c6dbcbdb46c0ce0a46647a851e4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1d1152e0c179d2918e15522d99cfea4abb7f5ea5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
