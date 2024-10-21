ee9f132ce6a53ff603edc80214ab3bc964ea9fe5
Matevz Morato <matevz.morato@gmail.com>
2024-10-21 16:30:54 +0200

    Update the visualizer

--- cmake/Depthai/DepthaiVisualizerConfig.cmake
+++ cmake/Depthai/DepthaiVisualizerConfig.cmake
@@ -1,2 +1,2 @@
 # "full commit hash of depthai visualizer static files"
-set(DEPTHAI_VISUALIZER_COMMIT "e934abbdbdde8de6ca31ff595e3f1c74cee89c49")
+set(DEPTHAI_VISUALIZER_COMMIT "356d40353a05f5d4707263b791e489f9e355f958")
