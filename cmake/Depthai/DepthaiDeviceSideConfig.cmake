65edc5ccee76d7c7832499a935db1b89bdbe9f30
Matevz Morato <matevz.morato@gmail.com>
2024-10-25 16:52:49 +0200

    [RVC2] Fix RVC2 stability

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "aa4df6690d5d7f21383376ed4627751187130d6f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "79e7b35c958b580ff3360ee8de697ea7f4e302af")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
