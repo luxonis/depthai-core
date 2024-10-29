acafbabf0142de616050e3ecd93e483f942e3ca0
SzabolcsGergely <szabi@luxonis.com>
2024-10-29 16:55:33 +0200

    FW: Stereo: issue debug prints once on calibration update event

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "efd7622159a172791cc3c1f59175bfbd1ccdab32")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5a168b72adfbd1bcc7c5e0e0cb8506a18304fcd4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
