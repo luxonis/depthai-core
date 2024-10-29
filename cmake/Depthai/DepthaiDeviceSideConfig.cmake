e342da287293dd58c1a695380c07e0758caca1d5
SzabolcsGergely <szabi@luxonis.com>
2024-10-29 14:26:35 +0200

    FW: rebuild guzzi lib

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6fa66a2e6be137fd6c24d3e707c468f89d2331fe")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "49d900d2174da6c1d616a636243494fd44e0d3f6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
