d071de8cadcff5263e2213e5df9feacc8ef234f7
Serafadam <adam.serafin@luxonis.com>
2024-11-04 13:04:03 +0100

    tbb port for basalt

---
+++ cmake/ports/tbb/portfile.cmake
@@ -0,0 +1,20 @@
+
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO oneapi-src/oneTBB
+	REF v2021.12.0
+	SHA512 64022bcb61cf7b2030a1bcc11168445ef9f0d69b70290233a7febb71cc7a12cc2282dddc045f84e30893efe276342f02fd78d176706268eeaefe9aac7446d4e9
+	HEAD_REF master
+)
+vcpkg_cmake_configure(
+	SOURCE_PATH "${SOURCE_PATH}"
+	OPTIONS
+		TBB_PREVIEW_GLOBAL_CONTROL=ON
+        TBB_TEST=OFF
+        CMAKE_CXX_VISIBILITY_PRESET=hidden
+        CMAKE_C_VISIBILITY_PRESET=hidden
+)
+
+vcpkg_cmake_install()
+
+
