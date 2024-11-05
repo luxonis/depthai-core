e988eedbe9c9d1224f4edee88acbe1506c8871d2
Filip Jeretina <filip.jeretina@luxonis.com>
2024-11-05 17:05:33 +0100

    FW: Support a generic OAK-T board.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "49d900d2174da6c1d616a636243494fd44e0d3f6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "eb287dbd66011c13687c083634849a122b4124db")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
