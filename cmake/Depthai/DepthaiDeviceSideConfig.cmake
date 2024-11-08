22b17f7cd18f2b8ccfa0ab83945aaabf2ee96e1a
Filip Jeretina <filip.jeretina@luxonis.com>
2024-11-08 15:43:23 +0100

    FW: Fix build

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "788ea84f719ed26055f0a1d283db5e621dc390ef")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1d1b6b793f21884ddb67e14433d54c13952aa4c9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
