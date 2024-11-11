67ea4dce19891c129a13da2974b7ee6c873f3932
Serafadam <adam.serafin@luxonis.com>
2024-11-11 13:30:49 +0100

    updates in xlink and cpr ports

--- cmake/ports/cpr/portfile.cmake
+++ cmake/ports/cpr/portfile.cmake
@@ -1,16 +1,38 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-	REPO Serafadam/cpr
-    REF c898870a8992c1a679c04b8da3979b8cbcb91a6d
-    SHA512 efa68d4e41668345988ce2403e4ef1f98d0e7f6bdca797174f8c6f5eb5dbdd818faa75aa583ce89391154e2cfe528e0128f1657421a2c8a0ddc71fe43b559a84
-    HEAD_REF vcpkg
+    REPO libcpr/cpr
+    REF ${VERSION}
+    SHA512 c314fc576fb8be36bf43326a8a2d8b22d6b2fbb3b494695b84dd8077fc0401981e49890172fc2229d1c68292be2820cd4231d58bcb64326cbe4b73933c092d76
+    HEAD_REF master
+    PATCHES
+        disable_werror.patch
+        multi_cert.patch
 )
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        ssl CPR_ENABLE_SSL
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-	OPTIONS
-	  -DBUILD_CPR_TESTS=OFF
+    OPTIONS
+        -DCPR_BUILD_TESTS=OFF
+        -DCPR_USE_SYSTEM_CURL=ON
+        ${FEATURE_OPTIONS}
+        # skip test for unused sanitizer flags
+        -DTHREAD_SANITIZER_AVAILABLE=OFF
+        -DADDRESS_SANITIZER_AVAILABLE=OFF
+        -DLEAK_SANITIZER_AVAILABLE=OFF
+        -DUNDEFINED_BEHAVIOUR_SANITIZER_AVAILABLE=OFF
+        -DALL_SANITIZERS_AVAILABLE=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/cpr)
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
--- cmake/ports/xlink/portfile.cmake
+++ cmake/ports/xlink/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 585a38fe4707e5f023de277135d8cb6ff9c4e0c4
     SHA512 e27834680374f348ffb590e181b3592df77c33d2de2c3b78061ff20d3315be5d4688da4ecfca8b3f914955367713ed53d50e61049b2f4eb95d6606b2426de7be
     HEAD_REF master
+    PATCHES
+        werror-switch.patch
 )
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
