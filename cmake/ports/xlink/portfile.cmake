73334eccc7313f7316e241ddf4540ad8ea35c9f7
Serafadam <adam.serafin@luxonis.com>
2024-11-11 14:02:09 +0100

    xlink hunter bump

--- cmake/ports/xlink/portfile.cmake
+++ cmake/ports/xlink/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         werror-switch.patch
+        hunter-bump.patch
 )
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
