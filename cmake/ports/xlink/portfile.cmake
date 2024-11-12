42b7de684051f94ca5ab9550a45a247b3737f006
Serafadam <adam.serafin@luxonis.com>
2024-11-12 14:07:46 +0100

    xlink hunter bump

--- cmake/ports/xlink/portfile.cmake
+++ cmake/ports/xlink/portfile.cmake
@@ -4,10 +4,12 @@ vcpkg_from_github(
     REF 6615e0721021c136dc50a3af5a5bf16919ffef87
     SHA512 35fb1c1ee8dea69a6bf577fb374fe6dd3cf815e7baef70671732fa8062d81fc34c17439fbe95f3bfe1a4266e0bc4e47b0d033c8abf586bbd7e0713891ffe6c28
     HEAD_REF develop_server
+    PATCHES
+        hunter.patch
 )
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        libusb          DXLINK_ENABLE_LIBUSB
+        libusb          XLINK_ENABLE_LIBUSB
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
