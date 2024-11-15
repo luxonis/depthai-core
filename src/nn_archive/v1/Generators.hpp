4783388646fb82c9f311ea0b5f3d43400140718d
Matevz Morato <matevz.morato@gmail.com>
2024-11-15 18:10:38 +0100

    Add new NNArchiveSchema

--- src/nn_archive/v1/Generators.hpp
+++ src/nn_archive/v1/Generators.hpp
@@ -22,8 +22,6 @@
 #include "depthai/nn_archive/v1/DataType.hpp"
 #include "depthai/nn_archive/v1/Head.hpp"
 #include "depthai/nn_archive/v1/Metadata.hpp"
-#include "depthai/nn_archive/v1/ObjectDetectionSubtypeYolo.hpp"
-#include "depthai/nn_archive/v1/ConfigVersion.hpp"
 
 namespace dai {
 namespace nn_archive {
@@ -52,12 +50,6 @@ namespace v1 {
     void from_json(const json & j, Config & x);
     void to_json(json & j, const Config & x);
 
-    void from_json(const json & j, ConfigVersion & x);
-    void to_json(json & j, const ConfigVersion & x);
-
-    void from_json(const json & j, ObjectDetectionSubtypeYolo & x);
-    void to_json(json & j, const ObjectDetectionSubtypeYolo & x);
-
     void from_json(const json & j, DataType & x);
     void to_json(json & j, const DataType & x);
 
@@ -65,13 +57,13 @@ namespace v1 {
     void to_json(json & j, const InputType & x);
 
     inline void from_json(const json & j, Metadata& x) {
-        x.postprocessorPath = get_stack_optional<std::string>(j, "postprocessor_path");
         x.anchors = get_stack_optional<std::vector<std::vector<std::vector<double>>>>(j, "anchors");
         x.classes = get_stack_optional<std::vector<std::string>>(j, "classes");
         x.confThreshold = get_stack_optional<double>(j, "conf_threshold");
         x.iouThreshold = get_stack_optional<double>(j, "iou_threshold");
         x.maxDet = get_stack_optional<int64_t>(j, "max_det");
         x.nClasses = get_stack_optional<int64_t>(j, "n_classes");
+        x.postprocessorPath = get_stack_optional<std::string>(j, "postprocessor_path");
         x.isSoftmax = get_stack_optional<bool>(j, "is_softmax");
         x.boxesOutputs = get_stack_optional<std::string>(j, "boxes_outputs");
         x.scoresOutputs = get_stack_optional<std::string>(j, "scores_outputs");
@@ -81,20 +73,20 @@ namespace v1 {
         x.nKeypoints = get_stack_optional<int64_t>(j, "n_keypoints");
         x.nPrototypes = get_stack_optional<int64_t>(j, "n_prototypes");
         x.protosOutputs = get_stack_optional<std::string>(j, "protos_outputs");
-        x.subtype = get_stack_optional<ObjectDetectionSubtypeYolo>(j, "subtype");
+        x.subtype = get_stack_optional<std::string>(j, "subtype");
         x.yoloOutputs = get_stack_optional<std::vector<std::string>>(j, "yolo_outputs");
         x.extraParams = j;
     }
 
     inline void to_json(json & j, const Metadata & x) {
         j = json::object();
-        j["postprocessor_path"] = x.postprocessorPath;
         j["anchors"] = x.anchors;
         j["classes"] = x.classes;
         j["conf_threshold"] = x.confThreshold;
         j["iou_threshold"] = x.iouThreshold;
         j["max_det"] = x.maxDet;
         j["n_classes"] = x.nClasses;
+        j["postprocessor_path"] = x.postprocessorPath;
         j["is_softmax"] = x.isSoftmax;
         j["boxes_outputs"] = x.boxesOutputs;
         j["scores_outputs"] = x.scoresOutputs;
@@ -110,6 +102,7 @@ namespace v1 {
 
     inline void from_json(const json & j, Head& x) {
         x.metadata = j.at("metadata").get<Metadata>();
+        x.name = get_stack_optional<std::string>(j, "name");
         x.outputs = get_stack_optional<std::vector<std::string>>(j, "outputs");
         x.parser = j.at("parser").get<std::string>();
     }
@@ -117,11 +110,13 @@ namespace v1 {
     inline void to_json(json & j, const Head & x) {
         j = json::object();
         j["metadata"] = x.metadata;
+        j["name"] = x.name;
         j["outputs"] = x.outputs;
         j["parser"] = x.parser;
     }
 
     inline void from_json(const json & j, PreprocessingBlock& x) {
+        x.daiType = get_stack_optional<std::string>(j, "dai_type");
         x.interleavedToPlanar = get_stack_optional<bool>(j, "interleaved_to_planar");
         x.mean = get_stack_optional<std::vector<double>>(j, "mean");
         x.reverseChannels = get_stack_optional<bool>(j, "reverse_channels");
@@ -130,6 +125,7 @@ namespace v1 {
 
     inline void to_json(json & j, const PreprocessingBlock & x) {
         j = json::object();
+        j["dai_type"] = x.daiType;
         j["interleaved_to_planar"] = x.interleavedToPlanar;
         j["mean"] = x.mean;
         j["reverse_channels"] = x.reverseChannels;
@@ -170,13 +166,17 @@ namespace v1 {
 
     inline void from_json(const json & j, Output& x) {
         x.dtype = j.at("dtype").get<DataType>();
+        x.layout = get_stack_optional<std::string>(j, "layout");
         x.name = j.at("name").get<std::string>();
+        x.shape = get_stack_optional<std::vector<int64_t>>(j, "shape");
     }
 
     inline void to_json(json & j, const Output & x) {
         j = json::object();
         j["dtype"] = x.dtype;
+        j["layout"] = x.layout;
         j["name"] = x.name;
+        j["shape"] = x.shape;
     }
 
     inline void from_json(const json & j, Model& x) {
@@ -195,7 +195,7 @@ namespace v1 {
     }
 
     inline void from_json(const json & j, Config& x) {
-        x.configVersion = j.at("config_version").get<ConfigVersion>();
+        x.configVersion = get_stack_optional<std::string>(j, "config_version");
         x.model = j.at("model").get<Model>();
     }
 
@@ -205,55 +205,37 @@ namespace v1 {
         j["model"] = x.model;
     }
 
-    inline void from_json(const json & j, ConfigVersion & x) {
-        if (j == "1.0") x = ConfigVersion::THE_10;
-        else { throw std::runtime_error("Input JSON does not conform to schema!"); }
-    }
-
-    inline void to_json(json & j, const ConfigVersion & x) {
-        switch (x) {
-            case ConfigVersion::THE_10: j = "1.0"; break;
-            default: throw std::runtime_error("Unexpected value in enumeration \"[object Object]\": " + std::to_string(static_cast<int>(x)));
-        }
-    }
-
-    inline void from_json(const json & j, ObjectDetectionSubtypeYolo & x) {
-        if (j == "yolov10") x = ObjectDetectionSubtypeYolo::YOLOV10;
-        else if (j == "yolov5") x = ObjectDetectionSubtypeYolo::YOLOV5;
-        else if (j == "yolov6") x = ObjectDetectionSubtypeYolo::YOLOV6;
-        else if (j == "yolov6r2") x = ObjectDetectionSubtypeYolo::YOLOV6_R2;
-        else if (j == "yolov7") x = ObjectDetectionSubtypeYolo::YOLOV7;
-        else if (j == "yolov8") x = ObjectDetectionSubtypeYolo::YOLOV8;
-        else { throw std::runtime_error("Input JSON does not conform to schema!"); }
-    }
-
-    inline void to_json(json & j, const ObjectDetectionSubtypeYolo & x) {
-        switch (x) {
-            case ObjectDetectionSubtypeYolo::YOLOV10: j = "yolov10"; break;
-            case ObjectDetectionSubtypeYolo::YOLOV5: j = "yolov5"; break;
-            case ObjectDetectionSubtypeYolo::YOLOV6: j = "yolov6"; break;
-            case ObjectDetectionSubtypeYolo::YOLOV6_R2: j = "yolov6r2"; break;
-            case ObjectDetectionSubtypeYolo::YOLOV7: j = "yolov7"; break;
-            case ObjectDetectionSubtypeYolo::YOLOV8: j = "yolov8"; break;
-            default: throw std::runtime_error("Unexpected value in enumeration \"[object Object]\": " + std::to_string(static_cast<int>(x)));
-        }
-    }
-
     inline void from_json(const json & j, DataType & x) {
-        if (j == "float16") x = DataType::FLOAT16;
+        if (j == "boolean") x = DataType::BOOLEAN;
+        else if (j == "float16") x = DataType::FLOAT16;
         else if (j == "float32") x = DataType::FLOAT32;
+        else if (j == "float64") x = DataType::FLOAT64;
         else if (j == "int32") x = DataType::INT32;
+        else if (j == "int4") x = DataType::INT4;
+        else if (j == "int64") x = DataType::INT64;
         else if (j == "int8") x = DataType::INT8;
+        else if (j == "string") x = DataType::STRING;
+        else if (j == "uint16") x = DataType::UINT16;
+        else if (j == "uint32") x = DataType::UINT32;
+        else if (j == "uint64") x = DataType::UINT64;
         else if (j == "uint8") x = DataType::UINT8;
         else { throw std::runtime_error("Input JSON does not conform to schema!"); }
     }
 
     inline void to_json(json & j, const DataType & x) {
         switch (x) {
+            case DataType::BOOLEAN: j = "boolean"; break;
             case DataType::FLOAT16: j = "float16"; break;
             case DataType::FLOAT32: j = "float32"; break;
+            case DataType::FLOAT64: j = "float64"; break;
             case DataType::INT32: j = "int32"; break;
+            case DataType::INT4: j = "int4"; break;
+            case DataType::INT64: j = "int64"; break;
             case DataType::INT8: j = "int8"; break;
+            case DataType::STRING: j = "string"; break;
+            case DataType::UINT16: j = "uint16"; break;
+            case DataType::UINT32: j = "uint32"; break;
+            case DataType::UINT64: j = "uint64"; break;
             case DataType::UINT8: j = "uint8"; break;
             default: throw std::runtime_error("Unexpected value in enumeration \"[object Object]\": " + std::to_string(static_cast<int>(x)));
         }
