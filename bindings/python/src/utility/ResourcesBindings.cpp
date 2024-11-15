1e0cd9876e265ff279f5bc0e542f3a2867ff218e
Serafadam <adam.serafin@luxonis.com>
2024-11-15 13:25:48 +0100

    try ci with pcl and rtabmap

--- bindings/python/src/utility/ResourcesBindings.cpp
+++ bindings/python/src/utility/ResourcesBindings.cpp
@@ -8,10 +8,10 @@ void ResourcesBindings::bind(pybind11::module& m, void* pCallstack){
     using namespace dai;
 
     // Bind Resources (if needed)
-    // py::class_<Resources, std::unique_ptr<Resources, py::nodelete>>(m, "Resources")
-    // .def(py::init([](){
-    //     return std::unique_ptr<Resources, py::nodelete>>(&Resources::getInstance());
-    // });
+    py::class_<Resources, std::unique_ptr<Resources, py::nodelete>>(m, "Resources")
+    .def(py::init([](){
+        return std::unique_ptr<Resources, py::nodelete>>(&Resources::getInstance());
+    });
 
 }
 
