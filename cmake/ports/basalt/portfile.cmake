804845654c9f33f345c2d320648682cf2ced9d91
Serafadam <adam.serafin@luxonis.com>
2024-11-15 14:07:06 +0100

    basalt on windows

--- cmake/ports/basalt/portfile.cmake
+++ cmake/ports/basalt/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     REF ac3cd0fbef91e889db27a1a779a5105ba715fef7
     SHA512 1e7694168f92af5f48f462a793fff528c46a4bd01d3417daf2031b651acba70ee9d61d38a33a1be217cc6e97baa67425e1a8fcc4e57a46033954757112326206
     HEAD_REF deps_test
+    PATCHES
+        win.patch
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
