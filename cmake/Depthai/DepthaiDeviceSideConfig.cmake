86f9bc183d4dc9ccb68d8db328964477abd3b5c1
alex-luxonis <alex@luxonis.com>
2024-11-18 17:58:26 +0200

    FW: flash-boot app-FW only, X_LINK_BOOTED state. Pipeline still sent from host

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9ed7c9ae4c232ff93a3500a585a6b1c00650e22c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "63cfa95771fab09c238a71286e1c0cfbc58df2f5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
