e7e46e06b36138d94f2cbfa811b642f6ee61603e
SzabolcsGergely <szabi@luxonis.com>
2024-11-20 21:16:02 +0200

    RVC2: Fix intrinsics used in ImageAlign

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ff377bfee122b3c2c9d9b9e04e77ab06a8afb9c3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "23e603b35f76cf2751908e75deeddef6e58e2234")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
