c588ce184adb7d56ce8aab7eb483d046badc2930
Matevz Morato <matevz.morato@gmail.com>
2024-11-21 20:02:00 +0100

    [RVC4 FW] Keep using the 2.23 SNPE library

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -4,4 +4,16 @@ set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
 # set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+93f7b75a885aa32f44c5e9f53b74470c49d2b1af")
+<<<<<<< HEAD
 set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+81617bcfe7b7da9eda9654b5b3d3d3254b59a47d")
+=======
+<<<<<<< HEAD
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+72e173caa7b97651f1ca4428091c7e46ed2c62f1")
+=======
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+c0ce8640f8d756602a697c69c2771f1efb81fe43")
+<<<<<<< HEAD
+
+>>>>>>> 7661f2988 (Rebasing to avoid mistakes in building)
+=======
+>>>>>>> 21ed94b3a ([RVC4 FW] Keep using the 2.23 SNPE library)
+>>>>>>> dc00af29e ([RVC4 FW] Keep using the 2.23 SNPE library)
