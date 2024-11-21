79de99d5a011c3429d8a6c59f394a4d7caa04c02
Matevz Morato <matevz.morato@gmail.com>
2024-11-21 14:57:45 +0100

    [FW] - add logs for timing of stereo filters

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "97329ec22d4ef0bc4961e09c673d2c6a33bdc6d8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4d360b5c56225f23e9a3d3a3999ce46c90cfdeaf")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
