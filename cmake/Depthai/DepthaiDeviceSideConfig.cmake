9434300a47677e20c4b9f0e6a38a3c5c6fc60f2c
SzabolcsGergely <szabi@luxonis.com>
2024-11-21 19:42:23 +0200

    RVC2: StereoDepth: add inputAlignTo input

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b399a12aaf02228147364b7357a738f14fdd6225")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "499735c9d334fb4badea686b85e6e9d377c22bac")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
