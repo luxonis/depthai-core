4d11c7d17d662ac3be0d274c9614c807af2dc1bb
alex-luxonis <alex@luxonis.com>
2024-11-26 02:46:31 +0200

    FW: fix logging-related long-run crash in flashed/standalone app mode

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4d360b5c56225f23e9a3d3a3999ce46c90cfdeaf")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f9e14e2721e6ffbcb0402342a6186d5e38dbd0c6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
