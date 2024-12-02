513c31115f40059350b2c392ba258ff3db885432
Matevz Morato <matevz.morato@gmail.com>
2024-12-02 15:31:37 +0100

    Only build deps in Release always

--- cmake/triplets/arm64-linux.cmake
+++ cmake/triplets/arm64-linux.cmake
@@ -3,6 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_BUILD_TYPE release)
 
 if(PORT MATCHES "libusb")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
--- cmake/triplets/x64-windows.cmake
+++ cmake/triplets/x64-windows.cmake
@@ -1,6 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_BUILD_TYPE release)
 
 if(PORT MATCHES "libusb")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
