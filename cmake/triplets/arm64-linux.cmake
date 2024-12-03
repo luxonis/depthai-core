631d2bbe65290113ceec93401bddf529f1550179
Matevz Morato <matevz.morato@gmail.com>
2024-12-03 20:17:50 +0100

    Fix triplet files

--- cmake/triplets/arm64-linux.cmake
+++ cmake/triplets/arm64-linux.cmake
@@ -1,13 +1,11 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-set(VCPKG_OSX_DEPLOYMENT_TARGET "11")
-
 set(VCPKG_BUILD_TYPE release)
 
-# Add ffmpeg after the shared libraries become relocatable
-if(PORT MATCHES "libusb")
+if(PORT MATCHES "libusb|ffmpeg")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
+    set(VCPKG_FIXUP_ELF_RPATH ON)
 endif()
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
--- cmake/triplets/x64-osx.cmake
+++ cmake/triplets/x64-osx.cmake
@@ -4,9 +4,11 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
+set(VCPKG_OSX_DEPLOYMENT_TARGET "11")
+
 set(VCPKG_BUILD_TYPE release)
 
-if(PORT MATCHES "libusb|ffmpeg")
+# Add ffmpeg after the shared libraries become relocatable
+if(PORT MATCHES "libusb")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
-
