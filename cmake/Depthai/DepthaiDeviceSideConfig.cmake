9da98f4fcecaadfdea2bfe7a86075ad3c5374379
SzabolcsGergely <szabi@luxonis.com>
2024-12-11 19:43:08 +0200

    RVC2: Update FW, change letterboxing behavior

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9ce144f0a61f6b83a31bd3716085e1c5438ae0bd")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "05657f1f9b182d7a75471c9b82388c5570cecf6c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
