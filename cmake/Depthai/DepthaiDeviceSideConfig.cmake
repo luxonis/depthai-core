b4f0772e5d61ea389dd3c73b6e7ee2a9e57da4a9
asahtik <asahtik@gmail.com>
2024-12-11 13:34:02 +0100

    Clamp remapped rect in SpatialDetectionNetwork

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eb2e8d316d9be5e867ce9b10d982e6e453d78bd1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "645253b14a01fdf9808192714f540e475ff30780")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
