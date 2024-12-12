d1204eb2c66db210274628a240ff41b5cbdce992
SzabolcsGergely <szabi@luxonis.com>
2024-12-12 17:52:04 +0200

    FW: RVC2: Fix transformation data for RGB-alignment

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eefd5abea2283c4e18ec09b4c649586f25edaad9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e5d39f75fa3d22e60a5a1a6e926a74cdde807d88")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
