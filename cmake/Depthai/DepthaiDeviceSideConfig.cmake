3fa2ba274e1a27a09d2d2e1b234bd837953282f2
asahtik <asahtik@gmail.com>
2024-12-12 12:10:51 +0100

    ImgTransformations fixes for aligned depth

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "645253b14a01fdf9808192714f540e475ff30780")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7b539a294865f427bf78c2952a4e921a8fbcbbf5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
