dfbccf660417242b28329162069404fb82c2a700
asahtik <asahtik@gmail.com>
2024-12-13 14:59:51 +0100

    FW: fix SpatialDetectionNetwork roi error

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "01251af7e2411b61b825c898cfe201b70343daed")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ca1de34f09fc871e63db84fe229be44c276f5117")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
