8d3db28168942517fc2514f659c12a7f23223a37
asahtik <asahtik@gmail.com>
2024-12-13 11:36:19 +0100

    FW: fix video encoder crash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "23c307c94db6e7a58eabbe100381ec7fbd9879d9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "01251af7e2411b61b825c898cfe201b70343daed")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
