8f25dfe50725ad9065633a145a4b94be7a37cbcb
asahtik <asahtik@gmail.com>
2024-12-17 12:05:00 +0100

    FW: Add sanity check when copying transformation data

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "989e418f7f5b64d35659085309f208560425257b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1927a33b9aef6db14c36002071bc03ca527284d1")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
