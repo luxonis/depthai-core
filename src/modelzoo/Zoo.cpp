8678b6bee80de12f7b4b1500cd1c1ea1d0726130
lnotspotl <lnotspotl@gmail.com>
2024-12-19 05:52:26 -0500

    Add hub metadata, check hash

--- src/modelzoo/Zoo.cpp
+++ src/modelzoo/Zoo.cpp
@@ -80,7 +80,7 @@ class ZooManager {
     /**
      * @brief Download model from model zoo
      */
-    void downloadModel();
+    void downloadModel(const nlohmann::json& responseJson);
 
     /**
      * @brief Return path to model in cache
@@ -88,6 +88,34 @@ class ZooManager {
      * @return std::string: Path to model
      */
     std::string loadModelFromCache() const;
+
+    /**
+     * @brief Get path to metadata file
+     *
+     * @return std::string: Path to metadata file
+     */
+    std::string getMetadataFilePath() const;
+
+    /**
+     * @brief Fetch model download links from Hub
+     *
+     * @return nlohmann::json: JSON with download links
+     */
+    nlohmann::json fetchModelDownloadLinks();
+
+    /**
+     * @brief Get files in folder
+     *
+     * @return std::vector<std::string>: Files in folder
+     */
+    std::vector<std::string> getFilesInFolder(const std::string& folder) const;
+
+    /**
+     * @brief Check if internet is available
+     *
+     * @return bool: True if internet is available
+     */
+    bool internetIsAvailable() const;
 
    private:
     // Description of the model
@@ -150,10 +178,14 @@ bool checkIsErrorHub(const cpr::Response& response) {
     return false;
 }
 
-std::vector<std::string> getFilesInFolder(const std::string& folder) {
+std::vector<std::string> ZooManager::getFilesInFolder(const std::string& folder) const {
+    auto metadata = utility::loadYaml(getMetadataFilePath());
+    auto downloadedFiles = utility::yamlGet<std::vector<std::string>>(metadata, "downloaded_files");
     std::vector<std::string> files;
-    for(const auto& entry : std::filesystem::directory_iterator(folder)) {
-        files.push_back(entry.path().string());
+    for(const auto& downloadedFile : downloadedFiles) {
+        if(std::filesystem::exists(combinePaths(folder, downloadedFile))) {
+            files.push_back(combinePaths(folder, downloadedFile));
+        }
     }
     return files;
 }
@@ -207,7 +239,7 @@ bool ZooManager::isModelCached() const {
     return std::filesystem::exists(getModelCacheFolderPath(cacheDirectory));
 }
 
-void ZooManager::downloadModel() {
+nlohmann::json ZooManager::fetchModelDownloadLinks() {
     // Add request parameters
     cpr::Parameters params;
 
@@ -254,7 +286,18 @@ void ZooManager::downloadModel() {
 
     // Extract download links from response
     nlohmann::json responseJson = nlohmann::json::parse(response.text);
+    return responseJson;
+}
+
+void ZooManager::downloadModel(const nlohmann::json& responseJson) {
+    // Extract download links from response
     auto downloadLinks = responseJson["download_links"].get<std::vector<std::string>>();
+    auto downloadHash = responseJson["hash"].get<std::string>();
+
+    // Metadata
+    YAML::Node metadata;
+    metadata["hash"] = downloadHash;
+    metadata["downloaded_files"] = std::vector<std::string>();
 
     // Download all files and store them in cache folder
     for(const auto& downloadLink : downloadLinks) {
@@ -270,7 +313,13 @@ void ZooManager::downloadModel() {
         std::ofstream file(filepath, std::ios::binary);
         file.write(downloadResponse.text.c_str(), downloadResponse.text.size());
         file.close();
+
+        // Add filename to metadata
+        metadata["downloaded_files"].push_back(filename);
     }
+
+    // Save metadata to file
+    utility::saveYaml(metadata, getMetadataFilePath());
 }
 
 std::string ZooManager::loadModelFromCache() const {
@@ -296,12 +345,32 @@ std::string getModelFromZoo(const NNModelDescription& modelDescription, bool use
     // Check if model is cached
     bool modelIsCached = zooManager.isModelCached();
     bool useCachedModel = useCached && modelIsCached;
+    bool internetIsAvailable = zooManager.internetIsAvailable();
+    nlohmann::json responseJson;
+
+    if(internetIsAvailable) {
+        responseJson = zooManager.fetchModelDownloadLinks();
+    }
 
     // Use cached model if present and useCached is true
     if(useCachedModel) {
-        std::string modelPath = zooManager.loadModelFromCache();
-        Logging::getInstance().logger.info("Using cached model located at {}", modelPath);
-        return modelPath;
+        if(!internetIsAvailable) {
+            std::string modelPath = zooManager.loadModelFromCache();
+            Logging::getInstance().logger.info("Using cached model located at {}", modelPath);
+            return modelPath;
+        }
+
+        auto responseHash = responseJson["hash"].get<std::string>();
+        auto metadata = utility::loadYaml(zooManager.getMetadataFilePath());
+        auto metadataHash = utility::yamlGet<std::string>(metadata, "hash");
+
+        if(responseHash == metadataHash) {
+            std::string modelPath = zooManager.loadModelFromCache();
+            Logging::getInstance().logger.info("Using cached model located at {}", modelPath);
+            return modelPath;
+        }
+
+        Logging::getInstance().logger.warn("Cached model hash does not match response hash, downloading anew ...");
     }
 
     // Remove cached model if present
@@ -309,12 +378,16 @@ std::string getModelFromZoo(const NNModelDescription& modelDescription, bool use
         zooManager.removeModelCacheFolder();
     }
 
+    if(!internetIsAvailable) {
+        throw std::runtime_error("No internet connection available. Please check your network settings and try again.");
+    }
+
     // Create cache folder
     zooManager.createCacheFolder();
 
     // Download model
     Logging::getInstance().logger.info("Downloading model from model zoo");
-    zooManager.downloadModel();
+    zooManager.downloadModel(responseJson);
 
     // Find path to model in cache
     std::string modelPath = zooManager.loadModelFromCache();
@@ -349,6 +422,21 @@ void downloadModelsFromZoo(const std::string& path, const std::string& cacheDire
             Logging::getInstance().logger.error("Failed to download model [{} / {}]: {}\n{}", i + 1, yamlFiles.size(), yamlFile, e.what());
         }
     }
+}
+
+bool ZooManager::internetIsAvailable() const {
+    constexpr int timeout_ms = 5000;
+    constexpr std::string_view host = "http://example.com";
+    try {
+        cpr::Response r = cpr::Get(cpr::Url{host}, cpr::Timeout{timeout_ms});
+        return r.status_code == cpr::status::HTTP_OK;
+    } catch(const std::exception& e) {
+        return false;
+    }
+}
+
+std::string ZooManager::getMetadataFilePath() const {
+    return combinePaths(getModelCacheFolderPath(cacheDirectory), "metadata.yaml");
 }
 
 }  // namespace dai
