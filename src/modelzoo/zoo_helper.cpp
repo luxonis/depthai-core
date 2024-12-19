a53017737d3de2db5bc3a478c606ee86f459cfd2
lnotspotl <lnotspotl@gmail.com>
2024-12-19 09:07:31 -0500

    Fix zoo_helper build

--- src/modelzoo/zoo_helper.cpp
+++ src/modelzoo/zoo_helper.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-#include <string>
+#include <argparse/argparse.hpp>
 
 #include "depthai/modelzoo/Zoo.hpp"
 
 int main(int argc, char* argv[]) {
-    // Check if the number of arguments is correct
-    if(argc != 3) {
-        std::cout << "Usage: zoo_helper folder_with_yaml_files cache_folder" << std::endl;
+
+    // Add arguments
+    argparse::ArgumentParser program("DepthAI Model Zoo Helper");
+    program.add_argument("--yaml_folder").help("Folder with YAML files describing models to download");
+    program.add_argument("--cache_folder").help("Cache folder to download models into");
+
+    // Parse arguments
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
         return EXIT_FAILURE;
     }
 
     // Unpack arguments
-    const std::string yamlFolder = argv[1];
-    const std::string cacheFolder = argv[2];
+    auto yamlFolder = program.get<std::string>("--yaml_folder");
+    auto cacheFolder = program.get<std::string>("--cache_folder");
+
+    // Print arguments
+    std::cout << "Downloading models defined in yaml files in folder: " << yamlFolder << std::endl;
+    std::cout << "Downloading models into cache folder: " << cacheFolder << std::endl;
 
     // Download models
     dai::downloadModelsFromZoo(yamlFolder, cacheFolder);
