e74550b0a5f5e71644ece81d24f78fd9dd23eb14
Matevz Morato <matevz.morato@gmail.com>
2024-12-30 14:22:30 +0100

    Bump the epsilon in benchmark test

--- tests/src/ondevice_tests/pipeline/node/benchmark_test.cpp
+++ tests/src/ondevice_tests/pipeline/node/benchmark_test.cpp
@@ -34,7 +34,7 @@ void testBenchmarkIn(bool benchmarkInRunOnHost, bool benchmarkOutRunOnHost, floa
         auto reportData = reportQueue->get<dai::BenchmarkReport>();
         REQUIRE(reportData != nullptr);
         REQUIRE(reportData->numMessagesReceived > 1);
-        REQUIRE(reportData->fps == Catch::Approx(fps).epsilon(0.05));
+        REQUIRE(reportData->fps == Catch::Approx(fps).epsilon(0.1));
     }
 }
 
@@ -51,7 +51,7 @@ void testCameraBenchmarking(float fps) {
         auto reportData = reportQueue->get<dai::BenchmarkReport>();
         REQUIRE(reportData != nullptr);
         REQUIRE(reportData->numMessagesReceived > 1);
-        REQUIRE(reportData->fps == Catch::Approx(fps).epsilon(0.05));
+        REQUIRE(reportData->fps == Catch::Approx(fps).epsilon(0.1));
     }
 }
 
