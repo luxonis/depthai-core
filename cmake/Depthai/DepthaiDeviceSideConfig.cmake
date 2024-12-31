1d10ac4d8bbcc9429087c688f03bc836a838abfc
Matevz Morato <matevz.morato@gmail.com>
2024-12-31 02:09:11 +0100

    Bump RVC2 and RVC4 FW to v3_develop
    RVC2 FW also includes missing MessageGroup bindings

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5e016a328ac84324fb3c6bd8904141191f29dc2e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "052ee5648d9d5a49e6367c4a81d14a9b9ae8bfcf")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
