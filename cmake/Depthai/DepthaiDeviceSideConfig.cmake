3a6a3b67d36faf4de0ea93f085f8cce407a44005
asahtik <asahtik@gmail.com>
2025-01-06 14:07:27 +0100

    FW: Fix imagemanipv2 issue on RVC2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "cd3ddb9d67f8e2a5007de6bf27818ec7c5916e24")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "435d0b10d52cb3e78b30f64b9a6e2e28da28cc76")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
