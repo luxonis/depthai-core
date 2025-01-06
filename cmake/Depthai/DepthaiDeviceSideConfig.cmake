9ec8927b35504f4f01dda702326b02b86c3c15ec
asahtik <asahtik@gmail.com>
2025-01-06 11:31:33 +0100

    Fix encoded_frame_tests [no ci]

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "fd872d7b445d70304d585652dadb8d25f5ec52d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "51bc2defa0658f5187228cdb6801134bbc37ea04")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
