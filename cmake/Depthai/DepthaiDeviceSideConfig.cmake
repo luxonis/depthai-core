2829e5d6f7216a6eaedfebe27dded9495dca473f
SzabolcsGergely <szabi@luxonis.com>
2025-01-07 15:42:01 +0200

    Stereo: PostProcessing: Handle edge case when median is last stage in post-processing pipeline AND depth output is enabled

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f9e14e2721e6ffbcb0402342a6186d5e38dbd0c6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e106a257e5c41a1167df769e53d6e2512a5a26d9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
