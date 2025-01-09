fe7061aea35a44500876569f84d9a94195631e65
asahtik <asahtik@gmail.com>
2025-01-09 12:19:26 +0100

    FW: PR fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9626f5a66abd91139233a6fef38a073bfe981120")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0019499dc72f4583f525da41e7e81b9a0ef4a629")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
