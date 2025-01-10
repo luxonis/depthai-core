8db82bbf4fd5f889af7767769d62517e3512f7b2
asahtik <asahtik@gmail.com>
2025-01-10 11:50:21 +0100

    FW: handle missing calib when setting initial transformation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0019499dc72f4583f525da41e7e81b9a0ef4a629")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9ee0a08d006cdab6191aa9f7e98b494880e24896")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
