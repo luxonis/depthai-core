3b6d0f41e26d378d7e9671fb5a8192c536fe4725
lnotspotl <lnotspotl@gmail.com>
2025-01-10 11:16:46 -0500

    Add helper script for zoo helper uploading to artifactory

---
+++ ci/upload-artifactory-zoo-helper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download jfrog cli
+curl -fL https://getcli.jfrog.io | sh
+
+# Set paths
+export PATH_PREFIX=luxonis-depthai-helper-binaries/zoo_helper
+export ZOO_HELPER_BINARY_LOCAL_PATH=build/zoo_helper
+
+# Get git hash  
+export ZOO_HELPER_GIT_HASH=$(git rev-parse HEAD | cut -c1-8)
+
+# Upload binary to artifactory
+jfrog config add --artifactory-url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
+jfrog rt u "$ZOO_HELPER_BINARY_LOCAL_PATH" "$PATH_PREFIX/$ZOO_HELPER_GIT_HASH/"
