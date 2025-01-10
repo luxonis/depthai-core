6fc514b1811964e8db26b3eff44108b1fbd6e82c
asahtik <asahtik@gmail.com>
2025-01-10 13:56:39 +0100

    FW: fix crash when using Cono/ColorCamera

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9ee0a08d006cdab6191aa9f7e98b494880e24896")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3c7d7c93a4c4c55d235ff7e406ed8824b9c18fef")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
