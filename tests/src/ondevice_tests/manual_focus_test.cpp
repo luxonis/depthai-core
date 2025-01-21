35ea6f7cc005a8445cc54acbfd4bf6486a31ea92
Jakob Socan <jakob.socan@luxonis.com>
2025-01-21 14:17:18 +0100

    Added initial manual focus config test

--- tests/src/ondevice_tests/manual_focus_test.cpp
+++ tests/src/ondevice_tests/manual_focus_test.cpp
@@ -27,3 +27,26 @@ TEST_CASE("Test manual focus") {
     }
     pipeline.stop();
 }
+
+TEST_CASE("Test manual focus initial config") {
+    dai::Pipeline pipeline;
+    auto fps = 30.0f;
+    auto camRgb = pipeline.create<dai::node::Camera>()->build();
+    auto* output = camRgb->requestOutput(std::make_pair(640, 480), std::nullopt, dai::ImgResizeMode::CROP, fps);
+    auto requestedLensPos = 0.643f;
+    std::cout << "Setting initial lens position to " << requestedLensPos << "\n" << std::flush;
+    auto ctrl = std::make_shared<dai::CameraControl>();
+    camRgb->initialControl.setManualFocusRaw(requestedLensPos);
+    auto outputQueue = output->createOutputQueue();
+    pipeline.start();
+    auto lastLensPosition = -1.0f;
+    for(int idx = 0; idx < static_cast<int>(fps); ++idx) {
+        auto imgFrame = outputQueue->get<dai::ImgFrame>();
+        lastLensPosition = imgFrame->getLensPositionRaw();
+    }
+    std::cout << "Lens position after 1s was: " << lastLensPosition << "\n" << std::flush;
+    auto lensPositionDiff = std::abs(lastLensPosition - requestedLensPos);
+    std::cout << "Diff was: " << lensPositionDiff << "\n" << std::flush;
+    REQUIRE(lensPositionDiff < 0.001);
+    pipeline.stop();
+}
