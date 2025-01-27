4860916ac3dd72efb6cdb95391ee1ddf54a78eb0
SzabolcsGergely <szabi@luxonis.com>
2025-01-27 17:23:52 +0200

    FW: ImageManipV2: set undistortion parameters to 0 after undistortion

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "976351fda3b70483e37882f989686bf0aa8c604d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9790e20042a593300bac8ade6c55cd3829403e41")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
