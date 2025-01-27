3947c929c7648a44177025e255cf62823ab5fe8a
asahtik <asahtik@gmail.com>
2025-01-27 12:49:39 +0100

    RVC2 FW: Add ImageManipConfigV2 bindings in script node

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "976351fda3b70483e37882f989686bf0aa8c604d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6c647e1b1660d45fe408e74f3aa0af852992203f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
