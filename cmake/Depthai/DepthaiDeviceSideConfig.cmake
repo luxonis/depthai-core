cc0b3365b5b6f6a1351ff3287bcf0d59823dbdb1
SzabolcsGergely <szabi@luxonis.com>
2025-01-28 16:55:39 +0200

    RVC2: Update FW; Improve ImgManipV2 output frame transformation data

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9790e20042a593300bac8ade6c55cd3829403e41")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a9d354351694c5dd96def768c0b0155354cab38c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
