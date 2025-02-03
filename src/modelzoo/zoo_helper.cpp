997bc7de696c65e9aea21a58c0c1eab0032cc19c
lnotspotl <lnotspotl@gmail.com>
2025-02-02 19:36:58 -0500

    Add defaults, format, add api_key as a parameter

--- src/modelzoo/zoo_helper.cpp
+++ src/modelzoo/zoo_helper.cpp
@@ -1,19 +1,26 @@
-#include <iostream>
 #include <argparse/argparse.hpp>
+#include <iostream>
 
 #include "depthai/modelzoo/Zoo.hpp"
 
 int main(int argc, char* argv[]) {
+    // Initialize parser
+    argparse::ArgumentParser program("DepthAI Model Zoo Helper");
 
     // Add arguments
-    argparse::ArgumentParser program("DepthAI Model Zoo Helper");
-    program.add_argument("--yaml_folder").help("Folder with YAML files describing models to download");
-    program.add_argument("--cache_folder").help("Cache folder to download models into");
+    const std::string DEFAULT_YAML_FOLDER = ".";
+    program.add_argument("--yaml_folder").default_value(DEFAULT_YAML_FOLDER).help("Folder with YAML files describing models to download");
+
+    const std::string DEFAULT_CACHE_FOLDER = dai::MODEL_ZOO_DEFAULT_CACHE_DIRECTORY;
+    program.add_argument("--cache_folder").default_value(DEFAULT_CACHE_FOLDER).help("Cache folder to download models into");
+
+    const std::string DEFAULT_API_KEY = "";
+    program.add_argument("--api_key").default_value(DEFAULT_API_KEY).help("API key to use for downloading models");
 
     // Parse arguments
     try {
         program.parse_args(argc, argv);
-    } catch (const std::runtime_error& err) {
+    } catch(const std::runtime_error& err) {
         std::cerr << err.what() << std::endl;
         std::cerr << program;
         return EXIT_FAILURE;
@@ -22,13 +29,17 @@ int main(int argc, char* argv[]) {
     // Unpack arguments
     auto yamlFolder = program.get<std::string>("--yaml_folder");
     auto cacheFolder = program.get<std::string>("--cache_folder");
+    auto apiKey = program.get<std::string>("--api_key");
 
     // Print arguments
     std::cout << "Downloading models defined in yaml files in folder: " << yamlFolder << std::endl;
     std::cout << "Downloading models into cache folder: " << cacheFolder << std::endl;
+    if(!apiKey.empty()) {
+        std::cout << "Using API key: " << apiKey << std::endl;
+    }
 
     // Download models
-    dai::downloadModelsFromZoo(yamlFolder, cacheFolder);
+    dai::downloadModelsFromZoo(yamlFolder, cacheFolder, apiKey);
 
     return EXIT_SUCCESS;
 }
