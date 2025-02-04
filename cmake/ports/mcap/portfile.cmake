7f650041a43960edf56060584fc8cd74bbbadb86
asahtik <asahtik@gmail.com>
2025-02-04 09:44:14 +0100

    Add NO_ZSTD definition to mcap proj

--- cmake/ports/mcap/portfile.cmake
+++ cmake/ports/mcap/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO luxonis/mcap_builder
-    REF 32c455dbe67df11f9e7f731e57aa9caa065af834
-    SHA512 d0e6681238b76449c272543bd2cc8bb62bb40ffd0cfd89fc279e0619087dc617f67ea6a6d4e66fde4a1e1be594b5a92fc99eb4663ce31b3832b9592d7bc51004
+    REF a0d93edb81e3cca041b8be45f65e58e65c8d4e0a
+    SHA512 e56824a96388069ee7dbaeb092a47bf3c73061c6e074230cc889987f1b957409bdcfe40ce2f6e4371b5b99574ecac544b4ca8479397c5b8cc4f2f9b2b9c903a6
     HEAD_REF main
 )
 vcpkg_cmake_configure(
