dec8f33d8d0a671fd4256b3c7546fdb3186ee887
Serafadam <adam.serafin@luxonis.com>
2025-02-05 10:38:55 +0100

    rgbdata serialization

--- include/depthai/pipeline/datatype/RGBDData.hpp
+++ include/depthai/pipeline/datatype/RGBDData.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "depthai/common/ADatatypeSharedPtrSerialization.hpp"
 #include "depthai/pipeline/datatype/Buffer.hpp"
 #include "depthai/pipeline/datatype/ImgFrame.hpp"
+#include "depthai/pipeline/datatype/ADatatype.hpp"
+#include "depthai/utility/Serialization.hpp"
 
 namespace dai {
 
@@ -17,14 +20,17 @@ class RGBDData : public Buffer {
 
     virtual ~RGBDData() = default;
 
-    ImgFrame rgbFrame;
-    ImgFrame depthFrame;
+    std::map<std::string, std::shared_ptr<ADatatype>> frames;
+    void setRGBFrame(const std::shared_ptr<ImgFrame>& frame);
+    void setDepthFrame(const std::shared_ptr<ImgFrame>& frame);
+    std::shared_ptr<ImgFrame> getRGBFrame();
+    std::shared_ptr<ImgFrame> getDepthFrame();
 
     void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
         metadata = utility::serialize(*this);
         datatype = DatatypeEnum::RGBDData;
     };
-    DEPTHAI_SERIALIZE(RGBDData, Buffer::sequenceNum, Buffer::ts, Buffer::tsDevice, rgbFrame, depthFrame);
+    DEPTHAI_SERIALIZE(RGBDData, frames,  Buffer::ts, Buffer::tsDevice, Buffer::sequenceNum);
 };
 
 }  // namespace dai
--- src/pipeline/datatype/RGBDData.cpp
+++ src/pipeline/datatype/RGBDData.cpp
@@ -1,5 +1,19 @@
 #include "depthai/pipeline/datatype/RGBDData.hpp"
 
 namespace dai {
-;  // TODO - no impl needed
+void RGBDData::setRGBFrame(const std::shared_ptr<ImgFrame>& frame) {
+    frames["rgb"] = frame;
+}
+
+void RGBDData::setDepthFrame(const std::shared_ptr<ImgFrame>& frame) {
+    frames["depth"] = frame;
+}
+
+std::shared_ptr<ImgFrame> RGBDData::getRGBFrame() {
+    return std::dynamic_pointer_cast<ImgFrame>(frames["rgb"]);
+}
+
+std::shared_ptr<ImgFrame> RGBDData::getDepthFrame() {
+    return std::dynamic_pointer_cast<ImgFrame>(frames["depth"]);
+}
 }  // namespace dai
