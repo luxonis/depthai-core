5e8e943dac8c4f6bd57576336fd349db2fb0f047
SzabolcsGergely <szabi@luxonis.com>
2025-02-20 21:25:57 +0200

    RVC2 FW: Further fixes for message group

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1b6ed70ef9d1e006d6deaeda8164db84c33e227a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4aa5fb3f4f0ba2e90ef27bea89717a76a0cbbc04")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
