286f73bb8b22f6acf926b4484439738e10ccecae
asahtik <asahtik@gmail.com>
2025-02-24 09:57:28 +0100

    RVC2 FW: Fix ImageManipV2 not rebuilding on some config changes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "02fa0d0b25194151c5b656ab222b6028cd5aa773")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3b5f2046b44abd89c2e3437eb1afeb82979a8c1b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
