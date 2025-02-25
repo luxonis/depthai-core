9561cad4c9961454aff86e8951743cdbb4398387
asahtik <asahtik@gmail.com>
2025-02-25 17:28:37 +0100

    Split mockimu tests

--- tests/src/ondevice_tests/pipeline/node/record_replay_test.cpp
+++ tests/src/ondevice_tests/pipeline/node/record_replay_test.cpp
@@ -85,44 +85,6 @@ TEST_CASE("RecordVideo node") {
     REQUIRE(std::filesystem::exists(helper.testFolder + "/recording_video/video.mp4"));
 }
 
-TEST_CASE("MockIn IMU") {
-    TestHelper helper;
-
-    dai::Pipeline p;
-
-    auto replayNode = p.create<dai::node::ReplayMetadataOnly>();
-    replayNode->setReplayFile(helper.testFolder + "/extracted/IMU.mcap");
-    replayNode->setLoop(false);
-
-    auto imu = p.create<dai::node::IMU>();
-    imu->enableIMUSensor(dai::IMUSensor::ACCELEROMETER_RAW, 500);
-    imu->enableIMUSensor(dai::IMUSensor::GYROSCOPE_RAW, 400);
-    imu->setBatchReportThreshold(100);
-
-    replayNode->out.link(imu->mockIn);
-
-    auto imuOut = imu->out.createOutputQueue();
-    auto replayOut = replayNode->out.createOutputQueue();
-
-    p.start();
-    auto imuData = imuOut->get<dai::IMUData>();
-    auto replayData = replayOut->get<dai::IMUData>();
-
-    unsigned int i = 0;
-    while(p.isRunning()) {
-        if(i >= NUM_MSGS) break;
-        if(imuData->getSequenceNum() > replayData->getSequenceNum()) {
-            replayData = replayOut->get<dai::IMUData>();
-        } else if(imuData->getSequenceNum() < replayData->getSequenceNum()) {
-            imuData = imuOut->get<dai::IMUData>();
-        } else {
-            REQUIRE(imuData->str() == replayData->str());
-            i++;
-        }
-    }
-    p.stop();
-}
-
 TEST_CASE("MockIn Camera") {
     TestHelper helper;
 
