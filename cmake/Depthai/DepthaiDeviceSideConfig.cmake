1678b9abb593a950baee1ca077e84dff30846740
asahtik <38485424+asahtik@users.noreply.github.com>
2025-02-26 10:34:19 +0100

    Fix ImageManipV2 not rebuilding on some config changes (#1254)
    * Fix ImageManipV2 not rebuilding on some config changes [no ci]
    
    * Added test for manip rebuild fix [no ci]
    
    * RVC4 FW: Fix ImageManipV2 not rebuilding on some config changes [no ci]
    
    * RVC2 FW: Fix ImageManipV2 not rebuilding on some config changes
    
    * Fix manipv2 rebuild test
    
    * Use string config as hash to rebuild [no ci]
    
    * Bump rvc4 fw
    
    * Fix test
    
    * Fix test for rvc2
    
    * Bump rvc4 fw

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "02fa0d0b25194151c5b656ab222b6028cd5aa773")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "28e44473f9c42ece1dadfea80d2077cde74f7301")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
