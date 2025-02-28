454a556b8c44f469c4e46b89497f14ee4c7f052e
Adam Serafin <adam.serafin@luxonis.com>
2025-02-28 12:28:22 +0100

    V3 Yolo model type deduction (#1225)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "28e44473f9c42ece1dadfea80d2077cde74f7301")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "911d3cddf2259bc77cc6f3939252cfd72da927ea")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
