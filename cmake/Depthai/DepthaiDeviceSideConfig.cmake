a14a3eb4c02829f1468a2d5d02623b8e40df9d76
Matevz Morato <matevz.morato@gmail.com>
2025-03-04 15:32:29 +0100

    Bump RVC2 and RVC4 FW to develop with DetectionParser fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "911d3cddf2259bc77cc6f3939252cfd72da927ea")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "813a8ba574dea43f32fac762d186340a4e050484")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
