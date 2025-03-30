2b4d780302f7ea7f6861c4d3b95ac912737b8158
SzabolcsGergely <szabi@luxonis.com>
2025-03-30 17:24:37 +0300

    Update FW: Fix decimation filter + RGB depth alignment with default stereo config

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a62b2ccb0bc493c2fb41694cb81c08887be24c52")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ac810ed65fbf5247658fea452698e759c294c8a5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
