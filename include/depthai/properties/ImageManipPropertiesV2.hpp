55cace5815fe28b07a6f1c293547d5fa849b20e7
asahtik <asahtik@gmail.com>
2025-04-30 10:31:31 +0200

    Add balanced option to manipv2 perf mode

--- include/depthai/properties/ImageManipPropertiesV2.hpp
+++ include/depthai/properties/ImageManipPropertiesV2.hpp
@@ -11,7 +11,7 @@ namespace dai {
  */
 struct ImageManipPropertiesV2 : PropertiesSerializable<Properties, ImageManipPropertiesV2> {
     enum class Backend : uint8_t { CPU, HW };
-    enum class PerformanceMode : uint8_t { AUTO, PERFORMANCE, LOW_POWER };
+    enum class PerformanceMode : uint8_t { AUTO, PERFORMANCE, BALANCED, LOW_POWER };
 
     /// Initial configuration for ImageManip node
     ImageManipConfigV2 initialConfig;
