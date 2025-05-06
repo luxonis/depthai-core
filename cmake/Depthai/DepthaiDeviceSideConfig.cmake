7b47539c3e4a4f8b16be2495fea2c38c958e0fc8
asahtik <asahtik@gmail.com>
2025-05-06 15:34:39 +0200

    RVC2 FW: Add name to ImgDetection

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4f0b0f3219b701b8e5408eea19648a8d0ec06fa7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3d7da1ac916fbfad83c850f4a21a34698da5458b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
