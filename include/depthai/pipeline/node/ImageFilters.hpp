1b53532993d2e3d9c2d2a6229fdcccf9a52141ce
lnotspotl <lnotspotl@gmail.com>
2025-05-06 07:25:55 -0400

    Update docstrings

--- include/depthai/pipeline/node/ImageFilters.hpp
+++ include/depthai/pipeline/node/ImageFilters.hpp
@@ -13,12 +13,16 @@ class ImageFilters : public DeviceNodeCRTP<DeviceNode, ImageFilters, ImageFilter
     constexpr static const char* NAME = "ImageFilters";
     using DeviceNodeCRTP::DeviceNodeCRTP;
 
+    /**
+     * Input for image frames to be filtered
+     */
     Node::Input input{
         *this,
         {"input", Node::DEFAULT_GROUP, Node::DEFAULT_BLOCKING, Node::DEFAULT_QUEUE_SIZE, {{{DatatypeEnum::ImgFrame, true}}}, Node::DEFAULT_WAIT_FOR_MESSAGE}};
-    
-    
-    
+
+    /**
+     * Filtered frame
+     */
     Node::Output output{*this, {"output", Node::DEFAULT_GROUP, {{{DatatypeEnum::ImgFrame, false}}}}};
 
     /**
@@ -67,9 +71,16 @@ class DepthConfidenceFilter : public DeviceNodeCRTP<DeviceNode, DepthConfidenceF
     constexpr static const char* NAME = "DepthConfidenceFilter";
     using DeviceNodeCRTP::DeviceNodeCRTP;
 
+    /**
+     * Depth frame image, expected ImgFrame type is RAW8 or RAW16.
+     */
     Node::Input depth{
         *this,
         {"depth", Node::DEFAULT_GROUP, Node::DEFAULT_BLOCKING, Node::DEFAULT_QUEUE_SIZE, {{{DatatypeEnum::ImgFrame, true}}}, Node::DEFAULT_WAIT_FOR_MESSAGE}};
+
+    /**
+     * Amplitude frame image, expected ImgFrame type is RAW8 or RAW16.
+     */
     Node::Input amplitude{*this,
                           {"amplitude",
                            Node::DEFAULT_GROUP,
@@ -78,9 +89,19 @@ class DepthConfidenceFilter : public DeviceNodeCRTP<DeviceNode, DepthConfidenceF
                            {{{DatatypeEnum::ImgFrame, true}}},
                            Node::DEFAULT_WAIT_FOR_MESSAGE}};
 
+    /**
+     * RAW16 encoded filtered depth frame
+     */
     Node::Output filteredDepth{*this, {"filteredDepth", Node::DEFAULT_GROUP, {{{DatatypeEnum::ImgFrame, false}}}}};
+
+    /**
+     * RAW16 encoded confidence frame
+     */
     Node::Output confidence{*this, {"confidence", Node::DEFAULT_GROUP, {{{DatatypeEnum::ImgFrame, false}}}}};
 
+    /**
+     * Config message for runtime filter configuration
+     */
     Node::Input config{*this,
                        {"config",
                         Node::DEFAULT_GROUP,
