c878d42369a18f549049b2bbe36a707dcdfcb82f
asahtik <asahtik@gmail.com>
2025-05-07 10:30:01 +0200

    RVC2 FW: Fix build

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "3d7da1ac916fbfad83c850f4a21a34698da5458b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "93eeaff81d82368bbe2c326465b1528f8ae93444")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
