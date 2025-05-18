906b34005770b4dc91831e355a3e6820ed6b111f
SzabolcsGergely <szabi@luxonis.com>
2025-05-18 04:14:52 +0300

    Camera: Fix small camera outputs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "211744dd005b80f51ceedec4d5a85d4bf59cee43")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "27eb8c01e0ce8f22087b86d2198a8a6fe4765905")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
