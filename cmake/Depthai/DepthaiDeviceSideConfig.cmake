cc9c5fe191c0cfc5adf02b0642d628efe73a6eb5
lnotspotl <lnotspotl@gmail.com>
2025-05-20 09:35:09 -0400

    Update DepthaiDeviceSideConfig.cmake

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bcd64465b4f61c53c845696cea74c81a823fb8d0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c5fce2452ec689a72d5eb22181a967c7e66a02bd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
