3573fa7306151432a3f5d9747dca1a875e110d80
Serafadam <adam.serafin@luxonis.com>
2025-05-21 18:49:33 +0200

    update side commit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "229909faa42e0bdb4ce54016d545395dc47aa266")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "07c9efe63918155ebe9934d0ffe670c17790c3db")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
