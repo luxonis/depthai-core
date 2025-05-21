f0b9c794c1c3341b30f1d11a3bb62c25d9d97aa7
SzabolcsGergely <szabi@luxonis.com>
2025-05-22 01:22:38 +0300

    Fix ColorCamera

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "59fcf7caea7916be8ddff2ddebb02a421f2ee069")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4ea75d21ab4692658b1b4fe257d0c65ebf76e6f5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
