cf17613d2f72d675f9b27fb88bd2cf58a45abdec
asahtik <38485424+asahtik@users.noreply.github.com>
2025-05-21 12:18:00 +0200

    Fix empty name field in SpatialImgDetections (#1330)
    * Serialize spatial img detection name field
    
    * RVC4 FW: Serialize spatial img detection name field
    
    * RVC2 FW: Serialize spatial img detection name field

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9ed0ae7d4e9a249e0836b7cb4e65c5e76cb4d2f8")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fd4f2c9dc2eef1f5be7cb29e25cca0df1ad1dc3b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
