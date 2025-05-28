a960ed1cb5b21fa9d4ef6a2cbecf679ba6af662c
alex-luxonis <alex@luxonis.com>
2025-05-28 11:43:53 +0300

    DM9098 R6+ with OV9782 center, OAK-D-SR-PoE with IMX378

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e2bd5cd83e7774889d2c274aab815a5117c54f85")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b0c0bf7503e8631dc4ebc5252963a3b8dc4e1386")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
