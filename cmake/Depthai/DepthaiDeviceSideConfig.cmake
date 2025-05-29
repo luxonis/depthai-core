c0b03d6325c3bb96582caee586a6e8adc1dc2269
SzabolcsGergely <szabi@luxonis.com>
2025-05-29 21:07:40 +0300

    Update RVC2 FW: handle ToF phase unwrapping underflow

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d668e3bfa9619436c70bf069788f31a2d224a8d5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1fa2b66c77fc84d0a97653b868f7e9c88bdce690")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
