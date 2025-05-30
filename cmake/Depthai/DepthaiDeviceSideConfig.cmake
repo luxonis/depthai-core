3caebda8bf6a8722fa0b63343d85a76537557436
Serafadam <adam.serafin@luxonis.com>
2025-05-30 14:06:24 +0200

    update lite t lr and set poe to default

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "74f70039b2c1a74085a798d7d86d7dbf5fa804b7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2a1f93801db04cf208ad386e99e24a8489c75891")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
