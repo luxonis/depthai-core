9876bf2d8a0ce88e6210a3736037c288377c8d48
Serafadam <adam.serafin@luxonis.com>
2025-05-30 21:54:01 +0200

    update rotation matrices

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "76c79b1b03257a89f59f6888545a2b4791c66ac2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1920862151f32687d077210faf3352ee01c64a5b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
