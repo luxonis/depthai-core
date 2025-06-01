00b26884cff435fe8193e195be769cd004de37af
asahtik <38485424+asahtik@users.noreply.github.com>
2025-06-01 11:44:26 +0200

    Rename ImgDetection name to labelName (#1341)
    * Rename name to labelName
    
    * RVC4 FW: rename name to labelName [no ci]
    
    * Revert manip_all_ops change [no ci]
    
    * RVC2 FW: Rename name to labelName in ImgDetection
    
    * Bump device fw commit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "323b3738e333f812abb1455b5c0c9b0b8be7e1c2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e54ddc1093d95597e7aad922cb11cbd0c9fe4bf2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
