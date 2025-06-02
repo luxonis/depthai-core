b516fe5f848c6f45bf7cf0438d1592dfb5f6bdd0
Matevz Morato <matevz.morato@gmail.com>
2025-06-02 20:07:15 +0200

    [RVC2 FW] Unify IMU across all devices and pre-rotate to camera CS

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e54ddc1093d95597e7aad922cb11cbd0c9fe4bf2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9a6c343e85d26e418a77bd5efecd3ba676972518")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
