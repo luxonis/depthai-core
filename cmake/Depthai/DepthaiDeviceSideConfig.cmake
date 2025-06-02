4954b82fdf5b2bb3afaa91a1deb8f142082f6b1b
alex-luxonis <alex@luxonis.com>
2025-06-02 11:38:09 +0300

    Fix to have new SR-PoE w/IMX378 as C83, and proper color/mono reporting

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b0c0bf7503e8631dc4ebc5252963a3b8dc4e1386")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a03a09ef6dc24e5a8a7d7f767687590cc7a15ca6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
