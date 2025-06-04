a9e2dd6a0c5c882a26e921c3ae20e40cd715340d
zrezke <jeretina.filip@gmail.com>
2025-06-04 13:56:40 +0200

    IMU: Don't re-probe the IMU when quirying the imu firmware update status.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a03a09ef6dc24e5a8a7d7f767687590cc7a15ca6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b05a054b9db4905db01e18db95891daf9ca02df0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
