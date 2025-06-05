e7e0f446ed339a200817484e83089ec4fa08c39f
lnotspotl <lnotspotl@gmail.com>
2025-06-05 03:52:31 -0400

    fix ambiguity

---
+++ examples/python/ModelZoo/mača/model_zoo.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.insert(1, r"C:\Users\kubjo\Documents\depthai-core\build\bindings\python\Release")
+
+import time
+import depthai as dai
+
+# Describe the model I want to download - two options: in program or from yaml file
+
+# Option 1: In program
+# modelDescription = dai.NNModelDescription(model="yolov6-nano", platform="RVC2", ...)
+
+# Option 2: From yaml file
+modelDescription = dai.NNModelDescription.fromYamlFile("./mymodel.yaml")
+
+# If you want, you can store the model description in a yaml file
+modelDescription.saveToYamlFile("./mymodel.yaml")
+
+# Return path to downloaded model - yolov6-nano-r2-288x512.tar.xz for this example
+print("Downloading")
+modelPath = dai.getModelFromZoo(modelDescription, useCached=False, progressFormat="pretty")
+print(modelPath)
+print(f"Model path: {modelPath}")
+
+# Load the model (most of the time it's a NNArchive)
+archive = dai.NNArchive(modelPath)
+
+print(f"Input size of the model {archive.getInputSize()}, name is {archive.getConfig().model.metadata.name}")
+# The arhive can then be used with any of the neural network nodes (NeuralNetwork, DetectionNetwork, SpatialDetectionNetwork)
