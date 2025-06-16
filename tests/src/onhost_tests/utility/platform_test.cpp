68122325b370b64d0ff891767c236be2b860dde2
lnotspotl <lnotspotl@gmail.com>
2025-06-16 11:21:57 -0400

    Add fslock process testing

--- tests/src/onhost_tests/utility/platform_test.cpp
+++ tests/src/onhost_tests/utility/platform_test.cpp
@@ -10,6 +10,8 @@
 #include <thread>
 #include <vector>
 
+#include "../../../include/subprocess/subprocess.hpp"
+
 #ifndef _WIN32
     #include <sys/wait.h>
 #endif
@@ -63,47 +65,30 @@ TEST_CASE("FolderLock basic functionality", "[platform]") {
     fs::remove_all(tempDir);
 }
 
-#ifndef _WIN32
-TEST_CASE("Process-level locking", "[platform]") {
+TEST_CASE("Process-level locking with dummy", "[platform]") {
     auto tempFile = fs::path(dai::platform::getTempPath()) / "test_lock_file.txt";
-    std::ofstream(tempFile).close();  // create file
-    std::cout << "tempFile: " << tempFile.string() << std::endl;
+    std::ofstream(tempFile).close();
 
     auto start = std::chrono::steady_clock::now();
 
-    pid_t pid1 = fork();
-    if(pid1 == 0) {
-        // Child process 1
-        auto lock = FileLock::lock(tempFile.string());
-        REQUIRE(lock->holding());
-        std::cout << "Process 1 holding lock" << lock->holding() << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        lock->unlock();
-        exit(0);
-    }
+    std::cout << "FSLOCK_DUMMY_PATH: " << FSLOCK_DUMMY_PATH << std::endl;
+    std::cout << "tempFile: " << tempFile << std::endl;
 
-    pid_t pid2 = fork();
-    if(pid2 == 0) {
-        // Child process 2
-        auto lock = FileLock::lock(tempFile.string());
-        REQUIRE(lock->holding());
-        std::cout << "Process 2 holding lock" << lock->holding() << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        lock->unlock();
-        exit(0);
-    }
+    // Launch two subprocesses
+    auto proc1 = subprocess::Popen({FSLOCK_DUMMY_PATH, tempFile.string(), "A"}, subprocess::output{subprocess::PIPE});
+    auto proc2 = subprocess::Popen({FSLOCK_DUMMY_PATH, tempFile.string(), "B"}, subprocess::output{subprocess::PIPE});
+    proc1.wait();
+    proc2.wait();
 
-    // Parent process waits for both children
-    int status;
-    waitpid(pid1, &status, 0);
-    waitpid(pid2, &status, 0);
+    std::cout << "proc1 stdout:\n" << proc1.communicate().first.buf.data() << std::endl;
+    std::cout << "proc2 stdout:\n" << proc2.communicate().first.buf.data() << std::endl;
 
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    REQUIRE(duration.count() >= 2000);  // Should be more than 2 seconds (1 second each process)
-    REQUIRE(duration.count() < 2300);   // thread3 can start immediately, it's a different file
+    std::cout << "duration: " << duration.count() << "ms" << std::endl;
+    REQUIRE(duration.count() >= 2000);
+    REQUIRE(duration.count() < 3000);
 }
-#endif
 
 TEST_CASE("Thread-level locking", "[platform]") {
     auto tempFile = fs::path(dai::platform::getTempPath()) / "test_thread_lock.txt";
