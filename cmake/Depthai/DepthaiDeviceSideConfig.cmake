f4bb6418752b3751716a86e1ba472b87690efc25
zrezke <jeretina.filip@gmail.com>
2025-07-02 11:52:10 +0200

    OAK-D-SR-PoE R4 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "b05a054b9db4905db01e18db95891daf9ca02df0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "31abb4984ed31e83b47c5bd4cc1183b3a07e9fad")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
