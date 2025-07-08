9f73b8b918debd929209abe1c0dd72152126f182
alex-luxonis <alex@luxonis.com>
2025-07-08 19:03:39 +0300

    IMX462: max 114fps, exposure down to 8us (AE and manual)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e66bad3801be490b32969ab9cdb1eed9fe2316b6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5254f0cfb32a4b76aacf454ca4aad546c6837806")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
