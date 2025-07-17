6d14e52aa3331be51447e3612be11808b2ce3e06
lnotspotl <lnotspotl@gmail.com>
2025-07-16 23:43:19 -0400

    update fw hashes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "03b5eda5d16316b7a845ad9e0b30ec181da02a82")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4da086cfbb15464b8af876bda75a161955e8aa5c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
