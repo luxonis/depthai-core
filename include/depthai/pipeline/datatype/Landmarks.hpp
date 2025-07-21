50b2a369d3374654f79b43f41efee293da87f24a
dragonblade316 <dragonblade316@gmail.com>
2025-07-20 20:27:21 -0600

    First try at adding covariance

--- include/depthai/pipeline/datatype/Landmarks.hpp
+++ include/depthai/pipeline/datatype/Landmarks.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <array>
 #include <vector>
-
-#include "depthai/pipeline/datatype/Buffer.hpp"
 
 #include "depthai/common/Point3d.hpp"
 #include "depthai/common/Quaterniond.hpp"
+#include "depthai/pipeline/datatype/Buffer.hpp"
+
+// optional
+#ifdef DEPTHAI_HAVE_OPENCV_SUPPORT
+    #include <opencv2/core/mat.hpp>
+    #include <opencv2/opencv.hpp>
+#endif
+
 
 
 // #include "utility/Serialization.hpp"
@@ -31,25 +38,33 @@ struct Landmark {
      * The orientation of the landmark relative to base_frame (the camera)
      */
     dai::Quaterniond quaternion;
+
+    /**
+     * The covariance of the landmark. If you do not know what this means, you can probably just fill with 0.01.
+     */
+    std::array<std::array<double, 6>, 6> covariance;
 };
 
-DEPTHAI_SERIALIZE_EXT(Landmark, id, size, translation, quaternion);
-
+DEPTHAI_SERIALIZE_EXT(Landmark, id, size, translation, quaternion, covariance);
 
 class Landmarks : public Buffer {
-    public:
-        Landmarks() = default;
-        virtual ~Landmarks() = default;
+   public:
+    Landmarks() = default;
+    virtual ~Landmarks() = default;
 
-    public:
-        std::vector<Landmark> landmarks;
+    #ifdef DEPTHAI_HAVE_OPENCV_SUPPORT
+    cv::Mat getCovarianceAsCVMat(Landmark landmark);
+    #endif
 
-        DEPTHAI_SERIALIZE(Landmarks, Buffer::sequenceNum, Buffer::ts, Buffer::tsDevice, landmarks);
-        void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
-            metadata = utility::serialize(*this);
-            datatype = DatatypeEnum::Landmarks;
-        }
 
+   public:
+    std::vector<Landmark> landmarks;
+
+    DEPTHAI_SERIALIZE(Landmarks, Buffer::sequenceNum, Buffer::ts, Buffer::tsDevice, landmarks);
+    void serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const override {
+        metadata = utility::serialize(*this);
+        datatype = DatatypeEnum::Landmarks;
+    }
 };
 
-}
+}  // namespace dai
