30d3615fc7fbe758352c1a9fef96e977e0c97d50
Matevz Morato <matevz.morato@gmail.com>
2025-07-24 20:23:08 +0200

    [RVC2 FW] Bump with correct sensor configs for OVs

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9be49836669a9bbe254075608303bd1ad921cd9f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6c09efc37b1d5022a044ad88bd369fc3342c4eb6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
