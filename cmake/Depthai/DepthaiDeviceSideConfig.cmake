71aa28310ec08e89d186d97610540fde246c5662
SzabolcsGergely <szabi@luxonis.com>
2025-07-25 21:47:54 +0300

    RVC2: Make getCalibration() always return the current calibration regardless of what's the source

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9be49836669a9bbe254075608303bd1ad921cd9f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ce3aed584f07f1d5522147bf27224350c924a56d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
