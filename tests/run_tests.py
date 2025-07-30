e47443883879261ecbf1a400a5412886d9aa6d47
Danilo <danilo.pejovic@luxonis.com>
2025-07-30 10:50:58 +0200

    fix typo

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     resultThreads = []
 
     # Filter configurations based on command-line arguments
-    if args.rvc4==args.rvc2:
+    if args.rvc4==args.rvc2==args.rvc4rgb:
         test_configs = [config for config in all_configs if "rvc2" in config.get("labels", []) or "rvc4" in config.get("labels", []) or "onhost" in config.get("labels", [])]
     elif args.rvc4:
         test_configs = [config for config in all_configs if "rvc4" in config.get("labels", [])]
