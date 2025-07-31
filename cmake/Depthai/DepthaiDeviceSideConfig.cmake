c8be58ce8635be2266c503a15db4a709d967115f
david <david.zahour@luxonis.com>
2025-07-31 16:36:11 +0200

    right-device-commit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9be49836669a9bbe254075608303bd1ad921cd9f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5098ae57b6a97334ce121c17261ffc2ad1d93d6f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
