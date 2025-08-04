db4d8572d6f930f02d7b4c1cf00ff4a0319a8cbf
asahtik <asahtik@gmail.com>
2025-08-04 10:32:32 +0200

    RVC2 FW: Add transformation to tracklets msg

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "fef7dc0beab3e77bb7f4a38823c968388c1c5eb9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7f5149ac464a1d05cf8dfad215aab4a4965da828")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
