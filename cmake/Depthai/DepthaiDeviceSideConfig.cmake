9cc096432ca641bbc1183eb8dcc869057ab13c6b
alex-luxonis <alex@luxonis.com>
2025-08-05 11:49:20 +0300

    RVC2 hw fsync IMX462 + AR0234

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6546e7454245e79981850decd9cde54f442a7754")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "bc53b71e0016c8e83aebae4b1dce2111b2b0a370")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
