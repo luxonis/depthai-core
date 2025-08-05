6052b576c240d7a9c0373578413fa1141df1c506
SzabolcsGergely <szabi@luxonis.com>
2025-08-05 18:32:04 +0300

    Add tests

--- tests/src/ondevice_tests/image_align_node_test.cpp
+++ tests/src/ondevice_tests/image_align_node_test.cpp
@@ -1,69 +1,72 @@
 #include <catch2/catch_all.hpp>
 #include <chrono>
 #include <thread>
-
 #include "depthai/depthai.hpp"
 
-TEST_CASE("Test ImageAlign node image to image alignment") {
-    using namespace std;
-    using namespace std::chrono;
-    using namespace std::chrono_literals;
+using namespace std;
+using namespace std::chrono;
+using namespace std::chrono_literals;
 
-    dai::Pipeline p;
-    auto leftCam = p.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_B);
-    auto rightCam = p.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_C);
-    auto align = p.create<dai::node::ImageAlign>();
+namespace {
+    void runImageAlignTest(bool useDepth, bool runOnHost) {
+        dai::Pipeline p;
+        auto leftCam = p.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_B);
+        auto rightCam = p.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_C);
+        std::shared_ptr<dai::node::StereoDepth> stereo;
+        auto align = p.create<dai::node::ImageAlign>();
 
-    auto leftOut = leftCam->requestOutput({1280, 800});
-    auto rightOut = rightCam->requestOutput({1280, 800});
+        auto leftOut = leftCam->requestOutput({1280, 800});
+        auto rightOut = rightCam->requestOutput({1280, 800});
 
-    leftOut->link(align->input);
-    rightOut->link(align->inputAlignTo);
+        if(useDepth) {
+            stereo = p.create<dai::node::StereoDepth>();
+            leftOut->link(stereo->left);
+            rightOut->link(stereo->right);
+            stereo->depth.link(align->input);
+        } else {
+            leftOut->link(align->input);
+        }
+        rightOut->link(align->inputAlignTo);
 
-    align->initialConfig->staticDepthPlane = 0x5AB1;
+        if(!useDepth) {
+            align->initialConfig->staticDepthPlane = 0x5AB1;
+        }
+        if(runOnHost) {
+            align->setRunOnHost(true);
+        }
 
-    auto alignedQueue = align->outputAligned.createOutputQueue();
+        auto alignedQueue = align->outputAligned.createOutputQueue();
+        p.start();
 
-    p.start();
-
-    // Retrieve N messages
-    constexpr size_t N = 20;
-    for(size_t i = 0; i < N; ++i) {
-        auto aligned = alignedQueue->get<dai::ImgFrame>();
-
-        REQUIRE(aligned != nullptr);
+        constexpr size_t N = 20;
+        for(size_t i = 0; i < N; ++i) {
+            auto aligned = alignedQueue->get<dai::ImgFrame>();
+            REQUIRE(aligned != nullptr);
+        }
     }
+}
+
+TEST_CASE("Test ImageAlign node image to image alignment") {
+    bool useDepth = false;
+    bool runOnHost = false;
+    runImageAlignTest(useDepth, runOnHost);
 }
 
 TEST_CASE("Test ImageAlign node depth to image alignment") {
-    using namespace std;
-    using namespace std::chrono;
-    using namespace std::chrono_literals;
-
-    dai::Pipeline p;
-    auto leftCam = p.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_B);
-    auto rightCam = p.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_C);
-    auto stereo = p.create<dai::node::StereoDepth>();
-    auto align = p.create<dai::node::ImageAlign>();
-
-    auto leftOut = leftCam->requestOutput({1280, 800});
-    auto rightOut = rightCam->requestOutput({1280, 800});
-
-    leftOut->link(stereo->left);
-    rightOut->link(stereo->right);
-
-    stereo->depth.link(align->input);
-    rightOut->link(align->inputAlignTo);
-
-    auto alignedQueue = align->outputAligned.createOutputQueue();
-
-    p.start();
-
-    // Retrieve N messages
-    constexpr size_t N = 20;
-    for(size_t i = 0; i < N; ++i) {
-        auto aligned = alignedQueue->get<dai::ImgFrame>();
-
-        REQUIRE(aligned != nullptr);
-    }
+    bool useDepth = true;
+    bool runOnHost = false;
+    runImageAlignTest(useDepth, runOnHost);
 }
+
+TEST_CASE("Test ImageAlign node image to image alignment on host") {
+    bool useDepth = false;
+    bool runOnHost = true;
+    runImageAlignTest(useDepth, runOnHost);
+}
+
+TEST_CASE("Test ImageAlign node depth to image alignment on host") {
+    bool useDepth = true;
+    bool runOnHost = true;
+    runImageAlignTest(useDepth, runOnHost);
+}
+
