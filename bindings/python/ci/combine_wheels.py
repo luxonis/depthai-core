dea6f4e91cbedcad19aff54e317c38516e0f4b36
lnotspotl <lnotspotl@gmail.com>
2025-08-05 22:00:04 -0400

    enable wheel bundling on Windows

--- bindings/python/ci/combine_wheels.py
+++ bindings/python/ci/combine_wheels.py
@@ -8,12 +8,14 @@ import os
 import time
 import tempfile
 import subprocess
+import sys
 
 from collections import defaultdict
 
 from dataclasses import dataclass
 
 logger = logging.getLogger(__name__)
+
 @dataclass
 class WheelInfo:
     wheel_name: str
@@ -23,58 +25,9 @@ class WheelInfo:
     abi_tag: str
     platform_tag: str
 
-def write_to_zip(zip_file: zipfile.ZipFile, path: str, file: str):
-    file_path = os.path.join(path, file)
-    if os.path.isdir(file_path):
-        for root, _, files in os.walk(file_path):
-            for f in files:
-                arcname = os.path.join(file, f)
-                try:
-                    # This will throw a KeyError if the file is not in the zip, in that case we write the file to the zip
-                    zip_file.getinfo(arcname)
-                except KeyError:
-                    zip_file.write(os.path.join(root, f), arcname)
-    else:
-        try:
-            # This will throw a KeyError if the file is not in the zip, in that case we write the file to the zip
-            zip_file.getinfo(file)
-        except KeyError:
-            zip_file.write(file_path, file)
+def combine_wheels_linux(args, wheel_infos):
 
-
-def main(args: argparse.Namespace):
-
-    FORMAT = "[%(asctime)s.%(msecs)03d] [%(levelname)s] [%(name)s] %(message)s"
-    DATEFMT = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(
-        level=args.log_level.upper(),
-        format=FORMAT,
-        datefmt=DATEFMT
-    )
-    ## Get a list of all wheels in the input folder
-    wheels = glob.glob(os.path.join(args.input_folder, "*.whl"))
-
-    ## Extract the wheel info from the wheels
-    wheel_infos = []
-    for wheel_path in wheels:
-        wheel_name = os.path.basename(wheel_path)[:-4] # remove the .whl extension
-        parts = wheel_name.split('-')
-        wheel_dvb = "-".join(parts[:2])
-        python_tag = parts[-3]
-        abi_tag = parts[-2]
-        platform_tag = parts[-1]
-        wheel_infos.append(WheelInfo(
-            wheel_name=wheel_name,
-            wheel_path=wheel_path,
-            wheel_dvb=wheel_dvb,
-            python_tag=python_tag,
-            abi_tag=abi_tag, 
-            platform_tag=platform_tag
-        ))
-    
-    for wheel_info in wheel_infos:
-        logger.info(f"Found wheel: {wheel_info}")
-
+    logger.info(f"Combining wheels for Linux!")
 
     ## Create a temporary directory for extracting wheels
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -157,6 +110,112 @@ def main(args: argparse.Namespace):
         logger.info("Output zip closed")
         logger.info(f"Output zip size: {os.path.getsize(output_zip_path) / (1024 * 1024):.2f} MB")
         logger.info(f"Combined wheel saved to {output_zip_path}")
+
+def combine_wheels_windows(args, wheel_infos):
+
+    logger.info(f"Combining wheels for Windows!")
+
+    ## Create a temporary directory for extracting wheels
+    with tempfile.TemporaryDirectory() as temp_dir:
+
+        combined_python_tag = ".".join([wheel_info.python_tag for wheel_info in wheel_infos])
+        combined_abi_tag = ".".join([wheel_info.abi_tag for wheel_info in wheel_infos])
+        combined_platform_tag = wheel_infos[0].platform_tag
+
+        logger.info(f"Combined python tag: {combined_python_tag}")
+        logger.info(f"Combined abi tag: {combined_abi_tag}")
+        logger.info(f"Combined platform tag: {combined_platform_tag}")
+        logger.info(f"Wheel DVB: {wheel_infos[0].wheel_dvb}")
+
+        # Create a zip file for the combined wheel
+        combined_wheel_name = f"{wheel_infos[0].wheel_dvb}-{combined_python_tag}-{combined_abi_tag}-{combined_platform_tag}.whl"
+        logger.info(f"Combined wheel name: {combined_wheel_name}")
+
+        output_zip_path = os.path.join(args.output_folder, combined_wheel_name)
+        output_zip = zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED, compresslevel=9)
+
+        # Extract each wheel into a subdirectory named after the wheel
+        for wheel_info in wheel_infos:
+            wheel_extract_dir = os.path.join(temp_dir, wheel_info.wheel_name)
+            os.makedirs(wheel_extract_dir, exist_ok=True)
+
+            logger.debug(f"Extracting {wheel_info.wheel_name} to {wheel_extract_dir}")
+            with zipfile.ZipFile(wheel_info.wheel_path, 'r') as wheel_zip:
+                wheel_zip.extractall(wheel_extract_dir)
+
+            ## Just copy everything over to the output zip
+            for file in os.listdir(wheel_extract_dir):
+                write_to_zip(output_zip, wheel_extract_dir, file)
+
+        output_zip.close()
+        logger.info("Output zip closed")
+        logger.info(f"Output zip size: {os.path.getsize(output_zip_path) / (1024 * 1024):.2f} MB")
+        logger.info(f"Combined wheel saved to {output_zip_path}")
+
+
+def write_to_zip(zip_file: zipfile.ZipFile, path: str, file: str):
+    file_path = os.path.join(path, file)
+    if os.path.isdir(file_path):
+        for root, _, files in os.walk(file_path):
+            for f in files:
+                arcname = file + "/" + f
+                if sys.platform == "win32":
+                    arcname = arcname.replace("\\", "/") # Convert backslashes to forward slashes on windows, that's what zipfile expects
+                try:
+                    # This will throw a KeyError if the file is not in the zip, in that case we write the file to the zip
+                    info = zip_file.getinfo(arcname)
+                except KeyError:
+                    if sys.platform == "win32":
+                        root = root.replace("\\", "/")
+                    zip_file.write(root + "/" + f, arcname)
+    else:
+        try:
+            # This will throw a KeyError if the file is not in the zip, in that case we write the file to the zip
+            zip_file.getinfo(file)
+        except KeyError:
+            zip_file.write(file_path, file)
+
+
+def main(args: argparse.Namespace):
+
+    FORMAT = "[%(asctime)s.%(msecs)03d] [%(levelname)s] [%(name)s] %(message)s"
+    DATEFMT = "%Y-%m-%d %H:%M:%S"
+    logging.basicConfig(
+        level=args.log_level.upper(),
+        format=FORMAT,
+        datefmt=DATEFMT
+    )
+    ## Get a list of all wheels in the input folder
+    wheels = glob.glob(os.path.join(args.input_folder, "*.whl"))
+
+    ## Extract the wheel info from the wheels
+    wheel_infos = []
+    for wheel_path in wheels:
+        wheel_name = os.path.basename(wheel_path)[:-4] # remove the .whl extension
+        parts = wheel_name.split('-')
+        wheel_dvb = "-".join(parts[:2])
+        python_tag = parts[-3]
+        abi_tag = parts[-2]
+        platform_tag = parts[-1]
+        wheel_infos.append(WheelInfo(
+            wheel_name=wheel_name,
+            wheel_path=wheel_path,
+            wheel_dvb=wheel_dvb,
+            python_tag=python_tag,
+            abi_tag=abi_tag, 
+            platform_tag=platform_tag
+        ))
+    
+    for wheel_info in wheel_infos:
+        logger.info(f"Found wheel: {wheel_info}")
+
+    if sys.platform == "linux":
+        combine_wheels_linux(args, wheel_infos)
+    elif sys.platform == "win32":
+        combine_wheels_windows(args, wheel_infos)
+    else:
+        raise ValueError(f"Unsupported platform: {sys.platform}")
+
 
 
 if __name__ == "__main__":
