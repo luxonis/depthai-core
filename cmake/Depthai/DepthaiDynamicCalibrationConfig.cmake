ebb1c34f40e9d92561414f25181db035c0ba53dd
Juraj Vanco <majvan@gmail.com>
2025-08-12 14:16:43 +0200

    Update DCL lib commit to mock DynamicCalibration class

--- cmake/Depthai/DepthaiDynamicCalibrationConfig.cmake
+++ cmake/Depthai/DepthaiDynamicCalibrationConfig.cmake
@@ -1,2 +1,2 @@
 # "full commit hash of Dynamic calibration library"
-set(DEPTHAI_DYNAMIC_CALIBRATION_VERSION "494aef0dd88c0e945b49704a00b13bc078943846")
+set(DEPTHAI_DYNAMIC_CALIBRATION_VERSION "52be7ab912ff25c570d7beacc1e7fd68db10b784")
