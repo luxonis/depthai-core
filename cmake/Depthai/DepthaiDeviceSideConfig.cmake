061edf8688c114f9a15994021a092f200abd52b0
SzabolcsGergely <szabi@luxonis.com>
2025-08-13 19:05:57 +0300

    Update RVC2 and RVC4 FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2d9a57c319acae008a37e335dec74ae5492b8faa")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "950ad61d0e72be7191a153597dbbce0cd5b60032")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
