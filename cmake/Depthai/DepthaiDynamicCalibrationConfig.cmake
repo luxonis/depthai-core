6b8e4ac3d3a2343e15b602412685eceb98ef5c7a
MaticTonin <matictonin@gmail.com>
2025-08-20 00:18:36 +0200

    Update DCL-version, macOS issue

--- cmake/Depthai/DepthaiDynamicCalibrationConfig.cmake
+++ cmake/Depthai/DepthaiDynamicCalibrationConfig.cmake
@@ -1,2 +1,2 @@
 # "full commit hash of Dynamic calibration library"
-set(DEPTHAI_DYNAMIC_CALIBRATION_VERSION "f945e18d3ff1b7de9edab21ff97eab3363c27cc7")
+set(DEPTHAI_DYNAMIC_CALIBRATION_VERSION "a7c0cfb7c08c866d3e058f22799b5516b5d3b007")
