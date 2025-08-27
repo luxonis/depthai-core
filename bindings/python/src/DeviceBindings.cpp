de5ca93e4d3e09d71c9d2f281a484110a527e698
Matevz Morato <matevz.morato@gmail.com>
2025-08-27 18:24:03 +0200

    Remove different formatting

--- bindings/python/src/DeviceBindings.cpp
+++ bindings/python/src/DeviceBindings.cpp
@@ -759,6 +759,20 @@ void DeviceBindings::bind(pybind11::module& m, void* pCallstack) {
             },
             DOC(dai, DeviceBase, flashCalibration))
         .def(
+            "setCalibration",
+            [](DeviceBase& d, CalibrationHandler ch) {
+                py::gil_scoped_release release;
+                return d.setCalibration(ch);
+            },
+            DOC(dai, DeviceBase, setCalibration))
+        .def(
+            "getCalibration",
+            [](DeviceBase& d) {
+                py::gil_scoped_release release;
+                return d.getCalibration();
+            },
+            DOC(dai, DeviceBase, getCalibration))
+        .def(
             "readCalibration2",
             [](DeviceBase& d) {
                 py::gil_scoped_release release;
@@ -779,20 +793,6 @@ void DeviceBindings::bind(pybind11::module& m, void* pCallstack) {
                 return d.factoryResetCalibration();
             },
             DOC(dai, DeviceBase, factoryResetCalibration))
-        .def(
-            "setCalibration",
-            [](DeviceBase& d, CalibrationHandler ch) {
-                py::gil_scoped_release release;
-                return d.setCalibration(ch);
-            },
-            DOC(dai, DeviceBase, setCalibration))
-        .def(
-            "getCalibration",
-            [](DeviceBase& d) {
-                py::gil_scoped_release release;
-                return d.getCalibration();
-            },
-            DOC(dai, DeviceBase, getCalibration))
         .def(
             "flashFactoryCalibration",
             [](DeviceBase& d, CalibrationHandler ch) {
