47b061318a0a64080d5248c4680409fddf52362e
alex-luxonis <alex@luxonis.com>
2025-08-27 12:10:26 +0300

    RVC2: IMX678 improvements

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bc53b71e0016c8e83aebae4b1dce2111b2b0a370")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e2c28f8ec080b958e30675b5ed4d39f65cdc0e89")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
