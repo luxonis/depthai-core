baf3af87384fc3b313eee719f9ce486a5eaeeb18
asahtik <asahtik@gmail.com>
2025-08-28 15:24:08 +0200

    RVC2 FW: Fix OT error when remapping

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "313c23f193ebc61a350168ce817b0824e3a484f3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "002adf06e24d42445ca4bc20e2363d644b8f8f04")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
