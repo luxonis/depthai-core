a8e7db1c21f2e7c5f794e1c1612fd554a12f07c0
asahtik <38485424+asahtik@users.noreply.github.com>
2025-08-28 09:09:34 +0200

    Fix remapped rect angle (#1423)
    * Fix calculation of min area outer rect to prevent 90deg rotation
    
    * Add rect remap test

--- tests/src/ondevice_tests/img_transformation_test.cpp
+++ tests/src/ondevice_tests/img_transformation_test.cpp
@@ -96,3 +96,29 @@ TEST_CASE("ImgTransformation in NeuralNetwork") {
     REQUIRE(!isIdentity(tensor->transformation->getSourceIntrinsicMatrix()));
     REQUIRE(!isIdentity(tensor->transformation->getSourceIntrinsicMatrixInv()));
 }
+
+TEST_CASE("ImgTransformation remap vertical") {
+    dai::Pipeline pipeline;
+    auto camera = pipeline.create<dai::node::Camera>()->build();
+    auto camOut1 = camera->requestOutput({600, 400});
+    auto camOut2 = camera->requestOutput({400, 600});
+    auto q1 = camOut1->createOutputQueue();
+    auto q2 = camOut2->createOutputQueue();
+    pipeline.start();
+    auto frame1 = q1->get<dai::ImgFrame>();
+    REQUIRE(frame1 != nullptr);
+    auto frame2 = q2->get<dai::ImgFrame>();
+    REQUIRE(frame2 != nullptr);
+    auto rect = dai::Rect(200, 100, 100, 200);
+    auto rRect = dai::RotatedRect(rect.normalize(frame1->getWidth(), frame1->getHeight()), 0.f);
+    auto remapped = frame1->transformation.remapRectTo(frame2->transformation, rRect);
+
+    auto sourceDen = rRect.denormalize(frame1->getWidth(), frame1->getHeight());
+    auto destDen = remapped.denormalize(frame2->getWidth(), frame2->getHeight());
+    auto sourceAR = sourceDen.size.width / sourceDen.size.height;
+    auto destAR = destDen.size.width / destDen.size.height;
+
+    REQUIRE_THAT(sourceAR, Catch::Matchers::WithinAbs(destAR, 0.01));
+
+    pipeline.stop();
+}
