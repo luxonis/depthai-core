e2f84df6f1e1aab1eab59dacb496d757123ccf55
Matevz Morato <matevz.morato@gmail.com>
2025-09-01 23:55:01 +0200

    Fix compiler warnings

--- src/pipeline/node/host/Replay.cpp
+++ src/pipeline/node/host/Replay.cpp
@@ -93,6 +93,10 @@ inline std::shared_ptr<Buffer> getMessage(const std::shared_ptr<google::protobuf
         case DatatypeEnum::ToFDepthConfidenceFilterConfig:
         case DatatypeEnum::RGBDData:
         case DatatypeEnum::ObjectTrackerConfig:
+        case DatatypeEnum::DynamicCalibrationControl:
+        case DatatypeEnum::DynamicCalibrationResult:
+        case DatatypeEnum::CalibrationQuality:
+        case DatatypeEnum::CoverageData:
             break;
     }
     throw std::runtime_error("Cannot replay message type: " + std::to_string((int)datatype));
