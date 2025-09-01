df5e6a1020474b1bb5f6c1f4182b6b338f765737
asahtik <asahtik@gmail.com>
2025-09-01 16:03:36 +0200

    FW: bump core on rvc2, fix camera & transformations with missing calib on rvc4

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "002adf06e24d42445ca4bc20e2363d644b8f8f04")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4b6e5276a41ad02b1b5fd8ced4dd570f0d8cc2b6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
