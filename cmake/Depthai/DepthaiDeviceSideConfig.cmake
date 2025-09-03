272fecd5bf5bdaeadc16e7439fad6cc01065d8b0
Matevz Morato <matevz.morato@gmail.com>
2025-09-03 18:10:10 +0200

    [RVC2 FW] Fix for the IMU RotationVector rotation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "262cdef0af530693c6b5d7d905f06c27bda5be25")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e6ef1dc25d3f974708d9593367364bebb736437a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
