1129ec94352de5e5a12053664155a848ade1960d
asahtik <asahtik@gmail.com>
2025-09-03 15:45:58 +0200

    RVC2 FW: Fix incorrect img plane pointers from xlinkin

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "262cdef0af530693c6b5d7d905f06c27bda5be25")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8169f3375c983fcba5a8ff18c1b6129f9ab2c32b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
