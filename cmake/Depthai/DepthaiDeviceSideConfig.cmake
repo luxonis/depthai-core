6d2fa3665c67d784bbbb15ca15ffb0ac3e50c90e
Matevz Morato <matevz.morato@gmail.com>
2025-09-03 19:33:16 +0200

    [RVC2 FW] Fix XLinkIn edge case

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e6ef1dc25d3f974708d9593367364bebb736437a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "502bcf9595c3bb1b054410e02a32109cfe30f406")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
