249c05649615799bafdfe75656f6ef29197dfb60
asahtik <asahtik@gmail.com>
2025-09-04 16:44:33 +0200

    Add thermal examples + set transformations on thermal on rvc2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c48c179e66522e82672e34f7a779e8de43e50c55")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6c4d1b36ed8adea672d6d656bc1e3f2388c3199d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
