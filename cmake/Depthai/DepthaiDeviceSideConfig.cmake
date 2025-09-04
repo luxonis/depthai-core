416479f2cf7380b8f8b64955e8e20b3109d9b2ae
asahtik <asahtik@gmail.com>
2025-09-04 10:03:58 +0200

    RVC2 FW: Remove unnecessary code

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "502bcf9595c3bb1b054410e02a32109cfe30f406")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "01d2a631379bc1c53a1c6a307bf4d5b6a1b0ecc6")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
