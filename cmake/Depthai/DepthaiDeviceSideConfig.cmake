54ed10ba6e9f5df42c5c764d8b19e316b24b13a6
lnotspotl <lnotspotl@gmail.com>
2025-09-15 15:24:09 -0400

    bump HW commit hashes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "743a90e515ae68ea1b14f45aaa0a6b11e562a026")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7896225482f51543ae2f213478ebae61cd877a6c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
