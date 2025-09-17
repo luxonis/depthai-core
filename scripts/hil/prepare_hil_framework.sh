654ab3c893c47d7a305cb08df3da40b47f48e664
Danilo Pejovic <pejovicdanilo@outlook.com>
2025-09-17 15:26:19 +0200

    new hil package

--- scripts/hil/prepare_hil_framework.sh
+++ scripts/hil/prepare_hil_framework.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "/home/$USER/hil_framework/lib_testbed/tools" >> $GITHUB_PATH
-echo "PYTHONPATH="$PYTHONPATH:/home/$USER/hil_framework"" >> $GITHUB_ENV
-echo "HIL_FRAMEWORK_PATH="/home/$USER/hil_framework"" >> $GITHUB_ENV
+# Set up a Python virtual environment
+python3 -m venv venv
+source venv/bin/activate
 
-pushd /home/$USER/hil_framework/ > /dev/null 2>&1 && git pull && git submodule update --init --recursive > /dev/null 2>&1 && popd > /dev/null 2>&1
-pushd /home/$USER/hil_framework/ > /dev/null 2>&1 && pip install -r requirements.txt  > /dev/null 2>&1 && popd > /dev/null 2>&1
+# Install required Python packages
+pip install hil-framework --upgrade --index-url "https://__token__:$PAT_TOKEN@gitlab.luxonis.com/api/v4/projects/213/packages/pypi/simple" > /dev/null 2>&1
