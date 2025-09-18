4f355e720d455171a3b2f3cd5f2acc37e9ddc4f0
Danilo <danilo.pejovic@luxonis.com>
2025-09-18 11:26:50 +0200

    run hil install inside a script

--- scripts/hil/prepare_hil_framework.sh
+++ scripts/hil/prepare_hil_framework.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Check if PAT token argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <PAT_TOKEN>"
+  exit 1
+fi
+
+PAT_TOKEN=$1
+
 # Set up a Python virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
 # Install required Python packages
-pip install hil-framework --upgrade --index-url "https://__token__:$PAT_TOKEN@gitlab.luxonis.com/api/v4/projects/213/packages/pypi/simple" > /dev/null 2>&1
+pip install hil-framework --upgrade \
+  --index-url "https://__token__:$PAT_TOKEN@gitlab.luxonis.com/api/v4/projects/213/packages/pypi/simple" \
+  > /dev/null 2>&1
