0bb2d62eaf96116bf28a29bf3c03c9b255aed797
lnotspotl <lnotspotl@gmail.com>
2025-09-25 18:22:38 -0400

    bump rvc2 fw hash - rvc4 hw remains unchanged

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7896225482f51543ae2f213478ebae61cd877a6c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9fc3ffc2089dcb51977da0208407f0dd88a33e04")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
