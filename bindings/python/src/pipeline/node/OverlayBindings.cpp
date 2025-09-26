5b4edf1af832365691967af890a78aec280f216f
aljazkonec1 <konec.aljaz1@gmail.com>
2025-09-26 09:00:21 +0200

    Bindings fix.

--- bindings/python/src/pipeline/node/OverlayBindings.cpp
+++ bindings/python/src/pipeline/node/OverlayBindings.cpp
@@ -9,8 +9,8 @@ void bind_overlay(pybind11::module& m, void* pCallstack) {
     using namespace dai;
     using namespace dai::node;
 
-    py::class_<OverlayProperties> overrlayProperties(m, "OverlayProperties", DOC(dai, OverlayProperties));
-    auto overrlayProperties = ADD_NODE(Overlay);
+    py::class_<OverlayProperties> overlayProperties(m, "OverlayProperties", DOC(dai, OverlayProperties));
+    auto overlay = ADD_NODE(Overlay);
 
     ///////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////
@@ -27,16 +27,15 @@ void bind_overlay(pybind11::module& m, void* pCallstack) {
 
     // Properties
 
-    overrlayProperties.def_readwrite("alpha", &OverlayProperties::alpha);
-    overrlayProperties.def_readwrite("interpolationType", &OverlayProperties::interpolationType);
+    overlayProperties.def_readwrite("alpha", &OverlayProperties::alpha);
+    overlayProperties.def_readwrite("interpolationType", &OverlayProperties::interpolationType);
 
     // Node
-    overrlayProperties.def_readonly("inputFrame", &Overlay::inputFrame, DOC(dai, node, Overlay, inputFrame))
+    overlay.def_readonly("inputFrame", &Overlay::inputFrame, DOC(dai, node, Overlay, inputFrame))
         .def_readonly("inputDetections", &Overlay::inputDetections, DOC(dai, node, Overlay, inputDetections))
         .def_readonly("out", &Overlay::out, DOC(dai, node, Overlay, out))
         .def("setOverlayAlpha", &Overlay::setOverlayAlpha, py::arg("alpha") = 0.5f, DOC(dai, node, Overlay, setOverlayAlpha))
-        .def("setInterpolationType", &Overlay::setInterpolationType, py::arg("interpolationType") = 1, DOC(dai, node, Overlay, setInterpolationType))
-        // ALIAS
-        daiNodeModule.attr("Overlay")
-        .attr("Properties") = overrlayProperties;
+        .def("setInterpolationType", &Overlay::setInterpolationType, py::arg("interpolationType") = 1, DOC(dai, node, Overlay, setInterpolationType));
+    // ALIAS
+    daiNodeModule.attr("Overlay").attr("Properties") = overlayProperties;
 }
