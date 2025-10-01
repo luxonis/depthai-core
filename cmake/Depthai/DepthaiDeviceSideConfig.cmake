12e7f33b84d2c6308d3415109902deb64bb65bd4
lnotspotl <lnotspotl@gmail.com>
2025-10-01 14:55:11 -0400

    Re-expose camera pool setters and getters, cap the number of pool frames (either by number or by size in bytes, however always at least 2) (#1435)
    * Re-expose camera pool setters and getters, update docs
    
    * update Camera properties
    
    * remove max pool size params for video, preview and still frame pools
    
    * bump fw hash
    
    * [skip ci] remove unnecessary Camera setters and getters
    
    * bump rvc2 fw hash
    
    * add setters and getters for pool sizes of Camera's internal ImageManips
    
    * bump FW hashes
    
    * [skip ci] rename output pool size setting name
    
    * bump fw hashes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "9fc3ffc2089dcb51977da0208407f0dd88a33e04")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "dc466b4462739d1edbaa00fdccbe63805bff8078")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
