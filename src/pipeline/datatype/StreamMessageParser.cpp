249df59cfc9987b4bdbc0c70cd62ecab0fb5726e
Serafadam <adam.serafin@luxonis.com>
2025-10-06 16:54:50 +0000

    remove rtabmap for now

--- src/pipeline/datatype/StreamMessageParser.cpp
+++ src/pipeline/datatype/StreamMessageParser.cpp
@@ -15,6 +15,7 @@
 #include "depthai/pipeline/datatype/BenchmarkReport.hpp"
 #include "depthai/pipeline/datatype/Buffer.hpp"
 #include "depthai/pipeline/datatype/CameraControl.hpp"
+#include "pipeline/datatype/MapData.hpp"
 #ifdef DEPTHAI_HAVE_DYNAMIC_CALIBRATION_SUPPORT
     #include "depthai/pipeline/datatype/DynamicCalibrationControl.hpp"
     #include "depthai/pipeline/datatype/DynamicCalibrationResults.hpp"
@@ -258,6 +259,9 @@ std::shared_ptr<ADatatype> StreamMessageParser::parseMessage(streamPacketDesc_t*
             break;
         case DatatypeEnum::ObjectTrackerConfig:
             return parseDatatype<ObjectTrackerConfig>(metadataStart, serializedObjectSize, data, fd);
+            break;
+        case DatatypeEnum::MapData:
+            return parseDatatype<MapData>(metadataStart, serializedObjectSize, data, fd);
             break;
 #ifdef DEPTHAI_HAVE_DYNAMIC_CALIBRATION_SUPPORT
         case DatatypeEnum::DynamicCalibrationControl:
