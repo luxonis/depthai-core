3c96aedf6fff437b972fe0afae0de706d4162358
alex-luxonis <alex@luxonis.com>
2025-10-07 14:09:32 +0300

    RVC2 FW: - FW internal ddrinit header: don't reset watchdog if configured (support for XLink#99) - IMX678 image quality improvements, + fixes for Camera node - OAK-D-SR-PoE with IMX378 support - OAK-T R8 support - BNO IMU: fix timestamp jump - fix some cases of device becoming offline in pipelines with IMU
    XLink:
    https://github.com/luxonis/XLink/pull/95 - [RVC2] Attempt to fix device bad state (USB/UNBOOTED)
    https://github.com/luxonis/XLink/pull/99 - usb_boot: enable watchdog prior to booting

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5254f0cfb32a4b76aacf454ca4aad546c6837806")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e205776e0357b343bd16d11c41532ae3360facf7")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
