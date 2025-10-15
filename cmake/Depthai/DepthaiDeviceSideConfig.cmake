d9ec3b9671480d8e95b96bd67f20b0ccd10d2118
Matevz Morato <matevz.morato@gmail.com>
2025-10-15 18:30:01 +0200

    [RVC2 FW] Remove shutdown regression on RVC2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e205776e0357b343bd16d11c41532ae3360facf7")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7016c714b4ddaac7b0ac773ad57c9bedd5b63576")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
