d65ddff45419fb4c8a0fdb4e30ea7ca9b2e90c90
aljazkonec1 <konec.aljaz1@gmail.com>
2025-10-21 13:17:58 +0200

    Fix formmating and Windows failed tests.

--- include/depthai/pipeline/datatype/NNData.hpp
+++ include/depthai/pipeline/datatype/NNData.hpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <chrono>
 #include <ctime>
+#include <iostream>
 #include <iterator>
 #include <limits>
 #include <optional>
--- include/depthai/pipeline/nodes.hpp
+++ include/depthai/pipeline/nodes.hpp
@@ -23,7 +23,6 @@
 #include "node/SPIIn.hpp"
 #include "node/SPIOut.hpp"
 #include "node/Script.hpp"
-#include "node/SpatialDetectionCalculator.hpp"
 #include "node/SpatialDetectionNetwork.hpp"
 #include "node/SpatialLocationCalculator.hpp"
 #include "node/StereoDepth.hpp"
