86e94fa4e7ea48e13e700e487fdbb040ee9d1d19
alex-luxonis <alex@luxonis.com>
2025-10-23 13:42:25 +0300

    RVC2 FW: add IMX577 tuning

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7016c714b4ddaac7b0ac773ad57c9bedd5b63576")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "97a03a069f3bc19a4e6d720664da8f2775c1d325")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
