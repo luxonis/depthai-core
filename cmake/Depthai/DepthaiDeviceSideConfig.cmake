cacf5aabbc20c5fd3d845a33a3b999391d804363
Matevz Morato <matevz.morato@gmail.com>
2025-10-27 15:48:14 +0100

    [RVC2 FW] Update FW with vertical support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0ada9b9800478cf2c7056cea5c2bab2c8f6a2cbd")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c97f20f3396b650696ce7de134e1d7882fa5336a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
