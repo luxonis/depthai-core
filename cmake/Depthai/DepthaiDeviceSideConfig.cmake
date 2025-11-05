eb81f1697ea90b0ed4bac4ae82c77168ee5f4a20
Matevz Morato <matevz.morato@gmail.com>
2025-11-05 22:04:48 +0100

    Update RVC2 FW with the new RPC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f81edb9c2328ee1f80a6f80c3fade0af0076a3ff")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7eac567f24bbee75d310514a730825a7f19d1350")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
