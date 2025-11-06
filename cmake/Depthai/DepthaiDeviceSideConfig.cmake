411134a6a01714497c7d0e89f0aa8d03e8d6d011
asahtik <asahtik@gmail.com>
2025-11-06 11:49:24 +0100

    RVC2 FW: Pipeline debugging bugfixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f81edb9c2328ee1f80a6f80c3fade0af0076a3ff")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "28de549d4b5cb0a6c659420f68a16a18477c147c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
---
+++ tests/src/ondevice_tests/pipeline_debugging_rvc2_test.cpp
@@ -0,0 +1,68 @@
+#include <fmt/base.h>
+
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "depthai/depthai.hpp"
+
+#define VIDEO_DURATION_SECONDS 5
+
+TEST_CASE("Object Tracker Pipeline Debugging") {
+    // Create pipeline
+    dai::Pipeline pipeline;
+    pipeline.enablePipelineDebugging();
+
+    // Define sources and outputs
+    auto replay = pipeline.create<dai::node::ReplayVideo>();
+    replay->setReplayVideoFile(VIDEO_PATH);
+    replay->setLoop(false);
+
+    // Create spatial detection network
+    dai::NNModelDescription modelDescription{"yolov6-nano"};
+    auto detectionNetwork = pipeline.create<dai::node::DetectionNetwork>()->build(replay, modelDescription);
+    detectionNetwork->setConfidenceThreshold(0.6f);
+    detectionNetwork->input.setBlocking(false);
+
+    // Create object tracker
+    auto objectTracker = pipeline.create<dai::node::ObjectTracker>();
+    objectTracker->setDetectionLabelsToTrack({0});  // track only person
+    objectTracker->setTrackerIdAssignmentPolicy(dai::TrackerIdAssignmentPolicy::SMALLEST_ID);
+
+    // Create output queues
+    auto tracklets = objectTracker->out.createOutputQueue();
+
+    // Link nodes
+    detectionNetwork->passthrough.link(objectTracker->inputTrackerFrame);
+
+    detectionNetwork->passthrough.link(objectTracker->inputDetectionFrame);
+    detectionNetwork->out.link(objectTracker->inputDetections);
+
+    // Start pipeline
+    pipeline.start();
+
+    auto start = std::chrono::steady_clock::now();
+    while(pipeline.isRunning() && std::chrono::steady_clock::now() - start < std::chrono::seconds(VIDEO_DURATION_SECONDS)) {
+        auto track = tracklets->get<dai::Tracklets>();
+    }
+
+    auto state = pipeline.getPipelineState().nodes().detailed();
+
+    for(const auto& [nodeId, nodeState] : state.nodeStates) {
+        auto node = pipeline.getNode(nodeId);
+        if(node->id == 11) continue;
+        // REQUIRE(nodeState.mainLoopTiming.isValid());
+        // if(!node->getInputs().empty()) REQUIRE(nodeState.inputsGetTiming.isValid());
+        // if(!node->getOutputs().empty()) REQUIRE(nodeState.outputsSendTiming.isValid());
+        for(const auto& [inputName, inputState] : nodeState.inputStates) {
+            if(std::string(node->getName()) == "ObjectTracker" && inputName == "inputConfig") continue;  // This example does not use inputConfig
+            REQUIRE(inputState.timing.isValid());
+        }
+        for(const auto& [outputName, outputState] : nodeState.outputStates) {
+            REQUIRE(outputState.timing.isValid());
+        }
+        for(const auto& [otherName, otherTiming] : nodeState.otherTimings) {
+            REQUIRE(otherTiming.isValid());
+        }
+    }
+}
