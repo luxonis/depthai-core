0c874741312681b19a054668fedc141d59fa6b37
asahtik <asahtik@gmail.com>
2025-11-10 13:57:01 +0100

    RVC2 FW: Fix script node issue with pipeline debugging

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "38564345773c52550731e509bbf5f1464c9f263d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "90b5b48739ee78ba1b9c08791e3845d6d4d2408b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
