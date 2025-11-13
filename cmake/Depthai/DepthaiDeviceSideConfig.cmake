7637f2e07dd9c5d4a6f8eda23279c53d8bade357
asahtik <asahtik@gmail.com>
2025-11-13 07:22:32 +0100

    RVC2 FW: Update core [no ci]

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e46193612333c5f8d036ec6dd87f35e8bf2d6240")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "da6d60a9395e28df10c53a8868ea71f73fc7697b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
