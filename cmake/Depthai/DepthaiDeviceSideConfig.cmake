7082cd6dd2fb896498f82c9a0f68ac06f4671801
lnotspotl <lnotspotl@gmail.com>
2025-11-13 16:28:41 +0100

    update rvc2 commit hash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "268a24aaaab8a6f3f065948dcc21a4cb42d9e00b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "95819cdb54dde82530c2548f35c55e2fcf4f4d32")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
