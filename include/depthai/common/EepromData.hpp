9b5f2cff2ee85e74277375553317d3c9e827b3f0
lnotspotl <lnotspotl@gmail.com>
2025-11-14 15:33:44 +0100

    change defaults

--- include/depthai/common/EepromData.hpp
+++ include/depthai/common/EepromData.hpp
@@ -28,7 +28,7 @@ struct EepromData {
     Extrinsics housingExtrinsics;
     std::vector<uint8_t> miscellaneousData;
     bool stereoUseSpecTranslation{true};
-    bool stereoEnableDistortionCorrection{true};
+    bool stereoEnableDistortionCorrection{false};
     CameraBoardSocket verticalCameraSocket = dai::CameraBoardSocket::AUTO;
 };
 
