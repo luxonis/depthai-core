fd9e3af1f4d03b2026e77b28ba32cd15e9e8b3bb
asahtik <asahtik@gmail.com>
2025-11-17 12:06:39 +0100

    RVC2 FW: Merge develop

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "daeb6c3741caf6f1416b97d61d4f495f8324b13f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5cf315c03f05c41a11a4a2747335f4f43123d277")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
