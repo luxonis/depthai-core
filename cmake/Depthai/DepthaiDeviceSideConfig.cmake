ae13d404dce4ad0e2cd1fba4e88abaacd95d8810
alex-luxonis <alex@luxonis.com>
2025-11-17 16:52:37 +0200

    RVC2 FW: fix IMU-related system hang at reboot, IMX577 tuning

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "dce3b2b64b47f27d91d37ada301ee4ed3a00ef83")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d7059bf11f8565e2e81dabec8bfe23a5719b1910")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
