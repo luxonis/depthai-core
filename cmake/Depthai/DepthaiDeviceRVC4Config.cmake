140f08bf5ffcc0f63b947ec1f54168e548e7bc77
Matevz Morato <matevz.morato@gmail.com>
2025-11-19 16:41:16 +0100

    [RVC2 & RVC4 FW]
    * Both bumped to develop
    * RVC4 FW added support for timestamping on
    LuxonisOS > 1.21.0

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+4d457a7c1c51d5c5a95cccb487df43bc0de308bd")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+ccb59dd506392110b0c85abee0d82e28c7d91f9e")
--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8bceca5833ec6f784c3c09950e37c98528f954d6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0f9a7793654b8f2fbed64759c162425eab2c8541")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
