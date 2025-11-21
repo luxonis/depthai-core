af37e9d1051a2966da0ca214d95e30d45979712e
Matevz Morato <matevz.morato@gmail.com>
2025-11-21 17:16:34 +0100

    [RVC2 FW]
    Fix FPPN ToF calibration on V3 in the middle
    of the frame.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "621e48a2a0375f4594f7f8875661c50d3d5950c9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "dd6bf31e9ae1e4f4d75804f077f575b967d1b757")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
