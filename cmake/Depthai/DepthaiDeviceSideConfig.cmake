6ae2f61f3303f6b238318703829b9a35ec0d4617
Matevz Morato <matevz.morato@gmail.com>
2025-11-26 13:01:17 +0100

    Fix thermal support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d7059bf11f8565e2e81dabec8bfe23a5719b1910")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f6083e8ec0afa948497100b39ec967a901ad57c4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
