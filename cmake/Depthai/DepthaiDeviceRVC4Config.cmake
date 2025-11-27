816c1e772ff9d9254c0023b4792751c148170f3b
asahtik <asahtik@gmail.com>
2025-11-27 11:18:56 +0100

    RVC4 FW: Merge develop

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+d70eb5c515369ca6e2eab8ad52b7391afb1e0d23")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+eaa6f94d045ad1ee034ce05976a68d6374ecfa8c")
