c741852de8331d2f10a263f9d7030b7479036933
lnotspotl <lnotspotl@gmail.com>
2025-11-30 14:27:41 +0100

    symlinks

--- examples/cpp/cmake/CreateSymlinks.cmake
+++ examples/cpp/cmake/CreateSymlinks.cmake
@@ -1,24 +1,31 @@
 # CreateSymlinks.cmake - Creates symbolic links for DLLs
 # Usage: cmake -DDLLS="dll1;dll2;..." -DDEST_DIR="path" -P CreateSymlinks.cmake
 
+message(DEBUG "Creating symlinks: ${DLLS} \n Destination path: ${DEST_DIR}")
+
 if(NOT DEFINED DLLS OR NOT DEFINED DEST_DIR)
     return()
 endif()
 
 foreach(dll IN LISTS DLLS)
-    if(dll STREQUAL "")
-        continue()
-    endif()
+    # Extract dll_name (without path)
     get_filename_component(dll_name "${dll}" NAME)
+
+    # Create destination
     set(link_path "${DEST_DIR}/${dll_name}")
-    # Remove existing file/link if present
+
+    # Remove original symlink if it exists
     if(EXISTS "${link_path}")
         file(REMOVE "${link_path}")
     endif()
+
+    message(DEBUG "Creating symlink: ${link_path} ---> ${dll}")
+
     # Create symbolic link
-    file(CREATE_LINK "${dll}" "${link_path}" SYMBOLIC RESULT result)
-    if(NOT result EQUAL 0)
-        message(WARNING "Failed to create symlink for ${dll_name}, falling back to copy")
-        file(COPY "${dll}" DESTINATION "${DEST_DIR}")
+    file(CREATE_LINK "${dll}" "${link_path}" SYMBOLIC RESULT result_var)
+
+    if(result_var)
+        message(WARNING "Failed to create symlink from '${dll}' to '${link_path}': ${result_var}")
     endif()
+
 endforeach()
