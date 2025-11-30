52e9df9ccf83688b5a727a0de9d95149d72adeb5
lnotspotl <lnotspotl@gmail.com>
2025-11-30 17:05:46 +0100

    back to copies for some folders

--- cmake/depthaiHelpers.cmake
+++ cmake/depthaiHelpers.cmake
@@ -19,6 +19,35 @@ macro(add_runtime_dependencies depending_target dependency)
     endif()
     # Copy the required dlls
     if(WIN32)
+        add_custom_command(TARGET ${depending_target} POST_BUILD COMMAND
+            ${CMAKE_COMMAND} -E copy_if_different ${dlls} ${depthai_dll_libraries} $<TARGET_FILE_DIR:${depending_target}>
+            COMMAND_EXPAND_LISTS
+        )
+        message(DEBUG "Required dlls for ${depending_target} are: ${required_dll_files}")
+    endif()
+endmacro()
+
+macro(add_runtime_dependencies_symlinked depending_target dependency)
+    if(TARGET ${dependency})
+        get_property(imported_configs TARGET ${dependency} PROPERTY IMPORTED_CONFIGURATIONS)
+        set(dlls "")
+        message(DEBUG "Adding runtime dependencies for ${depending_target} on ${dependency}. Imported configurations: ${imported_configs}")
+        foreach(cfg ${imported_configs})
+            message(DEBUG "Adding runtime dependencies for ${depending_target} on ${dependency} (${cfg})")
+            get_property(dll TARGET ${dependency} PROPERTY IMPORTED_LOCATION_${cfg})
+            message(DEBUG "Retrieved dll for ${cfg}: '${dll}'")
+            list(APPEND dlls $<$<CONFIG:${cfg}>:${dll}>)
+        endforeach()
+        message(DEBUG "Required dlls for ${depending_target} on ${dependency} are: ${dlls}")
+    endif()
+    # Create a list of required dll files
+    if(DEFINED required_dll_files)
+        list(APPEND required_dll_files ${dlls})
+    else()
+        set(required_dll_files ${dlls})
+    endif()
+    # Copy the required dlls
+    if(WIN32)
         foreach(dll_path IN LISTS required_dll_files)
             add_custom_command(TARGET ${depending_target} POST_BUILD COMMAND
                 ${CMAKE_COMMAND} -DDLLS=${dll_path} -DDEST_DIR=$<TARGET_FILE_DIR:${depending_target}>
