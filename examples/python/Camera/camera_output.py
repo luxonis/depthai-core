777b3f066839151c2330900185499933e330eb85
lnotspotl <lnotspotl@gmail.com>
2025-12-01 17:09:27 +0100

    add xlink brige example

--- examples/python/Camera/camera_output.py
+++ examples/python/Camera/camera_output.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
+import argparse
 import cv2
 import depthai as dai
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--fps_limit", type=float, default=None, help="Limit output FPS (float, optional)"
+)
+args = parser.parse_args()
 
 # Create pipeline
 with dai.Pipeline() as pipeline:
     # Define source and output
     cam = pipeline.create(dai.node.Camera).build()
-    videoQueue = cam.requestOutput((640,400)).createOutputQueue()
+    videoOutput = cam.requestOutput((640, 400))
+    videoQueue = videoOutput.createOutputQueue()
+
+    pipeline.build()
+
+    # Optionally update internal settings
+    # Note: xlink bridges are only generated after pipeline.build() is called
+    if args.fps_limit is not None:
+        xlinkBridge = videoOutput.getXLinkBridge()
+        assert xlinkBridge is not None
+        assert isinstance(xlinkBridge, dai.node.internal.XLinkOutBridge)
+        xlinkBridge.xLinkOut.setFpsLimit(args.fps_limit)
 
     # Connect to device and start pipeline
     pipeline.start()
