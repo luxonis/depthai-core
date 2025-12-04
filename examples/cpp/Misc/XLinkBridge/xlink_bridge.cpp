0ba8980a700baa05ad9fe433aa1956eea12d2b89
lnotspotl <lnotspotl@gmail.com>
2025-12-04 20:25:11 +0100

    add xlink_bridge cpp and python examples

---
+++ examples/cpp/Misc/XLinkBridge/xlink_bridge.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+#include "depthai/depthai.hpp"
+
+int main() {
+    // Create device
+    std::shared_ptr<dai::Device> device = std::make_shared<dai::Device>();
+
+    // Create pipeline
+    dai::Pipeline pipeline(device);
+
+    // Create nodes
+    auto cam = pipeline.create<dai::node::Camera>()->build();
+    auto videoOutput = cam->requestOutput(std::make_pair(640, 400));
+    auto videoQueue = videoOutput->createOutputQueue();
+
+    pipeline.build();
+
+    // Optionally update internal settings
+    // Note: xlink bridges are only generated after pipeline.build() is called
+    auto xlinkBridge = videoOutput->getXLinkBridge();
+    assert(xlinkBridge != nullptr);
+    assert(xlinkBridge->xLinkOut != nullptr);
+    xlinkBridge->xLinkOut->setFpsLimit(3.0);
+
+    // Start pipeline
+    pipeline.start();
+
+    while(true) {
+        auto videoIn = videoQueue->get<dai::ImgFrame>();
+        if(videoIn == nullptr) continue;
+
+        cv::imshow("video", videoIn->getCvFrame());
+
+        if(cv::waitKey(1) == 'q') {
+            break;
+        }
+    }
+
+    return 0;
+}
--- examples/python/Camera/camera_output.py
+++ examples/python/Camera/camera_output.py
@@ -1,32 +1,13 @@
 #!/usr/bin/env python3
 
-import argparse
 import cv2
 import depthai as dai
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--fps_limit", type=float, default=None, help="Limit output FPS (float, optional)"
-)
-args = parser.parse_args()
 
 # Create pipeline
 with dai.Pipeline() as pipeline:
     # Define source and output
     cam = pipeline.create(dai.node.Camera).build()
-    videoOutput = cam.requestOutput((640, 400))
-    videoQueue = videoOutput.createOutputQueue()
-
-    pipeline.build()
-
-    # Optionally update internal settings
-    # Note: xlink bridges are only generated after pipeline.build() is called
-    if args.fps_limit is not None:
-        xlinkBridge = videoOutput.getXLinkBridge()
-        assert xlinkBridge is not None
-        assert isinstance(xlinkBridge, dai.node.internal.XLinkOutBridge)
-        xlinkBridge.xLinkOut.setFpsLimit(args.fps_limit)
+    videoQueue = cam.requestOutput((640,400)).createOutputQueue()
 
     # Connect to device and start pipeline
     pipeline.start()
