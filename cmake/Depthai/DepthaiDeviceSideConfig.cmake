0a2e735c96053f5714e30e70cc841e5ab4d0dc56
asahtik <asahtik@gmail.com>
2025-12-09 11:57:09 +0100

    RVC2 FW: rename timingstats, build fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "98e3fdcaa307c79146328576d80e30e8f0203c66")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d43f186d7166cd68c3360aa446536890c6dd68c8")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
