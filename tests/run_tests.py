49fab7bd91025c4cc4b6b035ff88def96fe5ed92
stas.bucik <stas.bucik@luxonis.com>
2025-12-10 12:49:12 +0100

    Add multi device fsync test
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -86,7 +86,13 @@ if __name__ == "__main__":
         action="store_true",
         required=False,
     )
-    
+
+    parser.add_argument(
+        "--fsync",
+        action="store_true",
+        required=False,
+    )
+
     parser.add_argument(
         "--rvc4rgb",
         action="store_true",
@@ -117,6 +123,11 @@ if __name__ == "__main__":
             "labels": ["rvc4"],
         },
         {
+            "name": "RVC4 - Fsync",
+            "env": {"DEPTHAI_PLATFORM": "rvc4"},
+            "labels": ["rvc4fsync"],
+        },
+        {
             "name": "RVC4 - RGB",
             "env": {"DEPTHAI_PLATFORM": "rvc4"},
             "labels": ["rvc4rgb"],
@@ -137,7 +148,7 @@ if __name__ == "__main__":
     resultThreads = []
 
     # Filter configurations based on command-line arguments
-    if args.rvc4==args.rvc2==args.rvc4rgb:
+    if args.rvc4==args.rvc2==args.rvc4rgb==args.fsync:
         test_configs = [config for config in all_configs if "rvc2" in config.get("labels", []) or "rvc4" in config.get("labels", []) or "onhost" in config.get("labels", [])]
     elif args.rvc4:
         test_configs = [config for config in all_configs if "rvc4" in config.get("labels", [])]
@@ -145,6 +156,8 @@ if __name__ == "__main__":
         test_configs = [config for config in all_configs if "rvc2" in config.get("labels", []) or "onhost" in config.get("labels", [])]
     elif args.rvc4rgb:
         test_configs = [config for config in all_configs if "rvc4rgb" in config.get("labels", [])]
+    elif args.fsync:
+        test_configs = [config for config in all_configs if "rvc4fsync" in config.get("labels", [])]
 
 
     for config in test_configs:
