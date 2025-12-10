01432e504c33f707a0bc3f20557f4b6ebe104e14
Tommy Walker <tomprochazka0@gmail.com>
2025-12-10 14:37:49 +0100

    Add camera socket and IMU housing systems and only keep at most 10 of each to match CameraBoardSocket

--- include/depthai/common/HousingCoordinateSystem.hpp
+++ include/depthai/common/HousingCoordinateSystem.hpp
@@ -12,6 +12,16 @@ namespace dai {
  */
 enum class HousingCoordinateSystem : int32_t {
     AUTO = -1,
+    CAM_A,
+    CAM_B,
+    CAM_C,
+    CAM_D,
+    CAM_E,
+    CAM_F,
+    CAM_G,
+    CAM_H,
+    CAM_I,
+    CAM_J,
     FRONT_CAM_A,
     FRONT_CAM_B,
     FRONT_CAM_C,
@@ -22,22 +32,6 @@ enum class HousingCoordinateSystem : int32_t {
     FRONT_CAM_H,
     FRONT_CAM_I,
     FRONT_CAM_J,
-    FRONT_CAM_K,
-    FRONT_CAM_L,
-    FRONT_CAM_M,
-    FRONT_CAM_N,
-    FRONT_CAM_O,
-    FRONT_CAM_P,
-    FRONT_CAM_Q,
-    FRONT_CAM_R,
-    FRONT_CAM_S,
-    FRONT_CAM_T,
-    FRONT_CAM_U,
-    FRONT_CAM_V,
-    FRONT_CAM_W,
-    FRONT_CAM_X,
-    FRONT_CAM_Y,
-    FRONT_CAM_Z,
     VESA_A,
     VESA_B,
     VESA_C,
@@ -48,28 +42,33 @@ enum class HousingCoordinateSystem : int32_t {
     VESA_H,
     VESA_I,
     VESA_J,
-    VESA_K,
-    VESA_L,
-    VESA_M,
-    VESA_N,
-    VESA_O,
-    VESA_P,
-    VESA_Q,
-    VESA_R,
-    VESA_S,
-    VESA_T,
-    VESA_U,
-    VESA_V,
-    VESA_W,
-    VESA_X,
-    VESA_Y,
-    VESA_Z,
+    IMU,
 };
 
 inline std::string toString(HousingCoordinateSystem housing) {
     switch(housing) {
         case HousingCoordinateSystem::AUTO:
             return "AUTO";
+        case HousingCoordinateSystem::CAM_A:
+            return "CAM_A";
+        case HousingCoordinateSystem::CAM_B:
+            return "CAM_B";
+        case HousingCoordinateSystem::CAM_C:
+            return "CAM_C";
+        case HousingCoordinateSystem::CAM_D:
+            return "CAM_D";
+        case HousingCoordinateSystem::CAM_E:
+            return "CAM_E";
+        case HousingCoordinateSystem::CAM_F:
+            return "CAM_F";
+        case HousingCoordinateSystem::CAM_G:
+            return "CAM_G";
+        case HousingCoordinateSystem::CAM_H:
+            return "CAM_H";
+        case HousingCoordinateSystem::CAM_I:
+            return "CAM_I";
+        case HousingCoordinateSystem::CAM_J:
+            return "CAM_J";
         case HousingCoordinateSystem::FRONT_CAM_A:
             return "FRONT_CAM_A";
         case HousingCoordinateSystem::FRONT_CAM_B:
@@ -90,38 +89,6 @@ inline std::string toString(HousingCoordinateSystem housing) {
             return "FRONT_CAM_I";
         case HousingCoordinateSystem::FRONT_CAM_J:
             return "FRONT_CAM_J";
-        case HousingCoordinateSystem::FRONT_CAM_K:
-            return "FRONT_CAM_K";
-        case HousingCoordinateSystem::FRONT_CAM_L:
-            return "FRONT_CAM_L";
-        case HousingCoordinateSystem::FRONT_CAM_M:
-            return "FRONT_CAM_M";
-        case HousingCoordinateSystem::FRONT_CAM_N:
-            return "FRONT_CAM_N";
-        case HousingCoordinateSystem::FRONT_CAM_O:
-            return "FRONT_CAM_O";
-        case HousingCoordinateSystem::FRONT_CAM_P:
-            return "FRONT_CAM_P";
-        case HousingCoordinateSystem::FRONT_CAM_Q:
-            return "FRONT_CAM_Q";
-        case HousingCoordinateSystem::FRONT_CAM_R:
-            return "FRONT_CAM_R";
-        case HousingCoordinateSystem::FRONT_CAM_S:
-            return "FRONT_CAM_S";
-        case HousingCoordinateSystem::FRONT_CAM_T:
-            return "FRONT_CAM_T";
-        case HousingCoordinateSystem::FRONT_CAM_U:
-            return "FRONT_CAM_U";
-        case HousingCoordinateSystem::FRONT_CAM_V:
-            return "FRONT_CAM_V";
-        case HousingCoordinateSystem::FRONT_CAM_W:
-            return "FRONT_CAM_W";
-        case HousingCoordinateSystem::FRONT_CAM_X:
-            return "FRONT_CAM_X";
-        case HousingCoordinateSystem::FRONT_CAM_Y:
-            return "FRONT_CAM_Y";
-        case HousingCoordinateSystem::FRONT_CAM_Z:
-            return "FRONT_CAM_Z";
         case HousingCoordinateSystem::VESA_A:
             return "VESA_A";
         case HousingCoordinateSystem::VESA_B:
@@ -142,38 +109,8 @@ inline std::string toString(HousingCoordinateSystem housing) {
             return "VESA_I";
         case HousingCoordinateSystem::VESA_J:
             return "VESA_J";
-        case HousingCoordinateSystem::VESA_K:
-            return "VESA_K";
-        case HousingCoordinateSystem::VESA_L:
-            return "VESA_L";
-        case HousingCoordinateSystem::VESA_M:
-            return "VESA_M";
-        case HousingCoordinateSystem::VESA_N:
-            return "VESA_N";
-        case HousingCoordinateSystem::VESA_O:
-            return "VESA_O";
-        case HousingCoordinateSystem::VESA_P:
-            return "VESA_P";
-        case HousingCoordinateSystem::VESA_Q:
-            return "VESA_Q";
-        case HousingCoordinateSystem::VESA_R:
-            return "VESA_R";
-        case HousingCoordinateSystem::VESA_S:
-            return "VESA_S";
-        case HousingCoordinateSystem::VESA_T:
-            return "VESA_T";
-        case HousingCoordinateSystem::VESA_U:
-            return "VESA_U";
-        case HousingCoordinateSystem::VESA_V:
-            return "VESA_V";
-        case HousingCoordinateSystem::VESA_W:
-            return "VESA_W";
-        case HousingCoordinateSystem::VESA_X:
-            return "VESA_X";
-        case HousingCoordinateSystem::VESA_Y:
-            return "VESA_Y";
-        case HousingCoordinateSystem::VESA_Z:
-            return "VESA_Z";
+        case HousingCoordinateSystem::IMU:
+            return "IMU";
         default:
             return "UNKNOWN";
     }
