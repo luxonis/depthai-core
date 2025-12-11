2b658242644f43e70971b8b5890e5f52ba1e09d6
Tommy Walker <tomprochazka0@gmail.com>
2025-12-11 13:28:29 +0100

    Mark getHousingCoordinates as static

--- include/depthai/common/HousingCoordinateSystem.hpp
+++ include/depthai/common/HousingCoordinateSystem.hpp
@@ -125,8 +125,9 @@ const std::unordered_map<std::string, std::unordered_map<dai::HousingCoordinateS
 
 #else
 
-const std::unordered_map<std::string, std::unordered_map<dai::HousingCoordinateSystem, std::array<float, 3>>>& getHousingCoordinates() {
-  return {};
+static inline const std::unordered_map<std::string, std::unordered_map<dai::HousingCoordinateSystem, std::array<float, 3>>>& getHousingCoordinates() {
+    static std::unordered_map<std::string, std::unordered_map<dai::HousingCoordinateSystem, std::array<float, 3>>> data = {};
+    return data;
 }
 
 #endif
