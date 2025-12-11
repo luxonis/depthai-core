18206d75d673a8786c74ed09eb3a9d7e4252f953
Jure Rebernik <jure.rebernik@aviatnet.com>
2025-12-11 11:56:20 +0700

    uvc example: cleanup
    Signed-off-by: Jure Rebernik <jure.rebernik@aviatnet.com>

--- examples/cpp/UVC/uvc_example.hpp
+++ examples/cpp/UVC/uvc_example.hpp
@@ -4,11 +4,3 @@ extern "C" {
 #include "uvcgadget/video-source.h"
 #include "uvcgadget/depthai-source.h"
 }
-
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-// int depthai_uvc_get_buffer(struct video_source *s, struct video_buffer *buf);
-// #ifdef __cplusplus
-// }
-// #endif
