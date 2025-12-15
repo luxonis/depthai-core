ad761238b2fb09c4d10c1fd5c091bee4fa7e4e10
alex-luxonis <alex@luxonis.com>
2025-12-15 14:18:52 +0200

    RVC2 FW: fix FPN noise on some OV sensors at low exposure, CameraControl add setMisc("write-registers", "0x1234:0x56,0x0100:0x04")

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f6083e8ec0afa948497100b39ec967a901ad57c4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cf19ade64f1f499786fad8082c4b3f2ac2f0ab14")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
