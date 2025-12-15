f7d20ffdf99f8311d374dc8fcdb424c1a66b0b91
stas.bucik <stas.bucik@luxonis.com>
2025-12-15 08:13:50 +0100

    bump device
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7c1c190e45409c6e3ade394b31e33859f9cea725")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "89223541ac84d7628483f2f39af2fdf199476438")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
