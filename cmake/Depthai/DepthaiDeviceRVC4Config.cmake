cf7b43085e7abb2e957aff13def5f5ff819ba360
AljazD <aljaz.dukic@luxonis.com>
2025-12-16 13:54:05 +0100

    FW bump

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+3c9f1935c51c2a956292bfec10178e2efab6fdeb")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+6a04600dae3c69ffbbed2da4dfec63f6dcdee58f")
