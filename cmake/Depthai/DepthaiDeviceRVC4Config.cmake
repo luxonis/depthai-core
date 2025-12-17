fcdb1592229ea87807f0231acd13f16459506ad1
Jakub Fara <jakubfara77@gmail.com>
2025-12-17 16:45:08 +0100

    Update depthai-device

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+3036e4e8d431eb81f3aa566bae5bc5d5ae92acef")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+a820e8f0c470ce775ab325fd798eeddaa449d9b9")
