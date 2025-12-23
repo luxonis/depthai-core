d5928ec95d92af99007f50ff1f4ad339b96491aa
aljazkonec1 <konec.aljaz1@gmail.com>
2025-12-23 14:24:21 +0100

    bump fw

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+d5feaf8b8cf4cc98953b78888abd771e0f05aef4")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+3aca443b5dc887f1d1447c25d3378d3c93ff47ad")
