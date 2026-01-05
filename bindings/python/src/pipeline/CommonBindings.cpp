3d22171759ca8046f4cde3aac78ee2f7b4181729
aljazkonec1 <konec.aljaz1@gmail.com>
2026-01-05 16:50:43 +0100

    wsin fixes

--- bindings/python/src/pipeline/CommonBindings.cpp
+++ bindings/python/src/pipeline/CommonBindings.cpp
@@ -134,10 +134,11 @@ void CommonBindings::bind(pybind11::module& m, void* pCallstack) {
             [](KeypointsList& self, const std::vector<Keypoint>& kps) { self.Base::setKeypoints(kps); },
             py::arg("keypoints"),
             DOC(dai, KeypointsListT, setKeypoints))
-        .def("setKeypoints",
-             py::overload_cast<const std::vector<Point3f>>(&KeypointsList::setKeypoints),
-             py::arg("keypoints"),
-             DOC(dai, KeypointsListT, setKeypoints))
+        .def(
+            "setKeypoints",
+            [](KeypointsList& self, const std::vector<Point3f>& kps) { self.Base::setKeypoints(kps); },
+            py::arg("keypoints"),
+            DOC(dai, KeypointsListT, setKeypoints))
         .def("setKeypoints",
              py::overload_cast<const std::vector<Point2f>>(&KeypointsList::setKeypoints),
              py::arg("keypoints"),
@@ -193,10 +194,11 @@ void CommonBindings::bind(pybind11::module& m, void* pCallstack) {
             [](SpatialKeypointsList& self, const std::vector<SpatialKeypoint>& kps) { self.Base::setKeypoints(kps); },
             py::arg("keypoints"),
             DOC(dai, KeypointsListT, setKeypoints))
-        .def("setKeypoints",
-             py::overload_cast<const std::vector<Point3f>>(&SpatialKeypointsList::setKeypoints),
-             py::arg("keypoints"),
-             DOC(dai, KeypointsListT, setKeypoints))
+        .def(
+            "setKeypoints",
+            [](SpatialKeypointsList& self, const std::vector<Point3f>& kps) { self.Base::setKeypoints(kps); },
+            py::arg("keypoints"),
+            DOC(dai, KeypointsListT, setKeypoints))
         .def(
             "setKeypoints",
             [](SpatialKeypointsList& self, std::vector<SpatialKeypoint> keypoints, std::vector<Edge> edges) {
