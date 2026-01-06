e917f0a107461a787f44d6ce8f199659be982a93
asahtik <asahtik@gmail.com>
2026-01-06 15:52:34 +0100

    RVC2 FW: Update event aggregation implementation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "f6ba7c0e5302df2761d0d28f488fee9b364c98ab")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "24e21b45975f5737a0173514e92ffaae6d2d9125")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
