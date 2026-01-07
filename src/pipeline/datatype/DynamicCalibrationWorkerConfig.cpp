89691ef481dcd4b1138d8d29b5775d190994abc3
Jakub Fara <jakubfara77@gmail.com>
2026-01-07 14:47:44 +0100

    Fix DynamicCalibrationWorkerConfig

--- src/pipeline/datatype/DynamicCalibrationWorkerConfig.cpp
+++ src/pipeline/datatype/DynamicCalibrationWorkerConfig.cpp
@@ -4,4 +4,9 @@ namespace dai {
 
 DynamicCalibrationWorkerConfig::~DynamicCalibrationWorkerConfig() = default;
 
+void DynamicCalibrationWorkerConfig::serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const {
+    metadata = utility::serialize(*this);
+    datatype = this->getDatatype();
+}
+
 }  // namespace dai
