867941029592f20d0ca1998c0d30728ed3bfbaf2
Jakub Fara <jakubfara77@gmail.com>
2026-01-07 14:44:44 +0100

    Fix implementation of destructor in DynamicCalibrationWorkerConfig

---
+++ src/pipeline/datatype/DynamicCalibrationWorkerConfig.cpp
@@ -0,0 +1,7 @@
+#include "depthai/pipeline/datatype/DynamicCalibrationWorkerConfig.hpp"
+
+namespace dai {
+
+DynamicCalibrationWorkerConfig::~DynamicCalibrationWorkerConfig() = default;
+
+}  // namespace dai
