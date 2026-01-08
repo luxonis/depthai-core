3ae49932696ae0d39f89b5e1349447e04b6e9f04
asahtik <38485424+asahtik@users.noreply.github.com>
2026-01-08 12:26:55 +0100

    RVC2 FW: Remove false alarm warning in SpatialDetectionNetwork (#1608)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "24e21b45975f5737a0173514e92ffaae6d2d9125")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e011942ef6acfcc8b0941ff4ab5fbfb580219389")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
