55407a7eb97fcf0c67c4439985af5b8e2c7a84c9
alex-luxonis <alex@luxonis.com>
2026-01-12 16:31:58 +0200

    RVC2 FW: Sync/demux node fix

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "070213cfe49f96060d33e060f949eb7be0c996ee")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1ea24ca1a26f5e1b6607b3e13fcf96dea517c7c2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
