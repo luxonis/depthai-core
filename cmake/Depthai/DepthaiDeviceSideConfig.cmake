2063370271040c4b1060b39f0d0cd56015ebe376
aljazkonec1 <53098513+aljazkonec1@users.noreply.github.com>
2026-01-12 13:54:40 +0100

    Add labelName to `dai::Keypoint`, `DetectionParser` and improve bindings for `dai::ImgDetections` (#1618)
    * add Keypoints bindings
    
    * Add labelName attribute to dai::Keypoint
    
    * update examples
    
    * Bump FW and update test
    
    * Style check

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e011942ef6acfcc8b0941ff4ab5fbfb580219389")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5120e0e4a5d7bdbc737788bf7b9303cd05daf0b9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
