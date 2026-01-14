2b67ac6cf05b8fe03f89f41fc275b07cca65ff9b
asahtik <asahtik@gmail.com>
2026-01-14 13:51:27 +0100

    RVC2 FW: Fix tryGet & blocking state

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5120e0e4a5d7bdbc737788bf7b9303cd05daf0b9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "b7a1b08723583b8ab590ed6372862e61ea5551e5")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
