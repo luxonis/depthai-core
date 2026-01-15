0724a0bf777c900adb74b563dd421a115f625059
asahtik <asahtik@gmail.com>
2026-01-15 15:31:28 +0100

    RVC2 FW: Update node states consistently, use time window for fps calculation

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a406121b872f3502d3fc666e21441edf91707554")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d5153a69ba567ec4e0e052b3c230c03cebd2e71d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
