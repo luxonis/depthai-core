ab517b0153f93c387c283d4b6361d70923ba41c1
asahtik <38485424+asahtik@users.noreply.github.com>
2026-01-15 12:15:31 +0100

    RVC2 pipeline debugging fixes (#1627)
    * RVC2 FW: Fix tryGet & blocking state
    
    * Run node_pipeline_events test for RVC4 only
    
    * Remove rvc4rgb tag from pipeline debugging examples
    
    * Add default value to member to silence warning
    
    * RVC2 FW: Fix non blocking queue state
    
    * RVC2 FW: Update core

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c33ac02251144278ccfdfbcaeab54a3e4c44a29c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a406121b872f3502d3fc666e21441edf91707554")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
