96094f345d3cd55ea28c9091c7af0ee9242bf53e
aljazkonec1 <konec.aljaz1@gmail.com>
2026-01-19 16:38:34 +0100

    Bump fw and fix clang format

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d510b1ba0fb39b494b3f1dbbdac43b6bdb2ddd29")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "fdad0853e6993e63f35fa20f41f43a1b7058ba83")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
