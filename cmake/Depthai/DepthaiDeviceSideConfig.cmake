78d037ab95e98ee7060aa7f64e79ebae10822d04
alex-luxonis <alex@luxonis.com>
2026-01-19 11:37:33 +0200

    RVC2 OV9282: fix blooming at low exp, reduce AE min exp to 9us

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "8938e1b8f91721a0ce7510c2b47cf0b224bd0a43")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "393e4f64252329c13741f696de6863234c117768")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
