f10d2b323dca70b394266f2d37c03fe42725d1d4
alex-luxonis <alex@luxonis.com>
2026-01-20 13:53:10 +0200

    Extend CrashDump, add Device getState()

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1ea24ca1a26f5e1b6607b3e13fcf96dea517c7c2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "f41c5cf78952b3b77c84bb574492b92ad9b937a9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
