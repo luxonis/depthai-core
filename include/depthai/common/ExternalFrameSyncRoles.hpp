7e66bb2e5728e047f9d3d9c278d06c172505cd1a
stas.bucik <stas.bucik@luxonis.com>
2026-01-20 12:28:03 +0100

    Review: Fsync rename to frame sync
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

--- include/depthai/common/ExternalFrameSyncRoles.hpp
+++ include/depthai/common/ExternalFrameSyncRoles.hpp
@@ -5,7 +5,7 @@
 
 namespace dai {
 /**
- * Which Fsync role the device will have.
+ * Which external frame sync role the device should have.
  *
  * AUTO_DETECT denotes that the decision will be made by device.
  * It will choose between MASTER and SLAVE.
@@ -15,11 +15,11 @@ enum class ExternalFrameSyncRole : int32_t {
     // It signals to the device to automatically detect External frame sync configuration.
     AUTO_DETECT = -1,
 
-    // Fsync master.
+    // External frame sync master.
     // Device generates frame sync signal and outputs it to other devices.
     MASTER,
 
-    // Fsync slave.
+    // External frame sync slave.
     // Device must lock onto an external frame sync signal.
     SLAVE,
 };
