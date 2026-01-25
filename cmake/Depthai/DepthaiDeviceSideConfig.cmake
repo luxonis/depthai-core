a9da3e617fb5df5671fdb1afded96a780ab29391
lnotspotl <lnotspotl@gmail.com>
2026-01-25 14:22:27 +0100

    bump firmware hash

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "276d50325cb0954fc5afd51dae933c8474907e13")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "88ebf1d8511bd8652744038a3eb26ffdf8606781")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
