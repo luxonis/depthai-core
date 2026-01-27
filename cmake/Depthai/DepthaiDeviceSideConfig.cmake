4366b7f64b439074c2a467dd86c6c9b220b90da9
MaticTonin <matictonin@gmail.com>
2026-01-28 00:18:26 +0100

    Bump FW to latest to run tests

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "34ce5bbb381e5836bc2712bc63d0647c43b080f6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9ea9c8e4ae5fb24e02d3e1cf69e4b2294f365462")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
