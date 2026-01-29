e5c1d351ebf954114b135b9daea454af1223ff0f
asahtik <asahtik@gmail.com>
2026-01-29 15:10:31 +0100

    Update rvc4 fw

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+ad61a2047fcf728becea15dc9431f43941a2a934")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+5db1cea7beb98253d14ed186ddcd61e132236bda")
