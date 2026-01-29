7c35dfbeac7ff558c9eb4481b57d9f7e21fcb873
aljazkonec1 <53098513+aljazkonec1@users.noreply.github.com>
2026-01-29 14:15:35 +0100

    Fix: img_transformation_test regression (#1662)
    * Bump fw

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "34ce5bbb381e5836bc2712bc63d0647c43b080f6")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "174e63c577683a214e4b165acbea236a92952239")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
