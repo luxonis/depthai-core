3c1e86400b78736a0b37ce8467a6c23f1859ba78
alex-luxonis <alex@luxonis.com>
2026-02-24 14:00:39 +0200

    RVC4 FW update after merge with develop

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+3a3abc422e754e9327e57b659b26a08af358ad57")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+7d52eab0eaa5a2e9f20dbae7f5ced2fb7e3d4641")
