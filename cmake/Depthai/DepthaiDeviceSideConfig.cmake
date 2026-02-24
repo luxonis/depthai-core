f444a53584d4a8ae73f35f400758e23a2cadc9fb
aljazkonec1 <53098513+aljazkonec1@users.noreply.github.com>
2026-02-24 15:14:53 +0100

    Feat: Expand RVC2 python bindings (#1689)
    * Add test
    
    * Bump FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7c1c190e45409c6e3ade394b31e33859f9cea725")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "33cac86f43e0ff3464b03efdfe53a32d74aa3fa4")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
