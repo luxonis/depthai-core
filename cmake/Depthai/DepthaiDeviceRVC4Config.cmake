bf2cd6beb71315315426970d6f865e3f0cfcafad
alex-luxonis <alex@luxonis.com>
2026-03-02 12:32:42 +0200

    RVC4 FW: merged USB support

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+7d52eab0eaa5a2e9f20dbae7f5ced2fb7e3d4641")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+619c87a0023757020150faf07ccbddb13af24533")
