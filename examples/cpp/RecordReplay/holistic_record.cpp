669f7b00e8fad74ea19d1f695eba660feea462da
asahtik <asahtik@gmail.com>
2026-03-03 09:54:54 +0100

    Modify holistic_record example to be useful

--- examples/cpp/RecordReplay/holistic_record.cpp
+++ examples/cpp/RecordReplay/holistic_record.cpp
@@ -1,4 +1,6 @@
+#include <csignal>
 #include <filesystem>
+#include <opencv2/highgui.hpp>
 
 #include "depthai/common/CameraBoardSocket.hpp"
 #include "depthai/depthai.hpp"
@@ -9,58 +11,66 @@
     #error This example needs OpenCV support, which is not available on your system
 #endif
 
+// Signal handling for clean shutdown
+static bool isRunning = true;
+void signalHandler(int signum) {
+    isRunning = false;
+}
+
 int main(int argc, char** argv) {
+    // Register signal handler
+    std::signal(SIGINT, signalHandler);
+
     dai::Pipeline pipeline;
+
+    dai::RecordConfig config;
+    config.outputDir = argc > 1 ? std::string(argv[1]) : getDefaultRecordingPath();
+    // config.videoEncoding.enabled = true;  // Use video encoding
+    // config.videoEncoding.bitrate = 0;     // Automatic
+    // config.videoEncoding.profile = dai::VideoEncoderProperties::Profile::H264_MAIN;
+
+    pipeline.enableHolisticRecord(config);
+
     auto camA = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_A);
-    auto* camAOut = camA->requestOutput({600, 400});
+    auto* camAOut = camA->requestFullResolutionOutput();
     auto camB = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_B);
-    auto* camBOut = camB->requestOutput({600, 400});
+    auto* camBOut = camB->requestFullResolutionOutput();
     auto camC = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_C);
-    auto* camCOut = camC->requestOutput({600, 400});
+    auto* camCOut = camC->requestFullResolutionOutput();
+
+    auto* viewFinderOut = camA->requestOutput({640, 480});
 
     auto imu = pipeline.create<dai::node::IMU>();
 
     auto display = pipeline.create<dai::node::Display>();
 
     // enable ACCELEROMETER_RAW at 500 hz rate
-    imu->enableIMUSensor(dai::IMUSensor::ACCELEROMETER_RAW, 500);
+    imu->enableIMUSensor(dai::IMUSensor::ACCELEROMETER_RAW, 400);
     // enable GYROSCOPE_RAW at 400 hz rate
     imu->enableIMUSensor(dai::IMUSensor::GYROSCOPE_RAW, 400);
     imu->setBatchReportThreshold(100);
 
-    camAOut->link(display->input);
-    auto q = imu->out.createOutputQueue();
+    // Discard frames
+    auto sync = pipeline.create<dai::node::Sync>();
+    sync->setSyncAttempts(0);
+    camAOut->link(sync->inputs["camA"]);
+    camBOut->link(sync->inputs["camB"]);
+    camCOut->link(sync->inputs["camC"]);
+    imu->out.link(sync->inputs["imu"]);
 
-    auto camAqueue = camAOut->createOutputQueue();
-    auto camBqueue = camBOut->createOutputQueue();
-    auto camCqueue = camCOut->createOutputQueue();
-
-    dai::RecordConfig config;
-    config.outputDir = argc > 1 ? std::string(argv[1]) : getDefaultRecordingPath();
-    config.videoEncoding.enabled = true;  // Use video encoding
-    config.videoEncoding.bitrate = 0;     // Automatic
-    config.videoEncoding.profile = dai::VideoEncoderProperties::Profile::H264_MAIN;
-
-    pipeline.enableHolisticRecord(config);
+    auto viewFinderQueue = viewFinderOut->createOutputQueue();
 
     pipeline.start();
 
     auto start = std::chrono::steady_clock::now();
 
-    try {
-        while(std::chrono::steady_clock::now() - start < std::chrono::seconds(15)) {
-            auto imuData = q->get<dai::IMUData>();
-            auto imuPackets = imuData->packets;
-            for(auto& imuPacket : imuPackets) {
-                auto& acceleroValues = imuPacket.acceleroMeter;
-                auto& gyroValues = imuPacket.gyroscope;
-
-                // printf("Accelerometer [m/s^2]: x: %.3f y: %.3f z: %.3f \n", acceleroValues.x, acceleroValues.y, acceleroValues.z);
-                // printf("Gyroscope [rad/s]: x: %.3f y: %.3f z: %.3f \n", gyroValues.x, gyroValues.y, gyroValues.z);
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    while(isRunning && pipeline.isRunning()) {
+        auto frame = viewFinderQueue->get<dai::ImgFrame>();
+        cv::imshow("Video", frame->getCvFrame());
+        auto key = cv::waitKey(10);
+        if(key == 'q') {
+            break;
         }
-    } catch(...) {
     }
 
     pipeline.stop();
