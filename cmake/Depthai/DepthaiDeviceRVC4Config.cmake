7fb87cab42d83c9f9df9114ab9b850836a203136
Jakub Fara <jakubfara77@gmail.com>
2026-03-03 16:17:36 +0100

    Bump RVC4

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+5ee962d36af9c2f04ec126aa10cc8083dba61dde")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+cce6f3210886e5c6e66dd090eba5e01093f49ded")
