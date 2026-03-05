aeebd798e73b37e7f703ad7a23de7eaf35994943
AljazD <aljaz.dukic@luxonis.com>
2026-03-05 15:22:51 +0100

    Bump RVC2 FW, revert video encoder threshold changes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ee921a1f132528da63bc1fd2385c78a4342bcf2f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "86cc8f6aa527b7c1f4b62129decd68e12bcf7d8a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
