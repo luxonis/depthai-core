270f0ed19db1d0b0314d509970bf86efb01515d5
Tomas Fiala <tomas.fiala@luxonis.com>
2026-03-13 14:14:07 +0100

    Add PointCloud node tests

--- tests/src/ondevice_tests/pointcloud_test.cpp
+++ tests/src/ondevice_tests/pointcloud_test.cpp
@@ -7,13 +7,15 @@
 
 constexpr auto WIDTH = 640;
 constexpr auto HEIGHT = 400;
-std::shared_ptr<dai::MessageQueue> configurePipeline(bool sparse, dai::Pipeline& pipeline) {
+std::shared_ptr<dai::MessageQueue> configurePipeline(bool organized, dai::Pipeline& pipeline) {
     auto monoLeft = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_B);
     auto monoRight = pipeline.create<dai::node::Camera>()->build(dai::CameraBoardSocket::CAM_C);
     auto stereo = pipeline.create<dai::node::StereoDepth>();
     auto pointcloud = pipeline.create<dai::node::PointCloud>();
 
-    pointcloud->initialConfig->setSparse(sparse);
+    if(organized) {
+        pointcloud->initialConfig->setOrganized(true);
+    }
 
     monoLeft->requestOutput(std::make_pair(WIDTH, HEIGHT))->link(stereo->left);
     monoRight->requestOutput(std::make_pair(WIDTH, HEIGHT))->link(stereo->right);
@@ -21,9 +23,9 @@ std::shared_ptr<dai::MessageQueue> configurePipeline(bool sparse, dai::Pipeline&
     return pointcloud->outputPointCloud.createOutputQueue();
 }
 
-TEST_CASE("dense pointcloud") {
+TEST_CASE("organized pointcloud") {
     dai::Pipeline pipeline;
-    auto outQ = configurePipeline(false, pipeline);
+    auto outQ = configurePipeline(true, pipeline);
     pipeline.start();
     for(int i = 0; i < 10; ++i) {
         auto pcl = outQ->get<dai::PointCloudData>();
@@ -39,13 +41,13 @@ TEST_CASE("dense pointcloud") {
 
 TEST_CASE("sparse pointcloud") {
     dai::Pipeline pipeline;
-    auto outQ = configurePipeline(true, pipeline);
+    auto outQ = configurePipeline(false, pipeline);
     pipeline.start();
     for(int i = 0; i < 10; ++i) {
         auto pcl = outQ->get<dai::PointCloudData>();
         REQUIRE(pcl != nullptr);
-        REQUIRE(pcl->getWidth() == WIDTH);
-        REQUIRE(pcl->getHeight() == HEIGHT);
+        REQUIRE(pcl->getWidth() <= WIDTH * HEIGHT);
+        REQUIRE(pcl->getHeight() == 1);
         REQUIRE(pcl->getPoints().size() < WIDTH * HEIGHT);
         REQUIRE(pcl->getMinX() <= pcl->getMaxX());
         REQUIRE(pcl->getMinY() <= pcl->getMaxY());
