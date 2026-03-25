58794d6b5d9bb759d1394fce3729f15d1c170584
alex-luxonis <alex@luxonis.com>
2026-03-25 04:42:55 +0200

    RVC2 FW: IMX380 support

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d3fe6a1f0078219d242611bc1e896592e650d771")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4ba5dfab78e02b78190040870d99d69befe4c628")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
