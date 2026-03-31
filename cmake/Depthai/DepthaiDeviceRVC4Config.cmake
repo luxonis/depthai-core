8b50e43e6b8e06f854f7dbec9615b2bb1e983265
Simon Fojtu <simon.fojtu@gmail.com>
2026-03-31 13:40:59 +0200

    bump FW

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+fd9d375ec1d6efe4cba1f7f67db3329dfd8442bd")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+2c65b818b6e5dff19dcb10609e81e7adc9cd560e")
