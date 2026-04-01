57ac520e04f968a6bdb3d230774bd14da715aaca
aljazkonec1 <konec.aljaz1@gmail.com>
2026-04-01 15:33:38 +0200

    Set default rotation matrix.

--- include/depthai/common/Extrinsics.hpp
+++ include/depthai/common/Extrinsics.hpp
@@ -29,7 +29,7 @@ struct Extrinsics {
 
     Extrinsics(std::array<std::array<float, 4>, 4>& extrinsicsMatrix, CameraBoardSocket toCameraSocket, LengthUnit lengthUnit = LengthUnit::CENTIMETER);
 
-    std::vector<std::vector<float>> rotationMatrix;
+    std::vector<std::vector<float>> rotationMatrix = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
     /**
      *  (x, y, z) pose of destCameraSocket w.r.t currentCameraSocket obtained through calibration
      */
