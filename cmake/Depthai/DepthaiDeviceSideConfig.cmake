ae37578eca5bce1f5a23f5f80c88ea74941b6100
asahtik <asahtik@gmail.com>
2026-04-13 09:38:14 +0200

    RVC2 FW: Fix four points transform

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "df0b2454f6f6accd55e1f5b6e66952355ca9e0a1")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cc2cc04786c18c1d7c3caecd6645b8c1e27308e2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
