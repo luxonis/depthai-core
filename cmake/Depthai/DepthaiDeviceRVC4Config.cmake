0b12742cd0991d29acd6792c8bffbaf9cd3af598
Tomas Fiala <tomas.fiala@luxonis.com>
2026-04-16 15:42:51 +0200

    Update RVC4 device version

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+9ea79210b5e10cfc1fcaed42a71ce2a74d2cc4af")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+9ac3684ac6d9d6f3e8bb63f1de711be09b381ef4")
