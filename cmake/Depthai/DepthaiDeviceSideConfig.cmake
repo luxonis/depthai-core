2bd18aee415c705ef5bc9dba37b51b0cd247951f
alex-luxonis <alex@luxonis.com>
2026-04-17 11:53:51 +0300

    IMX380 updates, fix rotation (also for IMX577)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "1b782decbe69b86e2bf9157d536df98335a5151e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d6793eb9c3ba01fd9e3b06a8dcaecf1c57035c1a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
