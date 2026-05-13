0dc101c678e498146a3326eb951aa832885de507
Tommy Walker <tomprochazka0@gmail.com>
2026-05-13 10:52:38 +0200

    Update depthai device commit

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "358472a96039cc24ae416b9612210f04544c1928")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0ede29ac6b9a3f7786e44b5f7a9bf4838486c249")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
