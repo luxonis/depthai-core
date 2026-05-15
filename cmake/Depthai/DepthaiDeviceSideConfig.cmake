f8946505d372067573b7ae131284d5452add97ac
MaticTonin <matictonin@gmail.com>
2026-05-15 19:34:10 +0200

    Bump FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6855587409122eefbfe4b722e2dd2cf2c70db4e9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e327bf0f32b3d2e15e72bc5fe12d904976729ddb")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
