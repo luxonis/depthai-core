f18cd5cdd4e1857474be26237378a623422d5511
MaticTonin <matictonin@gmail.com>
2026-05-29 01:12:38 +0200

    Bump FW

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+0a6d45fa2db856ab3f83cb20642ea1be4e9ebdd3")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+8cb6736d1955d983f4ccca86ffbfde391993ba48")
