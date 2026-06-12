50a89e8602dda58ab2caaaecb8874496a8c36001
Matevz Morato <matevz.morato@gmail.com>
2026-06-12 11:37:59 +0200

    Bump FW with support for 63D ToF

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f6d2cc12723f414dce437d438d124f340f5789b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1f58b96ae8cae045e32b88f6c991d95b62145652")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
