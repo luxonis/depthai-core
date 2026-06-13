6f8d7cef9e3efc0bf5138280af7d7892f0c1ab0d
MaticTonin <matictonin@gmail.com>
2026-06-13 16:10:57 +0200

    Isolated OAK-ToF bringup FW only

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "353248d180136eb229dc7083c8c1f5bb75ee328c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "cbcf2360708a9e35570f639bcf469589f812163b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
