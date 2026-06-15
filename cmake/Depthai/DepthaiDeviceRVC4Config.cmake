d635ceaf4c60018af42a8e3a3c6d4e5e817f6e48
asahtik <asahtik@gmail.com>
2026-06-15 14:19:22 +0200

    RVC4 FW: Updated sync node, added tsSystem

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+885c87889c241dabf29cb3cd503bbdfb9be39c35")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+6edf0ab2c8d77b8a70630e6a11cff9f2d51f9cc3")
