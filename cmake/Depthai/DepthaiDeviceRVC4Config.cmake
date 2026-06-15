5cb2f2bda49524637c6af3e5f22a2ac1e6f90b58
Jakob Socan <jakob.socan@luxonis.com>
2026-06-15 14:27:32 +0200

    Bump FW

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+885c87889c241dabf29cb3cd503bbdfb9be39c35")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+18c3a4af4d6beca7074bdf06de7ff25414a07525")
