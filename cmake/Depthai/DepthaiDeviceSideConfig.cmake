e71b444e4605404499ebc56581da479ff2ecdd94
asahtik <asahtik@gmail.com>
2026-06-15 16:41:21 +0200

    Add bindings for copyBufferMetadataFrom

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "a6fead61e291b0a88fa80a9d3dd4f839106390ae")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "eb5f7e4e03c6a786d133bc42e021d457f0f7316e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
