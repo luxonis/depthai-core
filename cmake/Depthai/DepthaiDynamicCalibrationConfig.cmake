67bc4d2614bbb14d4f734969f97125f1880fa3f6
Jakub Fara <jakubfara77@gmail.com>
2026-06-15 13:25:38 +0200

    Bump DCL

--- cmake/Depthai/DepthaiDynamicCalibrationConfig.cmake
+++ cmake/Depthai/DepthaiDynamicCalibrationConfig.cmake
@@ -1,2 +1,2 @@
 # "full commit hash of Dynamic calibration library"
-set(DEPTHAI_DYNAMIC_CALIBRATION_VERSION "a4dd4c3b247f544a5ddd49026b1c3ffea02e9d73")
+set(DEPTHAI_DYNAMIC_CALIBRATION_VERSION "848ba66de1b880ce6cbcdf54232323f20743cc1a")
