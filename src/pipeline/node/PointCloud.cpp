4d6420209f85f15c22af2e67bb124f718474893d
Tomas Fiala <tomas.fiala@luxonis.com>
2026-06-18 14:01:38 +0200

    Simplify extrinsics check to single isEqualExtrinsics call

--- src/pipeline/node/PointCloud.cpp
+++ src/pipeline/node/PointCloud.cpp
@@ -665,19 +665,8 @@ void PointCloud::processColorized(std::shared_ptr<ImgFrame> depthFrame,
     }
 
     // Check extrinsics mismatches (non-fatal)
-    {
-        auto depthExtrinsics = depthFrame->transformation.getExtrinsics();
-        auto colorExtrinsics = colorFrame->transformation.getExtrinsics();
-        if(depthExtrinsics.toCameraSocket != colorExtrinsics.toCameraSocket) {
-            pimpl->logger->error("PointCloud: color extrinsics toCameraSocket ({}) does not match depth ({}) -- colorization may be misaligned",
-                                 toString(colorExtrinsics.toCameraSocket),
-                                 toString(depthExtrinsics.toCameraSocket));
-        } else if(!depthExtrinsics.isEqualExtrinsics(colorExtrinsics)) {
-            pimpl->logger->error(
-                "PointCloud: depth and color extrinsics differ (same toCameraSocket={} but different rotation/translation) "
-                "-- colorization may be misaligned",
-                toString(depthExtrinsics.toCameraSocket));
-        }
+    if(!depthFrame->transformation.getExtrinsics().isEqualExtrinsics(colorFrame->transformation.getExtrinsics())) {
+        pimpl->logger->error("PointCloud: depth and color extrinsics differ -- colorization may be misaligned");
     }
 
     // Check intrinsics and distortion mismatches (non-fatal)
