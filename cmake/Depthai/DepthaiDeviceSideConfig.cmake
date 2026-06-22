27c73394bc30c38fc4243c3d1c8b197b13a63b0e
asahtik <asahtik@gmail.com>
2026-06-22 13:43:59 +0200

    RVC2 FW: Add passthrough support for system timestamps on RVC2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "eb5f7e4e03c6a786d133bc42e021d457f0f7316e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "84206127bbf65759c2388a34d13d5e3a37a484da")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
