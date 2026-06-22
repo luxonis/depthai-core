d0d0577f19d1f76021910ff2d2585287ca0c8b98
MaticTonin <matictonin@gmail.com>
2026-06-23 01:09:19 +0200

    Bump RVC2 FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ce175a04ba95c1b71e927991f727aef965ab12ee")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5910255403c530d58ce5914eedbcad67e38cf280")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
