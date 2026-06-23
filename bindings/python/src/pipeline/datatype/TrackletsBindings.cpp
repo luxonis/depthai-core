f0d93fc7ca17ade9d03e1ba8f0dded8b338b92fd
aljazkonec1 <53098513+aljazkonec1@users.noreply.github.com>
2026-06-23 15:39:58 +0200

    Merge pull request #1767 from luxonis/fix/compiler_warnings
    Fix compiler warnings for pybind when building release

--- bindings/python/src/pipeline/datatype/TrackletsBindings.cpp
+++ bindings/python/src/pipeline/datatype/TrackletsBindings.cpp
@@ -1,5 +1,7 @@
 #include <memory>
 #include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include "DatatypeBindings.hpp"
 #include "pipeline/CommonBindings.hpp"
@@ -63,8 +65,8 @@ void bind_tracklets(pybind11::module& m, void* pCallstack) {
         .def_readwrite("unit", &Tracklets::unit, DOC(dai, Tracklets, unit))
         .def_property(
             "tracklets",
-            [](Tracklets& track) { return &track.tracklets; },
-            [](Tracklets& track, std::vector<Tracklet> val) { track.tracklets = val; },
+            [](Tracklets& track) -> std::vector<Tracklet>& { return track.tracklets; },
+            [](Tracklets& track, std::vector<Tracklet> val) { track.tracklets = std::move(val); },
             DOC(dai, Tracklets, tracklets))
         .def("getTimestamp", &Tracklets::Buffer::getTimestamp, DOC(dai, Buffer, getTimestamp))
         .def("getTimestampDevice", &Tracklets::Buffer::getTimestampDevice, DOC(dai, Buffer, getTimestampDevice))
