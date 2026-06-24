d1535006350a83be84a6111bb97ac9c04bf6b5f1
asahtik <asahtik@gmail.com>
2026-06-24 14:51:42 +0200

    RVC2: Rename function to setBufferMetadataFrom

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "84206127bbf65759c2388a34d13d5e3a37a484da")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "2bb5a5b1bd7f3d0b36baa8fa2b067203931fd34c")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
