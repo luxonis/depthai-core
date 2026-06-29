40639ebeca94bd33a71a1378a8f71284d66f0ca4
asahtik <asahtik@gmail.com>
2026-06-29 11:50:02 +0200

    Clangformat

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7eda5e151484331b47ddec2e602b990bac1ae09a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "a56222a88614a05565e70d2fb124026348f93cd2")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
