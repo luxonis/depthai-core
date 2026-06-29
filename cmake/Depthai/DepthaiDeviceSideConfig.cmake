507592739c0b886fce6c5661bcff98f61eeff1df
banulucian <banulucian@gmail.com>
2026-06-29 17:33:25 +0300

    Bump FW (63D mirror/vflip)

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "17f59d5b5d9437210c828522648f8c89578cf918")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "3ceb859669f6e363c8aac92f098b6da20ec9f5ea")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
