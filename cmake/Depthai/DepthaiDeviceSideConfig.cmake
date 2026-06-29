c6ac6e1f56f0823e5df4833fb1db4c1c1093a3f3
asahtik <asahtik@gmail.com>
2026-06-29 11:33:23 +0200

    RVC2 FW: PR Fixes

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "2bb5a5b1bd7f3d0b36baa8fa2b067203931fd34c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "7eda5e151484331b47ddec2e602b990bac1ae09a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
