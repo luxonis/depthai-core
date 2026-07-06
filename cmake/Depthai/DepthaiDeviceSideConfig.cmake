874258c51be7026d82e24d31e7e18cb36793ec6b
asahtik <asahtik@gmail.com>
2026-07-06 10:05:10 +0200

    RVC2 FW: Remap bboxes in SLC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4c68246b10383c119f64d6f48b39fd92c59935f3")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d62f5d10a08bb7c248b2778acc9be34f5977ad05")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
