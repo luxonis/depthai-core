a2e30b4c33876b09a8fc4ff8358416e0ba5e0791
MaticTonin <matictonin@gmail.com>
2026-07-08 14:13:39 +0200

    Bump FW for P1D0

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "5e3c6849fdb7c335926e2f47e0f979e2f475f5d5")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e2d8623ee31ecd2655e2adcd066f0337f0c8dfcd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
