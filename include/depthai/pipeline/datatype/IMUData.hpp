8e5acc134939066ae2b2ef59e7aa45dc00510925
asahtik <asahtik@gmail.com>
2026-07-09 15:05:35 +0200

    Declare deserializeProto

--- include/depthai/pipeline/datatype/IMUData.hpp
+++ include/depthai/pipeline/datatype/IMUData.hpp
@@ -252,6 +252,11 @@ class IMUData : public Buffer, public ProtoSerializable {
     std::vector<std::uint8_t> serializeProto(bool = false) const override;
 
     /**
+     * @brief Set from a deserialized protobuf message of this object
+     */
+    void deserializeProto(const std::vector<std::uint8_t>& bytes) override;
+
+    /**
      * Serialize schema to proto buffer
      *
      * @returns serialized schema
