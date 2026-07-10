5546c7ecd97cc728d2f730a45ce409c926999e50
asahtik <asahtik@gmail.com>
2026-07-10 18:01:35 +0200

    Verify datatype when loading message

--- src/pipeline/datatype/ADataType.cpp
+++ src/pipeline/datatype/ADataType.cpp
@@ -2,6 +2,7 @@
 
 namespace dai {
 
+ADatatypeInterface::~ADatatypeInterface() = default;
 ADatatype::~ADatatype() = default;
 
 void ADatatype::serialize(std::vector<std::uint8_t>& metadata, DatatypeEnum& datatype) const {
