c20931d3a74f5a46cc8158d730b295f6fc8977eb
asahtik <asahtik@gmail.com>
2026-07-22 13:32:19 +0200

    RVC2 FW: Implement resizer alpha scaling, RVC4 FW: merge develop

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0dad8dfee6e79892fa3229247e24012bdd27dbae")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "54135ab04cc524904bfb3432d707967569af2189")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
