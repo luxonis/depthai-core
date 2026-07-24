7642ddbfc39d575038a34dfcf6e3eeb81d2d5ac3
asahtik <38485424+asahtik@users.noreply.github.com>
2026-07-24 20:32:02 +0200

    Merge pull request #1878 from luxonis/feature/slc_remap_bbox
    Remap bboxes when not aligned in SLC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c3f4bc9cef3937fa987135129757d950d1fbbd19")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "e6ec7d64d0e96260e2a3ca666a890efe86553246")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
