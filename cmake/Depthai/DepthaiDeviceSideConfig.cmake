3c1ffe25025469c6928d9570cda8dea644d3659c
AljazD <aljaz.dukic@luxonis.com>
2026-07-27 09:42:34 +0200

    Bump RVC2/RVC4 FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e6ec7d64d0e96260e2a3ca666a890efe86553246")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "11288c001f2c2d0db972999ac94bd5d3fc42df06")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
