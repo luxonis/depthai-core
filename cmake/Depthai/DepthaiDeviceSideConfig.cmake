ab748bc9275f9c4ceac6e9016229519a2f0efbda
aljazkonec1 <konec.aljaz1@gmail.com>
2026-08-07 13:55:31 +0200

    Bump RVC2 and remove roundtrip example

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "606922d9ee49d16e114d9eb34b52e39777a1dc2d")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "1cb3cf22aa3923a248d54dc14c77d2a35aae67cd")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
