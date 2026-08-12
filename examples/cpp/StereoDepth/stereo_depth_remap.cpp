1d27132ea382ebf67802622919a6644e065a2228
ViacheslavTelelyukhin <slavatelelyukhin@gmail.com>
2026-08-12 18:14:53 +0200

    enforced transform validation in stereo_depth_remap.cppand reduced lo… (#1935)
    * enforced transform validation in stereo_depth_remap.cppand reduced log spam in camera_multiple_outputs.py
    
    * updated camera_multiple_outputs.cpp logging to be consistent with python version
    
    ---------
    
    Co-authored-by: Viacheslav <viacheslav.telelyukhin@luxonis.com>

--- examples/cpp/StereoDepth/stereo_depth_remap.cpp
+++ examples/cpp/StereoDepth/stereo_depth_remap.cpp
@@ -87,6 +87,7 @@ int main() {
         // Validate transformations
         if(!colorFrame->validateTransformations() || !stereoFrame->validateTransformations()) {
             std::cerr << "Invalid transformations!" << std::endl;
+            throw std::runtime_error("Invalid transformations!");
             continue;
         }
 
