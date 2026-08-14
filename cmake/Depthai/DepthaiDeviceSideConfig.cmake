31887e35bc24a532a654d0dffc9294fb8e290b53
aljazkonec1 <konec.aljaz1@gmail.com>
2026-08-14 15:50:24 +0200

    Bump FW to release versions

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "bd3b52024664dabc8c71c96554de1b2e29a14e0e")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ac60599e9650fbfec38ff4b5acb4448a402889b0")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
