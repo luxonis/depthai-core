aa978c4731bf159a6d3f8231cf32bfee0274ea6d
AljazD <aljaz.dukic@luxonis.com>
2026-08-21 08:18:33 +0200

    Update workflows to use windows 2025 runners

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -293,15 +293,15 @@ class CMakeBuild(build_ext):
             # cmake_args += ['-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE={}'.format(os.path.dirname(os.path.abspath(__file__)) + '/cmake/toolchain/msvc.cmake')]
             # cmake_args += ['-DVCPKG_TARGET_TRIPLET=x64-windows-static-crt'] # Keep the default triplet to avoid dual CRT issues
 
+            if not os.environ.get('CMAKE_GENERATOR', '').startswith('Ninja'):
+                # Detect whether 32 / 64 bit Python is used and compile accordingly
+                if sys.maxsize > 2**32:
+                    cmake_args += ['-A', 'x64']
+                else:
+                    cmake_args += ['-A', 'Win32']
 
-            # Detect whether 32 / 64 bit Python is used and compile accordingly
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            else:
-                cmake_args += ['-A', 'Win32']
-
-            # Add flag to build with maximum available threads
-            build_args += ['--', '/m']
+                # Add flag to build with maximum available threads
+                build_args += ['--', '/m']
         # Unix
         else:
             # if macos add some additional env vars
