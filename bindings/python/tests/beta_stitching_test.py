89894b3e0ba7627e34664356cd6ad69b78f96f52
Tomas Fiala <tomas.fiala@luxonis.com>
2026-08-27 15:35:51 +0200

    Fix Stitching Python test without device

--- bindings/python/tests/beta_stitching_test.py
+++ bindings/python/tests/beta_stitching_test.py
@@ -5,9 +5,9 @@ def test_stitching_is_exposed_only_through_beta_api():
     assert not hasattr(dai.node, "Stitching")
     assert dai.beta.node.Stitching.Properties is dai.beta.StitchingProperties
 
-    pipeline = dai.Pipeline()
-    stitching = pipeline.create(dai.beta.node.Stitching).build(2)
+    with dai.Pipeline(createImplicitDevice=False) as pipeline:
+        stitching = pipeline.create(dai.beta.node.Stitching).build(2)
 
-    assert isinstance(stitching, dai.beta.node.Stitching)
-    assert stitching.getNumInputs() == 2
-    assert stitching.getMode() == dai.beta.node.Stitching.Mode.PANORAMA
+        assert isinstance(stitching, dai.beta.node.Stitching)
+        assert stitching.getNumInputs() == 2
+        assert stitching.getMode() == dai.beta.node.Stitching.Mode.PANORAMA
