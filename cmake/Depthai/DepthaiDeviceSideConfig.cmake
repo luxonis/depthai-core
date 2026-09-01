586df2e96c72a7bb9cbf48c29fbd63b24709f901
aljazkonec1 <53098513+aljazkonec1@users.noreply.github.com>
2026-09-01 20:20:35 +0200

    Release 2.33.0.0 (#1967)
    * Bump FW with support for 63D ToF
    
    * Bump bootloader to v0.29
    
    ---------
    
    Co-authored-by: Matevz Morato <matevz.morato@gmail.com>
    Co-authored-by: MaticTonin <matictonin@gmail.com>
    Co-authored-by: banulucian <banulucian@gmail.com>

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "0f6d2cc12723f414dce437d438d124f340f5789b")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "6632d3a2dbdfbede61251ad5a448a8faa8dc18b9")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
