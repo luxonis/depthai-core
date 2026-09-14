cd436bba0f772f1f5923442bcd15d1f6897c8c34
asahtik <asahtik@gmail.com>
2026-09-14 09:39:31 +0200

    RVC2 FW: Add getEthernetLinkSpeed RPC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "40f5e0b83ee8148d5871e08a74521f05ee0760cd")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "385f588c1df8b0cc0e49b3b63526d6eb80ac263d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
